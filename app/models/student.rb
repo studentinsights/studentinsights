2a5c0b431025042fef6e0a2f1ca5d54c1a04c2ee
alexsoble <asoble@gmail.com>
2015-01-15 07:29:14 -0800

    Don't test the wrong thing

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,2 @@
 class Student < ActiveRecord::Base
-  # validates_presence_of :new_id, :grade, :race
-  # validates_inclusion_of :hispanic_latino, :limited_english, :low_income, in: [true, false]
 end
--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -4,35 +4,6 @@ describe Student do
 
   describe '#create' do
 
-    context 'when all demographics are present' do
-
-      it 'creates a new Student' do
-
-        new_student = Student.create(
-          new_id: 342,
-          grade: "K", 
-          hispanic_latino: false, 
-          race: "B", 
-          limited_english: true, 
-          low_income: true
-        )
-
-        expect( Student.all.length ).to eq( 1 ) 
-      end
-    end
-
-    context 'when demograpics are missing' do
-
-      it 'does not create a new Student' do
-
-        Student.create(
-          grade: "K", 
-          limited_english: false, 
-          low_income: false
-        ) 
-
-        expect( Student.all.length ).to eq( 0 ) 
-      end
-    end
   end
+
 end
