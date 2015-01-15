ecb8ad67d4f694ddfb98f41a7e6dfcbb9ad8957b
alexsoble <asoble@gmail.com>
2015-01-14 18:44:58 -0800

    Add paths to sample data for testing

--- spec/data_set_spec.rb
+++ spec/data_set_spec.rb
@@ -7,17 +7,29 @@ describe DataSet do
     context 'when all required sheets and headers are present' do
 
       context 'when columns match data format correctly' do
+
+        well_formatted_excel_path = "./SampleData/SampleData.xlsx"
+
       end
       
       context 'when columns mismatch data format' do
+
+        format_mismatch_excel_path = "./SampleData/SampleData_formats_wrong.xlsx"
+
       end
 
     end
 
     context 'when required sheets are missing' do
+      
+      missing_sheet_excel_path = "./SampleData/SampleData_sheets_missing.xlsx"
+    
     end
 
     context 'when required columns are missing' do
+
+      missing_columns_excel_path = "./SampleData/SampleData_columns_missing.xlsx"
+
     end
 
   end
