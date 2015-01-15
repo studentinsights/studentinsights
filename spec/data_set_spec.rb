ca0a88d370c09739fd7155f8ab5cac993e918c05
alexsoble <asoble@gmail.com>
2015-01-14 16:55:24 -0800

    Contexts for DataSet tests

---
+++ spec/data_set_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe DataSet do
+
+  describe '#process' do
+
+    context 'when all required sheets and headers are present' do
+
+      context 'when columns match data format correctly' do
+      end
+      
+      context 'when columns mismatch data format' do
+      end
+
+    end
+
+    context 'when required sheets are missing' do
+    end
+
+    context 'when required columns are missing' do
+    end
+
+  end
+  
+end
