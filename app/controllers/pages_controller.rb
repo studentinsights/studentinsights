ed7b412076688ddd9db010532106b34e8af8b41a
alexsoble <asoble@gmail.com>
2015-01-16 14:22:41 -0800

    Default to showing first room on homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,7 +4,7 @@ class PagesController < ApplicationController
     if pages_params.present?
       @room = Room.find_by_name(pages_params[:room])
     else
-      @room = Room.all.sample
+      @room = Room.first
     end
     @rooms_by_name = Room.all.sort_by { |r| r.name.to_i }.sort_by { |r| r.name.size }
     @students = Student.default_sort_by_math(@room)
