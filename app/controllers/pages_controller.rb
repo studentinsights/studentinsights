af11ee0d05bad1962351b7c8eaedeecc64dc4e29
alexsoble <asoble@gmail.com>
2015-01-15 18:04:00 -0800

    Add student counter for risk categories

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
 
   def index
     @students = Student.default_sort_by_math
+    @number_of_students = @students.map {|k, v| v.length }.sum
     @risk_categories = [ "High", "Medium", "Low" ]
   end
 
