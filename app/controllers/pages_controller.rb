23bf898368565e3a06538efc5e669cc4118dfb87
alexsoble <asoble@gmail.com>
2015-01-16 11:02:13 -0800

    Sort homerooms for select box

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
 
   def index
     @room = Room.all.sample
+    @rooms_by_name = Room.all.sort_by { |r| r.name.to_i }.sort_by { |r| r.name.size }
     @students = Student.default_sort_by_math(@room)
     @number_of_students = @students.map {|k, v| v.length }.sum
     @risk_categories = [ "High", "Medium", "Low" ]
