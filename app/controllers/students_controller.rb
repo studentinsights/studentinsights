248c3a6f4a261d96067148994eb699fe06538a80
alexsoble <asoble@gmail.com>
2015-02-12 09:09:26 -0500

    Remove code for abandoned slider feature

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,5 @@
 class StudentsController < ApplicationController
 
-  def sort_by_risk
-    params = sorting_params
-    @students_by_risk = Student.sort_by_risk(params)
-    render json: @students_by_risk
-  end
-
   def sorting_params
     params.require(:sort).permit(:subject, :lower_cutoff, :upper_cutoff, :room_id)
   end
