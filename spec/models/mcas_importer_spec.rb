0af9980e7e29be5eaef1b9f974863bf9217325d1
alexsoble <asoble@gmail.com>
2015-03-22 12:47:30 -0700

    Test MCAS import by school.
    * #18.

--- spec/models/mcas_importer_spec.rb
+++ spec/models/mcas_importer_spec.rb
@@ -6,19 +6,31 @@ RSpec.describe McasImporter do
     fixture_path = "#{Rails.root}/spec/fixtures/fake_mcas.csv"
     let(:healey) { FactoryGirl.create(:healey) }
     let(:brown) { FactoryGirl.create(:brown) }
-    let(:importer) { McasImporter.new(fixture_path, healey, "05") }
+    let(:healey_importer) { McasImporter.new(fixture_path, healey, "05") }
+    let(:brown_importer) { McasImporter.new(fixture_path, brown, "05") }
 
     context 'with good data' do
 
-      it 'creates a student' do
-        expect {
-          importer.import
-        }.to change(Student, :count).by(1)
+      context 'for Healey school' do
+
+        it 'creates a student' do
+          expect {
+            healey_importer.import
+          }.to change(Student, :count).by(1)
+        end
+
+        it 'imports a Healey student' do
+          healey_importer.import
+          expect(Student.last.first_name).to eq('Ben')
+        end
       end
 
-      it 'sets the student name correctly' do
-        importer.import
-        expect(Student.last.first_name).to eq('Ben')
+      context 'for Brown school' do
+
+        it 'imports a Brown student' do
+          brown_importer.import
+          expect(Student.last.first_name).to eq('Mari')
+        end
       end
     end
   end
