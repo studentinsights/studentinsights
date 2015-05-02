6b6bc1a32357e40fe03fe3350a3ab4ec6aeea540
alexsoble <asoble@gmail.com>
2015-05-02 06:15:14 -0500

    SSL not so necessary for localhost :blush: #67

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  force_ssl
+  force_ssl if Rails.env.production?
 
   before_filter :update_sanitized_params, if: :devise_controller?
 
