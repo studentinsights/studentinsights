28ab7be4d18842232ba8b598c105b80416024083
alexsoble <asoble@gmail.com>
2015-05-04 20:42:24 -0400

    For the student profile to live, the demo roster must die
    + We want the student names on the roster to link to profiles
    + Those links don't work with the fake data, because they're not real records & have no id
    + It's probably time now to build a legit demo site in the sky with the same code as the real site but fake data
    + #63

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,23 +3,4 @@ class PagesController < ApplicationController
   def about
   end
 
-  def roster_demo
-    @number_of_students = 12
-    @students = []
-
-    @number_of_students.times do 
-      student = Student.new(FakeStudent.data)
-      @students << student
-      student.mcas_results.build(FakeMcasResult.data)
-    end
-    
-    @analyzer = RiskAnalyzer.new @students 
-    @sorted_students = @analyzer.by_category
-    @risk_categories = @analyzer.by_category.keys
-
-    @homeroom = Homeroom.where(name: "Demo").first_or_create
-    @homerooms_by_name = [@homeroom]
-    render "students/index"
-  end
-
 end
--- app/presenters/student_presenter.rb
+++ app/presenters/student_presenter.rb
@@ -1,4 +1,5 @@
 class StudentPresenter < Struct.new(:student)
+  delegate :id, to: :student
 
   def full_name
     student.first_name + " " + student.last_name
