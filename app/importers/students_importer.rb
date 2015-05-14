a03501dfb9f7fb3c2a132b95b851be9a7d682cb8
alexsoble <asoble@gmail.com>
2015-05-14 16:59:01 -0500

    Rewrite StudentsImporter to work with data pipeline
    + Yuck, there was some ugly code in there before
    + TODO: Handle student school assignment
    + #80

--- app/importers/students_importer.rb
+++ app/importers/students_importer.rb
@@ -1,38 +1,38 @@
-class StudentsImporter < Struct.new :school_scope
+class StudentsImporter
   include X2Connector
 
-  def import(student_result, school_result)
-    student_result.each do |row|
-      school_result.each do |school_row|
-        if school_row["SKL_OID"] == row["STD_SKL_OID"]
-          school_local_id = school_row["SKL_SCHOOL_ID"]
-          grade = row["STD_GRADE_LEVEL"]
-          if school_local_id == school_scope.local_id
-            student = create_or_update_student(row)
-            homeroom_name = row["STD_HOMEROOM"]
-            assign_student_to_homeroom(student, homeroom_name)
-          end
-        end
-      end
+  def parse_row(row)
+    student = Student.where(state_id: row[:state_id]).first_or_create!
+    student.assign_attributes(
+      home_language: row[:home_language],
+      grade: row[:grade]
+    )
+    parsed_name = split_first_and_last_name(row[:full_name])
+    student.assign_attributes(parsed_name)
+    if student.save
+      assign_student_to_homeroom(student, row[:homeroom])
     end
   end
 
-  def create_or_update_student(row)
-    state_id = row["STD_ID_STATE"]
-    student = Student.where(state_id: state_id).first_or_create!
-    full_name = row["std_name_view"]
-    if full_name.present?
-      student.last_name = full_name.split(", ")[0]
-      student.first_name = full_name.split(", ")[1]
+  def split_first_and_last_name(name_view_from_x2)
+    name_split = name_view_from_x2.split(", ")
+    case name_split.size
+    when 2
+      full_name = { first_name: name_split[1], last_name: name_split[0] }
+    when 1
+      full_name = { first_name: nil, last_name: name_split[0] }
     end
-    student.home_language = row["STD_HOME_LANGUAGE_CODE"]
-    student.save
-    return student
   end
 
   def assign_student_to_homeroom(student, homeroom_name)
     homeroom = Homeroom.where(name: homeroom_name).first_or_create!
     student.homeroom_id = homeroom.id
+    student.save
+  end
+
+  def assign_student_to_school(student, school_local_id)
+    school = School.where(local_id: school_local_id).first_or_create!
+    student.school = school.id
     student.save
   end
 end
--- spec/importers/students_importer_spec.rb
+++ spec/importers/students_importer_spec.rb
@@ -2,62 +2,54 @@ require 'fixtures/fake_x2'
 require 'rails_helper'
 
 RSpec.describe StudentsImporter do
+  let(:importer) { StudentsImporter.new }
 
-  let(:healey) { FactoryGirl.create(:healey) }
-  let(:healey_importer) { StudentsImporter.new healey }
-
-  describe '#import' do
-    context 'importer for healey school' do
-      it 'imports a healey student' do
-        expect {
-          healey_importer.import([FakeX2::FAKE_HEALEY_STUDENT], [FakeX2::FAKE_HEALEY_SCHOOL])
-        }.to change(Student, :count).by(1)
+  describe '#parse_row' do
+    context 'good data' do
+      context 'student already exists' do
+        let!(:student) { FactoryGirl.create(:student_we_want_to_update) }
+        let(:row) { {state_id: '10', full_name: 'Pais, Casey', home_language: 'Chinese', grade: '1', homeroom: '701' } }
+        it 'updates student attributes' do
+          importer.parse_row(row)
+          expect(student.reload.home_language).to eq 'Chinese'
+        end
       end
-      it 'does not import a brown student' do
-        expect {
-          healey_importer.import([FakeX2::FAKE_BROWN_STUDENT], [FakeX2::FAKE_BROWN_SCHOOL])
-        }.to change(Student, :count).by(0)
+      context 'student does not already exist' do
+        let(:row) { {state_id: '10', full_name: 'Pais, Casey', home_language: 'Chinese', grade: '1', homeroom: '701' } }
+        it 'creates a new student' do
+          expect { importer.parse_row(row) }.to change(Student, :count).by 1
+        end
+        it 'sets the new student attributes correctly' do
+          importer.parse_row(row)
+          expect(Student.last.grade).to eq '1'
+        end
+      end
+    end
+    context 'bad data' do
+      context 'missing state id' do
+        let(:row) { { state_id: nil, full_name: 'Hoag, George', home_language: 'English', grade: '1', homeroom: '101' } }
+        it 'raises an error' do
+          expect{ importer.parse_row(row) }.to raise_error
+        end
       end
     end
   end
 
-  describe '#create_or_update_student' do
-    context 'student already exists' do
-      let!(:student) { FactoryGirl.create(:student_we_want_to_update) }
-      it 'updates the student' do
-        expect {
-          healey_importer.create_or_update_student(FakeX2::FAKE_STUDENT_WITH_NAME)
-        }.to change(Student, :count).by(0)
-      end
-      it 'updates the student home langauge correctly' do
-        healey_importer.create_or_update_student(FakeX2::FAKE_STUDENT_WITH_NAME)
-        expect(student.reload.home_language).to eq("Chinese")
+  describe '#split_first_and_last_name' do
+    context 'well formatted name' do
+      it 'assigns the first and last name correctly' do
+        name = 'Hoag, George'
+        expect(importer.split_first_and_last_name(name)).to eq(
+          { first_name: 'George', last_name: 'Hoag' }
+        )
       end
     end
-    context 'student does not already exist' do
-      it 'creates a new student' do
-        expect {
-          healey_importer.create_or_update_student(FakeX2::FAKE_STUDENT_WITH_NAME)
-        }.to change(Student, :count).by(1)
-      end
-      context 'student info contains a name field' do
-        it 'updates the student name correctly' do
-          new_student = healey_importer.create_or_update_student(FakeX2::FAKE_STUDENT_WITH_NAME)
-          expect(new_student.reload.first_name).to eq("Amir")
-          expect(new_student.reload.last_name).to eq("Hadjihabib")
-        end
-      end
-      context 'student info does not contains a name field' do
-        it 'does not raise an error' do
-          expect {
-            healey_importer.create_or_update_student(FakeX2::FAKE_STUDENT_WITHOUT_NAME)
-          }.to_not raise_error
-        end
-        it 'does not set a name' do
-          new_student = healey_importer.create_or_update_student(FakeX2::FAKE_STUDENT_WITHOUT_NAME)
-          expect(new_student.reload.first_name).to eq nil
-          expect(new_student.reload.last_name).to eq nil
-        end
+    context 'poorly formatted name' do
+      it 'assigns the result to the last name' do
+        name = 'Hoag'
+        expect(importer.split_first_and_last_name(name)).to eq(
+          { first_name: nil, last_name: 'Hoag' }
+        )
       end
     end
   end
@@ -67,7 +59,7 @@ RSpec.describe StudentsImporter do
       let!(:student) { FactoryGirl.create(:student_with_homeroom) }
       let!(:new_homeroom) { FactoryGirl.create(:homeroom) }
       it 'assigns the student to the homeroom' do
-        healey_importer.assign_student_to_homeroom(student, new_homeroom.name)
+        importer.assign_student_to_homeroom(student, new_homeroom.name)
         expect(student.homeroom_id).to eq(new_homeroom.id)
       end
     end
@@ -76,11 +68,11 @@ RSpec.describe StudentsImporter do
       let!(:new_homeroom_name) { "152I" }
       it 'creates a new homeroom' do
         expect {
-          healey_importer.assign_student_to_homeroom(student, new_homeroom_name)
+          importer.assign_student_to_homeroom(student, new_homeroom_name)
         }.to change(Homeroom, :count).by(1)
       end
       it 'assigns the student to the homeroom' do
-        healey_importer.assign_student_to_homeroom(student, new_homeroom_name)
+        importer.assign_student_to_homeroom(student, new_homeroom_name)
         new_homeroom = Homeroom.find_by_name(new_homeroom_name)
         expect(student.homeroom_id).to eq(new_homeroom.id)
       end
