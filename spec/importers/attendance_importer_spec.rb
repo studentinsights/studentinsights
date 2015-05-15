12c03f60ca435fbdf460499b6116c246a47a0107
alexsoble <asoble@gmail.com>
2015-05-15 18:57:29 -0500

    Give attendance importer spec a little more love

--- spec/importers/attendance_importer_spec.rb
+++ spec/importers/attendance_importer_spec.rb
@@ -6,16 +6,23 @@ RSpec.describe AttendanceImporter do
 
 	describe '#parse_row' do
 		context 'good data' do
+			let(:row) { {state_id: '10', absence: '1', tardy: '0', event_date: '2015-2-2'} }
 			context 'student already exists' do
 				let!(:student) { FactoryGirl.create(:student_we_want_to_update) }
-				let(:row) { {state_id: '10', absence: '1', tardy: '0', event_date: '2015-2-2'} }
-				it 'add attendence event to the correct student' do
+				it 'creates attendence event for the correct student' do
 					attendance_importer.parse_row(row)
 					expect(student.reload.attendance_events.size).to eq 1
 				end
+				it 'assigns absence correctly' do
+					attendance_importer.parse_row(row)
+					expect(student.reload.attendance_events.last.absence?).to be true
+				end
+				it 'assigns tardiness correctly' do
+					attendance_importer.parse_row(row)
+					expect(student.reload.attendance_events.last.tardy?).to be false
+				end
 			end
 			context 'student does not already exist' do
-				let(:row) { { state_id: '100', absence: '1', tardy: '0', event_date: '2015-2-2' } }
 				it 'creates a new student' do
 					expect {
 						attendance_importer.parse_row(row)
