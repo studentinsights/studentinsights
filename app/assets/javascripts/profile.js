409d39e23f65e1ce7b49e732f930fdd535dc4281
alexsoble <asoble@gmail.com>
2015-05-19 15:08:31 -0700

    Let's use highcharts for student profile chart

---
+++ app/assets/javascripts/profile.js
@@ -0,0 +1,11 @@
+$(function() {
+
+  if ($('body').hasClass('students') && $('body').hasClass('show')) {
+
+    $('#chart').highcharts({
+
+        // student profile chart goes here! g:0)
+
+    });
+  }
+});
