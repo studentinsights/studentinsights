ad31285612c768da9fd6f856748334b7933cb005
Amir Hadjihabib <beelz1@gmail.com>
2015-05-19 15:52:15 -0700

    Add Highchart theme and style
    Working on adding the chart to the student profile.

--- app/assets/javascripts/profile.js
+++ app/assets/javascripts/profile.js
@@ -3,9 +3,62 @@ $(function() {
   if ($('body').hasClass('students') && $('body').hasClass('show')) {
 
     $('#chart').highcharts({
-
-        // student profile chart goes here! g:0)
-
+		chart: {
+            type: 'areaspline'
+        },
+        title: {
+    		text: '',
+    		style: {
+        		display: 'none'
+    		}
+		},
+		subtitle: {
+   			text: '',
+			style: {
+        		display: 'none'
+    		}
+		},
+        legend: {
+            layout: 'horizontal',
+            align: 'right',
+            verticalAlign: 'top',
+        },
+        xAxis: {
+            categories: [
+                '2010 - 11',
+                '2011 - 12',
+                '2012 - 13',
+                '2013 - 14',
+                '2014 - 15',
+            ],
+        },
+        yAxis: {
+            title: {
+                text: '',
+    			style: {
+        			display: 'none'
+    			}
+            }
+        },
+        tooltip: {
+            shared: true
+        },
+        credits: {
+            enabled: false
+        },
+        plotOptions: {
+            areaspline: {
+                fillOpacity: 0
+            }
+        },
+        series: [{
+            name: 'Absences',
+            data: [3, 4, 3, 5, 4]
+        }, {
+            name: 'Tardies',
+            data: [1, 3, 4, 3, 3]
+        }]
     });
+
   }
 });
