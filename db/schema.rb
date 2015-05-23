13c3211cd8ad2069cf730e28c6d90c50a5d7d7d2
alexsoble <asoble@gmail.com>
2015-05-22 17:55:50 -0700

    Add student address column to student

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150522222605) do
+ActiveRecord::Schema.define(version: 20150523005511) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -136,6 +136,7 @@ ActiveRecord::Schema.define(version: 20150522222605) do
     t.integer  "school_id"
     t.boolean  "limited_english_proficient"
     t.boolean  "former_limited_english_proficient"
+    t.string   "student_address"
   end
 
 end
