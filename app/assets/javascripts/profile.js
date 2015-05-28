02f7ae32cfa015013feb133920628f1526e0b784
thfield <thfield@gmail.com>
2015-05-28 13:08:02 -0700

    Handle student zero-case charting

--- app/assets/javascripts/profile.js
+++ app/assets/javascripts/profile.js
@@ -5,11 +5,18 @@ $(function() {
     var profile_data = $("#profile-data")
     var attendance_events = profile_data.data('attendance-events')
     var discipline_incidents = profile_data.data('discipline-incidents')
+    var student_name = $("#student-name").text()
 
     function isAbsence(event) { return event.absence }
     function isTardy(event) { return event.tardy }
     function countAbsences(attendanceEvents) {return attendanceEvents.filter(isAbsence).length }
     function countTardies(attendanceEvents) { return attendanceEvents.filter(isTardy).length }
+
+    function checkZero(options) {
+      return options.series.every( function(element) {
+        return element.data.length == 0;
+      });
+    }
 
     var attendance_school_years = Object.keys(attendance_events)
     var absences_by_year = Object.keys(attendance_events).map(function(key) { return countAbsences(attendance_events[key]) })
@@ -49,87 +56,125 @@ $(function() {
     // }]
 
     var options = {
-		chart: {
-			renderTo: 'chart',
-            type: 'areaspline'
-        },
-        title: {
-    		text: '',
-    		style: {
-        		display: 'none'
-    		}
-		},
-		subtitle: {
-   			text: '',
-			style: {
-        		display: 'none'
-    		}
-		},
-        legend: {
-            layout: 'horizontal',
-            align: 'right',
-            verticalAlign: 'top',
+  		chart: {
+  			renderTo: 'chart',
+        type: 'areaspline'
+      },
+      title: {
+      		text: '',
+      		style: {
+          	display: 'none'
+      		}
+  		},
+  		subtitle: {
+  			text: '',
+  			style: {
+          		display: 'none'
+      	}
+  		},
+      legend: {
+          layout: 'horizontal',
+          align: 'right',
+          verticalAlign: 'top',
       		itemStyle: {
         		font: '12px "Open Sans", sans-serif !important;',
         		color: '#555555'
 
       		}
-        },
-        xAxis: {
-            categories: [
-                '2010 - 11',
-                '2011 - 12',
-                '2012 - 13',
-                '2013 - 14',
-                '2014 - 15',
-            ],
-        },
-        yAxis: {
-            title: {
-                text: '',
-    			style: {
-        			display: 'none'
-    			}
-            },
-            plotLines: [
-	            {
-	                color: '#B90504',
-	                width: 1,
-	                zIndex: 3,
-	                label: {
-	                    text: '',
-	                    align: 'center',
-	                    style: {
-	                        color: '#999999'
-	                    }
-	                }
-	            }
-	        ],
-        },
-        tooltip: {
-            shared: true
-        },
-        credits: {
-            enabled: false
-        },
-        plotOptions: {
-            areaspline: {
-                fillOpacity: 0
+      },
+      xAxis: {
+          categories: [
+              '2010 - 11',
+              '2011 - 12',
+              '2012 - 13',
+              '2013 - 14',
+              '2014 - 15',
+          ],
+      },
+      yAxis: {
+        title: {
+            text: '',
+      			style: {
+          			display: 'none'
+      			}
+          },
+          plotLines: [
+            {
+              color: '#B90504',
+              width: 1,
+              zIndex: 3,
+              label: {
+                  text: '',
+                  align: 'center',
+                  style: {
+                      color: '#999999'
+                  }
+              }
             }
+        ],
+      },
+      tooltip: {
+          shared: true
+      },
+      credits: {
+          enabled: false
+      },
+      plotOptions: {
+          areaspline: {
+              fillOpacity: 0
+          }
+      }
+    }
+
+    var zeroCase = {
+      chart: {
+        renderTo: 'chart',
+        events: {
+            load: function () {
+                var ren = this.renderer;
+                ren.label(student_name + ' has no ' + options.title.text + ' on record', 20, 40)
+                    .css({
+                        //fontWeight: 'bold'
+                    })
+                    .add();
+              }
         }
+      },
+      title: {
+        text: '',
+        style: {
+            display: 'none'
+        }
+      },
+      subtitle: {
+        text: '',
+        style: {
+          display: 'none'
+        }
+      },
+      legend: {
+        style: {
+            display: 'none'
+          }
+      }
+
     }
 
     options.series = attendance_series
-    var chart = new Highcharts.Chart(options);
+    options.title.text = 'absences or tardies'
+    var chart = checkZero(options) ? new Highcharts.Chart(zeroCase) : new Highcharts.Chart(options);
+    //var chart = new Highcharts.Chart(options);
 
 	$("#chart-type").on('change', function(){
 	    var selVal = $("#chart-type").val();
 	    if(selVal == "attendance" || selVal == '') {
 	        options.series = attendance_series
+          options.title.text = 'absences or tardies'
 	        options.xAxis.categories = attendance_school_years
 	    }
 	    else if(selVal == "behavior") {
 	        options.series = behavior_series
+          options.title.text = 'behavior incidents'
 	        options.xAxis.categories = discipline_school_years
 	    }
 	    // else if(selVal == "mcas-growth") {
@@ -144,7 +189,8 @@ $(function() {
 	    //     options.yAxis.plotLines[0].value = "40"
 	    //     options.xAxis.categories = ["Sept. 2010 - 11", "Jan. 2010 - 11", "May 2011 - 12", "Sept. 2011 - 12", "Jan. 2011 - 12", "May 2011 - 12", "Sept. 2012 - 13", "Jan. 2012 - 13", "May 2012 - 13", "Sept. 2013 - 14", "Jan. 2013 - 14", "May 2013 - 14"]
 	    // }
-	    var chart = new Highcharts.Chart(options);
+      var chart = checkZero(options) ? new Highcharts.Chart(zeroCase) : new Highcharts.Chart(options);
+      //var chart = new Highcharts.Chart(options);
 	});
   }
 });
