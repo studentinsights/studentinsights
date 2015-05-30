8d090ae467068b730985e66e6fe83c1b32bfe1dd
thfield <thfield@gmail.com>
2015-05-30 15:26:07 -0700

    generates fake data for each year

--- db/seeds/demo/demo_students.seeds.rb
+++ db/seeds/demo/demo_students.seeds.rb
@@ -32,17 +32,27 @@ AttendanceEvent.destroy_all
   attendance_event_generator = Rubystats::NormalDistribution.new(8.8, 10)
   # guestimating that 40% of students have discipline events
   2.in(5) do
-    discipline_event_generator.rng.round(0).times do
-      discipline_incident = DisciplineIncident.new(FakeDisciplineIncident.data)
-      discipline_incident.student_id = student.id
-      discipline_incident.save
+    5.times do |n|
+      date_begin = Time.local(2010 + n, 1, 1)
+      date_end = Time.local(2010 + n, 12, 31)
+      discipline_event_generator.rng.round(0).times do
+        discipline_incident = DisciplineIncident.new(FakeDisciplineIncident.data)
+        discipline_incident.student_id = student.id
+        discipline_incident.event_date = Time.at(date_begin + rand * (date_end.to_f - date_begin.to_f))
+        discipline_incident.save
+      end
     end
   end
   94.in(100) do
-    attendance_event_generator.rng.round(0).times do
-      attendance_event = AttendanceEvent.new(FakeAttendanceEvent.data)
-      attendance_event.student_id = student.id
-      attendance_event.save
+    5.times do |n|
+      date_begin = Time.local(2010 + n, 1, 1)
+      date_end = Time.local(2010 + n, 12, 31)
+      attendance_event_generator.rng.round(0).times do
+        attendance_event = AttendanceEvent.new(FakeAttendanceEvent.data)
+        attendance_event.student_id = student.id
+        attendance_event.event_date = Time.at(date_begin + rand * (date_end.to_f - date_begin.to_f))
+        attendance_event.save
+      end
     end
   end
 end
