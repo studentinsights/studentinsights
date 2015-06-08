ef7065408e755d505b6ab630784dd01a50878f33
alexsoble <asoble@gmail.com>
2015-06-07 20:35:45 -0400

    Give existing feature test some style <3

--- spec/features/sign_in_feature_spec.rb
+++ spec/features/sign_in_feature_spec.rb
@@ -2,11 +2,9 @@ require 'rails_helper'
 require 'capybara/rspec'
 
 describe "educator sign in", :type => :feature do
-
   context "educator with account signs in" do
     let(:educator) { FactoryGirl.create(:educator_with_homeroom) }
-    let(:homeroom) { FactoryGirl.create(:homeroom) } 
-
+    let(:homeroom) { FactoryGirl.create(:homeroom) }
     it "can access students page" do
       visit root_url
       click_link 'Sign In'
@@ -16,21 +14,15 @@ describe "educator sign in", :type => :feature do
       expect(page).to have_content 'Signed in successfully.'
       visit homeroom_students_url(homeroom)
     end
-
   end
-
   context "educator without account signs in" do
-
     it "cannot access students page" do
       visit root_url
       click_link 'Sign In'
       fill_in 'educator_email', with: "educatorname"
       fill_in 'educator_password', with: "password"
       click_button 'Log in'
-
       expect(page).to have_content 'Invalid email or password.'
-    end 
-
+    end
   end
-
 end
