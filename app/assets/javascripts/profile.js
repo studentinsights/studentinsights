2e7de33bf17cc7f02f0bffc8f66a942457d41e0e
alexsoble <asoble@gmail.com>
2015-06-19 19:56:23 -0700

    Clean up profile.js
    + Write helper functions to make adding new data series to the chart less wordy
    + Sketch out the functions we'll need to build STAR and MCAS charts
    + Apply more style consistency

--- app/assets/javascripts/profile.js
+++ app/assets/javascripts/profile.js
@@ -5,27 +5,44 @@ $(function() {
     var profile_data = $("#profile-data")
     var attendance_events = profile_data.data('attendance-events')
     var discipline_incidents = profile_data.data('discipline-incidents')
+    var mcas_results = profile_data.data('mcas-results')
+    var star_results = profile_data.data('star-results')
     var student_name = $("#student-name").text()
 
-    function isAbsence(event) { return event.absence }
-    function isTardy(event) { return event.tardy }
-    function countAbsences(attendanceEvents) {return attendanceEvents.filter(isAbsence).length }
-    function countTardies(attendanceEvents) { return attendanceEvents.filter(isTardy).length }
+    // Functions for adding absences, tardies, and behavior issues to the chart
+    function isAbsence (event) { return event.absence }
+    function isTardy (event) { return event.tardy }
+    function countAbsences (attendanceEvents) { return attendanceEvents.filter(isAbsence).length }
+    function countTardies (attendanceEvents) { return attendanceEvents.filter(isTardy).length }
+    function countSize (events) { return events.length }
 
-    var attendance_school_years = Object.keys(attendance_events).reverse()
-    var absences_by_year = attendance_school_years.map(function(key) { return countAbsences(attendance_events[key]) })
-    var tardies_by_year = attendance_school_years.map(function(key) { return countTardies(attendance_events[key]) })
+    // Functions for adding assessments to the chart
+    // function getStarMathPercentile (star_result) {}
+    // function getStarReadingPercentile (star_result) {}
+    // function getMcasMathScaled (mcas_result) {}
+    // function getMcasEnglishScaled (mcas_result) {}
+    // Functions to help the other functions to their jobs well
+
+    function schoolYears(events) { return Object.keys(events).reverse() }
+    function prepareEventsForChart(events, prepare_function) {
+      return schoolYears(events).map(function(key) { return prepare_function(events[key]) })
+    }
 
     function checkZero(options) {
-      return options.series.every( function(element) {
-        return element.data.every( function(el) {
+      return options.series.every(function(element) {
+        return element.data.every(function(el) {
           return el == 0
         });
       });
     }
 
-    var discipline_school_years = Object.keys(discipline_incidents).reverse()
-    var discipline_incidents_by_year = Object.keys(discipline_incidents).map(function(key) { return discipline_incidents[key].length })
+    var absences_by_year = prepareEventsForChart(attendance_events, countAbsences)
+    var tardies_by_year = prepareEventsForChart(attendance_events, countTardies)
+    var discipline_incidents_by_year = prepareEventsForChart(discipline_incidents, countSize)
+    // var star_math_percentile_by_year
+    // var star_reading_percentile_by_year
+    // var mcas_math_scaled_by_year
+    // var mcas_english_scaled_by_year
 
     var attendance_series = [{
             name: 'Absences',
@@ -33,13 +50,28 @@ $(function() {
         }, {
             name: 'Tardies',
             data: tardies_by_year
-    }]
-
+        }]
 
     var behavior_series = [{
-            name: 'Behavior incidents',
-            data: discipline_incidents_by_year
-        },]
+        name: 'Behavior incidents',
+        data: discipline_incidents_by_year
+    }]
+
+    // var star_series = [{
+    //         name: 'Math percentile',
+    //         data: star_math_percentile_by_year
+    //     }, {
+    //         name: 'Reading percentile',
+    //         data: star_reading_percentile_by_year
+    //     }]
+
+    // var mcas_series = [{
+    //         name: 'Math scaled score',
+    //         data: mcas_math_scaled_by_year
+    //     }, {
+    //         name: 'English scaled score',
+    //         data: mcas_english_scaled_by_year
+    //     }]
 
     var options = {
       chart: {
@@ -118,7 +150,7 @@ $(function() {
 
     // Default view is attendance series graph
     options.series = attendance_series
-    options.xAxis.categories = attendance_school_years
+    options.xAxis.categories = schoolYears(attendance_events)
 
     options.title.text = 'absences or tardies'
     var chart;
@@ -126,28 +158,28 @@ $(function() {
 
 	  $("#chart-type").on('change', function(){
 	    var selVal = $("#chart-type").val();
-	    if(selVal == "attendance" || selVal == '') {
+	    if (selVal == "attendance" || selVal == '') {
 	        options.series = attendance_series
           options.title.text = 'absences or tardies'
-	        options.xAxis.categories = attendance_school_years
-	    }
-	    else if(selVal == "behavior") {
+	        options.xAxis.categories = schoolYears(attendance_events)
+	    } else if (selVal == "behavior") {
 	        options.series = behavior_series
           options.title.text = 'behavior incidents'
-	        options.xAxis.categories = discipline_school_years
-	    }
-	    // else if(selVal == "mcas-growth") {
-	    //     options.series = mcas_series
-	    //     options.yAxis.plotLines[0].label.text = "MCAS Growth warning: Less than 40 points"
-	    //     options.yAxis.plotLines[0].value = "40"
-	    //     options.xAxis.categories = ["2010 - 11", "2011 - 12", "2013 - 14", "2014 - 15"]
-	    // }
-	    // else {
+          options.xAxis.categories = schoolYears(discipline_incidents)
+     //  } else if (selVal == "mcas") {
+     //      console.log('mcas!')
+     //      console.log(mcas_series)
+     //      options.series = mcas_series
+     //      options.title.text = 'MCAS results'
+     //      options.xAxis.categories = schoolYears(mcas_results)
+	    //     // options.yAxis.plotLines[0].label.text = "MCAS Growth warning: Less than 40 points"
+	    //     // options.yAxis.plotLines[0].value = "40"
+	    // } else if (selVal == "star") {
 	    //     options.series = star_series
-	    //     options.yAxis.plotLines[0].label.text = "STAR Percentile warning: Less than 40 points"
-	    //     options.yAxis.plotLines[0].value = "40"
-	    //     options.xAxis.categories = ["Sept. 2010 - 11", "Jan. 2010 - 11", "May 2011 - 12", "Sept. 2011 - 12", "Jan. 2011 - 12", "May 2011 - 12", "Sept. 2012 - 13", "Jan. 2012 - 13", "May 2012 - 13", "Sept. 2013 - 14", "Jan. 2013 - 14", "May 2013 - 14"]
-	    // }
+     //      options.xAxis.categories = schoolYears(star_results)
+	    //     // options.yAxis.plotLines[0].label.text = "STAR Percentile warning: Less than 40 points"
+	    //     // options.yAxis.plotLines[0].value = "40"
+	    }
       checkZero(options) ? zeroDraw() : chart = new Highcharts.Chart(options);
 	});
   }
