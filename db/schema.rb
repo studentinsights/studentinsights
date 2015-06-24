6f6e18f5619ef2ad1e69654ba72b8a2453a9538b
alexsoble <asoble@gmail.com>
2015-06-23 17:21:39 -0700

    Reset db/schema

--- db/schema.rb
+++ db/schema.rb
@@ -26,15 +26,6 @@ ActiveRecord::Schema.define(version: 20150619235056) do
     t.datetime "event_date"
   end
 
-  create_table "attendance_results", force: true do |t|
-    t.string   "school_year"
-    t.integer  "number_of_absences"
-    t.integer  "number_of_tardies"
-    t.integer  "student_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "discipline_incidents", force: true do |t|
     t.integer  "student_id"
     t.string   "incident_code"
