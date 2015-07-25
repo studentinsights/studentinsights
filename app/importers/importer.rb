7242e1256ec51da57ece775160105caeb9356942
alexsoble <asoble@gmail.com>
2015-07-25 09:34:06 -0700

    Create an import initializer
    + Methods that kick off data import go there

--- app/importers/importer.rb
+++ app/importers/importer.rb
@@ -1,33 +1,5 @@
 module Importer
 
-  def self.import_classes
-    [
-      StudentsImporter,
-      McasImporter,
-      StarMathImporter,
-      StarReadingImporter,
-      BehaviorImporter,
-      AttendanceImporter
-    ]
-  end
-
-  def self.import_all(options = {})
-    import_classes.each do |i|
-      begin
-        i.new(options).connect_and_import
-      rescue Exception => message
-        puts message
-      end
-    end
-    if Rails.env.development?
-      puts "#{Student.count} students"
-      puts "#{McasResult.count} MCAS results"
-      puts "#{StarResult.count} STAR results"
-      puts "#{DisciplineIncident.count} discipline incidents"
-      puts "#{AttendanceEvent.count} attendance events"
-    end
-  end
-
   def initialize(options = {})
     @school = options[:school]
     @recent_only = options[:recent_only]
