a96d24f3940b08f5dc0606358d19b77bf297c8f8
alexsoble <asoble@gmail.com>
2015-07-29 16:34:45 -0700

    Refactor X2 assessments importer

--- spec/importers/x2_assessment_importer_spec.rb
+++ spec/importers/x2_assessment_importer_spec.rb
@@ -10,28 +10,30 @@ RSpec.describe X2AssessmentImporter do
         let(:healey) { School.where(local_id: "HEA").first_or_create! }
         let(:healey_importer) { X2AssessmentImporter.new(school: healey) }
 
+        before(:each) do
+          healey_importer.import(file)
+        end
+
         it 'creates a student' do
-          expect { healey_importer.import(file) }.to change(Student, :count).by 1
+          expect(Student.count).to eq 1
         end
 
         context 'MCAS' do
-          let(:assessment_family) { AssessmentFamily.where(name: "MCAS") }
-
-          before(:each) do
-            healey_importer.import(file)
-          end
+          let(:assessment_families) { AssessmentFamily.where(name: "MCAS") }
+          let(:assessment_family) { AssessmentFamily.where(name: "MCAS").first }
 
           it 'creates assessment family' do
-            expect(assessment_family.count).to eq 1
+            expect(assessment_families.count).to eq 1
           end
           it 'creates two results' do
-            mcas_results = assessment_family.first.assessments
+            mcas_results = assessment_family.assessments
             expect(mcas_results.count).to eq 2
           end
           context 'Math' do
             let(:subject) { AssessmentSubject.where(name: "Math").first }
             it 'sets the scaled scores and performance levels, growth percentiles correctly' do
-              mcas_result = Assessment.where(assessment_subject_id: subject.id).last
+              mcas_result = Assessment.where(
+                assessment_subject_id: subject.id, assessment_family_id: assessment_family.id).last
               expect(mcas_result.scale_score).to eq(214)
               expect(mcas_result.performance_level).to eq('W')
               expect(mcas_result.growth_percentile).to eq(nil)
@@ -40,7 +42,8 @@ RSpec.describe X2AssessmentImporter do
           context 'ELA' do
             let(:subject) { AssessmentSubject.where(name: "ELA").first }
             it 'sets the scaled scores, performance levels, growth percentiles correctly' do
-              mcas_result = Assessment.where(assessment_subject_id: subject.id).last
+              mcas_result = Assessment.where(
+                assessment_subject_id: subject.id, assessment_family_id: assessment_family.id).last
               expect(mcas_result.scale_score).to eq(222)
               expect(mcas_result.performance_level).to eq('NI')
               expect(mcas_result.growth_percentile).to eq(70)
@@ -49,43 +52,35 @@ RSpec.describe X2AssessmentImporter do
         end
 
         context 'DIBELS' do
-          let(:assessment_family) { AssessmentFamily.where(name: "DIBELS") }
-          let(:family) { AssessmentFamily.where(name: "DIBELS").first }
-
-          before(:each) do
-            healey_importer.import(file)
-          end
+          let(:assessment_families) { AssessmentFamily.where(name: "DIBELS") }
+          let(:assessment_family) { AssessmentFamily.where(name: "DIBELS").first }
 
           it 'creates assessment family' do
-            expect(assessment_family.count).to eq 1
+            expect(assessment_families.count).to eq 1
           end
-          it 'creates two results' do
-            results = assessment_family.first.assessments
+          it 'creates one result' do
+            results = assessment_family.assessments
             expect(results.count).to eq 1
           end
           it 'sets the performance levels correctly' do
-            dibels_result = Assessment.where(assessment_family_id: family.id).last
+            dibels_result = Assessment.where(assessment_family_id: assessment_family.id).last
             expect(dibels_result.performance_level).to eq('Benchmark')
           end
         end
 
         context 'ACCESS' do
-          let(:assessment_family) { AssessmentFamily.where(name: "ACCESS") }
-          let(:family) { AssessmentFamily.where(name: "ACCESS").first }
-
-          before(:each) do
-            healey_importer.import(file)
-          end
+          let(:assessment_families) { AssessmentFamily.where(name: "ACCESS") }
+          let(:assessment_family) { AssessmentFamily.where(name: "ACCESS").first }
 
           it 'creates assessment family' do
-            expect(assessment_family.count).to eq 1
+            expect(assessment_families.count).to eq 1
           end
           it 'creates three results' do
-            results = assessment_family.first.assessments
+            results = assessment_family.assessments
             expect(results.count).to eq 3
           end
           it 'sets the scaled scores, performance levels, growth percentiles correctly' do
-            access_result = Assessment.where(assessment_family_id: family.id).last
+            access_result = Assessment.where(assessment_family_id: assessment_family.id).last
             expect(access_result.scale_score).to eq(367)
             expect(access_result.performance_level).to eq('4.9')
             expect(access_result.growth_percentile).to eq(92)
