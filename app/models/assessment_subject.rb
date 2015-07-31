d00130d185f35bbea4c334af19c40913c40f9848
alexsoble <asoble@gmail.com>
2015-07-30 22:49:57 -0700

    Fix class/instance method mixup

--- app/models/assessment_subject.rb
+++ app/models/assessment_subject.rb
@@ -1,15 +1,15 @@
 class AssessmentSubject < ActiveRecord::Base
   has_many :assessments
 
-  def math
+  def self.math
     find_by_name("Math")
   end
 
-  def ela
+  def self.ela
     find_by_name("ELA")
   end
 
-  def reading
+  def self.reading
     find_by_name("Reading")
   end
 end
