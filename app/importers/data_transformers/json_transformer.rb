03e81888f67130e85f05a7236a50a41e88a3a803
alexsoble <asoble@gmail.com>
2015-08-10 16:58:23 -0700

    Refactor JSON object cleaning code into its own struct

--- app/importers/data_transformers/json_transformer.rb
+++ app/importers/data_transformers/json_transformer.rb
@@ -3,86 +3,14 @@ class JsonTransformer
   def transform(file)
     require 'json'
     parsed = JSON.parse(file, symbolize_names: true)
-    cleaned = parsed.select { |row| clean_date_row(row) && clean_boolean_row(row) }
-    transformed = cleaned.each { |row| transform_row(row) }
-  end
-
-  def transform_row(row)
-    row[:local_id] = row[:local_id].to_s
-    if has_dates(row)
-      date_header = (row.keys & date_keys)[0]
-      row[date_header] = parse_date(row[date_header])
+    cleaned = parsed.select do |object|
+      cleaner = JsonObjectCleaner.new(object)
+      (cleaner.clean_date? && cleaner.clean_booleans?)
     end
-    if has_booleans(row)
-      these_boolean_headers = row.keys & boolean_keys
-      these_boolean_headers.each do |b|
-        row[b] = parse_boolean(row[b])
-      end
+    transformed = cleaned.each do |object|
+      cleaner = JsonObjectCleaner.new(object)
+      cleaner.transform_object
     end
-    return row
-  end
-
-  def clean_date_row(row)
-    return true unless has_dates(row)
-    date_header = (row.keys & date_keys)[0]
-    is_parsable_date(row[date_header])
-  end
-
-  def clean_boolean_row(row)
-    return true unless has_booleans(row)
-    result = true
-    these_boolean_headers = row.keys & boolean_keys
-    these_boolean_headers.each do |b|
-      if !is_parsable_boolean(row[b])
-        result = false
-      end
-    end
-    return result
-  end
-
-  def date_keys
-    [:event_date, :date_taken, :assessment_date]
-  end
-
-  def boolean_keys
-    [:asbence, :tardy, :has_exact_time]
-  end
-
-  def has_dates(row)
-    (row.keys & date_keys).present?
-  end
-
-  def has_booleans(row)
-    (row.keys & boolean_keys).present?
-  end
-
-  def is_parsable_boolean(value)
-    [true, false, '1', '0', 1, 0, 'true', 'false'].include? value
-  end
-
-  def is_parsable_date(value)
-    require 'date'
-    DateTime.strptime(value.to_s,'%s')
-  rescue ArgumentError
-    false
-  end
-
-  def parse_boolean(value)
-    case value
-    when "1", 1
-      true
-    when "0", 0
-      false
-    else
-      nil
-    end
-  end
-
-  def parse_date(event_date)
-    require 'date'
-    DateTime.strptime(event_date.to_s,'%s')
-  rescue ArgumentError
-    nil
   end
 
 end
