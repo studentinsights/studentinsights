773b44c81d0c3991c3e2ec281e85d0d78079a423
alexsoble <asoble@gmail.com>
2015-08-10 16:16:40 -0700

    Toughen up JsonTransformer against crummy data and test it #211

--- app/importers/data_transformers/json_transformer.rb
+++ app/importers/data_transformers/json_transformer.rb
@@ -3,52 +3,86 @@ class JsonTransformer
   def transform(file)
     require 'json'
     parsed = JSON.parse(file, symbolize_names: true)
-    parsed.each do |row|
-      transform_row row
-    end
-  end
-
-  def date_rows
-    [ :event_date, :date_taken, :assessment_date ]
-  end
-
-  def boolean_rows
-    [ :asbence, :tardy, :has_exact_time ]
+    cleaned = parsed.select { |row| clean_date_row(row) && clean_boolean_row(row) }
+    transformed = cleaned.each { |row| transform_row(row) }
   end
 
   def transform_row(row)
     row[:local_id] = row[:local_id].to_s
-
-    # Date rows to datetime
-    date_rows.each do |dr|
-      if row[dr].present?
-        row[dr] = parse_date(row[dr])
+    if has_dates(row)
+      date_header = (row.keys & date_keys)[0]
+      row[date_header] = parse_date(row[date_header])
+    end
+    if has_booleans(row)
+      these_boolean_headers = row.keys & boolean_keys
+      these_boolean_headers.each do |b|
+        row[b] = parse_boolean(row[b])
       end
     end
-
-    boolean_rows.each do |br|
-      if row[br].present?
-        row[br] = parse_boolean(row[br])
-      end
-    end
-
     return row
+  end
+
+  def clean_date_row(row)
+    return true unless has_dates(row)
+    date_header = (row.keys & date_keys)[0]
+    is_parsable_date(row[date_header])
+  end
+
+  def clean_boolean_row(row)
+    return true unless has_booleans(row)
+    result = true
+    these_boolean_headers = row.keys & boolean_keys
+    these_boolean_headers.each do |b|
+      if !is_parsable_boolean(row[b])
+        result = false
+      end
+    end
+    return result
+  end
+
+  def date_keys
+    [:event_date, :date_taken, :assessment_date]
+  end
+
+  def boolean_keys
+    [:asbence, :tardy, :has_exact_time]
+  end
+
+  def has_dates(row)
+    (row.keys & date_keys).present?
+  end
+
+  def has_booleans(row)
+    (row.keys & boolean_keys).present?
+  end
+
+  def is_parsable_boolean(value)
+    [true, false, '1', '0', 1, 0, 'true', 'false'].include? value
+  end
+
+  def is_parsable_date(value)
+    require 'date'
+    DateTime.strptime(value.to_s,'%s')
+  rescue ArgumentError
+    false
+  end
+
+  def parse_boolean(value)
+    case value
+    when "1", 1
+      true
+    when "0", 0
+      false
+    else
+      nil
+    end
   end
 
   def parse_date(event_date)
     require 'date'
     DateTime.strptime(event_date.to_s,'%s')
-  end
-
-  def parse_boolean(value)
-    case value
-    when "1"
-      true
-    when "0"
-      false
-    else
-      nil
-    end
+  rescue ArgumentError
+    nil
   end
 
 end
--- spec/importers/bulk_attendance_importer_spec.rb
+++ spec/importers/bulk_attendance_importer_spec.rb
@@ -1,37 +1,61 @@
-require 'rails_helper'
+ require 'rails_helper'
 
 RSpec.describe BulkAttendanceImporter do
   let(:bulk_attendance_importer) { BulkAttendanceImporter.new }
   describe '#import' do
-    let(:file) { File.open("#{Rails.root}/spec/fixtures/fake_attendance_export.txt") }
-    let(:transformer) { X2ExportCsvTransformer.new }
-    let(:csv) { transformer.transform(file) }
-    context 'mixed good & bad rows' do
-      it 'imports two valid attendance rows' do
-        bulk_attendance_importer.import(csv)
-        expect(AttendanceEvent.count).to eq 2
-      end
-      it 'sets the dates correctly' do
-        bulk_attendance_importer.import(csv)
-        event = AttendanceEvent.last
-        expect(event.event_date).to eq DateTime.new(2015, 9, 16)
-      end
-      context 'existing student' do
-        let!(:student) { FactoryGirl.create(:student_we_want_to_update) }
-        it 'assigns correct row to existing student' do
-          bulk_attendance_importer.import(csv)
-          expect(student.attendance_events.count).to eq 1
+    context 'json' do
+      let(:file) { File.open("#{Rails.root}/spec/fixtures/kipp_nj_fixtures/fake_att.json").read }
+      let(:transformer) { JsonTransformer.new }
+      let(:json) { transformer.transform(file) }
+      context 'mixed good & bad rows' do
+        it 'imports two valid attendance events, drops two non-valid events' do
+          bulk_attendance_importer.import(json)
+          expect(AttendanceEvent.count).to eq 2
         end
-        it 'sets the absence/tardy values correctly' do
-          bulk_attendance_importer.import(csv)
-          event = student.attendance_events.last
-          expect(event.absence).to be true
+        it 'sets the absence/tardy results correctly' do
+          bulk_attendance_importer.import(json)
+          absences = AttendanceEvent.where(absence: true)
+          tardies = AttendanceEvent.where(tardy: true)
+          expect(absences.count).to eq 2
+          expect(tardies.count).to eq 0
+        end
+        it 'sets the date correctly' do
+          bulk_attendance_importer.import(json)
+          expect(AttendanceEvent.last.event_date).to eq DateTime.new(2014, 8, 11)
         end
       end
-      context 'missing students' do
-        it 'creates new students' do
+    end
+    context 'csv' do
+      let(:file) { File.open("#{Rails.root}/spec/fixtures/fake_attendance_export.txt") }
+      let(:transformer) { X2ExportCsvTransformer.new }
+      let(:csv) { transformer.transform(file) }
+      context 'mixed good & bad rows' do
+        it 'imports two valid attendance rows' do
           bulk_attendance_importer.import(csv)
-          expect(Student.count).to eq 2
+          expect(AttendanceEvent.count).to eq 2
+        end
+        it 'sets the dates correctly' do
+          bulk_attendance_importer.import(csv)
+          event = AttendanceEvent.last
+          expect(event.event_date).to eq DateTime.new(2015, 9, 16)
+        end
+        context 'existing student' do
+          let!(:student) { FactoryGirl.create(:student_we_want_to_update) }
+          it 'assigns correct row to existing student' do
+            bulk_attendance_importer.import(csv)
+            expect(student.attendance_events.count).to eq 1
+          end
+          it 'sets the absence/tardy values correctly' do
+            bulk_attendance_importer.import(csv)
+            event = student.attendance_events.last
+            expect(event.absence).to be true
+          end
+        end
+        context 'missing students' do
+          it 'creates new students' do
+            bulk_attendance_importer.import(csv)
+            expect(Student.count).to eq 2
+          end
         end
       end
     end
