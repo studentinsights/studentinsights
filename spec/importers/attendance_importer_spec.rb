71453c63c735cbbffc33b36ad44f077d4040f497
alexsoble <asoble@gmail.com>
2015-08-11 20:36:43 -0700

    Update AttendanceImporterSpec to test new flow with CsvTransformer

--- spec/importers/attendance_importer_spec.rb
+++ spec/importers/attendance_importer_spec.rb
@@ -3,45 +3,43 @@ require 'rails_helper'
 RSpec.describe AttendanceImporter do
 
   let(:attendance_importer) { AttendanceImporter.new }
+  let(:import) { attendance_importer.import(csv) }
 
   describe '#import_row' do
-    context 'good data' do
-      let(:row) { {local_id: '10', absence: '1', tardy: '0', event_date: '2015-2-2'} }
+    context 'realistic data' do
+      let(:file) { File.open("#{Rails.root}/spec/fixtures/fake_attendance_export.txt") }
+      let(:transformer) { CsvTransformer.new }
+      let(:csv) { transformer.transform(file) }
+
       context 'student already exists' do
         let!(:student) { FactoryGirl.create(:student_we_want_to_update) }
         it 'creates attendence event for the correct student' do
-          attendance_importer.import_row(row)
+          import
           expect(student.reload.attendance_events.size).to eq 1
         end
         it 'assigns absence correctly' do
-          attendance_importer.import_row(row)
+          import
           expect(student.reload.attendance_events.last.absence?).to be true
         end
         it 'assigns tardiness correctly' do
-          attendance_importer.import_row(row)
+          import
           expect(student.reload.attendance_events.last.tardy?).to be false
+        end
+        it 'assigns the date correctly' do
+          import
+          event_date = student.reload.attendance_events.last.event_date
+          expect(event_date).to eq DateTime.new(2005, 9, 16)
         end
       end
       context 'student does not already exist' do
         it 'creates a new student' do
-          expect {
-            attendance_importer.import_row(row)
-          }.to change(Student, :count).by 1
+          expect { import }.to change(Student, :count).by 2
         end
         it 'assigns the attendance event to the new student' do
-          attendance_importer.import_row(row)
+          import
           new_student = Student.last.reload
           expect(new_student.attendance_events.size).to eq 1
         end
-      end
-    end
-    context 'null date' do
-      let(:row) { { local_id: '100', absence: '2015-2-2', tardy: '1', event_date: 'N' } }
-      it 'does not raise an error' do
-        expect { attendance_importer.import_row(row) }.not_to raise_error
-      end
-      it 'does not import an attendance event' do
-        expect { attendance_importer.import_row(row) }.to change(Student, :count).by 0
       end
     end
   end
