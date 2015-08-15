07e33fa6ac4ff95fce2a8bf0468c1eeb31291e1b
alexsoble <asoble@gmail.com>
2015-08-14 17:17:19 -0700

    Intervention end date optional

--- app/controllers/interventions_controller.rb
+++ app/controllers/interventions_controller.rb
@@ -5,7 +5,7 @@ class InterventionsController < ApplicationController
   def create
     @intervention = Intervention.new(intervention_params)
     @intervention.start_date = Time.zone.now
-    @intervention.end_date = Date.parse(intervention_params[:end_date])
+    @intervention.end_date = Date.parse(intervention_params[:end_date]) if intervention_params[:end_date].present?
     @intervention.save
     redirect_to student_path(@intervention.student) + "#interventions-row"
   end
