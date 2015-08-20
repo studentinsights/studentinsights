8e18b512e032dd8cd8040d5618c4f1fc9e0098ba
alexsoble <asoble@gmail.com>
2015-08-20 07:56:54 -0700

    Limit assessments shown in roster view to assessments relevant to homeroom
    + Using a hamhanded ERB approach, wondering how I could do this more elegantly without getting all js frameworky
    + Fixes #153

--- app/models/homeroom.rb
+++ app/models/homeroom.rb
@@ -7,8 +7,28 @@ class Homeroom < ActiveRecord::Base
   belongs_to :educator
 
   def update_grade(student)
-    # Set homeroom grade level to be first student's grade level
+    # Set homeroom grade level to be first student's grade level, since
+    # we don't have any crosswalk between homerooms and grades in
+    # Somerville's Student Information System
+
     return if self.grade.present?
+    return if student.grade.blank?
     update_attribute(:grade, student.grade)
   end
+
+  def show_dibels?
+    return true if grade.blank?  # Default to show
+    grade.to_i <= 4  # DIBELS is given K-3, show through grade 4
+  end
+
+  def show_mcas?
+    return true if grade.blank?  # Default to show
+    grade.to_i >= 3  # MCAS is given 3–8, 10
+  end
+
+  def show_star?
+    return true if grade.blank?  # Default to show
+    grade.to_i >= 2  # STAR is given 2–8
+  end
+
 end
