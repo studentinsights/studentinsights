80354369f21859d51b742f3ba043f4c3f8840d25
alexsoble <asoble@gmail.com>
2015-08-25 18:00:40 -0700

    Add fake intstructional reading levels

--- db/seeds/demo/demo_data/fake_star_reading_result_generator.rb
+++ db/seeds/demo/demo_data/fake_star_reading_result_generator.rb
@@ -2,19 +2,22 @@ class FakeStarReadingResultGenerator
 
   def initialize(student)
     @test_date = DateTime.new(2010, 9, 1)
-    @reading_percentile = rand(10..99)
     @student = student
+    @instructional_reading_level = @student.grade.to_f
+    @reading_percentile = rand(10..99)
   end
 
   def next
     @reading_percentile += rand(-15..15)
     @reading_percentile = [0, @reading_percentile, 100].sort[1]
+    @instructional_reading_level += rand(-1..1)
     @test_date += rand(30..60)  # days
 
     return {
       assessment_id: Assessment.star_reading.id,
       date_taken: @test_date,
       percentile_rank: @reading_percentile,
+      instructional_reading_level: @instructional_reading_level,
       student_id: @student.id
     }
   end
