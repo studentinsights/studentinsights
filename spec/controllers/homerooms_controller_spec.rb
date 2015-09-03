008c697a3305ff4f7cbc10e282e11a23c7e455f7
alexsoble <asoble@gmail.com>
2015-09-03 15:40:33 -0400

    Write new context/it blocks for educator homeroom permissions #278

--- spec/controllers/homerooms_controller_spec.rb
+++ spec/controllers/homerooms_controller_spec.rb
@@ -22,7 +22,7 @@ describe HomeroomsController, :type => :controller do
       before do
         sign_in(educator)
       end
-      context 'when there is no homeroom params' do
+      context 'no homeroom params' do
         it 'does not raise an error' do
           expect { make_request('garbage homeroom ids rule') }.not_to raise_error
         end
@@ -30,24 +30,46 @@ describe HomeroomsController, :type => :controller do
           make_request(educator.homeroom.slug)
           expect(assigns(:homeroom)).to eq(educator.homeroom)
         end
+        context 'when there are no students' do
+          before { make_request }
+          it 'is successful' do
+            make_request
+            expect(response).to be_success
+          end
+          it 'assigns rows to empty' do
+            expect(assigns(:rows)).to be_empty
+          end
+        end
+        context 'when there are students' do
+          let!(:first_student) { FactoryGirl.create(:student, homeroom: educator.homeroom) }
+          let!(:second_student) { FactoryGirl.create(:student, homeroom: educator.homeroom) }
+          before { make_request }
+          it 'assigns rows to a non-empty array' do
+            expect(assigns(:rows)).to be_a_kind_of Array
+            expect(assigns(:rows)).to_not be_empty
+          end
+        end
       end
-      context 'when there are no students' do
-        before { make_request }
+    end
+    context 'homeroom params' do
+      context 'homeroom belongs to educator' do
         it 'is successful' do
-          make_request
-          expect(response).to be_success
-        end
-        it 'assigns rows to empty' do
-          expect(assigns(:rows)).to be_empty
+
         end
       end
-      context 'when there are students' do
-        let!(:first_student) { FactoryGirl.create(:student, homeroom: educator.homeroom) }
-        let!(:second_student) { FactoryGirl.create(:student, homeroom: educator.homeroom) }
-        before { make_request }
-        it 'assigns rows to a non-empty array' do
-          expect(assigns(:rows)).to be_a_kind_of Array
-          expect(assigns(:rows)).to_not be_empty
+      context 'homeroom is same school and grade level as educator\'s' do
+        it 'is successful' do
+
+        end
+      end
+      context 'homeroom is different school, same grade level as educator\'s' do
+        it 'is not successful' do
+
+        end
+      end
+      context 'homeroom is different school and grade level as educator\'s' do
+        it 'is not successful' do
+
         end
       end
     end
@@ -56,9 +78,8 @@ describe HomeroomsController, :type => :controller do
         sign_in(educator_without_homeroom)
       end
       context 'when there is no homeroom params' do
-        it 'assigns the first homeroom' do
-          make_request('garbage homeroom ids rule')
-          expect(assigns(:homeroom)).to eq(Homeroom.first)
+        it 'redirects to error page' do
+
         end
       end
     end
