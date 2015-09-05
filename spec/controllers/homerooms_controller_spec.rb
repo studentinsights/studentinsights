475fb09d4f2dd4f873c1ebd0536bee7883b40143
alexsoble <asoble@gmail.com>
2015-09-05 10:32:49 -0400

    Write failing tests for educator homeroom permissions, refactor spec #278

--- spec/controllers/homerooms_controller_spec.rb
+++ spec/controllers/homerooms_controller_spec.rb
@@ -19,23 +19,16 @@ describe HomeroomsController, :type => :controller do
       end
     end
     context 'when educator with homeroom is logged in' do
-      before do
-        sign_in(educator)
-      end
+      before { sign_in(educator) }
       context 'no homeroom params' do
-        it 'does not raise an error' do
-          expect { make_request('garbage homeroom ids rule') }.not_to raise_error
+        before { make_request }
+        it 'is successful' do
+          expect(response).to be_success
         end
-        it 'assigns the educators homeroom' do
-          make_request(educator.homeroom.slug)
+        it 'assigns the educator\'s homeroom' do
           expect(assigns(:homeroom)).to eq(educator.homeroom)
         end
         context 'when there are no students' do
-          before { make_request }
-          it 'is successful' do
-            make_request
-            expect(response).to be_success
-          end
           it 'assigns rows to empty' do
             expect(assigns(:rows)).to be_empty
           end
@@ -43,8 +36,8 @@ describe HomeroomsController, :type => :controller do
         context 'when there are students' do
           let!(:first_student) { FactoryGirl.create(:student, homeroom: educator.homeroom) }
           let!(:second_student) { FactoryGirl.create(:student, homeroom: educator.homeroom) }
-          before { make_request }
           it 'assigns rows to a non-empty array' do
+            make_request
             expect(assigns(:rows)).to be_a_kind_of Array
             expect(assigns(:rows)).to_not be_empty
           end
@@ -52,34 +45,49 @@ describe HomeroomsController, :type => :controller do
       end
     end
     context 'homeroom params' do
+      context 'garbage params' do
+        it 'does not raise an error' do
+          expect { make_request('garbage homeroom ids rule') }.not_to raise_error
+        end
+        it 'assigns the educators homeroom' do
+          make_request('garbage homeroom ids rule')
+          expect(assigns(:homeroom)).to eq(educator.homeroom)
+        end
+      end
       context 'homeroom belongs to educator' do
         it 'is successful' do
-
+          make_request(educator.homeroom.slug)
+          expect(response).to be_success
         end
       end
-      context 'homeroom is same school and grade level as educator\'s' do
-        it 'is successful' do
-
+      context 'homeroom does not belong to educator' do
+        context 'homeroom is grade level as educator\'s' do
+          let(:homeroom) { FactoryGirl.create(:homeroom_in_same_school_and_grade_level_as_educator_homeroom) }
+          it 'is successful' do
+            make_request(homeroom.slug)
+            expect(response).to be_success
+          end
         end
-      end
-      context 'homeroom is different school, same grade level as educator\'s' do
-        it 'is not successful' do
-
-        end
-      end
-      context 'homeroom is different school and grade level as educator\'s' do
-        it 'is not successful' do
-
+        context 'homeroom is different grade level from educator\'s' do
+          let(:homeroom) { FactoryGirl.create(:homeroom) }
+          it 'redirects to educator\'s homeroom' do
+            make_request(homeroom.slug)
+            expect(response).to redirect_to(homeroom_path(educator.homeroom))
+          end
         end
       end
     end
     context 'when educator without homeroom is logged in' do
-      before do
-        sign_in(educator_without_homeroom)
+      before { sign_in(educator_without_homeroom) }
+      context 'no homeroom params' do
+        it 'raises a error' do
+          expect { make_request }.to raise_error ActionController::RoutingError
+        end
       end
-      context 'when there is no homeroom params' do
-        it 'redirects to error page' do
-
+      context 'homeroom params' do
+        let!(:homeroom) { FactoryGirl.create(:homeroom) }
+        it 'raises a error' do
+          expect { make_request(homeroom.slug) }.to raise_error ActionController::RoutingError
         end
       end
     end
