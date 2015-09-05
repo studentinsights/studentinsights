fbb47be903deca53d833346e2ada4bbcf7adfbc7
alexsoble <asoble@gmail.com>
2015-09-05 14:38:19 -0400

    Scope homerooms in dropdown to educator's permissions #278

--- app/controllers/homerooms_controller.rb
+++ app/controllers/homerooms_controller.rb
@@ -30,7 +30,7 @@ class HomeroomsController < ApplicationController
 
     @risk_levels = @homeroom.student_risk_levels.group(:level).count
     @risk_levels['null'] = if @risk_levels.has_key? nil then @risk_levels[nil] else 0 end
-    @homerooms_by_name = Homeroom.where.not(name: "Demo").order(:name)
+    @homerooms_by_name = current_educator.allowed_homerooms_by_name
   end
 
   private
