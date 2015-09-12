00823bfe03be5d91f62467ca433c048c3095ddd6
alexsoble <asoble@gmail.com>
2015-09-11 17:42:35 -0700

    Wire up some uberminimal AJAX to the backend

--- app/controllers/bulk_intervention_assignments_controller.rb
+++ app/controllers/bulk_intervention_assignments_controller.rb
@@ -1,6 +1,13 @@
 class BulkInterventionAssignmentsController < ApplicationController
 
+  def new
+    @bulk_intervention_assignment = BulkInterventionAssignment.new
+  end
+
   def create
+    assignment = BulkInterventionAssignment.new(params['bulk_intervention_assignment'])
+    @interventions = assignment.interventions
+    respond_to :js
   end
 
 end
--- app/models/bulk_intervention_assignment.rb
+++ app/models/bulk_intervention_assignment.rb
@@ -1,9 +1,22 @@
 class BulkInterventionAssignment
   include ActiveModel::Model
 
-  attr_accessor :student_ids, :intervention_type_id, :comment, :end_date
+  attr_accessor :student_ids, :intervention_type_id, :comment, :end_date,
+    :educator_id, :goal
 
-  def create_interventions
-
+  def interventions
+    if student_ids.present?
+      student_ids.map do |s|
+        Intervention.create(
+          comment: comment,
+          end_date: end_date,
+          intervention_type_id: intervention_type_id,
+          goal: goal,
+          educator_id: educator_id,
+          start_date: Date.new
+        )
+      end
+    end
   end
+
 end
