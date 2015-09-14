eb670eea924ed9bc7ad6b0abef9db7ed02dde5d5
alexsoble <asoble@gmail.com>
2015-09-14 10:21:51 -0700

    Add goals field to intervention model #277

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150830191132) do
+ActiveRecord::Schema.define(version: 20150914172000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -128,6 +128,7 @@ ActiveRecord::Schema.define(version: 20150830191132) do
     t.integer  "educator_id"
     t.integer  "number_of_hours"
     t.integer  "school_year_id"
+    t.text     "goal"
   end
 
   create_table "school_years", force: true do |t|
