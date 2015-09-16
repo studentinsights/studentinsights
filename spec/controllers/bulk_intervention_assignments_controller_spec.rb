c9b3af9acb80464a3e52ccdf1315756887d7c7f2
alexsoble <asoble@gmail.com>
2015-09-16 16:11:24 -0700

    Write context & expectations for bulk assignments controller spec

--- spec/controllers/bulk_intervention_assignments_controller_spec.rb
+++ spec/controllers/bulk_intervention_assignments_controller_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe BulkInterventionAssignmentsController, type: :controller do
-
+	describe '#create' do
+    context 'valid params' do
+      context 'one student selected' do
+        it 'creates one student intervention' do
+        end
+        it 'assigns the intervention values correctly' do
+        end
+      end
+      context 'three students' do
+        it 'creates three student interventions' do
+        end
+        it 'assigns the intervention values correctly' do
+        end
+      end
+    end
+    context 'invalid params' do
+      context 'one student selected' do
+        context 'invalid date' do
+          it 'returns an error message' do
+          end
+          it 'does not create an intervention' do
+          end
+        end
+      end
+    end
+  end
 end
