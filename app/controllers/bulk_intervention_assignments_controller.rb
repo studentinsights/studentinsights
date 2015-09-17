2b4ef487d25e8cac684853145a578490a337923d
alexsoble <asoble@gmail.com>
2015-09-16 19:59:52 -0700

    Make bulk interventions controller more sensitive to errors
    + Roll back entire database transaction if any of the intervention object creations fail
    + Pass on error messages to the user/educator through the modal AJAX form
    + One more case to think about, what if empty bulk_intervention_assignment paras are sent through?

--- app/controllers/bulk_intervention_assignments_controller.rb
+++ app/controllers/bulk_intervention_assignments_controller.rb
@@ -8,6 +8,10 @@ class BulkInterventionAssignmentsController < ApplicationController
     assignment = BulkInterventionAssignment.new(params['bulk_intervention_assignment'])
     @interventions = assignment.interventions
     respond_to :js
+  rescue ActiveRecord::RecordInvalid, ActiveRecord::RecordNotFound, ArgumentError => e
+    @message = e.message
+    @interventions = []
+    respond_to :js
   end
 
 end
--- app/models/bulk_intervention_assignment.rb
+++ app/models/bulk_intervention_assignment.rb
@@ -6,16 +6,20 @@ class BulkInterventionAssignment
 
   def interventions
     if student_ids.present?
-      student_ids.map do |s|
-        Intervention.create(
-          student_id: s.to_i,
-          comment: comment,
-          end_date: end_date,
-          intervention_type_id: intervention_type_id,
-          goal: goal,
-          educator_id: educator_id,
-          start_date: Date.new
-        )
+      Date.parse(end_date) if end_date.present?  # <~ Make sure we're passing valid dates in
+      ActiveRecord::Base.transaction do          # <~ Wrap in block so that if one assignment
+        student_ids.map do |s|                   #    attempt fails, all of them get rolled back
+          student = Student.find(s)              # <~ Make sure there's a real student object there
+          Intervention.create!(
+            student_id: student.id,
+            comment: comment,
+            end_date: end_date,
+            intervention_type_id: intervention_type_id,
+            goal: goal,
+            educator_id: educator_id,
+            start_date: Date.new
+          )
+        end
       end
     end
   end
