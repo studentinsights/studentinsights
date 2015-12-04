8fc52045f51edada71a6faf19d48754504daf9e5
Kevin Robinson <kevin.robinson.0@gmail.com>
2015-12-04 17:13:07 -0500

    Fix asset cleaning build in Rails build script

--- scripts/aws/rails/build.sh
+++ scripts/aws/rails/build.sh
@@ -8,12 +8,13 @@
 # Any tests should have run and passed at this point.
 
 echo "Cleaning previously built assets from dev or prod builds..."
-clean_assets
-
-echo "Building production assets..."
-docker-compose run -e 'RAILS_ENV=production' rails bundle exec rake assets:clean assets:precompile
+docker-compose run -e 'RAILS_ENV=production' rails bundle exec rake assets:clean
 # Note that assets:clean doesn't remove everything, and assets:clobber will delete the jquery-ui assets
 # in the public folder, so this could be improved.
+
+echo "Building production assets..."
+docker-compose run -e 'RAILS_ENV=production' rails bundle exec assets:precompile
+
 
 echo "Copying assets to S3..."
 aws s3 cp public/assets s3://somerville-teaching-tool-cdn/production/assets --recursive
@@ -26,6 +27,6 @@ echo "Pushing the production Rails image to the Docker registry..."
 docker push kevinrobinson/somerville-teacher-tool:somerville_production_rails
 
 echo "Clearing any assets we generated in the process..."
-clean_assets
+docker-compose run -e 'RAILS_ENV=production' rails bundle exec rake assets:clean
 
 echo "Done."
