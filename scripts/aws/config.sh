c5070dfe9f3fc7985ef7a03421015a7c886923fa
Kevin Robinson <kevin.robinson.0@gmail.com>
2015-12-04 15:04:01 -0500

    Remove configuration (not sensitive)

--- scripts/aws/config.sh
+++ scripts/aws/config.sh
@@ -2,23 +2,23 @@
 # used by other scripts.
 
 # Credentials for authenticating with the AWS API
-export KEY_NAME=hello-ec2
+export KEY_NAME=
 
 # For performing ssh operations on new boxes
-export SUPERUSER=ec2-user
-export SUPERUSER_PEM_FILE=~/.ssh/pem/ec2-user.pem
+export SUPERUSER=
+export SUPERUSER_PEM_FILE=
 
 # DNS and domain name configuration
-export DOMAIN_NAME=kevinrobinson-play.com
-export HOSTED_ZONE_ID=Z376DTOR3KK86V
+export DOMAIN_NAME=
+export HOSTED_ZONE_ID=
 
 # Security group names, all tied to a specific availability zone at the moment.
 # For now, create these manually and update here, but this could use
 # some work to make it work across regions.
-export SG_DEFAULT=sg-731c5e17
-export SG_SSH_ACCESS=sg-90aee1f4
-export SG_WEB_TRAFFIC=sg-5c0e4638
-export SG_POSTGRES=sg-47b9f623
+export SG_DEFAULT=
+export SG_SSH_ACCESS=
+export SG_WEB_TRAFFIC=
+export SG_POSTGRES=
 
 # AMI image for all nodes
-export AMI_IMAGE_ID=ami-f0091d91
+export AMI_IMAGE_ID=
