51b00bfb553bb978356e2d29103ed2828a04dcea
alexsoble <asoble@gmail.com>
2015-12-11 14:39:40 -0600

    Method names make more sense this way

--- app/charts/student_profile_chart.rb
+++ app/charts/student_profile_chart.rb
@@ -1,27 +1,27 @@
 class StudentProfileChart < Struct.new :data
 
-  def to_highcharts_interventions
+  def interventions_to_highcharts
     return if data[:interventions].blank?
     data[:interventions].with_start_and_end_dates.map do |intervention|
       intervention.to_highcharts
     end
   end
 
-  def to_highcharts_growth_percentile_series(student_assessments)
+  def growth_percentiles_to_highcharts(student_assessments)
     return if student_assessments.blank?
     student_assessments.map do |s|
       [s.date_taken.year, s.date_taken.month, s.date_taken.day, s.growth_percentile]
     end
   end
 
-  def to_highcharts_percentile_rank_series(student_assessments)
+  def percentile_ranks_to_highcharts(student_assessments)
     return if student_assessments.blank?
     student_assessments.map do |s|
       [s.date_taken.year, s.date_taken.month, s.date_taken.day, s.percentile_rank]
     end
   end
 
-  def to_highcharts_scale_score_series(student_assessments)
+  def scale_scores_to_highcharts(student_assessments)
     return if student_assessments.blank?
     student_assessments.map do |s|
       [s.date_taken.year, s.date_taken.month, s.date_taken.day, s.scale_score]
@@ -40,13 +40,13 @@ class StudentProfileChart < Struct.new :data
 
   def chart_data
     {
-      star_series_math_percentile: to_highcharts_percentile_rank_series(data[:star_math_results]),
-      star_series_reading_percentile: to_highcharts_percentile_rank_series(data[:star_reading_results]),
-      mcas_series_math_scaled: to_highcharts_scale_score_series(data[:mcas_math_results]),
-      mcas_series_ela_scaled: to_highcharts_scale_score_series(data[:mcas_ela_results]),
-      mcas_series_math_growth: to_highcharts_growth_percentile_series(data[:mcas_math_results]),
-      mcas_series_ela_growth: to_highcharts_growth_percentile_series(data[:mcas_ela_results]),
-      interventions: to_highcharts_interventions,
+      star_series_math_percentile: percentile_ranks_to_highcharts(data[:star_math_results]),
+      star_series_reading_percentile: percentile_ranks_to_highcharts(data[:star_reading_results]),
+      mcas_series_math_scaled: scale_scores_to_highcharts(data[:mcas_math_results]),
+      mcas_series_ela_scaled: scale_scores_to_highcharts(data[:mcas_ela_results]),
+      mcas_series_math_growth: growth_percentiles_to_highcharts(data[:mcas_math_results]),
+      mcas_series_ela_growth: growth_percentiles_to_highcharts(data[:mcas_ela_results]),
+      interventions: interventions_to_highcharts,
       behavior_series: reverse_for_highcharts(data[:discipline_incidents_by_school_year]),
       behavior_series_school_years: reverse_for_highcharts(data[:school_year_names]),
       attendance_series_absences: reverse_for_highcharts(data[:absences_count_by_school_year]),
--- spec/charts/student_profile_chart_spec.rb
+++ spec/charts/student_profile_chart_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 RSpec.describe StudentProfileChart do
 
-  describe 'to_highcharts_percentile_rank_series' do
+  describe 'percentile_ranks_to_highcharts' do
     let(:student_assessment) { FactoryGirl.create(:star_math_warning_assessment) }
     let(:input) { [student_assessment] }
     let(:student_profile_chart) { StudentProfileChart.new }
     it 'converts the student assessment to highcharts format' do
-      result = student_profile_chart.to_highcharts_percentile_rank_series(input)
+      result = student_profile_chart.percentile_ranks_to_highcharts(input)
       expect(result).to eq [[2015, 6, 19, 8]]
     end
   end
