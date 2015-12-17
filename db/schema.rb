c897b974b744b108fd5e62d32a7d5df69be2c3f6
alexsoble <asoble@gmail.com>
2015-12-17 16:15:12 -0600

    Default educator admin attribute to false

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151217214622) do
+ActiveRecord::Schema.define(version: 20151217221115) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,19 +67,19 @@ ActiveRecord::Schema.define(version: 20151217214622) do
   add_index "discipline_incidents", ["student_id"], name: "index_discipline_incidents_on_student_id", using: :btree
 
   create_table "educators", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "admin"
+    t.boolean  "admin",                  default: false
     t.string   "phone"
   end
 
