b7e432ddda143a877a2356001d208f9efe594dd3
alexsoble <asoble@gmail.com>
2016-01-19 19:46:46 -0500

    Move styles and colors into their own files

---
+++ app/assets/javascripts/react_helpers/styles.js
@@ -0,0 +1,30 @@
+(function(root) {
+
+  var styles = {
+    fontSize: 12,
+
+    header: {
+      display: 'flex',
+      alignItems: 'center',
+      flexDirection: 'column',
+      backgroundColor: '#eee'
+    },
+
+    columnsContainer: {
+      display: 'flex',
+      flexDirection: 'row'
+    },
+
+    summary: {
+      marginTop: 0,
+      borderTop: '1px solid #ccc',
+      background: 'white',
+      paddingTop: 5,
+      paddingLeft: 30,
+      paddingBottom: 20
+    }
+  };
+
+  root.styles = styles;
+
+})(window)
--- app/assets/javascripts/school/overview.js
+++ app/assets/javascripts/school/overview.js
@@ -205,38 +205,6 @@ $(function() {
     });
 
 
-
-    // colors, styles
-    var colors = {
-      selection: 'rgb(255, 204, 138)'
-    };
-
-    var styles = {
-      fontSize: 12,
-
-      header: {
-        display: 'flex',
-        alignItems: 'center',
-        flexDirection: 'column',
-        backgroundColor: '#eee'
-      },
-
-      columnsContainer: {
-        display: 'flex',
-        flexDirection: 'row'
-      },
-
-      summary: {
-        marginTop: 0,
-        borderTop: '1px solid #ccc',
-        background: 'white',
-        paddingTop: 5,
-        paddingLeft: 30,
-        paddingBottom: 20
-      }
-    };
-
-
     // page
     var SchoolOverviewPage = React.createClass({
       propTypes: {
