10fbe03859a903269b0f211d219ef422c5b7cdbc
alexsoble <asoble@gmail.com>
2016-01-21 14:07:31 -0500

    Make importer spec more descriptive

--- spec/importers/importer_spec.rb
+++ spec/importers/importer_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe do
 
   describe '#import' do
+
     let(:import_class) {
       Class.new do
         include Importer
@@ -10,26 +11,29 @@ RSpec.describe do
         def remote_file_name; '' end
       end
     }
-    context 'students' do
-      context 'given a csv' do
-        let(:fixture_path) { "#{Rails.root}/spec/fixtures/fake_students_export.txt" }
-        let(:file) { File.open(fixture_path) }
-        let(:transformer) { CsvTransformer.new }
-        let(:csv) { transformer.transform(file) }
-        context 'not scoped to healey school' do
-          let(:importer) { import_class.new }
-          it 'imports two Somerville High School students' do
-            expect { importer.import(csv) }.to change(Student, :count).by 2
-          end
-        end
-        context 'scoped to healey school' do
-          let(:healey_school) { FactoryGirl.create(:healey) }
-          let(:importer) { import_class.new(school_scope: healey_school) }
-          it 'does not import Somerville High School students' do
-            expect { importer.import(csv) }.to change(Student, :count).by 0
-          end
+
+    context 'CSV with 1 Somerville High School student and 1 Healey student' do
+
+      let(:fixture_path) { "#{Rails.root}/spec/fixtures/fake_students_export.txt" }
+      let(:file) { File.open(fixture_path) }
+      let(:transformer) { CsvTransformer.new }
+      let(:csv) { transformer.transform(file) }
+
+      context 'no scope' do
+        let(:importer) { import_class.new }
+        it 'imports both students' do
+          expect { importer.import(csv) }.to change(Student, :count).by 2
         end
       end
+
+      context 'scope is Healey School' do
+        let(:healey_school) { FactoryGirl.create(:healey) }
+        let(:importer) { import_class.new(school_scope: healey_school) }
+        it 'only imports the Healey student' do
+          expect { importer.import(csv) }.to change(Student, :count).by 1
+        end
+      end
+
     end
   end
 end
