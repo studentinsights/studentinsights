55ef6f1f51ef29a11a6544102659293263adfa62
alexsoble <asoble@gmail.com>
2016-01-26 16:54:59 -0500

    Get staff login name from X2

--- x2_export/educators_export.sql
+++ x2_export/educators_export.sql
@@ -6,6 +6,7 @@ SELECT
   'staff_type',
   'homeroom',
   'status',
+  'login_name',
   'school_local_id'
 UNION ALL
 SELECT
@@ -15,10 +16,15 @@ SELECT
   STF_STAFF_TYPE,
   STF_HOMEROOM,
   STF_STATUS,
+  USR_LOGIN_NAME,
   SKL_SCHOOL_ID
 FROM staff
 INNER JOIN school
   ON staff.STF_SKL_OID=school.SKL_OID
+INNER JOIN person
+  ON staff.STF_PSN_OID=person.PSN_OID
+INNER JOIN user_info
+  ON person.PSN_OID=user_info.USR_PSN_OID
 WHERE STF_STATUS = 'Active'
 AND STF_ID_LOCAL IS NOT NULL
   INTO OUTFILE "E:/_BACKUP_MYSQL/CodeForAmerica/educators_export.txt"
