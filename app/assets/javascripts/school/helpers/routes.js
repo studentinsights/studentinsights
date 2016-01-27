b034e14c5aea82516ff7c9be65a6a3bacdc20a06
alexsoble <asoble@gmail.com>
2016-01-26 20:56:25 -0500

    Make overview page links work nicely in different environments
    + baseURl shouldn't be set to production URL; this breaks the app in development and demo environments
    + Plus the URL can change over time

--- app/assets/javascripts/school/helpers/routes.js
+++ app/assets/javascripts/school/helpers/routes.js
@@ -1,14 +1,13 @@
 (function() {
   // Routing functions
   window.shared || (window.shared = {});
-  
-  var baseUrl = 'https://somerville-teacher-tool.herokuapp.com'; // for local hacking
+
   window.shared.Routes = {
     student: function(id) {
-      return baseUrl + '/students/' + id;
+      return '/students/' + id;
     },
     homeroom: function(id) {
-      return baseUrl + '/homerooms/' + id;
+      return '/homerooms/' + id;
     }
   };
 })();
