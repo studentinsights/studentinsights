958b6c2f084338a866c029bfb441dc83617463d3
Kevin Robinson <kevin.robinson.0@gmail.com>
2016-01-28 13:42:44 -0500

    Remove DevelopmentFixtures, let's use seed data for the majority of use cases instead

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -3,23 +3,13 @@ class SchoolsController < ApplicationController
   before_action :authenticate_admin!      # Defined in ApplicationController.
 
   def show
-    use_fixtures = false
-    students = if use_fixtures then DevelopmentFixtures.new.students_show else Student.serialized_data end
-
     @serialized_data = {
-      students: students,
+      students: Student.serialized_data,
       intervention_types: InterventionType.all
     }
   end
 
-  # To build local fixtures from production data:
-  # copy body of controller code into Rails console, run the code in the body of this method,
-  # print it as JSON and then save it in the /data/schools/star_reading
   def star_reading
-    # Toggle between using demo development data and real data loaded in as a JSON fixture
-    use_fixtures = false
-    students_with_star_reading = if use_fixtures then DevelopmentFixtures.new.students_star_reading else students_with_star_reading() end
-
     @serialized_data = {
       :students_with_star_reading => students_with_star_reading,
       :intervention_types => InterventionType.all
