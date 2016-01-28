1d015888cd5f2264dd25ad9c51781c94dfeb9f31
Alex Soble and Doc Ritezel <asoble+alex+doc@gmail.com>
2016-01-28 03:39:26 -0500

    Test create_demo_student

---
+++ spec/tasks/db_seed_demo_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+Dir["#{Rails.root}/db/seeds/demo/demo_data/*.rb"].each {|file| require file }
+
+RSpec.describe 'rake db:seed:demo' do
+
+  describe '#create_demo_student' do
+    let!(:school) { FactoryGirl.create(:school) }
+    let!(:homeroom) { FactoryGirl.create(:homeroom) }
+
+    before do
+      InterventionType.seed_somerville_intervention_types
+      Assessment.seed_somerville_assessments
+    end
+
+    it 'can run without raising an error' do
+      expect { create_demo_student(homeroom) }.not_to raise_error
+    end
+  end
+
+end
