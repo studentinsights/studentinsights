026df9179efe5bf7b47dddfc5f3f8bc1b70ebeb9
Alex Soble and Doc Ritezel <asoble+alex+doc@gmail.com>
2016-01-27 21:33:21 -0500

    Verify data integrity of student school year at db level

---
+++ db/migrate/20160128022731_add_useful_indices_to_student_school_years.rb
@@ -0,0 +1,10 @@
+class AddUsefulIndicesToStudentSchoolYears < ActiveRecord::Migration
+  def change
+    change_column_null :student_school_years, :student_id, false
+    change_column_null :student_school_years, :school_year_id, false
+    change_column_null :student_school_years, :created_at, false
+    change_column_null :student_school_years, :updated_at, false
+
+    add_index :student_school_years, [:student_id, :school_year_id], name: "index_student_school_years_on_student_id_and_school_year_id", using: :btree, unique: true
+  end
+end
