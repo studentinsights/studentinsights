58a6e0f7e56e460e8f7d617f0abb05fa7f01e0c4
Alex Soble <asoble@gmail.com>
2016-01-29 15:36:29 -0500

    Refactor educator import to use row initializer pattern

---
+++ app/importers/educator_row.rb
@@ -0,0 +1,39 @@
+class EducatorRow < Struct.new(:row)
+
+  def self.build(row)
+    new(row).build
+  end
+
+  def build
+    educator = Educator.first_or_initialize(local_id: row[:local_id])
+
+    educator.assign_attributes(
+      state_id: row[:state_id],
+      full_name: row[:full_name],
+      staff_type: row[:staff_type],
+      admin: is_admin?,
+      school_id: school_rails_id
+    )
+
+    return educator
+  end
+
+  private
+
+  def is_admin?
+    row[:staff_type].present? && row[:staff_type] == "Administrator"
+  end
+
+  def school_local_id
+    row[:school_local_id]
+  end
+
+  def school_rails_id
+    school_ids_dictionary[school_local_id] if school_local_id.present?
+  end
+
+  def school_ids_dictionary
+    SchoolLocalIdToAppId.instance.ids_dictionary
+  end
+
+end
--- app/importers/educators_importer.rb
+++ app/importers/educators_importer.rb
@@ -13,36 +13,10 @@ class EducatorsImporter
   end
 
   def import_row(row)
-    educator = Educator.where(local_id: row[:local_id]).first_or_create!
-
-    # Set homeroom
-    update_homeroom(educator, row[:homeroom]) if row[:homeroom]
-
-    # Set admin status
-    admin_status = (row[:staff_type].present? && row[:staff_type].downcase == "administrator")
-
-    # Look up school ID
-    school_local_id = row[:school_local_id]
-    school_id = SchoolLocalIdToAppId.instance.ids_dictionary[school_local_id] if school_local_id.present?
-
-    # Set staff_type, name, and IDs
-
-    # There are many ways to set attributes in ActiveRecord.
-    # 'update': saves to db, runs validations, runs callbacks, resets updated_at
-
-    educator.update(
-      state_id: row[:state_id],
-      local_id: row[:local_id],
-      full_name: row[:full_name],
-      staff_type: row[:staff_type],
-      admin: admin_status,
-      school_id: school_id
-    )
-  end
-
-  def update_homeroom(educator, homeroom_name)
-    homeroom = Homeroom.find_by_name!(homeroom_name)
-    homeroom.update(educator: educator) if homeroom
+    educator = EducatorRow.build(row)
+    educator.save!
+    homeroom = Homeroom.find_by_name!(row[:homeroom]) if row[:homeroom].present?
+    homeroom.update(educator: educator) if homeroom.present?
   end
 
 end
