cb8d9c03c7d33f89e02ae90728dbbdf567408734
Alex Soble <asoble@gmail.com>
2016-01-30 12:37:36 -0500

    Use row-importer pattern to simplify student assessments import

--- app/importers/student_assessment_importer.rb
+++ app/importers/student_assessment_importer.rb
@@ -14,52 +14,20 @@ class StudentAssessmentImporter
     CsvTransformer.new
   end
 
-  def assessment_white_list
+  def import_row(row)
+    return unless assessment_name_inclues_whitelisted_name(row)
+    student_assessment = StudentAssessmentRow.build(row)
+    student_assessment.save!
+  end
+
+  def assessment_name_inclues_whitelisted_name(row)
+    assessment_whitelist.any? do |name|
+      name.in? row[:assessment_test]
+    end
+  end
+
+  def assessment_whitelist
     ["ACCESS", "WIDA-ACCESS", "DIBELS", "MCAS", "MAP", "MELA-O", "MEPA", "STAR"]
   end
 
-  def check_white_list(row)
-    name = row[:assessment_test]
-    pass = false
-    assessment_white_list.each do |assessment|
-      pass = true if name.include? assessment
-    end
-    return pass
-  end
-
-  def import_row(row)
-    return unless check_white_list(row)
-    standardize_access_test_names(row)
-    chuck_non_numerical_growth_scores(row)
-
-    student = Student.where(local_id: row[:local_id]).first_or_create!
-    assessment = Assessment.where(
-      subject: row[:assessment_subject],
-      family: row[:assessment_test]
-    ).first_or_create!
-
-    result = StudentAssessment.where(
-      student_id: student.id,
-      date_taken: row[:assessment_date],
-      assessment_id: assessment.id
-    ).first_or_create!
-
-    result.update_attributes(
-      scale_score: row[:assessment_scale_score],
-      performance_level: row[:assessment_performance_level],
-      growth_percentile: row[:assessment_growth]
-    )
-  end
-
-  def standardize_access_test_names(row)
-    if row[:assessment_test] == "WIDA-ACCESS"
-      row[:assessment_test] = "ACCESS"
-    end
-  end
-
-  def chuck_non_numerical_growth_scores(row)
-    if !/\D/.match(row[:assessment_growth]).nil?
-      row[:assessment_growth] = nil
-    end
-  end
 end
