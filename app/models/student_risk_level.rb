a23cdeda8e982d37e6e46df50c01e771abbc1603
Alex Soble <asoble@gmail.com>
2016-01-30 09:43:24 -0500

    Precompute and store factors for student risk level

--- app/models/student_risk_level.rb
+++ app/models/student_risk_level.rb
@@ -21,24 +21,36 @@ class StudentRiskLevel < ActiveRecord::Base
   end
 
   def mcas_or_star_at_level(this_level)
-    mcas_math.risk_level == this_level || star_math.risk_level == this_level \
-    || mcas_ela.risk_level == this_level || star_reading.risk_level == this_level
+    mcas_math_risk_level == this_level || star_math_risk_level == this_level \
+    || mcas_ela_risk_level == this_level || star_reading_risk_level == this_level
   end
 
   def mcas_and_star_risk_nil?
-    mcas_math.risk_level == nil && star_math.risk_level == nil \
-    && mcas_ela.risk_level == nil && star_reading.risk_level == nil
+    mcas_math_risk_level == nil && star_math_risk_level == nil \
+    && mcas_ela_risk_level == nil && star_reading_risk_level == nil
+  end
+
+  def limited_english_proficiency_risk_level
+    return 3 if limited_english_proficiency == "Limited"
   end
 
   def update_risk_level!
-    update_attributes(level: calculate_level)
+    update!(
+      mcas_math_risk_level: mcas_math.risk_level,
+      star_math_risk_level: star_math.risk_level,
+      mcas_ela_risk_level: mcas_ela.risk_level,
+      star_reading_risk_level: star_reading.risk_level,
+      limited_english_proficiency_risk_level: limited_english_proficiency_risk_level
+    )
+
+    update!(level: calculate_level)
     update_explanation
   end
 
   def calculate_level
     # As defined by Somerville Public Schools
 
-    if mcas_or_star_at_level(3) || limited_english_proficiency == "Limited"
+    if mcas_or_star_at_level(3) || limited_english_proficiency_risk_level == 3
       level = 3
     elsif mcas_or_star_at_level(2)
       level = 2
@@ -77,7 +89,7 @@ class StudentRiskLevel < ActiveRecord::Base
 
   def level_3_explanations
     explanations = []
-    explanations << "#{name} is limited English proficient." if limited_english_proficiency == "Limited"
+    explanations << "#{name} is limited English proficient." if limited_english_proficiency_risk_level == 3
     explanations << "#{name}'s MCAS Math performance level is Warning." if mcas_math.risk_level == 3
     explanations << "#{name}'s STAR Math performance is in the warning range (below 10)." if star_math.risk_level == 3
     explanations << "#{name}'s MCAS English Language Arts performance level is Warning." if mcas_ela.risk_level == 3
