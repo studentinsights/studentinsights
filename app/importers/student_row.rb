1ba47a5aa94efa4ff2d5f99c8b771e07b0629a3d
Alex Soble <asoble@gmail.com>
2016-01-31 11:41:34 -0500

    Make attributes being passed to student explicit; better name for name_view_attributes

--- app/importers/student_row.rb
+++ app/importers/student_row.rb
@@ -7,19 +7,37 @@ class StudentRow < Struct.new(:row)
   def build
     student = Student.find_or_initialize_by(local_id: row[:local_id])
 
-    attributes = Hash[row].except(:local_id, :school_local_id, :full_name, :homeroom)
-                          .merge(split_first_and_last_name)
+    demographic_attributes = {
+      state_id: row[:state_id],
+      home_language: row[:home_language],
+      program_assigned: row[:program_assigned],
+      limited_english_proficiency: row[:limited_english_proficiency],
+      sped_placement: row[:sped_placement],
+      disability: row[:disability],
+      sped_level_of_need: row[:sped_level_of_need],
+      plan_504: row[:plan_504],
+      student_address: row[:student_address],
+      grade: row[:grade],
+      registration_date: row[:registration_date],
+      free_reduced_lunch: row[:free_reduced_lunch]
+    }
+
+    attributes = demographic_attributes.merge(name_view_attributes)
 
     student.assign_attributes(attributes)
 
     return student
   end
 
-  def split_first_and_last_name
+  def name_view_attributes
     name_split = row[:full_name].split(", ")
 
-    return { first_name: name_split[1], last_name: name_split[0] } if name_split.size == 2
-    return { first_name: nil, last_name: name_split[0] } if name_split.size == 1
+    case name_split.size
+    when 2
+      { first_name: name_split[1], last_name: name_split[0] }
+    when 1
+      { first_name: nil, last_name: name_split[0] }
+    end
   end
 
 end
--- spec/importers/student_row_spec.rb
+++ spec/importers/student_row_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 RSpec.describe StudentRow do
 
-  describe '#split_first_and_last_name' do
+  describe '#name_view_attributes' do
     let(:student_row) { described_class.new(row) }
 
     context 'well formatted name' do
       let(:row) { { full_name: 'Hoag, George' } }
 
       it 'assigns the first and last name correctly' do
-        expect(student_row.split_first_and_last_name).to eq(
+        expect(student_row.name_view_attributes).to eq(
           { first_name: 'George', last_name: 'Hoag' }
         )
       end
@@ -19,7 +19,7 @@ RSpec.describe StudentRow do
       let(:row) { { full_name: 'Hoag' } }
 
       it 'assigns the result to the last name' do
-        expect(student_row.split_first_and_last_name).to eq(
+        expect(student_row.name_view_attributes).to eq(
           { first_name: nil, last_name: 'Hoag' }
         )
       end
