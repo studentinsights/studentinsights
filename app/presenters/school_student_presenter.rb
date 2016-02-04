08e91c464d153f739513a79de50543506eca1dd0
Alex Soble and Doc Ritezel <asoble+alex+doc@gmail.com>
2016-02-04 14:36:29 -0500

    Refactor out SchoolsController methods to SchoolStudentPresenter class

---
+++ app/presenters/school_student_presenter.rb
@@ -0,0 +1,31 @@
+class SchoolStudentPresenter < Struct.new(:student)
+  def self.from_students(*additional_includes)
+    Student.all.includes([
+      :interventions,
+      :student_risk_level,
+      :homeroom,
+      :student_school_years
+    ] + additional_includes).map { |s| new(s) }
+  end
+
+  def as_json
+    student.as_json.merge(
+      interventions: student.interventions,
+      student_risk_level: student.student_risk_level.as_json,
+      discipline_incidents_count: most_recent_school_year.discipline_incidents.count,
+      absences_count: most_recent_school_year.absences.count,
+      tardies_count: most_recent_school_year.tardies.count,
+      homeroom_name: student.try(:homeroom).try(:name)
+    )
+  end
+
+  def as_json_with_star_reading
+    as_json.merge(star_reading_results: student.star_reading_results)
+  end
+
+  private
+
+  def most_recent_school_year
+    student.most_recent_school_year
+  end
+end
