b9455df3603038dee4cc9e9b06c1683b071340dc
Alex Soble <asoble@gmail.com>
2016-02-03 22:37:01 -0500

    Richer educator summary report

--- app/models/educator.rb
+++ app/models/educator.rb
@@ -49,10 +49,37 @@ class Educator < ActiveRecord::Base
 
   def self.summary
     puts; puts "=== EDUCATOR REPORT ==="
-    puts; puts "=== ADMINS ==="
+
+    puts; puts "=== Admins:"
     puts Educator.where(admin: true).pluck(:full_name)
-    puts; puts "=== HOMEROOM TEACHERS ==="
-    puts Educator.all.select { |e| e.homeroom.present? }.map { |e| e.full_name }
+
+    puts; puts "=== Non-admins with school-wide access:"
+    puts Educator.where(admin: false, schoolwide_access: true).pluck(:full_name)
+
+    puts; puts "=== Staff members with grade level-based access:"
+    Educator.where(admin: false)
+            .where(schoolwide_access: false)
+            .select { |e| e.homeroom.blank? }
+            .select { |e| e.grade_level_access.size > 0 }
+            .each do |educator|
+      puts "#{educator.full_name} -- #{educator.grade_level_access}"
+    end
+
+    puts; puts "=== Homeroom teachers:"
+    Educator.all.select { |e| e.homeroom.present? }.each do |educator|
+      puts "#{educator.full_name} -- #{educator.homeroom.name} -- Grade #{educator.homeroom.grade}"
+    end
+
+    puts; puts "=== Without any authorizations:"
+    Educator.where(admin: false)
+            .where(schoolwide_access: false)
+            .select { |e| e.grade_level_access.empty? }
+            .select { |e| e.homeroom.blank? }
+            .each do |educator|
+      puts educator.full_name
+    end
+
+    return
   end
 
 end
