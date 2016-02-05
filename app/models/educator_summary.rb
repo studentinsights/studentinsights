7b5f73f3378b3524b111d64d1b343949a93e55a4
Alex Soble <asoble@gmail.com>
2016-02-05 11:49:13 -0500

    Add option to scope educator report by school

--- app/models/educator_summary.rb
+++ app/models/educator_summary.rb
@@ -1,16 +1,28 @@
 class EducatorSummary
 
+  def initialize(options = {})
+
+    if options[:school_scope].present?
+      school = School.find_by_local_id!(options[:school_scope])
+      @educators = school.educators
+    else
+      @educators = Educator
+      report
+    end
+
+  end
+
   def report
     puts; puts "=== EDUCATOR REPORT ==="
 
     puts; puts "=== Admins:"
-    puts Educator.where(admin: true).pluck(:full_name)
+    puts @educators.where(admin: true).pluck(:full_name)
 
     puts; puts "=== Non-admins with school-wide access:"
-    puts Educator.where(admin: false, schoolwide_access: true).pluck(:full_name)
+    puts @educators.where(admin: false, schoolwide_access: true).pluck(:full_name)
 
     puts; puts "=== Staff members with grade level-based access:"
-    Educator.where(admin: false)
+    @educators.where(admin: false)
             .where(schoolwide_access: false)
             .select { |e| e.homeroom.blank? }
             .select { |e| e.grade_level_access.size > 0 }
@@ -19,20 +31,20 @@ class EducatorSummary
     end
 
     puts; puts "=== Staff members limited to student profiles of ELLs:"
-    puts Educator.where(restricted_to_english_language_learners: true).pluck(:full_name)
+    puts @educators.where(restricted_to_english_language_learners: true).pluck(:full_name)
 
     puts; puts "=== Staff members limited to student profiles of SPED students:"
-    puts Educator.where(restricted_to_sped_students: true).pluck(:full_name)
+    puts @educators.where(restricted_to_sped_students: true).pluck(:full_name)
 
     puts; puts "=== Homeroom teachers:"
-    Educator.all.select { |e| e.homeroom.present? }
+    @educators.all.select { |e| e.homeroom.present? }
                 .sort_by { |e| e.homeroom.grade }
                 .each do |educator|
       puts "#{educator.full_name} -- #{educator.homeroom.name} -- Grade #{educator.homeroom.grade}"
     end
 
     puts; puts "=== Without any authorizations:"
-    Educator.where(admin: false)
+    @educators.where(admin: false)
             .where(schoolwide_access: false)
             .select { |e| e.grade_level_access.empty? }
             .select { |e| e.homeroom.blank? }
@@ -40,7 +52,7 @@ class EducatorSummary
       puts educator.full_name
     end
 
-    if Educator.where(local_id: nil).present?
+    if @educators.where(local_id: nil).present?
       puts; puts "=== Without local IDs:"
       puts
       puts "    These are hand-rolled educator records."
@@ -50,7 +62,7 @@ class EducatorSummary
       puts
       puts "    Give them a local ID when you have a moment!"
       puts "    Otherwise they have the potential to block X2 import down the line."
-      puts Educator.where(local_id: nil).pluck(:email)
+      puts @educators.where(local_id: nil).pluck(:email)
     end
 
     return
