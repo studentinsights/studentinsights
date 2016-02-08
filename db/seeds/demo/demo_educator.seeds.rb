83378816e8b8c53c5d24dc0ed803993e69de2bb5
Kevin Robinson <kevin.robinson.0@gmail.com>
2016-02-08 16:27:57 -0500

    Update demo task to respect newly added validation for local_id

--- db/seeds/demo/demo_educator.seeds.rb
+++ db/seeds/demo/demo_educator.seeds.rb
@@ -4,9 +4,11 @@ Educator.destroy_all
 Educator.create!([{
   email: "demo@example.com",
   password: "demo-password",
+  local_id: '350',
   admin: true
 }, {
   email: "fake-fifth-grade@example.com",
   password: "demo-password",
+  local_id: '450',
   admin: false
 }])
