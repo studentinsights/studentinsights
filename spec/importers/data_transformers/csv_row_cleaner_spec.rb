6ddc18f338416a30f955dc91f3b5190f8c3630de
Alex Soble <asoble@gmail.com>
2016-02-09 21:07:55 -0500

    Test #clean_date? method

--- spec/importers/data_transformers/csv_row_cleaner_spec.rb
+++ spec/importers/data_transformers/csv_row_cleaner_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 
 RSpec.describe CsvRowCleaner do
 
-  let(:transformed_row) { described_class.new(row).transform_row }
+  let(:row_cleaner) { described_class.new(row) }
+  let(:transformed_row) { row_cleaner.transform_row }
 
   describe '#transform_row' do
 
@@ -21,6 +22,32 @@ RSpec.describe CsvRowCleaner do
 
       it 'does nothing' do
         expect(transformed_row).to eq row
+      end
+    end
+  end
+
+  describe '#clean_date?' do
+
+    context 'Ruby can parse the date' do
+      let(:row) { CSV::Row.new([:event_date, :pizza_slices_count], ['2015-10-11', '3']) }
+      it 'returns true' do
+        expect(row_cleaner.clean_date?).to eq Date.new(2015, 10, 11)
+      end
+    end
+
+    context 'date is total garbage' do
+      let(:row) { CSV::Row.new([:event_date, :pizza_slices_count], ['** loud noise **', '3']) }
+
+      it 'returns false' do
+        expect(row_cleaner.clean_date?).to eq false
+      end
+    end
+
+    context 'date deeply illogical' do
+      let(:row) { CSV::Row.new([:event_date, :pizza_slices_count], ['2015-13-11', '3']) }
+
+      it 'returns false' do
+        expect(row_cleaner.clean_date?).to eq false
       end
     end
 
