003d33e36404590190a5d3dfe297f5f1f264adda
Alex Soble <asoble@gmail.com>
2016-02-09 21:31:23 -0500

    Prettify / simplify

--- app/importers/data_transformers/csv_transformer.rb
+++ app/importers/data_transformers/csv_transformer.rb
@@ -2,15 +2,15 @@ class CsvTransformer
   require 'csv'
 
   def transform(file)
-    csv = CSV.parse(file, headers: true, header_converters: :symbol, converters: lambda { |h| nil_converter(h) })
-    csv.delete_if do |row|
-      CsvRowCleaner.new(row).dirty_data?
-    end
-    csv.each do |row|
-      cleaner = CsvRowCleaner.new(row)
-      cleaner.transform_row
-    end
-    return csv
+    csv = CSV.parse(file, headers: true,
+                          header_converters: :symbol,
+                          converters: lambda { |h| nil_converter(h) })
+
+    csv.delete_if { |row| CsvRowCleaner.new(row).dirty_data? }
+
+    csv.each { |row| CsvRowCleaner.new(row).transform_row }
+
+    csv
   end
 
   def nil_converter(value)
