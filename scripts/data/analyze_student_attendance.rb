3600489a684db165a73a8aa633fbdb56750bccc4
Alex Soble <asoble@gmail.com>
2016-02-10 16:27:24 -0500

    Add data dictionary definitions to script

--- scripts/data/analyze_student_attendance.rb
+++ scripts/data/analyze_student_attendance.rb
@@ -59,3 +59,15 @@ puts 'COUNTS FOR INDICATOR == "0" (FALSE)'; puts
 indicators.map do |indicator|
   puts analysis.count_versus_total(indicator, '0')
 end
+
+
+# From the Aspen / X2 data dictionary:
+
+# ATT_ABSENT_IND => Absent?
+# ATT_ABSENT_IND_02 => Absent PM?
+# ATT_DISMISSED_IND => Dismissed?
+# ATT_DISMISSED_IND_02 => Dismissed PM?
+# ATT_EXCUSED_IND => Excused?
+# ATT_EXCUSED_IND_02 => Excused PM?
+# ATT_TARDY_IND => Tardy?
+# ATT_TARDY_IND_02 => Tardy PM?
