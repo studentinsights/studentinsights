083a96761af365c6d8497e1da9c9d62dc5d00d57
Kevin Robinson <kevin.robinson.0@gmail.com>
2016-02-11 10:47:53 -0500

    Profile v2 - Factoring out TakeNotes UI component, in prep for saving

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -48,6 +48,7 @@
 //= require_tree ./student
 //= require ./student_profile_v2/highcharts_wrapper
 //= require ./student_profile_v2/scales
+//= require ./student_profile_v2/take_notes
 //= require_tree ./student_profile_v2
 //= require_tree ./roster
 //= require_tree ./school_overview
