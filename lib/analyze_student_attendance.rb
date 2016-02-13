e1afb563c3993d4485ff531d03b7ceb63815778b
Alex Soble <asoble@gmail.com>
2016-02-13 12:00:08 -0500

    Tighten up AnalyzeStudentAttendance code

--- lib/analyze_student_attendance.rb
+++ lib/analyze_student_attendance.rb
@@ -9,39 +9,35 @@ class AnalyzeStudentAttendance < Struct.new(:path)
       replace: ''
     }
 
-    File.read(path).encode('UTF-8', 'binary', encoding_options)
-                   .gsub(/\\\\/, '')
-                   .gsub(/\\"/, '')
+    @file ||= File.read(path).encode('UTF-8', 'binary', encoding_options)
+                             .gsub(/\\\\/, '')
+                             .gsub(/\\"/, '')
   end
 
   def data
     csv_options = {
       headers: true,
       header_converters: :symbol,
-      converters: lambda { |h| nil_converter(h) }
+      converters: ->(h) { nil_converter(h) }
     }
 
-    @rows ||= begin
-      rows = []
-      CSV.parse(contents, csv_options) { |row| rows << row }
-      rows
-    end
+    @parsed_csv ||= CSV.parse(contents, csv_options)
   end
 
   def nil_converter(value)
-    value == '\N' ? nil : value
+    value unless value == '\N'
   end
 
-  def select_by_column_value(column_name, column_value)
-    data.select { |row| row[column_name] == column_value }
+  def select_by_column(column, value)
+    data.select { |row| row[column] == value }
   end
 
-  def count_for_column_value(column_name, column_value)
-    select_by_column_value(column_name, column_value).size
+  def count_for_column(column, value)
+    select_by_column(column, value).size
   end
 
-  def count_versus_total(column_name, column_value)
-    "#{column_name}  =>  #{count_for_column_value(column_name, column_value)} out of #{data.length}"
+  def count_versus_total(column, value)
+    "#{column} => #{count_for_column(column, value)} out of #{data.length}"
   end
 
 end
