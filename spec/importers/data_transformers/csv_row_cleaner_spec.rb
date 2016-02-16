b799a0a2fc72847539f16086c5a716f5c6393f2b
Alex Soble <asoble@gmail.com>
2016-02-15 20:45:37 -0700

    Handle attendance rows with nonsense dates
    + We don't want dates from the future or from the far past
    + Dirty date discovery via https://github.com/codeforamerica/somerville-teacher-tool/issues/96#issue-79227099
    + #4

--- spec/importers/data_transformers/csv_row_cleaner_spec.rb
+++ spec/importers/data_transformers/csv_row_cleaner_spec.rb
@@ -30,10 +30,33 @@ RSpec.describe CsvRowCleaner do
 
     context 'there is a date column' do
       context 'Ruby can parse the date' do
-        let(:row) { CSV::Row.new([:event_date, :pizza_slices_count], ['2015-10-11', '3']) }
-        it 'returns true' do
-          expect(row_cleaner.clean_date?).to eq Date.new(2015, 10, 11)
+
+        context 'date is not in the future or the far past' do
+          let(:row) { CSV::Row.new([:event_date, :pizza_slices_count], ['2015-10-11', '3']) }
+          it 'returns true' do
+            expect(row_cleaner.clean_date?).to eq true
+          end
         end
+
+        context 'date is in the future' do
+          let(:row) {
+            CSV::Row.new([:event_date, :pizza_slices_count], ['2215-10-11', '3'])
+          }
+          it 'returns true' do
+            expect(row_cleaner.clean_date?).to eq false
+          end
+
+        end
+
+        context 'date is in the far past' do
+          let(:row) {
+            CSV::Row.new([:event_date, :pizza_slices_count], ['1015-10-11', '3'])
+          }
+          it 'returns true' do
+            expect(row_cleaner.clean_date?).to eq false
+          end
+        end
+
       end
 
       context 'date is total garbage' do
