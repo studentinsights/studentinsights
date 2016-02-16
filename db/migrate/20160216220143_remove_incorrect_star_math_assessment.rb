2baf2161bdfd6d69585de4944ba792c4b4efe4e8
Alex Soble <asoble@gmail.com>
2016-02-16 15:02:35 -0700

    Add migration for destroying incorrectly named STAR assessment #75

---
+++ db/migrate/20160216220143_remove_incorrect_star_math_assessment.rb
@@ -0,0 +1,14 @@
+class RemoveIncorrectStarMathAssessment < ActiveRecord::Migration
+  def change
+    # "STAR Math" => incorrect
+    # "STAR Mathematics" => correct
+    begin
+      incorrect = Assessment.find_by_family_and_subject("STAR", "Math")
+      unless incorrect.nil?
+        incorrect.destroy!
+        Student.update_recent_student_assessments
+      end
+    rescue ActiveRecord::RecordNotFound
+    end
+  end
+end
