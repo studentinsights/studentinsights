0d63c09383a675f8291cf2bf19357c17bef3eea4
Alex Soble <asoble@gmail.com>
2016-02-16 11:36:45 -0700

    Destroy corresponding student assessments when we destroy an assessment

--- app/models/assessment.rb
+++ app/models/assessment.rb
@@ -1,5 +1,5 @@
 class Assessment < ActiveRecord::Base
-  has_many :student_assessments
+  has_many :student_assessments, dependent: :destroy
   has_many :students, through: :student_assessments
 
   def self.seed_somerville_assessments
