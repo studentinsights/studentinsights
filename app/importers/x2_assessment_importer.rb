df2fe84b23376c28bf438eb8f87f5bcbdfc8c3ae
Alex Soble <asoble@gmail.com>
2016-02-16 17:12:32 -0700

    Give more accurate names to classes that import X2 assessments

--- app/importers/x2_assessment_importer.rb
+++ app/importers/x2_assessment_importer.rb
@@ -1,4 +1,6 @@
-class StudentAssessmentImporter
+class X2AssessmentImporter
+  # Aspen X2 is the name of Somerville's Student Information System.
+
   WHITELIST = Regexp.union(/ACCESS/, /WIDA-ACCESS/, /DIBELS/, /MCAS/, /MAP/, /MELA-O/, /MEPA/).freeze
 
   def remote_file_name
@@ -17,7 +19,7 @@ class StudentAssessmentImporter
 
   def import_row(row)
     return unless row[:assessment_test].match(WHITELIST)
-    StudentAssessmentRow.build(row).save!
+    X2AssessmentRow.build(row).save!
   end
 
 end
