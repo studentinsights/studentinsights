75a2346236d0ce5f3694b6b3990f79a7eb739dbf
Alex <asoble@gmail.com>
2016-02-19 23:15:18 -0500

    Refactor BehaviorImporter spec
    + Old spec was very tightly integrated with other classes like Importer and CsvTransformer
    + Made it hard to change those classes, because side effects spilled into BehaviorImporter spec
    + Boiled spec down into more of a real unit test

--- spec/importers/behavior_importer_spec.rb
+++ spec/importers/behavior_importer_spec.rb
@@ -3,68 +3,101 @@ require 'rails_helper'
 
 RSpec.describe BehaviorImporter do
 
-  let(:importer) {
-    Importer.new(current_file_importer: described_class.new)
-  }
-
-  before { FactoryGirl.create(:student, local_id: '11') }
-  before { FactoryGirl.create(:student, local_id: '12') }
-  before { FactoryGirl.create(:student, local_id: '13') }
-  before { FactoryGirl.create(:student, local_id: '14') }
-
-  let(:import) { importer.start_import(csv) }
-
   describe '#import_row' do
-    context 'realistic data ("good" case), not great data' do
-      let(:file) { File.open("#{Rails.root}/spec/fixtures/fake_behavior_export.txt", "r:CP1252") }  # Windows 1252
-      let(:transformer) { CsvTransformer.new }
-      let(:csv) { transformer.transform(file) }
-      let!(:student_we_want_to_update) { FactoryGirl.create(:student_we_want_to_update) }
+    let(:importer) { described_class.new }
+    before { importer.import_row(row) }
+    let(:incidents) { student.reload.most_recent_school_year.discipline_incidents }
+    let(:incident) { incidents.last }
 
-      before do |example|
-        import unless example.metadata[:skip_before]
-      end
+    context 'typical row' do
+      let(:student) { FactoryGirl.create(:student, local_id: '10') }
+      let(:row) {
+        {
+          local_id: student.local_id,
+          incident_code: "Hitting",
+          event_date: Date.new(2015, 10, 1),
+          incident_time: "13:00:00",
+          incident_location: "Classroom",
+          incident_description: "Hit another student.",
+          school_local_id: "SHS"
+        }
+      }
 
-      it 'does not raise errors about byte sequence, length, missing date/time values' do
-        expect { import }.not_to raise_error
+      it 'creates discipline incident for the correct student' do
+        expect(incidents.size).to eq 1
       end
-      it 'creates new discipline incidents', skip_before: true do
-        expect { import }.to change(DisciplineIncident, :count).by 4
+      it 'assigns the incident code correctly' do
+        expect(incident.incident_code).to eq 'Hitting'
       end
-
-      context 'student we want to update' do
-        it 'creates discipline incident for the correct student' do
-          expect(student_we_want_to_update.reload.most_recent_school_year.discipline_incidents.size).to eq 1
-        end
-        it 'assigns the incident code correctly' do
-          incident = student_we_want_to_update.reload.most_recent_school_year.discipline_incidents.last
-          expect(incident.incident_code).to eq 'Hitting'
-        end
+      it 'sets has exact time to true' do
+        expect(incident.has_exact_time).to eq true
       end
-      context 'has exact time' do
-        let(:incident_with_time_data) { Student.find_by_local_id("10").reload.most_recent_school_year.discipline_incidents.last }
-        it 'sets has exact time to true' do
-          expect(incident_with_time_data.has_exact_time).to eq true
-        end
-        it 'assigns the date and time correctly' do
-          expect(incident_with_time_data.occurred_at).to eq Time.utc(2015, 10, 1, 13, 00)
-        end
-      end
-      context 'time missing' do
-        let(:incident_without_time_data) { Student.find_by_local_id("13").most_recent_school_year.discipline_incidents.last }
-        it 'sets has exact time to false' do
-          expect(incident_without_time_data.has_exact_time).to eq false
-        end
-        it 'assigns the date without a time' do
-          expect(incident_without_time_data.occurred_at).to eq Time.utc(2015, 10, 3)
-        end
-      end
-      context 'description text has non UTF-8 byte sequence' do
-        let(:incident_with_non_utf8_description) { Student.find_by_local_id("12").most_recent_school_year.discipline_incidents.last }
-        it 'fights back' do
-          expect(incident_with_non_utf8_description.reload.incident_description).to eq("pencil that didn’t need to be")
-        end
+      it 'assigns the date and time correctly' do
+        expect(incident.occurred_at).to eq Time.utc(2015, 10, 1, 13, 00)
       end
     end
+
+    context 'very long incident description' do
+      let!(:student) { FactoryGirl.create(:student, local_id: '11') }
+      let(:big_block_of_text) { "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." }
+      let(:row) {
+        {
+          local_id: student.local_id,
+          incident_code: "Lorem ipsuming",
+          event_date: Date.new(2015, 10, 1),
+          incident_time: "13:00:00",
+          incident_location: "Classroom",
+          incident_description: big_block_of_text,
+          school_local_id: "SHS"
+        }
+      }
+
+      it 'assigns the description correctly' do
+        expect(incident.incident_description).to eq big_block_of_text
+      end
+
+    end
+
+    context 'time missing' do
+      let!(:student) { FactoryGirl.create(:student, local_id: '13') }
+      let(:row) {
+        {
+          local_id: student.local_id,
+          incident_code: "Bullying",
+          event_date: Date.new(2015, 10, 3),
+          incident_time: nil,
+          incident_location: "Classroom",
+          incident_description: "Bullied another student.",
+          school_local_id: "SHS"
+        }
+      }
+
+      it 'sets has exact time to false' do
+        expect(incident.has_exact_time).to eq false
+      end
+      it 'assigns the date without a time' do
+        expect(incident.occurred_at).to eq Time.utc(2015, 10, 3)
+      end
+    end
+
+    context 'description text has non UTF-8 byte sequence' do
+      let!(:student) { FactoryGirl.create(:student, local_id: '12') }
+      let(:row) {
+        {
+          local_id: student.local_id,
+          incident_code: "Unauthorized pencil sharpening",
+          event_date: Date.new(2015, 10, 2),
+          incident_time: "13:00:00",
+          incident_location: "Classroom",
+          incident_description: "pencil that didn’t need to be",
+          school_local_id: "SHS"
+        }
+      }
+
+      it 'fights back' do
+        expect(incident.reload.incident_description).to eq("pencil that didn’t need to be")
+      end
+    end
+
   end
 end
