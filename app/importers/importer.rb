4c639d8405c0913df973dcb60a3cbb03b7e9edac
Alex <asoble@gmail.com>
2016-02-20 01:05:12 -0500

    Remove unused method

--- app/importers/importer.rb
+++ app/importers/importer.rb
@@ -46,35 +46,4 @@ class Importer
       end
     end
   end
-
-
-  # FOR DEVELOPMENT ONLY (useful when you don't want to redownload files every time):
-
-  # def connect_transform_import_locally
-  #   file_importers.each do |file_importer|
-
-  #     path = @client.file_tmp_path(file_importer.remote_file_name)
-  #     unless File.exist? path
-  #       @client.download_file_to_tmp(file_importer.remote_file_name)
-  #     end
-
-  #     file_as_string = File.read(path).encode('UTF-8',
-  #                                             'binary',
-  #                                             invalid: :replace,
-  #                                             undef: :replace,
-  #                                             replace: '')
-
-  #     data = file_importer.data_transformer.transform(file_as_string)
-
-  #     progress_bar = ProgressBar.new(data.size, file_importer.remote_file_name)
-
-  #     data.each.each_with_index do |row, index|
-  #       row.delete_if { |key, value| key.blank? }
-  #       file_importer.import_row(row) if filter.include?(row)
-  #       @log.print(progress_bar.current_status(index))
-  #     end
-
-  #   end
-  # end
-
 end
