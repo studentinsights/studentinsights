fb625d7e587f9cff6798ada8650b8f88b94f9ac0
Brandon Wieber <brandon.m.wieber@gmail.com>
2016-03-01 19:55:51 -0500

    Added keys to services

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,8 +1,9 @@
 class Service < ActiveRecord::Base
   belongs_to :student
-  belongs_to :educator
+  belongs_to :provided_by_educator, class_name: 'Educator'
+  belongs_to :recorded_by_educator, class_name: 'Educator'
   belongs_to :service_type
 
-  validates_presence_of :educator_id, :student_id, :service_type_id, :recorded_at, :date_started
+  validates_presence_of :recorded_by_educator_id, :provided_by_educator_id, :student_id, :service_type_id, :recorded_at, :date_started
 
 end
--- db/migrate/20160228205806_create_services.rb
+++ db/migrate/20160228205806_create_services.rb
@@ -2,9 +2,9 @@ class CreateServices < ActiveRecord::Migration
   def change
     create_table :services do |t|
       t.integer :student_id
-      t.integer :educator_id
+      t.integer :provided_by_educator_id
+      t.integer :recorded_by_educator_id
       t.integer :service_type_id
-      t.text :text
       t.datetime :recorded_at
       t.datetime :date_started
 
