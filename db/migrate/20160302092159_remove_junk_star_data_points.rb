2ca5928ffa02be783a2b0eafa7917046dd9699ad
Alex <asoble@gmail.com>
2016-03-02 14:58:07 +0100

    Add guard to migration

--- db/migrate/20160302092159_remove_junk_star_data_points.rb
+++ db/migrate/20160302092159_remove_junk_star_data_points.rb
@@ -1,5 +1,6 @@
 class RemoveJunkStarDataPoints < ActiveRecord::Migration
   def change
-    Assessment.find_by_family('STAR').student_assessments.where(percentile_rank: nil).destroy_all
+    star = Assessment.find_by_family('STAR')
+    star.student_assessments.where(percentile_rank: nil).destroy_all unless star.nil?
   end
 end
