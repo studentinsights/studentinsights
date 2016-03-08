26e9c8dc59c33996096c191633d3e26e021a8c1a
= <really.eli@gmail.com>
2016-03-08 17:06:59 -0500

    Color-code newsfeed items by type.

--- app/assets/javascripts/student_profile_v2/profile_details.js
+++ app/assets/javascripts/student_profile_v2/profile_details.js
@@ -20,56 +20,69 @@
 
       _.each(this.props.attendanceData.tardies, function(obj){
         events.push({
-          message: name + ' was tardy.', date: Date.parse(obj.occurred_at)
+          message: name + ' was tardy.', date: Date.parse(obj.occurred_at),
+          color: '#e8fce8'
         });
       });
       _.each(this.props.attendanceData.absences, function(obj){
         events.push({
-          message: name + ' was absent.', date: Date.parse(obj.occurred_at)
+          message: name + ' was absent.', date: Date.parse(obj.occurred_at),
+          color: '#e8fce8'
+        });
+      });
+      _.each(this.props.attendanceData.discipline_incidents, function(obj){
+        events.push({
+          message: obj.incident_description + ' in the ' + obj.incident_location,
+          date: Date.parse(obj.occurred_at),
+          color: '#e8fce8'
         });
       });
       _.each(this.props.chartData.mcas_series_ela_scaled, function(quad){
         var year = quad[0], month = quad[1], day = quad[2], score = quad[3];
         events.push({
           message: name + ' scored a ' + score + ' on the ELA section of the MCAS.',
-          date: new Date(year, month, day)
+          date: new Date(year, month, day),
+          color: '#ffe7d6'
         });
       });
       _.each(this.props.chartData.mcas_series_math_scaled, function(quad){
         var year = quad[0], month = quad[1], day = quad[2], score = quad[3];
         events.push({
           message: name + ' scored a ' + score + ' on the Math section of the MCAS.',
-          date: new Date(year, month, day)
+          date: new Date(year, month, day),
+          color: '#e8e9fc'
         });
       });
       _.each(this.props.chartData.star_series_reading_percentile, function(quad){
         var year = quad[0], month = quad[1], day = quad[2], score = quad[3];
         events.push({
-          message: name + ' scored a ' + score + '% on the Reading section of STAR.',
-          date: new Date(year, month, day)
+          message: name + ' scored in the ' + score + 'th percentile on the Reading section of STAR.',
+          date: new Date(year, month, day),
+          color: '#ffe7d6'
         });
       });
       _.each(this.props.chartData.star_series_math_percentile, function(quad){
         var year = quad[0], month = quad[1], day = quad[2], score = quad[3];
         events.push({
-          message: name + ' scored a ' + score + '% on the Math section of STAR.',
-          date: new Date(year, month, day)
+          message: name + ' scored in the ' + score + 'th percentile on the Math section of STAR.',
+          date: new Date(year, month, day),
+          color: '#e8e9fc'
         });
       });
-      // TODO: incorporate discipline incidents.
       return _.sortBy(events, 'date').reverse();
     },
 
     render: function() {
       return dom.div({},
-        dom.ul({},
-          this.getEvents().map(function(obj){
-            var key = [obj.date, obj.message].join();
-            return dom.li({ key: key },
-              moment(obj.date).format("MMMM Do, YYYY") + ": " + obj.message
-            );
-          })
-        )
+        this.getEvents().map(function(obj){
+          var key = [obj.date, obj.message].join();
+          return dom.div({ key: key, style: {backgroundColor: obj.color, marginBottom: '10px', borderRadius: '10px'}},
+            dom.div({ style: {padding: '10px'}},
+              dom.p({}, moment(obj.date).format("MMMM Do, YYYY") + ":"),
+              obj.message
+            )
+          );
+        })
       );
     }
   });
