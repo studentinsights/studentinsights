93831472ffecd07d889012beb10cac396b3a83cf
Kevin Robinson <kevin.robinson.0@gmail.com>
2016-03-09 07:22:51 -0500

    Fix McasRow importer spec to explicitly test side effects of creating Assessment type records'

--- spec/importers/mcas_row_spec.rb
+++ spec/importers/mcas_row_spec.rb
@@ -2,51 +2,71 @@ require 'rails_helper'
 
 RSpec.describe McasRow do
 
-  let(:student) { FactoryGirl.create(:student) }
-  before { McasRow.build(row).save! }
-  let(:assessment) { Assessment.last }
+  context 'with no Assessment records in the database' do
+    before { Assessment.destroy_all }
+    let(:student) { FactoryGirl.create(:student) }
+    after { Assessment.seed_somerville_assessments }
 
-  context 'MCAS Mathematics' do
-    let(:row) {
-      {
-        assessment_test: 'MCAS',
-        assessment_subject: 'Mathematics',
-        assessment_name: 'MCAS 2016 Mathematics',
-        local_id: student.local_id,
-        assessment_date: Date.today
+    context 'MCAS Mathematics' do
+      let(:row) {
+        {
+          assessment_test: 'MCAS',
+          assessment_subject: 'Mathematics',
+          assessment_name: 'MCAS 2016 Mathematics',
+          local_id: student.local_id,
+          assessment_date: Date.today
+        }
       }
-    }
 
-    it 'creates an assessment with family MCAS and subject Mathematics' do
-      expect(assessment.family).to eq 'MCAS'
-      expect(assessment.subject).to eq 'Mathematics'
+      context 'when the Assessment family already exists' do
+        let(:assessment) { Assessment.where(family: 'MCAS', subject: 'Mathematics').first }
+        before { McasRow.build(row).save! }
+        
+        it 'creates a student assessment record' do
+          expect(StudentAssessment.count).to eq 1
+        end
+
+        it 'does not create another Assessment record' do
+          expect(Assessment.count).to eq 1
+        end
+      end
+
+      context 'when the Assessment family does not yet exist' do
+        before { McasRow.build(row).save! }
+        
+        it 'creates a student assessment record' do
+          expect(StudentAssessment.count).to eq 1
+        end
+
+        it 'also creates an Assessment with family MCAS and subject Mathematics as a side effect' do
+          expect(Assessment.count).to eq 1
+          expect(Assessment.last.family).to eq 'MCAS'
+          expect(Assessment.last.subject).to eq 'Mathematics'
+        end
+      end
     end
 
-    it 'creates a student assessment result' do
-      expect(StudentAssessment.count).to eq 1
-    end
-
-  end
-
-  context 'MCAS English Language Arts' do
-    let(:row) {
-      {
-        assessment_test: 'MCAS',
-        assessment_subject: 'Arts',
-        assessment_name: 'MCAS 2016 English Language Arts',
-        local_id: student.local_id,
-        assessment_date: Date.today
+    context 'MCAS English Language Arts, when the ELA Assessment does not yet exist' do
+      let(:row) {
+        {
+          assessment_test: 'MCAS',
+          assessment_subject: 'Arts',
+          assessment_name: 'MCAS 2016 English Language Arts',
+          local_id: student.local_id,
+          assessment_date: Date.today
+        }
       }
-    }
+      before { McasRow.build(row).save! }
 
-    it 'creates an assessment' do
-      expect(Assessment.count).to eq 1
-      expect(StudentAssessment.count).to eq 1
-    end
+      it 'creates a student assessment record' do
+        expect(StudentAssessment.count).to eq 1
+      end
 
-    it 'sets the correct subject' do
-      expect(Assessment.last.subject).to eq 'ELA'
+      it 'creates a new Assessment as a side-effect' do
+        expect(Assessment.count).to eq 1
+        expect(Assessment.last.subject).to eq 'ELA'
+        
+      end
     end
   end
-
 end
