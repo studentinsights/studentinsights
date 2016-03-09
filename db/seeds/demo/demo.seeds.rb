dc04e95430f9f0401a0b8bd362b633a3d1299500
Kevin Robinson <kevin.robinson.0@gmail.com>
2016-03-09 06:38:29 -0500

    Fix bug with seeding test db, factor out constant seeding into DatabaseConstants

--- db/seeds/demo/demo.seeds.rb
+++ db/seeds/demo/demo.seeds.rb
@@ -1,15 +1,18 @@
+require "#{Rails.root}/db/seeds/database_constants"
+
 puts "Creating demo schools, homerooms, interventions..."
 raise "empty yer db" if School.count > 0 ||
                         Student.count > 0 ||
                         InterventionType.count > 0 ||
                         Assessment.count > 0
 
-puts 'Seeding constants...'
-Assessment.seed_somerville_assessments
-InterventionType.seed_somerville_intervention_types
-EventNoteType.seed_somerville_event_note_types
-
 healey = School.create(name: "Arthur D Healey")
+
+# The local demo data setup uses the Somerville database constants
+# (eg., the set of `ServiceType`s) for generating local demo data and
+# for tests.
+puts 'Seeding database constants for Somerville...'
+DatabaseConstants.new.seed!
 
 puts "Creating demo educators..."
 Educator.destroy_all
