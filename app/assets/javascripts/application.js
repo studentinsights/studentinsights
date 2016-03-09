33791b3d44f712841409db2e1567fc9578b5df36
Kevin Robinson <kevin.robinson.0@gmail.com>
2016-03-09 08:25:39 -0500

    Factor out SlicePanel components and styles, revise hover styling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -42,10 +42,14 @@
 //= require session_timeout_warning
 //= require student_searchbar
 //= require_tree ./helpers
+
+// shared react compoents:
+//= require ./components/fixed_table
+//= require ./components/collapsable_table
 //= require_tree ./components
 
 // student profile page v2:
-// components:
+// pure ui components:
   //= require ./student_profile_v2/service_color
   //= require ./student/profile_charts/profile_chart_settings
   //= require ./student_profile_v2/feed_helpers
---
+++ app/assets/javascripts/components/collapsable_table.js
@@ -0,0 +1,64 @@
+(function() {
+
+  window.shared || (window.shared = {});
+  var dom = window.shared.ReactHelpers.dom;
+  var createEl = window.shared.ReactHelpers.createEl;
+  var merge = window.shared.ReactHelpers.merge;
+
+  var FixedTable = window.shared.FixedTable;
+  var styles = window.shared.styles;
+
+  // Table for SlicePanels that shows a set of filters for a particular
+  // dimension, and supports collapsing the list when there are no items
+  // that fix particular values.
+  var CollapsableTable = window.shared.CollapsableTable = React.createClass({
+    displayName: 'CollapsableTable',
+    getDefaultProps: function() {
+      return {
+        minHeight: 132,
+        limit: 5,
+        className: ''
+      };
+    },
+
+    getInitialState: function() {
+      return {
+        isExpanded: false
+      };
+    },
+
+    onCollapseClicked: function(e) {
+      this.setState({ isExpanded: false });
+    },
+
+    onExpandClicked: function(e) {
+      this.setState({ isExpanded: true });
+    },
+
+    render: function() {
+      var truncatedItems = (this.state.isExpanded)
+        ? this.props.items
+        : this.props.items.slice(0, this.props.limit);
+      return dom.div({ className: 'CollapsableTable' },
+        createEl(FixedTable, merge(this.props, {
+          items: truncatedItems,
+          children: this.renderCollapseOrExpand()
+        }))
+      );
+    },
+
+    renderCollapseOrExpand: function() {
+      if (this.props.items.length <= this.props.limit) return;
+      return dom.a({
+        style: {
+          fontSize: styles.fontSize,
+          color: '#999',
+          paddingTop: 5,
+          display: 'block'
+        },
+        onClick: (this.state.isExpanded) ? this.onCollapseClicked : this.onExpandClicked
+      }, (this.state.isExpanded) ? '- Hide details' : '+ Show all');
+
+    }
+  });
+})();
