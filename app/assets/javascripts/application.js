858c516dd3e1b280b09889ad9770d4f0ac20d1b1
Kevin Robinson <kevin.robinson.0@gmail.com>
2016-03-16 09:15:10 -0400

    Revert "Risk Bubble"

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -50,7 +50,6 @@
 
 // student profile page v2:
 // pure ui components:
-  //= require ./student_profile_v2/risk_bubble
   //= require ./student_profile_v2/service_color
   //= require ./student/profile_charts/profile_chart_settings
   //= require ./student_profile_v2/feed_helpers
