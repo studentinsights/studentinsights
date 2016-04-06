4bddcf6082b4fb938e506fd1d15b6762e11bd2b9
Kevin Robinson <kevin.robinson.0@gmail.com>
2016-04-05 21:47:22 -0400

    Remove spec_helper line

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,3 @@
-ActiveRecord::Migration.maintain_test_schema!
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
