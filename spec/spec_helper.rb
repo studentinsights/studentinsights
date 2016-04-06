06f2640583b6f1e39f6547bd1d04cb0de58a697c
Kevin Robinson <kevin.robinson.0@gmail.com>
2016-04-05 21:34:35 -0400

    Force test migration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,4 @@
+ActiveRecord::Migration.maintain_test_schema!
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
