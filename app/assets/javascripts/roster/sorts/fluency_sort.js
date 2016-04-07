f430cdc13db52d12808735dae59049c6c5980033
Ronny Almog <ronny2205@gmail.com>
2016-04-07 22:03:02 +0000

    refactoring the sort methods

--- app/assets/javascripts/roster/sorts/fluency_sort.js
+++ app/assets/javascripts/roster/sorts/fluency_sort.js
@@ -8,15 +8,15 @@
   var fluency_levels = ["FLEP-Transitioning", "FLEP", "Fluent"];
 
   compareFluencyLevel = function(a, b) {
-    var clean_a = clean_cell(a);
-    var clean_b = clean_cell(b);
+    var clean_a = $.trim(a);
+    var clean_b = $.trim(b);
     var a_index = fluency_levels.indexOf(clean_a);
     var b_index = fluency_levels.indexOf(clean_b);
     return a_index - b_index;
   };
 
   Tablesort.extend('fluency_sort', function(item) {
-    cleaned_cell = clean_cell(item);
+    cleaned_cell = $.trim(item);
     fluency_levels.indexOf(cleaned_cell) > 0;
   }, function(a, b) {
       return compareFluencyLevel(b, a);
--- app/assets/javascripts/roster/sorts/level_of_need_sort.js
+++ app/assets/javascripts/roster/sorts/level_of_need_sort.js
@@ -1,22 +1,18 @@
 // Extend TableSort with levels of need
 (function(){
 
-  function clean_cell(i) {
-    return $.trim(i);
-  }
-
   var need_levels = ["High", "Moderate", "Low >= 2", "Low < 2", "—"];
 
   compareNeedLevel = function(a, b) {
-    var clean_a = clean_cell(a);
-    var clean_b = clean_cell(b);
+    var clean_a = $.trim(a);
+    var clean_b = $.trim(b);
     var a_index = need_levels.indexOf(clean_a);
     var b_index = need_levels.indexOf(clean_b);
     return a_index - b_index;
   };
 
   Tablesort.extend('need_sort', function(item) {
-    cleaned_cell = clean_cell(item);
+    cleaned_cell = $.trim(item);
     need_levels.indexOf(cleaned_cell) > 0;
   }, function(a, b) {
       return compareNeedLevel(b, a);
