08b6eb2bc3e5fca61c0011b7ae59c01ee0afc9f7
Ronny Almog <ronny2205@gmail.com>
2016-04-07 22:03:02 +0000

    refactoring the sort methods

--- app/assets/javascripts/roster/sorts/fluency_sort.js
+++ app/assets/javascripts/roster/sorts/fluency_sort.js
@@ -1,23 +1,20 @@
 // Extend TableSort with Fluency levels
 (function(){
 
-  function clean_cell(i) {
-    return $.trim(i);
-  }
-
   var fluency_levels = ["FLEP-Transitioning", "FLEP", "Fluent"];
 
   compareFluencyLevel = function(a, b) {
-    var clean_a = clean_cell(a);
-    var clean_b = clean_cell(b);
+    var clean_a = $.trim(a);
+    var clean_b = $.trim(b);
     var a_index = fluency_levels.indexOf(clean_a);
     var b_index = fluency_levels.indexOf(clean_b);
     return a_index - b_index;
   };
 
   Tablesort.extend('fluency_sort', function(item) {
-    cleaned_cell = clean_cell(item);
-    fluency_levels.indexOf(cleaned_cell) > 0;
+    return (
+      fluency_levels.indexOf($.trim(item)) > 0
+    );
   }, function(a, b) {
       return compareFluencyLevel(b, a);
   });
--- app/assets/javascripts/roster/sorts/prog_assign_sort.js
+++ app/assets/javascripts/roster/sorts/prog_assign_sort.js
@@ -1,23 +1,20 @@
 // Extend TableSort with program assigned levels
 (function(){
 
-  function clean_cell(i) {
-    return $.trim(i);
-  }
-
   var program_levels = ["Reg Ed", "2Way English", "2Way Spanish", "Sp Ed"];
 
   compareProgramLevel = function(a, b) {
-    var clean_a = clean_cell(a);
-    var clean_b = clean_cell(b);
+    var clean_a = $.trim(a);
+    var clean_b = $.trim(b);
     var a_index = program_levels.indexOf(clean_a);
     var b_index = program_levels.indexOf(clean_b);
     return a_index - b_index;
   };
 
   Tablesort.extend('prog_assign_sort', function(item) {
-    cleaned_cell = clean_cell(item);
-    program_levels.indexOf(cleaned_cell) > 0;
+    return (
+      program_levels.indexOf($.trim(item)) > 0 
+    );
   }, function(a, b) {
       return compareProgramLevel(b, a);
   });
