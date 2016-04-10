5a3d54e0b4123cb5143c8aee10d7f21c56819292
Alex <asoble@gmail.com>
2016-04-10 16:41:36 -0400

    Break FileImport logic into smaller methods

--- app/importers/file_importers/file_import.rb
+++ app/importers/file_importers/file_import.rb
@@ -4,18 +4,32 @@ class FileImport < Struct.new :file_importer
     :import_row, :filter, :progress_bar, to: :file_importer
 
   def import
+    log_start_of_import
+    fetch_data
+    import_data
+  end
+
+  private
+
+  def file
+    client.read_file(remote_file_name)
+  end
+
+  def log_start_of_import
     log.write("\nImporting #{remote_file_name}...")
+  end
 
-    file = client.read_file(remote_file_name)
+  def fetch_data
     transformer = data_transformer
-    data = transformer.transform(file)
+    @data = transformer.transform(file)
     pre_cleanup_csv_size = transformer.pre_cleanup_csv_size
+    CleanupReport.new(log, remote_file_name, pre_cleanup_csv_size, @data.size).print
+  end
 
-    CleanupReport.new(log, remote_file_name, pre_cleanup_csv_size, data.size).print
-
-    data.each.each_with_index do |row, index|
+  def import_data
+    @data.each.each_with_index do |row, index|
       import_row(row) if filter.include?(row)
-      ProgressBar.new(log, remote_file_name, data.size, index + 1).print if progress_bar
+      ProgressBar.new(log, remote_file_name, @data.size, index + 1).print if progress_bar
     end
   end
 
