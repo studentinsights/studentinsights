99209727d47cee5fd2d8d2972c6178fe43ec70cd
Kevin Weaver <kevinbweaver@gmail.com>
2016-04-10 21:48:22 -0400

    add EventNote factory

---
+++ spec/factories/event_notes.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+
+	factory :event_note do
+		recorded_at DateTime.new(2016, 4, 9)
+		association :educator
+		association :student
+		event_note_type EventNoteType.first
+	end
+end
