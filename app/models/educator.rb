25fe641bfaa58b2283ef3c76dfb0f651bff60222
Alex <asoble@gmail.com>
2016-04-16 09:28:39 -0400

    Remove association from educators

--- app/models/educator.rb
+++ app/models/educator.rb
@@ -12,7 +12,6 @@ class Educator < ActiveRecord::Base
   has_many    :students, through: :homeroom
   has_many    :interventions
   has_many    :progress_notes, through: :interventions
-  has_many    :student_notes
 
   validates :email, presence: true, uniqueness: true
   validates :local_id, presence: true, uniqueness: true
