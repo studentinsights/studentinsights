cfd38a1d570d32731a3f52b285830fa9749d500d
Alex Soble <asoble@gmail.com>
2016-04-24 13:46:32 -0400

    Add additional disability sort categories

--- app/assets/javascripts/sorts/disability_sort.js
+++ app/assets/javascripts/sorts/disability_sort.js
@@ -4,7 +4,7 @@
       item.search(/(High|Moderate|Low < 2)/i) !== -1
       );
     }, function(a,b) {
-    var disability = ['Low < 2','Moderate','High'];
+    var disability = ['None', 'Low < 2', 'Low >= 2', 'Moderate', 'High'];
     return disability.indexOf(b) - disability.indexOf(a);
   });
 }());
