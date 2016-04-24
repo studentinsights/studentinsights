8a119601afdc7eb083bdfb48715053e158ba23c7
Alex Soble <asoble@gmail.com>
2016-04-24 13:51:48 -0400

    Minor style tweaks to disability_sort

--- app/assets/javascripts/sorts/disability_sort.js
+++ app/assets/javascripts/sorts/disability_sort.js
@@ -1,9 +1,7 @@
  (function(){
   Tablesort.extend('disability', function(item) {
-     return (
-      item.search(/(High|Moderate|Low < 2)/i) !== -1
-      );
-    }, function(a,b) {
+    return item.search(/(High|Moderate|Low < 2|Low >= 2|None)/i) !== -1;
+  }, function(a,b) {
     var disability = ['None', 'Low < 2', 'Low >= 2', 'Moderate', 'High'];
     return disability.indexOf(b) - disability.indexOf(a);
   });
