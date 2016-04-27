8eb828b81dfef1de3fdea3d895d2408207cec469
= <really.eli@gmail.com>
2016-04-27 14:32:05 -0400

    Skeleton of spec file for ProfileChart.

---
+++ spec/javascripts/student_profile/profile_chart_spec.js
@@ -0,0 +1,8 @@
+describe('ProfileBarCharts', function() {
+  var ProfileBarChart = window.shared.ProfileBarChart;
+
+  describe('#lastNMonthNamesFrom', function(){
+    it('works when this test was written', function(){
+    });
+  });
+});
