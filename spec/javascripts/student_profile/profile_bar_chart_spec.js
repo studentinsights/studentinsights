a09a5cc4e673f6b64c63f0fab621b5398847879c
= <really.eli@gmail.com>
2016-04-29 13:28:43 -0400

    Added a simple integration test to verify that the new id prop works as intended.

--- spec/javascripts/student_profile/profile_bar_chart_spec.js
+++ spec/javascripts/student_profile/profile_bar_chart_spec.js
@@ -1,5 +1,30 @@
 describe('ProfileBarCharts', function() {
   var ProfileBarChart = window.shared.ProfileBarChart;
+  var createEl = window.shared.ReactHelpers.createEl;
+  var merge = window.shared.ReactHelpers.merge;
+  var SpecSugar = window.shared.SpecSugar;
+
+  var helpers = {
+    renderInto: function(el, props) {
+      var mergedProps = merge({
+        events: [],
+        id: "",
+        titleText: "",
+        monthsBack: 0
+      }, props || {});
+      return ReactDOM.render(createEl(ProfileBarChart, mergedProps), el);
+    }
+  };
+
+  SpecSugar.withTestEl('integration tests', function() {
+    it('is wrapped in a div with the given id', function() {
+      var el = this.testEl;
+      helpers.renderInto(el, {id: 'foo'});
+
+      var div = $(el).children().first();
+      expect(div.attr('id')).toEqual("foo");
+    });
+  });
 
   describe('#lastNMonthNamesFrom', function(){
     it('works when this test was written', function(){
