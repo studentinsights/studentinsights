8911e0c7b54c2975ee0228cdb1f0c57d81f73489
Alex Soble <asoble@gmail.com>
2016-05-01 19:11:47 -0500

    Initial assignment of homerooms to schools

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160428030255) do
+ActiveRecord::Schema.define(version: 20160502001001) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
