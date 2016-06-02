df128c5666fca19f1d9a911d4d2539d6413f4f16
Alex Soble <asoble@gmail.com>
2016-06-02 14:57:24 -0500

    Remove dashboards, routes, controllers we don't need

--- config/routes.rb
+++ config/routes.rb
@@ -2,27 +2,6 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :educators
-resources :schools
-resources :student_assessments
-resources :students
-resources :homerooms
-resources :student_risk_levels
-resources :absences
-resources :assessments
-resources :discipline_incidents
-resources :discontinued_services
-resources :event_notes
-resources :event_note_revisions
-resources :event_note_types
-resources :interventions
-resources :intervention_types
-resources :precomputed_query_docs
-resources :school_years
-resources :services
-resources :service_types
-resources :student_school_years
-resources :tardies
-
     root to: "educators#index"
   end
 
