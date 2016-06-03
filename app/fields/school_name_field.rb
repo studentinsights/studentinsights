e020a042b00cd386aa28a8daa24409998fa2ad6c
Alex Soble <asoble@gmail.com>
2016-06-03 13:02:15 -0500

    Prefer school name, less cryptic

--- app/fields/school_name_field.rb
+++ app/fields/school_name_field.rb
@@ -2,6 +2,6 @@ require "administrate/field/base"
 
 class SchoolNameField < Administrate::Field::Base
   def to_s
-    data.try(:local_id) || data.try(:name)
+    data.try(:name) || data.try(:local_id)
   end
 end
