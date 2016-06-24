dd2c904195929189dc8fea2c413a1fb4a491ff99
Alex Soble <asoble@gmail.com>
2016-06-24 04:49:41 -0500

    Provide decent defaults to DIBELS row spec

--- spec/importers/rows/dibels_row_spec.rb
+++ spec/importers/rows/dibels_row_spec.rb
@@ -4,7 +4,12 @@ RSpec.describe DibelsRow do
 
   let(:student) { FactoryGirl.create(:student) }
   let(:row) {
-    { assessment_test: 'DIBELS', local_id: student.local_id, assessment_date: Date.today }
+    {
+      assessment_test: 'DIBELS',
+      local_id: student.local_id,
+      assessment_date: Date.today,
+      assessment_performance_level: 'DECENT'
+    }
   }
 
   let(:assessment) { Assessment.last }
