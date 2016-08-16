63c0c42c0beb5dd5699ca898796284c81ff60ae0
Alex Soble <asoble@gmail.com>
2016-08-16 10:10:43 -0500

    Get rid of slug candidates now that we have a default homeroom name strategy

--- app/models/homeroom.rb
+++ app/models/homeroom.rb
@@ -1,6 +1,6 @@
 class Homeroom < ActiveRecord::Base
   extend FriendlyId
-  friendly_id :slug_candidates, use: :slugged
+  friendly_id :name, use: :slugged
   validates :name, presence: true, uniqueness: { scope: [:name, :school] }
   validates :slug, uniqueness: true, presence: true
   validates :school, presence: true
@@ -8,10 +8,6 @@ class Homeroom < ActiveRecord::Base
   has_many :student_risk_levels, through: :students
   belongs_to :educator
   belongs_to :school
-
-  def slug_candidates
-    [ :name, school.try(:local_id) + '-homeroom' ]
-  end
 
   def update_grade(student)
     # Set homeroom grade level to be first student's grade level, since
