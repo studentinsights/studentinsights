28f744c4f41fea223144655d897595ff920011d6
Alex Soble <asoble@gmail.com>
2016-08-17 07:03:35 -0500

    First pass at updating students who graduate to HS

--- app/importers/file_importers/students_importer.rb
+++ app/importers/file_importers/students_importer.rb
@@ -18,6 +18,26 @@ class StudentsImporter < Struct.new :school_scope, :client, :log, :progress_bar
 
   def import_row(row)
     student = StudentRow.new(row, school_ids_dictionary).build
+
+    if student.grade.in? ['9', '10', '11', '12', 'SP']
+      handle_high_school_student(student)
+    else
+      handle_elementary_student(student, row)
+    end
+  end
+
+  def handle_high_school_student(student)
+    puts 'HS, no import!' && return if student.new_record?  # We don't want to import HS students into the db
+                                                            # since this app isn't being used at the high school
+
+    puts 'HS, update row!'
+    student.enrollment_status = 'High School'
+    student.grade = 'HS'
+    student.school = nil
+    student.save!
+  end
+
+  def handle_elementary_student(student, row)
     if student.save!
       assign_student_to_homeroom(student, row[:homeroom])
       student.create_student_risk_level!
