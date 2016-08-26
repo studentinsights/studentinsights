8beef59907b9e6dac0c48054530a54abb22f5ce6
Kevin Robinson <kevin.robinson.0@gmail.com>
2016-08-26 19:00:43 -0400

    Add Mixpanel and Mailgun envs to reporting task"
    Update Mixpanel report to include email job

--- app/reports/mixpanel_report.rb
+++ app/reports/mixpanel_report.rb
@@ -4,8 +4,15 @@ require 'json'
 # Querys the MixPanel API for aggregate, non-personally identifiable usage data.
 # Intended to be run weekly.
 class MixpanelReport
-  def initialize(api_secret)
-    @api_secret = api_secret
+  # Parse the Heroku environment variables into the URL form the Mailgun API expects
+  def self.mailgun_url_from_env(env)
+    api_key = env['MAILGUN_API_KEY']
+    domain = env['MAILGUN_DOMAIN']
+    "https://api:#{api_key}@api.mailgun.net/v3/#{domain}/messages"
+  end
+
+  def initialize(mixpanel_api_secret)
+    @mixpanel_api_secret = mixpanel_api_secret
     @buffer = []
   end
 
@@ -13,40 +20,55 @@ class MixpanelReport
   def run
     @buffer = []
     event_name = 'PAGE_VISIT'
-    schools = [
-      { :name => 'Healey', :id => 2 },
-      { :name => 'West', :id => 6 },
-      { :name => 'East', :id => 5 }
-    ]
-    schools.each do |school|
+
+    output 'Hello!'
+    output
+    output 'This is the usage report for Student Insights.'
+    output 'It shows the number of visits and the number of unique users each week.'
+    output 'For these reports, a week starts on Monday and ends on Sunday.'
+    output
+    output 'Unique users is the better number to watch, since visits will be more variable.'
+    output 'The total across all schools is shown first, with individual schools after.'
+    output 'Please reply to this email if you have any questions!'
+    output
+    output
+
+    print_totals(event_name)
+    output
+    output
+    output
+    output
+
+    schools_for_report.each do |school|
       output school[:name]
       print_summary_header
       output school_summary_table(event_name, school[:id])
       output
     end
 
-    print_totals(event_name)
     @buffer.join("\n")
   end
 
+  # Prints to stdout and sends an email to each address in `target_emails`
   def run_and_email!(mailgun_url, target_emails)
     puts 'Running report...'
-    output = run
+    report_text = run()
     puts 'Done.'
     puts
     puts 'Report output:'
-    puts output
+    puts report_text
     puts
 
     puts 'Sending emails...'
-    target_emails.each do |email|
+    target_emails.each do |target_email|
+      date_text = DateTime.now.beginning_of_day.strftime('%B %e, %Y')
       post_data = Net::HTTP.post_form(URI.parse(mailgun_url), {
         :from => "Student Insights job <kevin.robinson.0@gmail.com>",
-        :to => "Kevin Robinson <kevin.robinson.0@gmail.com>",
-        :subject => "Student Insights Usage Report for #{DateTime.now.beginning_of_day}",
-        :text => output
+        :to => target_email,
+        :subject => "Student Insights Usage Report for #{date_text}",
+        :html => "<html><body><pre style='font: monospace; font-size: 12px;'>#{report_text}</pre>"
       })
-      puts "email: #{email}"
+      puts "to: #{target_email}"
       puts "code: #{post_data.code}"
       puts "body: #{post_data.body}"
       puts
@@ -57,12 +79,21 @@ class MixpanelReport
   end
 
   private
-  def output(string)
+  def schools_for_report
+    local_ids = ['HEA', 'WSNS', 'ESCS', 'BRN', 'KDY', 'AFAS', 'WHCS']
+    local_ids.map do |local_id|
+      school = School.find_by_local_id(local_id)
+      { name: school.name, id: school.id }
+    end
+  end
 
+  def output(string = '')
+    @buffer << string
+    nil
   end
 
   def print_totals(event_name)
-    output 'TOTAL'
+    output 'Total Student Insights usage'
     print_summary_header
     visits = extract(event_name, query_for({
       event: event_name,
@@ -137,7 +168,7 @@ class MixpanelReport
 
   def query_for(params)
     to_date = Date.today
-    from_date = to_date - 28
+    from_date = to_date - 35
 
     params_list = params.keys.map do |key|
       "-d #{key}='#{params[key]}'"
@@ -145,7 +176,7 @@ class MixpanelReport
     cmd = ([
       "curl https://mixpanel.com/api/2.0/segmentation",
       "-s",
-      "-u #{@api_secret}: ",
+      "-u #{@mixpanel_api_secret}: ",
       "-d from_date='#{from_date}' -d to_date='#{to_date}' "
     ] + params_list).join ' '
     output = `#{cmd}`
