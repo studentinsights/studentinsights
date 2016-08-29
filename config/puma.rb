2d6efedeba916d05894239c8e628f8546b22697f
= <really.eli@gmail.com>
2016-08-29 15:06:21 -0400

    Switched out gems and config files, modified Procfile.

---
+++ config/puma.rb
@@ -0,0 +1,7 @@
+threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
+threads threads_count, threads_count
+
+preload_app!
+
+port        ENV['PORT']     || 3000
+environment ENV['RACK_ENV'] || 'development'
