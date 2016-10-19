9ad86f2b8e422d879cbbfa575c67f7828e104f66
Alex Soble <asoble@gmail.com>
2016-10-19 18:48:29 -0500

    Update factory

--- spec/models/event_note_spec.rb
+++ spec/models/event_note_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 RSpec.describe EventNote, type: :model do
 
   context 'is_restricted defined' do
-    let(:note) { FactoryGirl.build(:event_note, is_restricted: true) }
+    let(:note) { FactoryGirl.build(:event_note) }
     it 'is valid' do
       expect(note).to be_valid
     end
   end
 
   context 'is_restricted undefined' do
-    let(:note) { FactoryGirl.build(:event_note) }
+    let(:note) { FactoryGirl.build(:event_note, is_restricted: false) }
     it 'is invalid' do
       expect(note).to be_invalid
     end
