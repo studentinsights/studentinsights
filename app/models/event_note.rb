078aa99c1ec524de229b56fdce390d7e1dba2677
Alex Soble <asoble@gmail.com>
2016-10-19 18:56:12 -0500

    Turns out we want to validate inclusion, not presence
    + http://guides.rubyonrails.org/active_record_validations.html#inclusion

--- app/models/event_note.rb
+++ app/models/event_note.rb
@@ -3,6 +3,6 @@ class EventNote < ActiveRecord::Base
   belongs_to :student
   belongs_to :event_note_type
 
-  validates :educator_id, :student_id, :event_note_type_id,
-            :recorded_at, :is_restricted, presence: true
+  validates :educator_id, :student_id, :event_note_type_id, :recorded_at, presence: true
+  validates :is_restricted, inclusion: { in: [true, false] }
 end
