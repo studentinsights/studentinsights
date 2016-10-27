a348f6904aba2eb71e7535913b3e72fde175eaaf
Alex Soble <asoble@gmail.com>
2016-10-27 11:29:21 -0500

    Update expectation

--- spec/javascripts/student_profile/profile_chart_spec.js
+++ spec/javascripts/student_profile/profile_chart_spec.js
@@ -7,9 +7,9 @@ describe('ProfileBarCharts', function() {
             ProfileChart.prototype.getSchoolYearStartPositions(28, moment.utc("2016-09-10"), 5)
         ).toEqual(
             _.object([
-                [moment.utc("2016-08-15").valueOf(), "2016<br><b>Grade 5</b>"],
-                [moment.utc("2015-08-15").valueOf(), "2015<br><b>Grade 4</b>"],
-                [moment.utc("2014-08-15").valueOf(), "2015<br><b>Grade 3</b>"]
+                [moment.utc("2016-08-15").valueOf(), "<b>Grade 5<br>started</b>"],
+                [moment.utc("2015-08-15").valueOf(), "<b>Grade 4<br>started</b>"],
+                [moment.utc("2014-08-15").valueOf(), "<b>Grade 3<br>started</b>"]
             ])
         );
     })
