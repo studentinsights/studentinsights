8769f71eaff3cfe214d2d1c0a9aa4d11dd7df3ec
Alex Soble <asoble@gmail.com>
2016-11-18 13:43:51 -0600

    Use #invoke_all

--- app/workers/import_worker.rb
+++ app/workers/import_worker.rb
@@ -3,6 +3,6 @@ class ImportWorker
 
   def perform
     load File.expand_path("#{Rails.root}/lib/tasks/import.thor", __FILE__)
-    Import::Start.new.invoke
+    Import::Start.new.invoke_all
   end
 end
