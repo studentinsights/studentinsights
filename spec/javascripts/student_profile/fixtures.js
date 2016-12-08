45dffa04f693d0b56fa5bdf9839b28d7eab0f806
Alex Soble <asoble@gmail.com>
2016-12-08 11:36:06 -0600

    Add attachment_urls to fixture data

--- spec/javascripts/student_profile/fixtures.js
+++ spec/javascripts/student_profile/fixtures.js
@@ -19,7 +19,8 @@
         "recorded_at": "2016-02-26T22:20:55.398Z",
         "created_at": "2016-02-26T22:20:55.416Z",
         "updated_at": "2016-02-26T22:20:55.416Z",
-        "is_restricted": false
+        "is_restricted": false,
+        "attachment_urls": []
       },
       {
         "id": 4,
@@ -30,7 +31,8 @@
         "recorded_at": "2016-02-27T19:23:26.835Z",
         "created_at": "2016-02-27T19:23:26.836Z",
         "updated_at": "2016-02-27T19:23:26.836Z",
-        "is_restricted": true
+        "is_restricted": true,
+        "attachment_urls": []
       }
     ],
     "services": {
@@ -489,7 +491,8 @@
           "text": "okay!",
           "recorded_at": "2016-02-11T21:28:02.102Z",
           "created_at": "2016-02-11T21:28:02.103Z",
-          "updated_at": "2016-02-11T21:28:02.103Z"
+          "updated_at": "2016-02-11T21:28:02.103Z",
+          "attachment_urls": []
         },
         {
           "id": 2,
@@ -499,7 +502,8 @@
           "text": "cool!",
           "recorded_at": "2016-02-11T21:29:18.166Z",
           "created_at": "2016-02-11T21:29:18.167Z",
-          "updated_at": "2016-02-11T21:29:18.167Z"
+          "updated_at": "2016-02-11T21:29:18.167Z",
+          "attachment_urls": []
         },
         {
           "id": 3,
@@ -509,7 +513,8 @@
           "text": "sweet",
           "recorded_at": "2016-02-11T21:29:30.287Z",
           "created_at": "2016-02-11T21:29:30.288Z",
-          "updated_at": "2016-02-11T21:29:30.288Z"
+          "updated_at": "2016-02-11T21:29:30.288Z",
+          "attachment_urls": []
         }
       ],
       "services": {
