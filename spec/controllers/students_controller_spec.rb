1dc6b3d7f76d873a2c5df8e4537a6bba6333ee99
Alex Soble <asoble@gmail.com>
2016-12-17 09:32:21 -0600

    Update students controller spec

--- spec/controllers/students_controller_spec.rb
+++ spec/controllers/students_controller_spec.rb
@@ -306,53 +306,42 @@ describe StudentsController, :type => :controller do
   describe '#names' do
     let(:school) { FactoryGirl.create(:healey) }
 
-    def make_request(query)
+    def make_request
       request.env['HTTPS'] = 'on'
-      get :names, q: query, format: :json
+      get :names, format: :json
     end
 
     context 'admin educator logged in' do
       let!(:educator) { FactoryGirl.create(:educator, :admin, school: school) }
       before { sign_in(educator) }
-      context 'query matches student name' do
-        let!(:juan) { FactoryGirl.create(:student, first_name: 'Juan', school: school, grade: '5') }
-        let!(:jacob) { FactoryGirl.create(:student, first_name: 'Jacob', grade: '5') }
+      let!(:juan) {
+        FactoryGirl.create(:student, first_name: 'Juan', last_name: 'P', school: school, grade: '5')
+      }
 
-        it 'is successful' do
-          make_request('j')
-          expect(response).to be_success
-        end
-        it 'returns student name and id' do
-          make_request('j')
-          expect(assigns(:sorted_results)).to eq [{ label: "Juan - HEA - 5", value: juan.id }]
-        end
-      end
-      context 'does not match student name' do
-        it 'is successful' do
-          make_request('j')
-          expect(response).to be_success
-        end
-        it 'returns an empty array' do
-          make_request('j')
-          expect(assigns(:sorted_results)).to eq []
-        end
+      let!(:jacob) {
+        FactoryGirl.create(:student, first_name: 'Jacob', grade: '5')
+      }
+
+      it 'returns an array of student labels and ids that match the educator\'s students' do
+        make_request
+        expect(response).to be_success
+        expect(JSON.parse(response.body)).to eq [{ "label" => "Juan P - HEA - 5", "id" => juan.id }]
       end
     end
     context 'educator without authorization to students' do
       let!(:educator) { FactoryGirl.create(:educator) }
       before { sign_in(educator) }
-      context 'query matches student name' do
-        let(:healey) { FactoryGirl.create(:healey) }
-        let!(:juan) { FactoryGirl.create(:student, first_name: 'Juan', school: healey, grade: '5') }
-        it 'returns an empty array' do
-          make_request('j')
-          expect(assigns(:sorted_results)).to eq []
-        end
+      let(:healey) { FactoryGirl.create(:healey) }
+      let!(:juan) { FactoryGirl.create(:student, first_name: 'Juan', school: healey, grade: '5') }
+
+      it 'returns an empty array' do
+        make_request
+        expect(JSON.parse(response.body)).to eq []
       end
     end
     context 'educator not logged in' do
       it 'is not successful' do
-        make_request('j')
+        make_request
         expect(response.status).to eq 401
         expect(response.body).to include "You need to sign in before continuing."
       end
