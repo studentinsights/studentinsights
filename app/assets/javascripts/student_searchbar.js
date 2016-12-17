21e4230da27ff27a28c8c94b45300e807464e638
Alex Soble <asoble@gmail.com>
2016-12-17 09:48:44 -0600

    Remove minLength == 2
    + jQuery default is 1
    + Now that we're not doing any extra database calls for each letter, this doesn't cost us anything performance-wise and sends feedback to the user more quickly

--- app/assets/javascripts/student_searchbar.js
+++ app/assets/javascripts/student_searchbar.js
@@ -6,7 +6,6 @@ $(function() {
       url: '/students/names',
       success: function (data) {
         $("#student-searchbar").autocomplete({
-          minLength: 2,
           source: data,
           select: function(e, ui) {
             window.location.pathname = '/students/' + ui.item.id;
