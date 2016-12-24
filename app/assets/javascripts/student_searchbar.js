9dc74a74a58784e3dd50ef67f89b3f4d2b344055
Alex Soble <asoble@gmail.com>
2016-12-24 08:40:09 -0600

    Cache student names data with window.sessionStorage for searchbar
    + This will stop the client-side from querying the database for student names on every page load

--- app/assets/javascripts/student_searchbar.js
+++ app/assets/javascripts/student_searchbar.js
@@ -1,21 +1,43 @@
 $(function() {
 
-  if ($('#student-searchbar').length > 0) {
+  function setupSearchBarAutocomplete (names) {
+    $("#student-searchbar").autocomplete({
+      source: names,
+      select: function(e, ui) {
+        var MixpanelUtils = window.shared.MixpanelUtils;
+        MixpanelUtils.track('SEARCHBAR_SELECTED_STUDENT', {});
+        window.location.pathname = '/students/' + ui.item.id;
+      },
+    });
+  };
 
+  function downloadStudentNames () {
     $.get({
       url: '/students/names',
       success: function (data) {
-        $("#student-searchbar").autocomplete({
-          source: data,
-          select: function(e, ui) {
-            var MixpanelUtils = window.shared.MixpanelUtils;
-            MixpanelUtils.track('SEARCHBAR_SELECTED_STUDENT', {});
-            window.location.pathname = '/students/' + ui.item.id;
-          },
-        });
-      }
-    })
+        setupSearchBarAutocomplete(data);
 
+        if (window.sessionStorage) {
+          window.sessionStorage.student_names_cache = JSON.stringify(data);
+        } else {
+          throw 'no session storage';
+        };
+      }
+    });
+  };
+
+  if ($('#student-searchbar').length > 0) {
+    if (!(window.sessionStorage)) {
+      downloadStudentNames();       // Query for names if we have no local storage
+      throw 'no session storage';   // Let rollbar know we're not caching
+    };
+
+    var namesCache = window.sessionStorage.student_names_cache;
+
+    if (namesCache) return setupSearchBarAutocomplete(JSON.parse(namesCache));
+
+    return downloadStudentNames();  // Student names haven't cached yet,
+                                    // so let's download and cache them
   };
 
 });
