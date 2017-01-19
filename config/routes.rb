9a1e36ae3a302077f916ab2d9474a75ddcc2b48a
Alex Soble <asoble@gmail.com>
2017-01-19 16:24:58 -0600

    Set up endpoint for autosuggesting service type names
    + #820

--- config/routes.rb
+++ config/routes.rb
@@ -33,6 +33,7 @@ Rails.application.routes.draw do
     end
   end
   resources :services, only: [:destroy]
+  resources :service_types, only: [:index]
   resources :event_note_attachments, only: [:destroy]
 
   resources :homerooms, only: [:show]
---
+++ spec/controllers/service_types_controller_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+describe ServiceTypesController, :type => :controller do
+
+  describe '#index' do
+    def make_request
+      request.env['HTTPS'] = 'on'
+      get :index, format: :json
+    end
+
+    let(:parsed_response) { JSON.parse(response.body) }
+
+    context 'educator logged in' do
+      let(:educator) { FactoryGirl.create(:educator) }
+      it 'returns an array of student lasids' do
+        sign_in(educator)
+        make_request
+        expect(parsed_response).to eq [
+          "Afterschool Tutoring",
+           "Attendance Contract",
+           "Attendance Officer",
+           "Behavior Contract",
+           "Counseling, in-house",
+           "Counseling, outside",
+           "Freedom School",
+           "Math intervention",
+           "Reading intervention",
+           "SomerSession",
+           "Summer Program for English Language Learners"
+          ]
+      end
+    end
+
+    context 'no educator logged in' do
+      it 'returns an error' do
+        make_request
+        expect(parsed_response).to eq({"error"=>"You need to sign in before continuing."})
+      end
+    end
+
+  end
+
+end
