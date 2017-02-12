cae705ef8aa9d7e0a91b8951bc43300a93d608a5
Kevin Robinson <kevin.robinson.0@gmail.com>
2017-02-12 18:09:20 -0500

    Move class loading setup into application.rb

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,32 @@ module SomervilleTeacherTool
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # --- Student Insights additions below ---
+    Rails.application.config.tap do |config|
+      class_paths = [
+        "#{config.root}/app/models",
+        "#{config.root}/app/jobs",
+        "#{config.root}/app/importers/clients",
+        "#{config.root}/app/importers/data_transformers",
+        "#{config.root}/app/importers/file_importers",
+        "#{config.root}/app/importers/filters",
+        "#{config.root}/app/importers/rows",
+        "#{config.root}/app/importers/sources",
+        "#{config.root}/app/importers/student_services",
+        "#{config.root}/lib"
+      ]
+
+      config.eager_load_paths = (config.eager_load_paths + class_paths).uniq
+      class_paths.each do |class_path| 
+        config.autoload_paths << class_path
+      end
+
+      config.generators do |g|
+        g.stylesheets false
+        g.javascripts false
+        g.helpers false
+      end
+    end
   end
 end
