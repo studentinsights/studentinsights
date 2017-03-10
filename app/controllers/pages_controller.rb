a710e6f22262496682b481183912ce274e47fef1
Alex Soble <asoble@gmail.com>
2017-03-10 15:44:52 -0600

    No authorization requirements for let's encrypt verification

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
 
-  skip_before_action :authenticate_educator!, only: [:about]  # Inherited from ApplicationController.
+  skip_before_action :authenticate_educator!, only: [:about, :lets_encrypt_endpoint]  # Inherited from ApplicationController.
 
   def about
   end
