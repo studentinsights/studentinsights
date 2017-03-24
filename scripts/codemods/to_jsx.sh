ae145cbbd9969463375f12d3bba9e3a5f8c07d4c
Alex Soble <asoble@gmail.com>
2017-03-24 10:06:55 -0400

    Loop throgh the dirs which contain JS => JSX files
    + Some of the JS is super vanilla or Rails-related and doesn't need JSX
    + Also, comment out the `yarn` command -- dependency installation was breaking for some reason; I used npm install -g jscodeshift to get around it

--- scripts/codemods/to_jsx.sh
+++ scripts/codemods/to_jsx.sh
@@ -6,37 +6,57 @@
 # - (does not do ES6 class transform)
 # usage: scripts/codemods/to_jsx.sh app/assets/javascripts/student_profile/note_card.js
 #
-TARGET=$1
 
-# # dom.div({ foo: 'bar'}, 'hi!') -> <div foo="bar">{hi!}</div>
-find $TARGET -name '*.js*' |xargs perl -pi -e 's/dom\.(\w+)\(/React\.createElement\("$1", /g'
+JSX_DIRS=(
+  components
+  helpers
+  restricted_notes
+  school_overview
+  service_uploads
+  star_math
+  star_reading
+  student_profile
+)
 
-# # createEl(Foo, { name: 'Kevin' }, 'hi!') -> <Foo name="Kevin">{hi!}</Foo>
-find $TARGET -name '*.js*' |xargs perl -pi -e 's/createEl\(/React\.createElement\(/g'
+for dir in ${JSX_DIRS[@]}; do
+  echo "Transforming files in ${dir}..."
 
-# for getting codemod dependencies
-mkdir -p tmp/codemods
+  for TARGET in $(find "app/assets/javascripts/${dir}" -name '*.js'); do
+    echo "Transforming {$TARGET}...";
 
-# Tiny fork, see: https://github.com/kevinrobinson/react-codemod
-if [ ! -d tmp/codemods/react-codemod ] ; then
-  git clone --depth 1 https://github.com/kevinrobinson/react-codemod tmp/codemods/react-codemod
-fi
-pushd tmp/codemods/react-codemod
-yarn
-popd
-jscodeshift -t tmp/codemods/react-codemod/transforms/create-element-to-jsx.js $TARGET
-# TODO(kr) ES6 class transform
-# jscodeshift -t tmp/codemods/react-codemod/transforms/class.js $TARGET
+    # # dom.div({ foo: 'bar'}, 'hi!') -> <div foo="bar">{hi!}</div>
+    find "$TARGET" -name '*.js*' |xargs perl -pi -e 's/dom\.(\w+)\(/React\.createElement\("$1", /g'
+
+    # # createEl(Foo, { name: 'Kevin' }, 'hi!') -> <Foo name="Kevin">{hi!}</Foo>
+    find "$TARGET" -name '*.js*' |xargs perl -pi -e 's/createEl\(/React\.createElement\(/g'
+
+    # for getting codemod dependencies
+    mkdir -p tmp/codemods
+
+    # Tiny fork, see: https://github.com/kevinrobinson/react-codemod
+    if [ ! -d tmp/codemods/react-codemod ] ; then
+      git clone --depth 1 https://github.com/kevinrobinson/react-codemod tmp/codemods/react-codemod
+    fi
+
+    # TODO(ARS) -- figure out why this `yarn` is breaking
+    # pushd tmp/codemods/react-codemod
+    # yarn
+    # popd
+    jscodeshift -t tmp/codemods/react-codemod/transforms/create-element-to-jsx.js "$TARGET"
+    # TODO(kr) ES6 class transform
+    # jscodeshift -t tmp/codemods/react-codemod/transforms/class.js $TARGET
 
 
-# See https://github.com/cpojer/js-codemod
-if [ ! -d tmp/codemods/js-codemod ] ; then
-  git clone --depth=1 https://github.com/cpojer/js-codemod tmp/codemods/js-codemod
-fi
-pushd tmp/codemods/js-codemod
-yarn
-popd
-jscodeshift -t tmp/codemods/js-codemod/transforms/no-vars.js $TARGET
+    # See https://github.com/cpojer/js-codemod
+    if [ ! -d tmp/codemods/js-codemod ] ; then
+      git clone --depth=1 https://github.com/cpojer/js-codemod tmp/codemods/js-codemod
+    fi
+    pushd tmp/codemods/js-codemod
+    yarn
+    popd
+    jscodeshift -t tmp/codemods/js-codemod/transforms/no-vars.js "$TARGET"
 
-# Rename
-mv "$TARGET" "${TARGET%.js}.jsx"
+    # Rename
+    mv "$TARGET" "${TARGET%.js}.jsx"
+  done;
+done;
