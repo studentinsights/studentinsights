70c5fd5a0f090a3abdd63e3572eb3faf8a6361f8
Alex Soble <asoble@gmail.com>
2017-05-04 12:08:27 -0500

    Rip our Tablesort in School Overview page because it's a mess
    + It can't handle a custom sort with date very well :-/
    + So let's rip it out of the School Overview page and just handle the sorting ourselves with ES6 & React
    + Still using Tablesort for the Homeroom view
    + And still using the CSS for the little carets

--- app/assets/javascripts/components/students_table.jsx
+++ app/assets/javascripts/components/students_table.jsx
@@ -12,8 +12,12 @@ export default React.createClass({
     students: React.PropTypes.arrayOf(React.PropTypes.object).isRequired
   },
 
-  componentDidMount: function() {
-    new Tablesort(document.querySelector('.students-table'), { descending: false });
+  getInitialState () {
+    return {
+      sortBy: 'first_name',
+      sortType: 'string',
+      sortDesc: true
+    };
   },
 
   mergeInDateOfLastSST (student) {
@@ -33,32 +37,133 @@ export default React.createClass({
     return this.props.students.map(student => this.mergeInDateOfLastSST(student));
   },
 
-  render: function() {
+  sortByDate (a, b, sortBy) {
+    const dateA = moment(a[sortBy], 'MM/D/YY')
+    const dateB = moment(b[sortBy], 'MM/D/YY')
+
+    if (!dateA.isValid() && !dateB.isValid()) return 0;
+
+    if (!dateB.isValid() || dateA.isAfter(dateB)) return -1;
+    if (!dateA.isValid() || dateA.isBefore(dateB)) return 1;
+
+    return 0;
+  },
+
+  sortByString (a, b, sortBy) {
+    const stringA = a[sortBy].toUpperCase(); // ignore upper and lowercase
+    const stringB = b[sortBy].toUpperCase(); // ignore upper and lowercase
+
+    if (stringA < stringB) return -1;
+    if (stringA > stringB) return 1;
+    return 0;
+  },
+
+  sortByNumber (a, b, sortBy) {
+    const numA = parseInt(a[sortBy]);
+    const numB = parseInt(b[sortBy]);
+
+    if (!Number.isInteger(numA) && !Number.isInteger(numB)) return 0;
+
+    if (!Number.isInteger(numA) || numA > numB) return -1;
+    if (!Number.isInteger(numB) || numA < numB) return 1;
+
+    return 0;
+  },
+
+  sortByCustomEnum (a, b, sortBy, customEnum) {
+    const indexA = customEnum.indexOf(a[sortBy]);
+    const indexB = customEnum.indexOf(b[sortBy]);
+
+    if (indexA > indexB) return 1;
+    if (indexB > indexA) return -1;
+    return 0;
+  },
+
+  sortByActiveServices (a, b) {
+    const numA = a.active_services.length;
+    const numB = b.active_services.length;
+
+    if (numA > numB) return 1;
+    if (numB > numA) return -1;
+    return 0;
+  },
+
+  orderedStudents () {
+    const sortedStudents = this.sortedStudents();
+
+    if (!this.state.sortDesc) return sortedStudents.reverse();
+
+    return sortedStudents;
+  },
+
+  sortedStudents () {
+    const students = this.studentsWithDateOfLastSST();
+    const sortBy = this.state.sortBy;
+    const sortType = this.state.sortType;
+    let customEnum;
+
+    switch(sortType) {
+      case 'string':
+        return students.sort((a, b) => this.sortByString(a, b, sortBy));
+      case 'number':
+        return students.sort((a, b) => this.sortByNumber(a, b, sortBy));
+      case 'date':
+        return students.sort((a, b) => this.sortByDate(a, b, sortBy));
+      case 'free_reduced_lunch':
+        return students.sort((a, b) => this.sortByString(a, b, sortBy));
+      case 'grade':
+        customEnum = ['PK', 'KF', '1', '2', '3', '4', '5', '6', '7', '8'];
+        return students.sort((a, b) => this.sortByCustomEnum(a, b, sortBy, customEnum))
+      case 'sped_level_of_need':
+        customEnum = ['—', 'Low < 2', 'Low >= 2', 'Moderate', 'High'];
+        return students.sort((a, b) => this.sortByCustomEnum(a, b, sortBy, customEnum));
+      case 'limited_english_proficiency':
+        customEnum = ['FLEP-Transitioning', 'FLEP', 'Fluent'];
+        return students.sort((a, b) => this.sortByCustomEnum(a, b, sortBy, customEnum));
+      case 'program_assigned':
+        customEnum = ['Reg Ed', '2Way English', '2Way Spanish', 'Sp Ed'];
+        return students.sort((a, b) => this.sortByCustomEnum(a, b, sortBy, customEnum));
+      case 'active_services':
+        return students.sort((a, b) => this.sortByActiveServices(a, b));
+      default:
+        return students;
+    }
+  },
+
+  onClickHeader (sortBy, sortType) {
+    if (sortBy === this.state.sortBy) {
+      this.setState({ sortDesc: !this.state.sortDesc });
+    } else {
+      this.setState({ sortBy: sortBy, sortType: sortType });
+    }
+  },
+
+  render () {
     return (
       <div className='StudentsTable'>
         <table className='students-table' style={{ width: '100%' }}>
           <thead>
             <tr>
-              {this.renderHeader('Name', { className: 'sort-default' }) /* className is read by Tablesort */}
-              {this.renderHeader('Last SST', { 'data-sort-method': 'sst_date' })}
-              {this.renderHeader('Grade')}
-              {this.renderHeader('Disability', { 'data-sort-method': 'disability' })}
-              {this.renderHeader('Low Income')}
-              {this.renderHeader('LEP')}
-              {this.renderHeader('STAR Reading')}
-              {this.renderHeader('MCAS ELA')}
-              {this.renderHeader('STAR Math')}
-              {this.renderHeader('MCAS Math')}
-              {this.renderHeader('Discipline Incidents')}
-              {this.renderHeader('Absences')}
-              {this.renderHeader('Tardies')}
-              {this.renderHeader('Services')}
-              {this.renderHeader('Program', { 'data-sort-method': 'prog_assign_sort' })}
-              {this.renderHeader('Homeroom')}
+              {this.renderHeader('Name', 'first_name', 'string')}
+              {this.renderHeader('Last SST', 'dateOfLastSST', 'date')}
+              {this.renderHeader('Grade', 'grade', 'grade')}
+              {this.renderHeader('Disability', 'sped_level_of_need', 'sped_level_of_need')}
+              {this.renderHeader('Low Income', 'free_reduced_lunch', 'free_reduced_lunch')}
+              {this.renderHeader('LEP', 'limited_english_proficiency', 'limited_english_proficiency')}
+              {this.renderHeader('STAR Reading', 'most_recent_star_reading_percentile', 'number')}
+              {this.renderHeader('MCAS ELA', 'most_recent_mcas_ela_scaled', 'number')}
+              {this.renderHeader('STAR Math', 'most_recent_star_math_percentile', 'number')}
+              {this.renderHeader('MCAS Math', 'most_recent_mcas_math_scaled', 'number')}
+              {this.renderHeader('Discipline Incidents', 'discipline_incidents_count', 'number')}
+              {this.renderHeader('Absences', 'absences_count', 'number')}
+              {this.renderHeader('Tardies', 'tardies_count', 'number')}
+              {this.renderHeader('Services', 'active_services', 'active_services')}
+              {this.renderHeader('Program', 'program_assigned', 'program_assigned')}
+              {this.renderHeader('Homeroom', 'homeroom_id', 'number')}
             </tr>
           </thead>
           <tbody>
-            {this.studentsWithDateOfLastSST().map(function(student) {
+            {this.orderedStudents().map(student => {
               return (
                 <tr key={student.id}>
                   <td>
@@ -96,7 +201,7 @@ export default React.createClass({
     );
   },
 
-  renderNumberCell: function(children) {
+  renderNumberCell (children) {
     return (
       <td style={{textAlign: 'right', width: '5em', paddingRight: '3em'}}>
         {children}
@@ -104,7 +209,7 @@ export default React.createClass({
     );
   },
 
-  renderUnless: function(ignoredValue, value) {
+  renderUnless (ignoredValue, value) {
     const valueText = (value === null || value === undefined) ? 'None' : value;
     return (
       <span style={{opacity: (valueText === ignoredValue) ? 0.1 : 1 }}>
@@ -113,18 +218,27 @@ export default React.createClass({
     );
   },
 
-  renderCount: function(count) {
+  renderCount (count) {
     return (count === 0) ? null : count;
   },
 
-  renderHeader: function(caption, options) {
+  headerClassName (sortBy) {
+    // Using tablesort classes here for the cute CSS carets,
+    // not for the acutal table sorting JS (that logic is handled by this class).
+
+    if (sortBy !== this.state.sortBy) return 'sort-header';
+
+    if (this.state.sortDesc) return 'sort-header sort-down';
+
+    return 'sort-header sort-up';
+  },
+
+  renderHeader (caption, sortBy, sortType) {
     const pieces = caption.split(' ');
 
-    const className = (options ? options['className'] : null);
-    const dataSortMethod = (options ? options['data-sort-method'] : null);
-
     return (
-      <th className={className} data-sort-method={dataSortMethod}>
+      <th onClick={this.onClickHeader.bind(null, sortBy, sortType)}
+          className={this.headerClassName(sortBy)}>
         {pieces[0]}
         <br/>
         {pieces[1]}
