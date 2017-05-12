ea9add0022a0e322171006df8ca9063fb269deee
Alex Soble <asoble@gmail.com>
2017-05-12 14:38:05 -0500

    Fix ordering

--- app/assets/javascripts/student_profile/help_bubble.jsx
+++ app/assets/javascripts/student_profile/help_bubble.jsx
@@ -11,17 +11,20 @@
     getInitialState: function(){
       return {modalIsOpen: false};
     },
+
+    componentWillMount: function(){
+      // This needs to be called for some reason, and we need to do it by the time the DOM exists.
+      ReactModal.setAppElement(document.body);
+    },
+
     closeModal: function(e){
       this.setState({modalIsOpen: false});
       e.preventDefault();
     },
+
     openModal: function(e){
       this.setState({modalIsOpen: true});
       e.preventDefault();
-    },
-    componentWillMount: function(){
-      // This needs to be called for some reason, and we need to do it by the time the DOM exists.
-      ReactModal.setAppElement(document.body);
     },
 
     render: function(){
