ed5852cbde1c97e00c3cafc8ed919809a863c224
Alex Soble <asoble@gmail.com>
2017-05-18 10:26:34 -0500

    Fix syntax mistakes

--- app/models/import_record.rb
+++ app/models/import_record.rb
@@ -1,11 +1,11 @@
 class ImportRecord < ActiveRecord::Base
 
   def completed?
-    record.time_ended.present?
+    time_ended.present?
   end
 
   def time_to_complete
-    record.time_ended - record.time_started
+    time_ended - time_started
   end
 
 end
