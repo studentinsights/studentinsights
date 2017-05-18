deeb9e248ed6cbf549dcc782301756b74939673e
Alex Soble <asoble@gmail.com>
2017-05-18 10:21:35 -0500

    Handle imports that did not complete; show difference of time in words

--- app/models/import_record.rb
+++ app/models/import_record.rb
@@ -1,2 +1,11 @@
 class ImportRecord < ActiveRecord::Base
+
+  def completed?
+    record.time_ended.present?
+  end
+
+  def time_to_complete
+    record.time_ended - record.time_started
+  end
+
 end
