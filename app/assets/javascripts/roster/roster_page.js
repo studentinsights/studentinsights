b0013e22f339adfd38ee4c6b0d650faf6cc40a4c
Alex Soble <asoble@gmail.com>
2017-06-02 05:00:38 -0400

    Keep the jQuery code for navigation between homerooms
    + It's not super-horrific code and it's separate from the main thread of this work which is the homeroom roster table

--- app/assets/javascripts/roster/roster_page.js
+++ app/assets/javascripts/roster/roster_page.js
@@ -2,10 +2,9 @@ $(function() {
 
   if ($('body').hasClass('homerooms') && $('body').hasClass('show')) {
 
-    // Initialize table sort on roster table
-    // $('#homeroom-select').bind('change', function() {
-    //   window.location.pathname = '/homerooms/' + $(this).val();
-    // });
+    $('#homeroom-select').bind('change', function() {
+      window.location.pathname = '/homerooms/' + $(this).val();
+    });
 
   }
 
