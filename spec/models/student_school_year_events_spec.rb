d987791a98d24aa376d811bfdb3de40b361a923e
Alex Soble <asoble@gmail.com>
2017-06-16 06:51:57 -0500

    Test the days

--- spec/models/student_school_year_events_spec.rb
+++ spec/models/student_school_year_events_spec.rb
@@ -18,12 +18,14 @@ RSpec.describe StudentSchoolYearEvents, type: :model do
   describe '#filtered_absences' do
     let(:filter_from_date) { DateTime.new(2016, 8, 1) }
     let(:filter_to_date) { DateTime.new(2016, 8, 22) }
+
     let(:filtered) {
       subject.filtered_absences(filter_from_date, filter_to_date)
     }
 
     it 'filters absences correctly' do
       expect(filtered.count).to eq 2
+      expect(filtered.map { |f| f.occurred_at.day }).to eq [20, 15]
     end
 
   end
