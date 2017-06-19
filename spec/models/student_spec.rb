efd65ae3450cd346ad15a923a4ae517f6b79cadd
Alex Soble <asoble@gmail.com>
2017-06-18 19:17:37 -0500

    Remove spec for method that's gone

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -195,46 +195,6 @@ RSpec.describe Student do
     end
   end
 
-  describe '#find_student_school_years' do
-    context 'school years in the 2010s' do
-      let!(:sy_2014_2015) { FactoryGirl.create(:sy_2014_2015) }
-      let!(:sy_2013_2014) { FactoryGirl.create(:sy_2013_2014) }
-      let!(:sy_2012_2013) { FactoryGirl.create(:sy_2012_2013) }
-
-      context 'student has registration date' do
-        let!(:student) { FactoryGirl.create(:student_who_registered_in_2013_2014) }
-        it 'calculates student school years based on registration date' do
-          Timecop.freeze(Time.new(2015, 5, 24)) do
-            expect(student.find_student_school_years).to eq([sy_2014_2015, sy_2013_2014])
-          end
-        end
-      end
-
-      context 'student has numerical grade level (1 through 12)' do
-        context 'student is in 2nd grade' do
-          let(:student) { FactoryGirl.create(:second_grade_student) }
-          it 'assumes student has been attending SPS since K' do
-            Timecop.freeze(Time.new(2015, 5, 24)) do
-              expect(student.find_student_school_years).to eq([sy_2014_2015, sy_2013_2014, sy_2012_2013])
-            end
-          end
-        end
-      end
-
-      context 'student has non-numerical grade level' do
-        context 'student grade level is PK' do
-          let(:student) { FactoryGirl.create(:pre_k_student) }
-          it 'assumes student is in 1st year of SPS' do
-            Timecop.freeze(Time.new(2015, 5, 24)) do
-              expect(student.find_student_school_years).to eq([sy_2014_2015])
-            end
-          end
-        end
-      end
-
-    end
-  end
-
   describe '#update_recent_student_assessments' do
     context 'has student assessments' do
       let(:student) { FactoryGirl.create(:student_with_mcas_math_advanced_and_star_math_warning_assessments) }
