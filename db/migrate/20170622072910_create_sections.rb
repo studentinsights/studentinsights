9d86f4085ce83be1b7ccdbf8de4d1a23e5772692
Justin Hildebrandt <jhilde@gmail.com>
2017-07-06 12:05:16 -0400

    Adding a basic UI for courses and sections to test the import of data.

--- db/migrate/20170622072910_create_sections.rb
+++ db/migrate/20170622072910_create_sections.rb
@@ -1,7 +1,10 @@
 class CreateSections < ActiveRecord::Migration[5.0]
   def change
     create_table :sections do |t|
-      t.string    :local_id
+      t.string    :section_number
+      t.string    :term_local_id
+      t.string    :schedule
+      t.string    :room_number
       t.datetime  :created_at
       t.datetime  :updated_at
       t.integer   :school_id
