fb605463b6843ebf92a7b2390afe832656ab8e23
Alex Soble <asoble@gmail.com>
2017-07-12 14:25:16 -0500

    Log student local ID before attempting to create IEP record in db

--- app/jobs/iep_pdf_import_job.rb
+++ app/jobs/iep_pdf_import_job.rb
@@ -72,6 +72,8 @@ class IepPdfImportJob
     end
 
     records.map do |record|
+      log "storing iep for student ##{record[:local_id]} to db..."
+
       student = Student.find_by_local_id!(record[:local_id])
 
       IepDocument.create!(
