caf803c2ceab106944c4ed9e74c41e122a0f8c6f
Justin Hildebrandt <jhilde@gmail.com>
2017-07-12 11:39:59 -0400

    Changing terminology on models to course_number and section_number rather than local_id.

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,5 @@
 class Course < ActiveRecord::Base
-  validates :local_id, presence: true, uniqueness: { scope: [:local_id, :school_id] }
+  validates :course_number, presence: true, uniqueness: { scope: [:course_number, :school_id] }
   validates :school, presence: true
   has_many :sections
   belongs_to :school
--- app/models/section.rb
+++ app/models/section.rb
@@ -1,5 +1,5 @@
 class Section < ActiveRecord::Base
-  validates :local_id, presence: true, uniqueness: { scope: [:local_id, :course_id, :school_id] }
+  validates :section_number, presence: true, uniqueness: { scope: [:section_number, :course_id, :school_id, :term_local_id] }
   validates :school, presence: true
   validates :course, presence: true
   belongs_to :school
