c0c7d2dc96f3a5e6730d7824ec0f5e68796a5d8d
Justin Hildebrandt <jhilde@gmail.com>
2017-07-12 11:41:20 -0400

    Adding importers for courses and sections. Currently same row separated into two related objects.

---
+++ app/importers/file_importers/courses_sections_importer.rb
@@ -0,0 +1,26 @@
+class CoursesSectionsImporter < Struct.new :school_scope, :client, :log, :progress_bar
+
+  def remote_file_name
+    'courses_sections_export.txt'
+  end
+
+  def data_transformer
+    CsvTransformer.new
+  end
+
+  def filter
+    SchoolFilter.new(['HEA', 'WSNS', 'ESCS', 'BRN', 'KDY', 'AFAS', 'WHCS', 'SHS'])
+  end
+
+  def school_ids_dictionary
+    @dictionary ||= School.all.map { |school| [school.local_id, school.id] }.to_h
+  end
+
+  def import_row(row)
+    course = CourseRow.new(row, school_ids_dictionary).build
+    course.save!
+    section = SectionRow.new(row, school_ids_dictionary, course.id).build
+    section.save
+  end
+
+end
