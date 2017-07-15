a6db4d80ea1144207b7f72cf6bd0b8c6c8be0601
Justin Hildebrandt <jhilde@gmail.com>
2017-07-14 20:28:20 -0400

    Adding db and model for Student Section Assignment.

---
+++ db/migrate/20170715002437_create_educator_section_assignments.rb
@@ -0,0 +1,8 @@
+class CreateEducatorSectionAssignments < ActiveRecord::Migration[5.0]
+  def change
+    create_table :educator_section_assignments, id: false do |t|
+      t.belongs_to :section, index: true, foreign_key: true
+      t.belongs_to :educator, index: true, foreign_key: true
+    end
+  end
+end
