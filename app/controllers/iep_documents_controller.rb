94aca7589dbd221d3cd152ad0c264b4092b1dba5
Alex Soble <asoble@gmail.com>
2017-07-17 09:27:20 -0500

    Sketch out how we'll handle authorization and non-test bucket/objects

--- app/controllers/iep_documents_controller.rb
+++ app/controllers/iep_documents_controller.rb
@@ -1,17 +1,34 @@
 class IepDocumentsController < ApplicationController
 
+  before_action :find_iep_document, :authorize!
+
   def show
+    @file_name = @iep_document.file_name
+    @bucket_name = ENV['AWS_S3_IEP_BUCKET']
+
     object = s3.get_object({
-      bucket: 'test-test-mc-testing',
-      key: 'newspaper-article.pdf'
+      bucket: @bucket_name,
+      key: @file_name
     })
 
-    pdf = object.body.read
-
-    send_data pdf, filename: "eeeeeee.pdf", type: :pdf
+    send_data object.body.read, filename: @file_name, type: :pdf
   end
 
   private
+
+    def find_iep_document
+      @iep_document = IepDocument.find_by_id!(params[:id])
+    end
+
+    def student
+      @iep_document.student
+    end
+
+    def authorize!
+      unless current_educator.is_authorized_for_student(student)
+        raise Exceptions::EducatorNotAuthorized
+      end
+    end
 
     def s3
       @client ||= Aws::S3::Client.new
