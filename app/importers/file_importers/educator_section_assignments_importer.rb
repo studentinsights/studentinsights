88c3fa98661d208efc7fa4c49ac30a552f475c2b
Justin Hildebrandt <jhilde@gmail.com>
2017-07-25 20:17:28 -0400

    Hardening educator section assignments to better handle nulls or missing educator or sections.

--- app/importers/file_importers/educator_section_assignments_importer.rb
+++ app/importers/file_importers/educator_section_assignments_importer.rb
@@ -8,13 +8,17 @@ class EducatorSectionAssignmentsImporter < Struct.new :school_scope, :client, :l
     CsvTransformer.new
   end
 
+  def filter
+    SchoolFilter.new(school_scope)
+  end
+
   def delete_rows()
     EducatorSectionAssignment.delete_all
   end
 
   def import_row(row)
     educator_section_assignment = EducatorSectionAssignmentRow.new(row).build
-    educator_section_assignment.save!
+    educator_section_assignment.save! if educator_section_assignment
   end
 
 end
