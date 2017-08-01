5a4ebd8c408cb6c3a97f4ed14f4e720e69b7f265
Alex Soble <asoble@gmail.com>
2017-08-01 13:37:32 -0500

    Add some more progress notes along the way

--- new_district.sh
+++ new_district.sh
@@ -28,6 +28,11 @@ echo "Pushing code to Heroku app..."
 git remote add $HEROKU_GIT_REMOTE_NAME $HEROKU_GIT_REMOTE_ADDRESS
 git push $HEROKU_GIT_REMOTE_NAME master
 
+echo "Migrating the database..."
+
 heroku run rake db:migrate --app $HEROKU_APP_NAME
 
+echo "Setting config vars..."
+
 heroku config:set DISTRICT_NAME="$DISTRICT_NAME" --app $HEROKU_APP_NAME
+
