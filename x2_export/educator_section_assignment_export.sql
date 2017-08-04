a78e579664dd0160a1d5f9defcd138dd70d779cc
Justin Hildebrandt <jhilde@gmail.com>
2017-08-04 18:16:31 -0400

    Modifying queries to add inner joins for school and staff / students. There seem to be some records with nulls. Advancing school year to 2018.

--- x2_export/educator_section_assignment_export.sql
+++ x2_export/educator_section_assignment_export.sql
@@ -19,11 +19,11 @@ LEFT JOIN schedule_master
   ON schedule.SCH_OID = schedule_master.MST_SCH_OID
 LEFT JOIN course_school
   ON schedule_master.MST_CSK_OID = course_school.CSK_OID
-LEFT JOIN school
+INNER JOIN school
   ON course_school.CSK_SKL_OID = school.SKL_OID
-LEFT JOIN staff
+INNER JOIN staff
   ON schedule_master.MST_STF_OID_PRIMARY = staff.STF_OID
-AND CTX_SCHOOL_YEAR=2017
+AND CTX_SCHOOL_YEAR=2018
   INTO OUTFILE "E:/_BACKUP_MYSQL/CodeForAmerica/educator_section_assignment_export.txt"
   FIELDS TERMINATED BY ','
   ENCLOSED BY '"'
--- x2_export/student_section_assignment_export.sql
+++ x2_export/student_section_assignment_export.sql
@@ -19,13 +19,13 @@ LEFT JOIN schedule_master
   ON schedule.SCH_OID = schedule_master.MST_SCH_OID
 LEFT JOIN course_school
   ON schedule_master.MST_CSK_OID = course_school.CSK_OID
-LEFT JOIN school
+INNER JOIN school
   ON course_school.CSK_SKL_OID = school.SKL_OID
 INNER JOIN student_schedule
   ON schedule_master.MST_OID = student_schedule.SSC_MST_OID
 INNER JOIN student
   ON student_schedule.SSC_STD_OID = student.STD_OID
-AND CTX_SCHOOL_YEAR=2017
+AND CTX_SCHOOL_YEAR=2018
   INTO OUTFILE "E:/_BACKUP_MYSQL/CodeForAmerica/student_section_assignment_export.txt"
   FIELDS TERMINATED BY ','
   ENCLOSED BY '"'
