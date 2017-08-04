67e28d4bad251a55936aacc14209da468eda7d7d
Justin Hildebrandt <jhilde@gmail.com>
2017-08-04 18:00:15 -0400

    Updating query to remove courses where school is NULL. For some reason these are present in X2's database.

--- x2_export/courses_sections_export.sql
+++ x2_export/courses_sections_export.sql
@@ -23,7 +23,7 @@ LEFT JOIN schedule_master
   ON schedule.SCH_OID = schedule_master.MST_SCH_OID
 LEFT JOIN course_school
   ON schedule_master.MST_CSK_OID = course_school.CSK_OID
-LEFT JOIN school
+INNER JOIN school
   ON course_school.CSK_SKL_OID = school.SKL_OID
 AND CTX_SCHOOL_YEAR=2017
   INTO OUTFILE "E:/_BACKUP_MYSQL/CodeForAmerica/courses_sections_export.txt"
