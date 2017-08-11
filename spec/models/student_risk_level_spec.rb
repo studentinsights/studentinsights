119b88ca761202894732b3b751f68633cf56a495
Alex Soble <asoble@gmail.com>
2017-08-11 14:11:14 -0500

    Remove a bunch of calls to decorate on the spec side

--- spec/models/student_risk_level_spec.rb
+++ spec/models/student_risk_level_spec.rb
@@ -101,7 +101,7 @@ RSpec.describe StudentRiskLevel, type: :model do
     context 'missing MCAS and STAR results' do
       context 'not limited English' do
         let(:student) { FactoryGirl.create(:student) }
-        let(:student_risk_level) { StudentRiskLevel.create!(student: student).decorate }
+        let(:student_risk_level) { StudentRiskLevel.create!(student: student) }
         it 'has an explanation' do
           expect(student_risk_level.explanation).to eq "This student is at Risk N/A because:<br/><br/><ul><li>There is not enough information to tell.</li></ul>"
         end
@@ -109,7 +109,7 @@ RSpec.describe StudentRiskLevel, type: :model do
 
       context 'limited english' do
         let(:student) { FactoryGirl.create(:limited_english_student) }
-        let(:student_risk_level) { StudentRiskLevel.create!(student: student).decorate }
+        let(:student_risk_level) { StudentRiskLevel.create!(student: student) }
         it 'has a correct explanation' do
           expect(student_risk_level.explanation).to eq "This student is at Risk 3 because:<br/><br/><ul><li>This student is limited English proficient.</li></ul>"
         end
@@ -120,7 +120,7 @@ RSpec.describe StudentRiskLevel, type: :model do
       context 'has MCAS math but not MCAS ela' do
         context 'has a W value for MCAS math' do
           let(:student) { FactoryGirl.create(:student_with_mcas_math_warning_assessment) }
-          let!(:student_risk_level) { StudentRiskLevel.create!(student: student).decorate }
+          let!(:student_risk_level) { StudentRiskLevel.create!(student: student) }
           it 'has a correct explanation' do
             correct_explanation = "This student is at Risk 3 because:<br/><br/><ul><li>This student's MCAS Math performance level is Warning.</li></ul>"
             expect(student_risk_level.explanation).to eq correct_explanation
@@ -133,7 +133,7 @@ RSpec.describe StudentRiskLevel, type: :model do
       context 'has STAR math but not STAR reading' do
         context 'STAR math is between 30 and 85' do
           let(:student) { FactoryGirl.create(:student_with_star_assessment_between_30_85) }
-          let!(:student_risk_level) { StudentRiskLevel.create!(student: student).decorate }
+          let!(:student_risk_level) { StudentRiskLevel.create!(student: student) }
           it 'has a correct explanation' do
             correct_explanation = "This student is at Risk 1 because:<br/><br/><ul><li>This student's STAR Math performance is above 30.</li></ul>"
             expect(student_risk_level.explanation).to eq correct_explanation
@@ -145,7 +145,7 @@ RSpec.describe StudentRiskLevel, type: :model do
     context 'has both MCAS and STAR results' do
       context 'MCAS is advanced but STAR is warning' do
         let(:student) { FactoryGirl.create(:student_with_mcas_math_advanced_and_star_math_warning_assessments) }
-        let!(:student_risk_level) { StudentRiskLevel.create!(student: student).decorate }
+        let!(:student_risk_level) { StudentRiskLevel.create!(student: student) }
         it 'has a correct explanation' do
           correct_explanation = "This student is at Risk 3 because:<br/><br/><ul><li>This student's STAR Math performance is in the warning range (below 10).</li></ul>"
           expect(student_risk_level.explanation).to eq correct_explanation
