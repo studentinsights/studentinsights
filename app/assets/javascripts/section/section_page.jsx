d9c6975ea0ff92a80e2932c2bbfe20bc6ab91755
Justin Hildebrandt <jhilde@gmail.com>
2017-08-26 15:56:31 -0400

    Implementing roster custom sorts for relevant columns

--- app/assets/javascripts/section/section_page.jsx
+++ app/assets/javascripts/section/section_page.jsx
@@ -1,5 +1,6 @@
 import Roster from '../components/roster.jsx';
 import SectionHeader from './section_header.jsx';
+import SortHelpers from '../helpers/sort_helpers.jsx';
 
 (function(root) {
   window.shared || (window.shared = {});
@@ -24,38 +25,50 @@ import SectionHeader from './section_header.jsx';
         </a>
       );
     },
+    
+    programSorter: function(a, b, sortBy) {
+      return SortHelpers.sortByCustomEnum(a,b,sortBy,['Reg Ed', '2Way English', '2Way Spanish', 'Sp Ed'])
+    },
+
+    languageProficiencySorter: function(a, b, sortBy) {
+      return SortHelpers.sortByCustomEnum(a,b,sortBy,['FLEP-Transitioning', 'FLEP', 'Fluent'])
+    },
+
+    mcasPerformanceSorter: function(a, b, sortBy) {
+      return SortHelpers.sortByCustomEnum(a,b,sortBy,['A','P','NI','W','F'])
+    },
 
     render: function() {
       const columns = [
         {label: 'Name', key: 'first_name', cell:this.styleStudentName},
-        {label: 'Program Assigned', key: 'program_assigned'},
+        {label: 'Program Assigned', key: 'program_assigned', sortFunc: this.programSorter},
         
         // SPED & Disability
         {label: 'Disability', group: 'SPED & Disability', key: 'disability'},
         {label: '504 Plan', group: 'SPED & Disability', key: 'plan_504'},
         
         // Language
-        {label: 'Fluency', group: 'Language', key: 'limited_english_proficiency'},
+        {label: 'Fluency', group: 'Language', key: 'limited_english_proficiency', sortFunc: this.languageProficiencySorter},
         {label: 'Home Language', group: 'Language', key: 'home_language'},
 
         {label: 'Free / Reduced Lunch', key: 'free_reduced_lunch'},
-        {label: 'Absences', key: 'most_recent_school_year_absences_count'},
-        {label: 'Tardies', key: 'most_recent_school_year_tardies_count'},
-        {label: 'Discipline Incidents', key: 'most_recent_school_year_discipline_incidents_count'},
+        {label: 'Absences', key: 'most_recent_school_year_absences_count', sortFunc: SortHelpers.sortByNumber},
+        {label: 'Tardies', key: 'most_recent_school_year_tardies_count', sortFunc: SortHelpers.sortByNumber},
+        {label: 'Discipline Incidents', key: 'most_recent_school_year_discipline_incidents_count', sortFunc: SortHelpers.sortByNumber},
         
         // STAR: Math
-        {label: 'Percentile', group: 'STAR: Math', key:'most_recent_star_math_percentile'},
+        {label: 'Percentile', group: 'STAR: Math', key:'most_recent_star_math_percentile', sortFunc: SortHelpers.sortByNumber},
 
         // STAR: Reading
-        {label: 'Percentile', group: 'STAR: Reading', key:'most_recent_star_reading_percentile'},
+        {label: 'Percentile', group: 'STAR: Reading', key:'most_recent_star_reading_percentile', sortFunc: SortHelpers.sortByNumber},
         
         // MCAS: Math
-        {label: 'Performance', group: 'MCAS: Math', key:'most_recent_mcas_math_performance'},
-        {label: 'Score', group: 'MCAS: Math', key:'most_recent_mcas_math_scaled'},
+        {label: 'Performance', group: 'MCAS: Math', key:'most_recent_mcas_math_performance', sortFunc: this.mcasPerformanceSorter},
+        {label: 'Score', group: 'MCAS: Math', key:'most_recent_mcas_math_scaled', sortFunc: SortHelpers.sortByNumber},
         
         // MCAS: ELA
-        {label: 'Performance', group: 'MCAS: ELA', key:'most_recent_mcas_ela_performance'},
-        {label: 'Score', group: 'MCAS: ELA', key:'most_recent_mcas_ela_scaled'}
+        {label: 'Performance', group: 'MCAS: ELA', key:'most_recent_mcas_ela_performance', sortFunc: this.mcasPerformanceSorter},
+        {label: 'Score', group: 'MCAS: ELA', key:'most_recent_mcas_ela_scaled', sortFunc: SortHelpers.sortByNumber}
       ];
       
       return (
@@ -70,7 +83,7 @@ import SectionHeader from './section_header.jsx';
             <Roster
               rows={this.props.students}
               columns={columns}
-              initialSort='first_name'/>
+              initialSortIndex={0}/>
           </div>
         </div>
       );
