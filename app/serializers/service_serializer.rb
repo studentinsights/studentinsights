dddc39c038a4b63ef79bed0812b172af23394736
Alex Soble <asoble@gmail.com>
2017-08-30 15:01:30 -0500

    Move over methods

--- app/serializers/service_serializer.rb
+++ app/serializers/service_serializer.rb
@@ -1,2 +1,27 @@
 class Service < Struct.new :service
+
+  def serialize_service
+    discontinued_service = service.discontinued_services.order(:recorded_at).last
+    service.as_json.symbolize_keys.slice(*[
+      :id,
+      :student_id,
+      :provided_by_educator_name,
+      :service_type_id,
+      :date_started,
+      :recorded_by_educator_id,
+      :recorded_at
+    ]).merge({
+      discontinued_by_educator_id: discontinued_service.try(:recorded_by_educator_id),
+      discontinued_recorded_at: discontinued_service.try(:recorded_at)
+    })
+  end
+
+  def self.service_types_index
+    index = {}
+    ServiceType.all.each do |service_type|
+      index[service_type.id] = service_type.as_json.symbolize_keys.slice(:id, :name)
+    end
+    index
+  end
+
 end
