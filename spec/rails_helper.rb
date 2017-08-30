8e8fe36d8901a585c01d0c7a482aa72afd219807
Alex Soble <asoble@gmail.com>
2017-08-30 16:55:06 -0500

    All specs should be run with Time Zone ==> Eastern Time
    + My local machine is usually on Central Time but our specs expect values of Time.now to equal results in Eastern Time, make sure test environment always thinks it's Eastern Time to keep everything consistent

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,8 @@ SimpleCov.start
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
+ENV['TZ'] ||= 'America/New_York'
+
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
