403c00e196c95c44f1521d17248c979d8e181122
Alex Soble <asoble@gmail.com>
2017-08-30 13:40:17 -0500

    Pull methods about serializing event notes into one class

--- app/helpers/serialize_data_helper.rb
+++ app/helpers/serialize_data_helper.rb
@@ -15,39 +15,6 @@ module SerializeDataHelper
     })
   end
 
-  def serialize_event_note(event_note)
-    attachments = event_note.event_note_attachments.map do |event_note_attachment|
-      event_note_attachment.as_json.symbolize_keys.slice(:id, :url)
-    end
-    serialize_event_note_without_attachments(event_note).merge({
-      attachments: attachments
-    })
-  end
-
-  def serialize_event_note_without_attachments(event_note)
-    event_note.as_json(include: :event_note_revisions).symbolize_keys.slice(*[
-      :id,
-      :student_id,
-      :educator_id,
-      :event_note_type_id,
-      :text,
-      :recorded_at,
-      :is_restricted,
-      :event_note_revisions
-    ])
-  end
-
-  def serialize_event_note_for_school_overview(event_note)
-    event_note.as_json.symbolize_keys.slice(*[
-      :id,
-      :student_id,
-      :educator_id,
-      :event_note_type_id,
-      :recorded_at,
-      :is_restricted,
-    ])
-  end
-
   # deprecated
   def serialize_intervention(intervention)
     {
@@ -72,11 +39,4 @@ module SerializeDataHelper
     index
   end
 
-  def event_note_types_index
-    index = {}
-    EventNoteType.all.each do |event_note_type|
-      index[event_note_type.id] = event_note_type.as_json.symbolize_keys.slice(:id, :name)
-    end
-    index
-  end
 end
