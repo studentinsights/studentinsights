b5b4e0b982831405ce0b0adfaf90371c2a76623b
Alex Soble <asoble@gmail.com>
2017-08-30 21:22:50 -0500

    Write unit tests for saving the correct student searchbar JSON

--- spec/models/educator_spec.rb
+++ spec/models/educator_spec.rb
@@ -239,13 +239,24 @@ RSpec.describe Educator do
 
   describe '#save_student_searchbar_json' do
     context 'educator has permissions for a few students' do
-      it 'saves the correct JSON' do
+      let(:school) { FactoryGirl.create(:school, local_id: 'Big River High') }
+      let!(:betsy) { FactoryGirl.create(:student, first_name: 'Betsy', last_name: 'Ramirez', school: school, grade: '3') }
+      let!(:bettina) { FactoryGirl.create(:student, first_name: 'Bettina', last_name: 'Abbas', school: school, grade: '3') }
+      let(:educator) { FactoryGirl.create(:educator, districtwide_access: true) }
 
+      it 'saves the correct JSON' do
+        educator.save_student_searchbar_json
+        expect(educator.student_searchbar_json).to eq(
+          "[{\"label\":\"Betsy Ramirez - Big River High - 3\",\"id\":#{betsy.id}},{\"label\":\"Bettina Abbas - Big River High - 3\",\"id\":#{bettina.id}}]"
+        )
       end
     end
     context 'educator has permissions for no students' do
-      it 'saves the correct JSON' do
+      let(:educator) { FactoryGirl.create(:educator) }
 
+      it 'saves the correct JSON' do
+        educator.save_student_searchbar_json
+        expect(educator.student_searchbar_json).to eq("[]")
       end
     end
   end
