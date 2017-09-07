05d08280e083b3a4e9417c2ca53705fbdedc482f
Judy Ngai <judyngai@Judys-MacBook-Pro.local>
2017-09-07 11:23:18 -0400

    remove will

--- app/assets/javascripts/student_profile/record_service.jsx
+++ app/assets/javascripts/student_profile/record_service.jsx
@@ -193,7 +193,7 @@
               minDate: undefined
             }} />
           <div style={{ marginTop: 20 }}>
-            When did/will they end?
+            When did they end?
           </div>
           <Datepicker
             styles={{ input: styles.datepickerInput }}
