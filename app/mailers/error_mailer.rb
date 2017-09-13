f25f392d775258f8995ae7b50a497177ca07f2a7
Alex Soble <asoble@gmail.com>
2017-09-13 11:46:09 +0300

    Send more detailed info about data import timing
    + So that we can debug #1023

--- app/mailers/error_mailer.rb
+++ app/mailers/error_mailer.rb
@@ -1,10 +1,11 @@
 class ErrorMailer < ActionMailer::Base
 
-  def error_report(error)
+  def error_report(error, extra_info = nil)
     @target_emails = ENV['ERROR_MAILER_LIST'].split(',')
     @subject = "ERROR: Student Insights ErrorMailer"
     @error_message = error.message
     @error_backtrace = error.backtrace
+    @extra_info = extra_info
 
     mail(
       to: @target_emails,
