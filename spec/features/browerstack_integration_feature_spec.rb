62568e1dded85e5c4d0b47dd9d2f9074e68d58c3
Alex Soble <asoble@gmail.com>
2017-09-21 15:38:12 -0500

    Remove dots

--- spec/features/browerstack_integration_feature_spec.rb
+++ spec/features/browerstack_integration_feature_spec.rb
@@ -6,7 +6,6 @@ caps = Selenium::WebDriver::Remote::Capabilities.new
 caps['browserstack.local'] = 'true'
 caps['browserstack.localIdentifier'] = ENV['BROWSERSTACK_LOCAL_IDENTIFIER']
 # Add other capabilities like browser name, version and os name, version
-...
 
 driver = Selenium::WebDriver.for(:remote,
   :url => "http://alexsoble:#{ENV['BROWSERSTACK_ACCESS_KEY']}@hub-cloud.browserstack.com/wd/hub",
