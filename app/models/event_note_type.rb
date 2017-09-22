d80e6afc61602336235b073e63cc9ae1e9a40a43
Alex Soble <asoble@gmail.com>
2017-09-22 10:21:23 -0500

    Add migration to add 10th Grade Experience Team type
    + #1061

--- app/models/event_note_type.rb
+++ app/models/event_note_type.rb
@@ -1,3 +1,4 @@
+
 class EventNoteType < ActiveRecord::Base
   def self.seed_somerville_event_note_types
     EventNoteType.destroy_all
@@ -6,7 +7,8 @@ class EventNoteType < ActiveRecord::Base
       { id: 301, name: 'MTSS Meeting' },
       { id: 302, name: 'Parent conversation' },
       { id: 304, name: 'Something else' },
-      { id: 305, name: '9th Grade Experience' }
+      { id: 305, name: '9th Grade Experience' },
+      { id: 306, name: '10th Grade Experience' },
     ])
   end
 end
