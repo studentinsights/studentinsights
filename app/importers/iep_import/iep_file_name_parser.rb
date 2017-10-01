6e03ce9321118f758f3d273a4af0ae5d1cf674cf
Alex Soble <asoble@gmail.com>
2017-10-01 15:59:47 -0500

    Remove file date parsing

--- app/importers/iep_import/iep_file_name_parser.rb
+++ app/importers/iep_import/iep_file_name_parser.rb
@@ -8,10 +8,6 @@ class IepFileNameParser < Struct.new :path, :zip_filename
     path.split("/").last
   end
 
-  def file_date
-    Date.strptime(date_zip_basename, '%m-%d-%Y').to_s
-  end
-
   def check_iep_at_a_glance
     raise 'oh no!' if pdf_basename.split('_')[1] != 'IEPAtAGlance'
   end
@@ -20,10 +16,6 @@ class IepFileNameParser < Struct.new :path, :zip_filename
 
     def pdf_basename
       Pathname.new(path).basename.sub_ext('').to_s
-    end
-
-    def date_zip_basename
-      Pathname.new(zip_filename).basename.sub_ext('').to_s
     end
 
 end
