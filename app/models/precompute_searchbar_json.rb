3224df2e8331dda7022ba8313bd9936ff05c059a
Alex Soble <asoble@gmail.com>
2017-10-02 13:24:08 -0500

    Call #to_json

--- app/models/precompute_searchbar_json.rb
+++ app/models/precompute_searchbar_json.rb
@@ -10,7 +10,7 @@ class PrecomputeSearchbarJson
 
   def self.for(educator)
     if educator.districtwide_access
-      educator.student_searchbar_json = districtwide_access_json
+      educator.student_searchbar_json = districtwide_access_json.to_json
     else
       educator.student_searchbar_json = SearchbarHelper.names_for(educator).to_json
     end
