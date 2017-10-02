154a34bee76c00d36c4001154e019a1513928efc
Alex Soble <asoble@gmail.com>
2017-10-02 12:48:16 -0500

    Pull searchbar precomputing into a service object; only precomptue districtwide JSON once
    + #1121

--- app/helpers/searchbar_helper.rb
+++ app/helpers/searchbar_helper.rb
@@ -11,4 +11,13 @@ module SearchbarHelper
       }
     end
   end
+
+  def self.names_for_all_students
+    Student.with_school.map do |student|
+      {
+        label: "#{student.first_name} #{student.last_name} - #{student.school.local_id} - #{student.grade}",
+        id: student.id
+      }
+    end
+  end
 end
---
+++ app/models/precompute_searchbar_json.rb
@@ -0,0 +1,29 @@
+class PrecomputeSearchbarJson
+
+  def self.for_all_educators
+    Educator.find_each { |educator| save_student_searchbar_json(educator) }
+  end
+
+  def self.for_educators_who_log_in
+    educators_who_log_in.find_each { |educator| save_student_searchbar_json(educator) }
+  end
+
+  def educators_who_log_in
+    Educator.where("sign_in_count > ?", 0)
+  end
+
+  def districtwide_access_json
+    @json_for_all ||= SearchbarHelper.names_for_all_students
+  end
+
+  def save_student_searchbar_json(educator)
+    if educator.districtwide_access
+      educator.student_searchbar_json = districtwide_access_json
+    else
+      educator.student_searchbar_json = SearchbarHelper.names_for(educator).to_json
+    end
+
+    educator.save!
+  end
+
+end
