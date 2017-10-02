8e401757e35dbbab0b1dd8c528d5c1f0aed7aba4
Alex Soble <asoble@gmail.com>
2017-10-02 13:01:12 -0500

    Limit searchbar precomputing to educator's school
    + Fix #1120

--- app/helpers/searchbar_helper.rb
+++ app/helpers/searchbar_helper.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 module SearchbarHelper
   def self.names_for(educator)
-    authorized_students = Student.with_school.select do |student|
+    authorized_students = educator.school.students.select do |student|
       educator.is_authorized_for_student(student)
     end
+
     authorized_students.map do |student|
       {
         label: "#{student.first_name} #{student.last_name} - #{student.school.local_id} - #{student.grade}",
