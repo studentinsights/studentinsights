97958889d3044013c6e87f74f80aaf94648de1da
Alex Soble <asoble@gmail.com>
2017-10-02 13:19:58 -0500

    Move related methods over to new spec

--- spec/models/educator_spec.rb
+++ spec/models/educator_spec.rb
@@ -249,29 +249,6 @@ RSpec.describe Educator do
     end
   end
 
-  describe '#save_student_searchbar_json' do
-    context 'educator has permissions for a few students' do
-      let(:school) { FactoryGirl.create(:school, local_id: 'Big River High') }
-      let!(:betsy) { FactoryGirl.create(:student, first_name: 'Betsy', last_name: 'Ramirez', school: school, grade: '3') }
-      let!(:bettina) { FactoryGirl.create(:student, first_name: 'Bettina', last_name: 'Abbas', school: school, grade: '3') }
-      let(:educator) { FactoryGirl.create(:educator, districtwide_access: true) }
-
-      it 'saves the correct JSON' do
-        educator.save_student_searchbar_json
-        expect(educator.student_searchbar_json).to eq(
-          "[{\"label\":\"Betsy Ramirez - Big River High - 3\",\"id\":#{betsy.id}},{\"label\":\"Bettina Abbas - Big River High - 3\",\"id\":#{bettina.id}}]"
-        )
-      end
-    end
-    context 'educator has permissions for no students' do
-      let(:educator) { FactoryGirl.create(:educator) }
-
-      it 'saves the correct JSON' do
-        educator.save_student_searchbar_json
-        expect(educator.student_searchbar_json).to eq("[]")
-      end
-    end
-  end
   describe '#allowed_sections' do
     let!(:school) { FactoryGirl.create(:healey) }
     let!(:other_school) { FactoryGirl.create(:brown) }
