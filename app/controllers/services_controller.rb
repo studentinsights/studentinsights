c36a65bccbf5145f183d6f9b9f9da7fd2d76a0ed
Judy Ngai <judyngai@Judys-MacBook-Pro.local>
2017-10-05 13:16:53 -0400

    simplify permitted params and quick fixes to render

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -12,11 +12,7 @@ class ServicesController < ApplicationController
     service_id = params[:id]
     if Service.find(service_id).date_started > Time.now
       Service.find(service_id).destroy
-      respond_to do |format|
-        format.json { head :no_content }
-      end
-      #format.json { head :no_content }
-      #redirect_back(fallback_location: root_path)
+      format.json { head :no_content }
     else
       discontinued_service = DiscontinuedService.new({
         service_id: service_id,
@@ -25,9 +21,11 @@ class ServicesController < ApplicationController
       })
 
       serializer = ServiceSerializer.new(Service.find(service_id))
-      discontinued_service.save
-      render json: serializer.serialize_service
-      #render json: { errors: discontinued_service.errors.full_messages }, status: 422
+      if discontinued_service.save
+        render json: serializer.serialize_service
+      else
+        render json: { errors: discontinued_service.errors.full_messages }, status: 422
+      end
     end
   end
 end
