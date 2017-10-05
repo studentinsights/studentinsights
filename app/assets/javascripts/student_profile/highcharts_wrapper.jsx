762916c7ea3ebcd09fdee167cbd5c53a924e5505
EricDS <edavidsonsawyer@gmail.com>
2017-10-04 22:01:41 -0400

    restoring original highcharts wrapper

--- app/assets/javascripts/student_profile/highcharts_wrapper.jsx
+++ app/assets/javascripts/student_profile/highcharts_wrapper.jsx
@@ -1,29 +1,30 @@
+(function() {
+  window.shared || (window.shared = {});
 
-window.shared || (window.shared = {});
+  // This component wraps Highcharts so that you can provide Highcharts
+  // options as props, and it bridges calling into the library.
+  window.shared.HighchartsWrapper = React.createClass({
+    displayName: 'HighchartsWrapper',
 
-// This component wraps Highcharts so that you can provide Highcharts
-// options as props, and it bridges calling into the library.
-export default React.createClass({
-  displayName: 'HighchartsWrapper',
+    componentDidMount: function(props, state) {
+      $(this._chartEl).highcharts(this.props);
+    },
 
-  componentDidMount: function(props, state) {
-    $(this._chartEl).highcharts(this.props);
-  },
+    componentWillUnmount: function(props, state) {
+      delete this._chartEl;
+    },
 
-  componentWillUnmount: function(props, state) {
-    delete this._chartEl;
-  },
+    onRefForChart: function(el) {
+      this._chartEl = el;
+    },
 
-  onRefForChart: function(el) {
-    this._chartEl = el;
-  },
+    render: function() {
+      return (
+        <div className="HighchartsWrapper">
+          <div ref={this.onRefForChart} />
+        </div>
+      );
+    },
 
-  render: function() {
-    return (
-      <div className="HighchartsWrapper">
-        <div ref={this.onRefForChart} />
-      </div>
-    );
-  },
-
-});
+  });
+})();
