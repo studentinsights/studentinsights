0930e81ed0c81345a67d02b42d9138efc6d8cda3
Alex Soble <asoble@gmail.com>
2017-10-06 14:21:46 -0500

    Make sure one importer blowing up doesn't stop the whole train
    + Fix #1068

--- app/importers/file_importers/file_import.rb
+++ app/importers/file_importers/file_import.rb
@@ -4,10 +4,14 @@ class FileImport < Struct.new :file_importer
     :import_row, :filter, :progress_bar, :delete_rows, to: :file_importer
 
   def import
-    log_start_of_import
-    fetch_data
-    import_data
-    delete_data if deletion_models.include?(file_importer.class)
+    begin
+      log_start_of_import
+      fetch_data
+      import_data
+      delete_data if deletion_models.include?(file_importer.class)
+    rescue =>
+      ErrorMailer.error_report(error, nil).deliver_now if Rails.env.production?
+    end
   end
 
   private
