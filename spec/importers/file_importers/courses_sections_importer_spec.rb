7a52223e785ab25b37713ac313a31ee419c5f148
Justin Hildebrandt <jhilde@gmail.com>
2017-10-17 21:15:37 -0400

    Updating one importer spec to check for proper import_record_detail logging.

--- spec/importers/file_importers/courses_sections_importer_spec.rb
+++ spec/importers/file_importers/courses_sections_importer_spec.rb
@@ -15,9 +15,11 @@ RSpec.describe CoursesSectionsImporter do
                     section_schedule:'3(M-R)',
                     section_room_number:'232B'
                 } }
+      let!(:import_record_detail) {FactoryGirl.create(:import_record_detail)}
 
         before do
-          described_class.new.import_row(row)
+          importer = described_class.new(nil, nil, log, nil, import_record_detail)
+          importer.import_row(row)
         end
 
         it 'creates a course' do
@@ -31,7 +33,62 @@ RSpec.describe CoursesSectionsImporter do
         it 'creates a section of the course' do
           expect(Section.first.course).to eq(Course.first)
         end
+
+        it 'logs one row processed, one row created' do
+          expected_statuses = [
+            "processed": 1,
+            "excluded": 0,
+            "created": 1,
+            "updated": 0,
+            "deleted": 0,
+            "rejected": 0
+          ]
+          expect(import_record_detail.rows_summary).to match_array(expected_statuses)
+        end
       end
+
+      context 'updates correctly' do
+        let!(:section) {FactoryGirl.create(:section)}
+        let(:row) { { course_number:section.course_number,
+                      course_description:section.course_description,
+                      section_number:section.section_number,
+                      school_local_id:section.course.school.local_id,
+                      term_local_id:section.term_local_id,
+                      section_schedule:section.schedule,
+                      section_room_number:'NEW ROOM'
+                  } }
+        let!(:import_record_detail) {FactoryGirl.create(:import_record_detail)}
+
+          before do
+            importer = described_class.new(nil, nil, log, nil, import_record_detail)
+            importer.import_row(row)
+          end
+
+          it 'leaves the course' do
+            expect(Course.count).to eq(1)
+          end
+
+          it 'leaves the section' do
+            expect(Section.count).to eq(1)
+          end
+
+          it 'updates the section' do
+            section.reload
+            expect(section.room_number).to eq("NEW ROOM")
+          end
+
+          it 'logs one row processed, one row updated' do
+            expected_statuses = [
+              "processed": 1,
+              "excluded": 0,
+              "created": 0,
+              "updated": 1,
+              "deleted": 0,
+              "rejected": 0
+            ]
+            expect(import_record_detail.rows_summary).to match_array(expected_statuses)
+          end
+        end
 
       context 'missing school_local_id' do
         let(:row) { { course_number:'ART-205',
@@ -41,9 +98,10 @@ RSpec.describe CoursesSectionsImporter do
                     section_schedule:'3(M-R)',
                     section_room_number:'232B'
                 } }
+        let!(:import_record_detail) {FactoryGirl.create(:import_record_detail)}
 
         before do
-          importer = described_class.new(nil, nil, log, nil)
+          importer = described_class.new(nil, nil, log, nil, import_record_detail)
           importer.import_row(row)
         end
 
@@ -53,6 +111,18 @@ RSpec.describe CoursesSectionsImporter do
 
         it 'does not create a section' do
           expect(Section.count).to eq(0)
+        end
+
+        it 'logs one row processed, one row created' do
+          expected_statuses = [
+            "processed": 1,
+            "excluded": 0,
+            "created": 0,
+            "updated": 0,
+            "deleted": 0,
+            "rejected": 1
+          ]
+          expect(import_record_detail.rows_summary).to match_array(expected_statuses)
         end
       end
     end
