3e5930778a420cf9a6952934a83a25261795d358
Alex Soble <asoble@gmail.com>
2017-10-20 11:19:21 -0500

    Add migration to detect and upgrade Next Generation MCAS records

---
+++ db/migrate/20171020155409_migrate_next_gen_mcas.rb
@@ -0,0 +1,33 @@
+class MigrateNextGenMcas < ActiveRecord::Migration[5.1]
+  def change
+    return if Assessment.find_by(family: 'Next Gen MCAS')
+
+    puts "⚙  ⚙  ⚙  Making new assessment models for Next Gen..."
+    next_gen_mcas_math = Assessment.create!(family: 'Next Gen MCAS', subject: 'Mathematics')
+    next_gen_mcas_ela = Assessment.create!(family: 'Next Gen MCAS', subject: 'ELA')
+
+    return if StudentAssessment.count == 0  # Nothing to migrate here!
+
+    puts "⚙  ⚙  ⚙  Looking up old assessment models for MCAS..."
+    mcas_math = Assessment.find_by(family: 'MCAS', subject: 'Mathematics')
+    mcas_ela = Assessment.find_by(family: 'MCAS', subject: 'ELA')
+
+    [mcas_math, mcas_ela].each do |assessment|
+      subject = assessment.subject
+      new_assessment_id = Assessment.find_by(family: 'Next Gen MCAS', subject: subject).id
+
+      puts "detecting and migrating Next Gen MCAS for #{subject}..."; puts;
+
+      student_assessments = assessment.student_assessments
+
+      student_assessments.map do |student_assessment|
+        if student_assessment.scale_score && student_assessment.scale_score > 399
+          student_assessment.assessment_id = new_assessment_id
+          student_assessment.save
+        end
+      end
+
+      puts "checked #{student_assessments.size} student_assessments..."; puts;
+    end
+  end
+end
