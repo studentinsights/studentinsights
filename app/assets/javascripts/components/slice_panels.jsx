4dec5dc4dc6c11d9f6f62e059501fd02d172f132
Alex Soble <asoble@gmail.com>
2017-10-23 13:41:05 -0500

    Use median MCAS scale score instead of mean to make decisions about Slice Panels
    + Weighted average of scores isn't really important here; median is a better way of asking, "are most students on the Next Gen or the old MCAS scales?"

--- app/assets/javascripts/components/slice_panels.jsx
+++ app/assets/javascripts/components/slice_panels.jsx
@@ -108,10 +108,10 @@ import _ from 'lodash';
         this.createItem('Advanced', Filters.Range(key, [260, 281])),
       ];
 
-      const mcasScaledScores = _.compact(_.map(this.props.students, key));
-      const meanScaledScore = _.sum(mcasScaledScores) / mcasScaledScores.length;
+      const sortedScores = _.map(this.props.students, key).sort();
+      const medianScore = sortedScores[Math.floor(sortedScores.length / 2)];
 
-      if (meanScaledScore > 280) return nullItem.concat(nextGenMCASFilters, oldMCASFilters);
+      if (medianScore > 280) return nullItem.concat(nextGenMCASFilters, oldMCASFilters);
 
       return nullItem.concat(oldMCASFilters, nextGenMCASFilters);
     },
