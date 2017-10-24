1f22552b4d9b6a3015200aba395f18590bd303ef
Alex Soble <asoble@gmail.com>
2017-10-24 12:23:11 -0500

    Improve logging in migration

--- db/migrate/20171020155409_migrate_next_gen_mcas.rb
+++ db/migrate/20171020155409_migrate_next_gen_mcas.rb
@@ -2,23 +2,29 @@ class MigrateNextGenMcas < ActiveRecord::Migration[5.1]
   def change
     return if Assessment.find_by(family: 'Next Gen MCAS')
 
-    puts "⚙  ⚙  ⚙  Making new assessment models for Next Gen..."
+    puts "⚙  ⚙  ⚙  Making new assessment models for Next Gen..."; puts;
+
     next_gen_mcas_math = Assessment.create!(family: 'Next Gen MCAS', subject: 'Mathematics')
+
     next_gen_mcas_ela = Assessment.create!(family: 'Next Gen MCAS', subject: 'ELA')
 
     return if StudentAssessment.count == 0  # Nothing to migrate here!
 
-    puts "⚙  ⚙  ⚙  Looking up old assessment models for MCAS..."
+    puts "⚙  ⚙  ⚙  Looking up old assessment models for MCAS..."; puts;
+
     mcas_math = Assessment.find_by(family: 'MCAS', subject: 'Mathematics')
+
     mcas_ela = Assessment.find_by(family: 'MCAS', subject: 'ELA')
 
     [mcas_math, mcas_ela].each do |assessment|
       subject = assessment.subject
       new_assessment_id = Assessment.find_by(family: 'Next Gen MCAS', subject: subject).id
 
-      puts "detecting and migrating Next Gen MCAS for #{subject}..."; puts;
+      puts "Detecting and migrating Next Gen MCAS for #{subject}..."; puts;
 
       student_assessments = assessment.student_assessments
+
+      puts "Found #{student_assessments.size} student assessments..."; puts;
 
       student_assessments.map do |student_assessment|
         if student_assessment.scale_score && student_assessment.scale_score > 399
@@ -27,7 +33,9 @@ class MigrateNextGenMcas < ActiveRecord::Migration[5.1]
         end
       end
 
-      puts "checked #{student_assessments.size} student_assessments..."; puts;
+      migrated_student_assessments = StudentAssessment.where(assessment_id: new_assessment_id)
+
+      puts "Migrated #{migrated_student_assessments.size} student assessments..."; puts;
     end
   end
 end
