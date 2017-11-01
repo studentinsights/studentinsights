f6cdfc0a8f4340e65d680d41df18be86b578eeff
Justin Hildebrandt <jhilde@gmail.com>
2017-11-01 09:00:27 -0400

    Changing name of var from schoolContext to plain old school.

--- app/assets/javascripts/helpers/customization_helpers.js
+++ app/assets/javascripts/helpers/customization_helpers.js
@@ -1,13 +1,13 @@
-function shouldDisplay(element, schoolContext) {
+function shouldDisplay(element, school) {
   switch(element) {
   case 'house':
-    if (schoolContext && schoolContext.local_id) {
-      return "SHS" === schoolContext.local_id;
+    if (school && school.local_id) {
+      return "SHS" === school.local_id;
     }
     return false;
   case 'counselor': 
-    if (schoolContext && schoolContext.school_type) {
-      return "HS" === schoolContext.school_type;
+    if (school && school.school_type) {
+      return "HS" === school.school_type;
     }
     return false;
   default:
