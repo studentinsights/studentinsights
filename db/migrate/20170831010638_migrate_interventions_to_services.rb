49f6cddbba18a1195f4cde2563a446822a18b4e7
Alex Soble <asoble@gmail.com>
2017-11-20 12:58:04 -0500

    Improve the data migration
    + Wrap in a transaction
    + Do much more printing/logging to the command line so we can see what's going on and what's working

--- db/migrate/20170831010638_migrate_interventions_to_services.rb
+++ db/migrate/20170831010638_migrate_interventions_to_services.rb
@@ -5,16 +5,32 @@ class MigrateInterventionsToServices < ActiveRecord::Migration[5.0]
     # Only run this migration with Somerville
     return unless district_name == 'Somerville'
 
-    # Don't run the migration if we don't have a Jill ID lookup configured
-    return unless ENV['JILL_ID'].present?
-
     # Don't run if there are no deprecated Interventions to migrate
     return if Intervention.count == 0
+
+    # Don't run the migration if we don't have a Jill ID lookup configured
+    raise 'No Jill ID!' unless ENV['JILL_ID'].present?
 
     Intervention.all.each do |intervention|
       InterventionMigrationHelper.new(intervention).migrate
     end
 
-    Intervention.destroy_all
+    ActiveRecord::Base.transaction do
+      puts "Found #{Intervention.count} deprecated intervention records!"; puts
+      puts "Found #{Service.count} service records!"; puts
+      puts "Migrating interventions to services..."; puts
+
+      Intervention.all.each do |intervention|
+        InterventionMigrationHelper.new(intervention).migrate
+        print '.'
+      end
+
+      puts "Found #{Intervention.count} deprecated intervention records!"; puts
+      puts "Found #{Service.count} service records!"; puts
+
+      puts "Destroying deprecated intervention records ..."
+
+      Intervention.destroy_all
+    end
   end
 end
