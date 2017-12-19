22a47badf1db30a991ab60c561f5070e0cf25813
Alex Soble <asoble@gmail.com>
2017-12-19 10:15:35 -0600

    Unwrap at the initialize level

--- config/initializers/district_config.rb
+++ config/initializers/district_config.rb
@@ -1,3 +1,6 @@
-config_data = YAML.load(File.open("district-config.yml"))
+district_key = ENV.fetch('DISTRICT_KEY')
+
+config_data = YAML.load(File.open("config/district_#{district_key}.yml"))
+                  .fetch("remote_filenames")
 
 DistrictConfig.get_remote_filenames_from_config(config_data)
