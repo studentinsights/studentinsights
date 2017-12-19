31bc0361defa251ace30570c8eac513e1c59e74f
Alex Soble <asoble@gmail.com>
2017-12-19 10:45:46 -0600

    Simplify, since we're reading from a hash now

--- app/config_objects/district_config.rb
+++ app/config_objects/district_config.rb
@@ -1,59 +1,45 @@
 class DistrictConfig
 
-  cattr_reader :remote_filenames
+  cattr_accessor :remote_filenames
 
-  ### CONFIGURING FILE NAMES FOR REMOTE DATA IMPORT ###
+  def self.set_remote_filenames
+    remote_filenames = self.remote_filenames_from_yml
 
-  # To configure a Student Insights instance to read data from remote SFTP,
-  # set filenames for the 11 expected remote files in district-config.yml.
-  # These are the keys we expect under `remote_filenames`:
+    self.validate_remote_filenames(remote_filenames)
 
-  def self.expected_remote_filename_keys
+    self.remote_filenames = remote_filenames_from_yml
+  end
+
+  def self.district_key
+    ENV.fetch('DISTRICT_KEY')
+  end
+
+  def self.remote_filenames_from_yml
+    YAML.load(File.open("config/district_#{self.district_key}.yml"))
+        .fetch("config")
+        .fetch("remote_filenames")
+  end
+
+  def self.expected_keys
     [
       'FILENAME_FOR_STUDENTS_IMPORT',
       'FILENAME_FOR_EDUCATORS_IMPORT',
       'FILENAME_FOR_BEHAVIOR_IMPORT',
       'FILENAME_FOR_ASSESSMENT_IMPORT',
       'FILENAME_FOR_ATTENDANCE_IMPORT',
-      'FILENAME_FOR_STAR_READING_IMPORT',
-      'FILENAME_FOR_STAR_MATH_IMPORT',
       'FILENAME_FOR_STUDENT_AVERAGES_IMPORT',
       'FILENAME_FOR_COURSE_SECTION_IMPORT',
       'FILENAME_FOR_STUDENTS_SECTION_ASSIGNMENT_IMPORT',
       'FILENAME_FOR_EDUCATOR_SECTION_ASSIGNMENT_IMPORT',
+      'FILENAME_FOR_STAR_READING_IMPORT',
+      'FILENAME_FOR_STAR_MATH_IMPORT',
     ]
   end
 
-  def self.get_remote_filenames_from_config(config_source)
-    @@remote_filenames = {}
-
-    self.expected_remote_filename_keys.each do |config_key|
-      filename = self.fetch_import_filename(config_source, config_key)
-
-      @@remote_filenames[config_key] = filename
+  def self.validate_remote_filenames(remote_filenames)
+    if remote_filenames.keys.sort != self.expected_keys.sort
+      raise "Unexpected keys!"
     end
-
-    @@remote_filenames.freeze
-  end
-
-  def self.fetch_import_filename(config_source, var_name)
-    config_source.fetch(var_name) { |name| self.handle_unset_import_filename(name) }
-  end
-
-  def self.handle_unset_import_filename(missing_name)
-    raise "Unset configuration: #{missing_name}" if Rails.env.production?
-
-    # In `Rails.env.development`, we don't need to set all these variables
-    # because they're only needed in a few very specific use cases.
-
-    # The two development use cases that would need these variables are:
-      # (1) test production data import process
-      # (2) import production data to local environment to test something locally
-
-    # In `Rails.env.test`, we use fixture .txt files and mocks to simulate imported
-    # data, so these variables don't need to be set.
-
-    return nil
   end
 
 end
--- config/initializers/district_config.rb
+++ config/initializers/district_config.rb
@@ -1,6 +1,1 @@
-district_key = ENV.fetch('DISTRICT_KEY')
-
-config_data = YAML.load(File.open("config/district_#{district_key}.yml"))
-                  .fetch("remote_filenames")
-
-DistrictConfig.get_remote_filenames_from_config(config_data)
+DistrictConfig.set_remote_filenames
