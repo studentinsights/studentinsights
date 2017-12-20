b7023cddd75576221c585b5894a16a361b2455d0
Alex Soble <asoble@gmail.com>
2017-12-20 13:17:29 -0600

    Convert service color to ES6 module

---
+++ app/assets/javascripts/student_profile/service_color.js
@@ -0,0 +1,20 @@
+export default function serviceColor(serviceTypeId) {
+  const Colors = {
+    purple: '#e8e9fc',
+    orange: '#ffe7d6',
+    green: '#e8fce8',
+    gray: '#eee'
+  };
+
+  const map = {
+    507: Colors.orange,
+    502: Colors.green,
+    503: Colors.green,
+    504: Colors.green,
+    505: Colors.gray,
+    506: Colors.gray,
+    508: Colors.purple
+  };
+
+  return map[serviceTypeId] || null;
+};
