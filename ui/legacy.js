6bb32ad55a59e3e460cc0fc76a22373e7b5b7d02
Alex Soble <asoble@gmail.com>
2017-12-20 13:51:35 -0600

    Convert Educator JS to ES6 module

--- ui/legacy.js
+++ ui/legacy.js
@@ -13,7 +13,6 @@ import '../app/assets/javascripts/student_profile/risk_bubble.jsx';
 import '../app/assets/javascripts/student_profile/service_color.jsx';
 import '../app/assets/javascripts/student_profile/provided_by_educator_dropdown.jsx';
 import '../app/assets/javascripts/student_profile/profile_chart_settings.jsx';
-import '../app/assets/javascripts/student_profile/educator.jsx';
 import '../app/assets/javascripts/student_profile/datepicker.jsx';
 import '../app/assets/javascripts/student_profile/highcharts_wrapper.jsx';
 import '../app/assets/javascripts/student_profile/bar_chart_sparkline.jsx';
