2f570df84f1a2b9c391e06f66cf40b5d4915bd39
Alex Soble <asoble@gmail.com>
2017-12-26 12:05:00 -0800

    Test minimum valid options

--- spec/importers/import_task_spec.rb
+++ spec/importers/import_task_spec.rb
@@ -2,4 +2,24 @@ require 'rails_helper'
 
 RSpec.describe ImportTask do
 
+  let(:minimum_valid_options) {
+    {
+      'district' => 'somerville',
+    }
+  }
+
+  describe '#initialize' do
+    context 'with valid options provided' do
+      it 'does not raise an error; supplies the correct defaults' do
+        expect { ImportTask.new(options: minimum_valid_options) }.not_to raise_error
+      end
+    end
+
+    context 'with no options provided' do
+      it 'does not raise an error; supplies the correct defaults' do
+        expect { ImportTask.new(options: {}) }.to raise_error KeyError
+      end
+    end
+  end
+
 end
