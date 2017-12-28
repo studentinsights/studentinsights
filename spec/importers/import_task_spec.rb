6a26c874221a719b86e8ccf39b069f5995cb33fc
Alex Soble <asoble@gmail.com>
2017-12-27 18:36:44 -0800

    Remove spec coverage of district option that doesn't make sense anymore
    + We removed district as a configurable option a couple of commits ago (we're trusting ENV as our only source of truth on this), so the specs that were testing required district configuration don't make sense anymore

--- spec/importers/import_task_spec.rb
+++ spec/importers/import_task_spec.rb
@@ -2,29 +2,8 @@ require 'rails_helper'
 
 RSpec.describe ImportTask do
 
-  let(:minimum_valid_options) {
-    {
-      'district' => 'somerville',
-      'test_mode' => true
-    }
-  }
-
-  describe '#initialize' do
-    context 'with valid options provided' do
-      it 'does not raise an error; supplies the correct defaults' do
-        expect { ImportTask.new(options: minimum_valid_options) }.not_to raise_error
-      end
-    end
-
-    context 'with no options provided' do
-      it 'does not raise an error; supplies the correct defaults' do
-        expect { ImportTask.new(options: {}) }.to raise_error KeyError
-      end
-    end
-  end
-
   describe '#connect_transform_import' do
-    let(:task) { ImportTask.new(options: minimum_valid_options) }
+    let(:task) { ImportTask.new(options: {'test_mode' => true}) }
 
     it 'doesn\'t blow up (smoke test)' do
       task.connect_transform_import
