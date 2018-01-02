ca93bfd86d3d17a44f5ebbcee9557f83879ab89e
Alex Soble <asoble@gmail.com>
2018-01-02 12:46:52 -0600

    Add code comment explaining :created_at will be in Rails format

---
+++ spec/javascripts/service_uploads/PastServiceUploads.test.js
@@ -0,0 +1,32 @@
+import ReactDOM from 'react-dom';
+import PastServiceUploads from '../../../app/assets/javascripts/service_uploads/PastServiceUploads';
+
+describe('null serviceUploads', function () {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+
+    ReactDOM.render(
+      <PastServiceUploads
+        serviceUploads={null}
+        onClickDeleteServiceUpload={jest.fn()}
+      />, div);
+  });
+});
+
+describe('serviceUploads', function () {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+
+    ReactDOM.render(
+      <PastServiceUploads
+        serviceUploads={[
+          {
+            file_name: 'test_file.csv',
+            created_at: 'Tue, 02 Jan 2018 15:48:53 UTC +00:00', // Rails created_at
+            services: [],
+          }
+        ]}
+        onClickDeleteServiceUpload={jest.fn()}
+      />, div);
+  });
+});
