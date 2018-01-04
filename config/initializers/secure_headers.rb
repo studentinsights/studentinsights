b9faa8cf455f51fe0768722cf843821e375e3da1
Alex Soble <asoble@gmail.com>
2018-01-04 17:06:40 -0600

    Opt out of CSP rules in test/development envs

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -1,5 +1,13 @@
-if Rails.env.production?
-  SecureHeaders::Configuration.default do |config|
-    config.x_download_options = nil
+SecureHeaders::Configuration.default do |config|
+
+  # Unblock PDF downloading for student report:
+  config.x_download_options = nil
+
+  # Turn off Content Security Policy (CSP) rules for development and test envs,
+  # in dev this breaks JS because we don't send JS over https in development;
+  # same with test mode.
+  if Rails.env.test? || Rails.env.development?
+    config.csp = SecureHeaders::OPT_OUT
   end
+
 end
