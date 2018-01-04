7e12ddd0b02046bed98f431bbb08f975475ee757
Alex Soble <asoble@gmail.com>
2018-01-04 17:22:02 -0600

    Try to unblock PDF downloading
    + Refused to load plugin data from 'https://somerville-teacher-tool-demo.herokuapp.com/students/2/student_report.pdf?sections=notes,services,attendance,discipline_incidents,assessments&from_date=08/15/2016&to_date=01/04/2018' because it violates the following Content Security Policy directive: "object-src 'none'".

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -2,6 +2,11 @@ SecureHeaders::Configuration.default do |config|
 
   # Unblock PDF downloading for student report
   config.x_download_options = nil
+  config.csp = {
+    default_src: %w('self' https:),
+    script_src: %w(https:),
+    object_src: %w('self')
+  }
 
   # Turn off Content Security Policy (CSP) rules for development and test envs
   if Rails.env.test? || Rails.env.development?
