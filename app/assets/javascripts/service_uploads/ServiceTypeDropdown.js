8c655bfa4c8f2136ea3f9a9017a11cbd9112246f
Alex Soble <asoble@gmail.com>
2018-01-03 19:05:50 -0600

    Bind toggleOpenMenu in constructor

--- app/assets/javascripts/service_uploads/ServiceTypeDropdown.js
+++ app/assets/javascripts/service_uploads/ServiceTypeDropdown.js
@@ -2,6 +2,12 @@ import React from 'react';
 
 class ServiceTypeDropdown extends React.Component {
 
+  constructor(props) {
+    super(props);
+
+    this.toggleOpenMenu = this.toggleOpenMenu.bind(this);
+  }
+
   componentDidMount() {
     const self = this;
 
