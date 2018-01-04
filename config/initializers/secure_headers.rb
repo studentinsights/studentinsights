522a139d9347425e2b357ad900f1e317ba4c3307
Alex Soble <asoble@gmail.com>
2018-01-04 17:08:28 -0600

    Trim the code comments

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -1,11 +1,9 @@
 SecureHeaders::Configuration.default do |config|
 
-  # Unblock PDF downloading for student report:
+  # Unblock PDF downloading for student report
   config.x_download_options = nil
 
-  # Turn off Content Security Policy (CSP) rules for development and test envs,
-  # in dev this breaks JS because we don't send JS over https in development;
-  # same with test mode.
+  # Turn off Content Security Policy (CSP) rules for development and test envs
   if Rails.env.test? || Rails.env.development?
     config.csp = SecureHeaders::OPT_OUT
   end
