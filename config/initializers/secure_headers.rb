a540ae6b71412fe84e7556b88e7e3cda75be95a4
Alex Soble <asoble@gmail.com>
2018-01-05 11:43:05 -0600

    Opt out of CSP for now
    + Blocks Rollbar and Mixpanel from running inline

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -3,22 +3,11 @@ SecureHeaders::Configuration.default do |config|
   # Unblock PDF downloading for student report
   config.x_download_options = nil
 
-  config.csp = {
-    default_src: %w('self' https:),  # This is the same as Configuration.default.
-                                     # SecureHeaders requires a non-nil default_src csp.
-
-    script_src: %w(https:),          # This is the same as Configuration.default.
-                                     # SecureHeaders requires a non-nil script_src csp.
-
-    object_src: %w('self'),          # This is more lenient than Configuration.default.
-                                     # This enables viewing the Student Report PDF.
-
-    plugin_types: %w(application/pdf)
-  }
+  # Opt out of CSP; these block Rollbar and Mixpanel in production
+  config.csp = SecureHeaders::OPT_OUT
 
   # Turn off Content Security Policy (CSP) rules for development and test envs
   if Rails.env.test? || Rails.env.development?
-    config.csp = SecureHeaders::OPT_OUT
     config.cookies = SecureHeaders::OPT_OUT
   end
 
