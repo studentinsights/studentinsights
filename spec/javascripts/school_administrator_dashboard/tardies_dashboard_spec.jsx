80652788edd7c5143e2a6b5fa57e1828b436b1a4
EricDS <edavidsonsawyer@gmail.com>
2018-01-07 10:30:52 -0500

    adding smoke test and resolving conflict

---
+++ spec/javascripts/school_administrator_dashboard/tardies_dashboard_spec.jsx
@@ -0,0 +1,20 @@
+import ReactDom from 'react-dom';
+import SchoolTardiesDashboard from '../../../app/assets/javascripts/school_administrator_dashboard/dashboard_components/tardies_dashboard/school_tardies_dashboard.jsx';
+
+describe('data', () => {
+  it('renders successfully', () => {
+    const div = document.createElement('div');
+
+    //This can be made into a functional test once the data flow through the dashboards are finalized
+    const schoolTardyEvents = {};
+    const homeroomTardyEvents = {};
+    const dashboardStudents = [];
+
+    ReactDom.render(
+      <SchoolTardiesDashboard
+        schoolTardyEvents = {schoolTardyEvents}
+        homeroomTardyEvents = {homeroomTardyEvents}
+        dashboardStudents = {dashboardStudents}
+      />, div);
+  });
+});
