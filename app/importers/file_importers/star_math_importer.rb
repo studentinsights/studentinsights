61f62654aee6021cf71658487b4be7d2fc332d2b
Alex Soble <asoble@gmail.com>
2018-01-16 10:02:45 -0600

    Add more config to store each district's STAR zip file name

--- app/importers/file_importers/star_math_importer.rb
+++ app/importers/file_importers/star_math_importer.rb
@@ -4,13 +4,17 @@ class StarMathImporter < Struct.new :school_scope, :client, :log, :progress_bar
     return unless remote_file_name
 
     @data = CsvDownloader.new(
-      log: log, remote_file_name: remote_file_name, client: client, transformer: data_transformer
+      log: log, remote_file_name: zip_file_name, client: client, transformer: data_transformer
     ).get_data
 
     @data.each.each_with_index do |row, index|
       import_row(row) if filter.include?(row)
       ProgressBar.new(log, remote_file_name, @data.size, index + 1).print if progress_bar
     end
+  end
+
+  def zip_file_name
+    LoadDistrictConfig.new.remote_filenames.fetch('FILENAME_FOR_STAR_ZIP_FILE')
   end
 
   def remote_file_name
