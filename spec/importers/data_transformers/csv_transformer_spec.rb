cbb09a3cb131d07dbf731abbfb543e19e6b6fd2e
Alex Soble <asoble@gmail.com>
2018-01-19 14:27:59 -0600

    Return File.read, not File.open, in the specs

--- spec/importers/data_transformers/csv_transformer_spec.rb
+++ spec/importers/data_transformers/csv_transformer_spec.rb
@@ -5,7 +5,9 @@ RSpec.describe CsvTransformer do
   describe '#transform' do
     context 'with good data' do
       context 'headers in csv' do
-        let!(:file) { File.open("#{Rails.root}/spec/fixtures/fake_behavior_export.txt") }
+        # Return a File.read string just like the CsvDownloader class does:
+        let!(:file) { File.read("#{Rails.root}/spec/fixtures/fake_behavior_export.txt") }
+
         let(:headers_symbols) {[:local_id,:school_local_id,:assessment_date,:assessment_scale_score,:assessment_performance_level,:assessment_growth,:assessment_name,:assessment_subject,:assessment_test]}
         let(:transformer) { CsvTransformer.new }
         let(:output) { transformer.transform(file) }
@@ -17,7 +19,9 @@ RSpec.describe CsvTransformer do
         end
       end
       context 'headers not in csv' do
-        let!(:file) { File.open("#{Rails.root}/spec/fixtures/fake_no_headers.csv") }
+        # Return a File.read string just like the CsvDownloader class does:
+        let!(:file) { File.read("#{Rails.root}/spec/fixtures/fake_no_headers.csv") }
+
         let(:headers) {["section_number","student_local_id","school_local_id","course_number","term_local_id","grade"]}
         let(:headers_symbols) {[:section_number,:student_local_id,:school_local_id,:course_number,:term_local_id,:grade]}
         let(:transformer) { CsvTransformer.new(headers:headers) }
