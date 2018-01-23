ed5404cdfc88f70a00d7083a7ec58164e50da5be
Alex Soble <asoble@gmail.com>
2018-01-23 17:12:42 -0600

    Sketch out controller code

--- app/controllers/service_types_controller.rb
+++ app/controllers/service_types_controller.rb
@@ -7,4 +7,18 @@ class ServiceTypesController < ApplicationController
     render json: ServiceType.pluck(:name).sort
   end
 
+  def is_service_working
+    attendance_officer = ServiceType.find(502)
+    student_ids = attendance_officer.services.map(&:student_ids)
+
+    student_names_and_chart_data = student_ids.map |id|
+      student = Student.find(id)
+
+      {
+        name: "#{student.first_name} #{student.last_name}",
+        chart_data: StudentProfileChart.new(student).chart_data
+      }
+    end
+  end
+
 end
