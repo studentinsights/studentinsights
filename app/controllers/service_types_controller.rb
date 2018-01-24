e81cd1459dc43d202e03b63894162fcacc7649f3
Alex Soble <asoble@gmail.com>
2018-01-23 18:23:41 -0600

    Add more auth

--- app/controllers/service_types_controller.rb
+++ app/controllers/service_types_controller.rb
@@ -3,6 +3,14 @@ class ServiceTypesController < ApplicationController
 
   # Authentication by default inherited from ApplicationController.
 
+  before_action :authorize_for_districtwide_access_admin, only: [:is_service_working]
+
+  def authorize_for_districtwide_access_admin
+    unless current_educator.admin? && current_educator.districtwide_access?
+      render json: { error: "You don't have the correct authorization." }
+    end
+  end
+
   def index
     render json: ServiceType.pluck(:name).sort
   end
