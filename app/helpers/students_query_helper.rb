b5c6de9315945b5f011f98b26e47c18b405f81fd
Alex Soble <asoble@gmail.com>
2018-02-02 16:13:52 -0600

    Use #select to pull fewer event note fields into school overview query

--- app/helpers/students_query_helper.rb
+++ app/helpers/students_query_helper.rb
@@ -1,6 +1,15 @@
 require 'digest'
 
 module StudentsQueryHelper
+  INCLUDE_FOR_EVENT_NOTES = [
+    :id,
+    :student_id,
+    :educator_id,
+    :event_note_type_id,
+    :recorded_at,
+    :is_restricted,
+  ]
+
   # Used to serializes a Student into a hash with other fields joined in (that are used to perform
   # filtering and slicing in the UI).
   # This may be slow if you're doing it for many students without eager includes.
@@ -38,7 +47,7 @@ module StudentsQueryHelper
   def mutable_fields_for_slicing(student_ids)
     summer_service_type_ids = ServiceType.where(summer_program: true).pluck(:id)
     {
-      all_event_notes: EventNote.where(student_id: student_ids),
+      all_event_notes: EventNote.where(student_id: student_ids).select(INCLUDE_FOR_EVENT_NOTES),
       all_active_services: Service.where(student_id: student_ids).active,
       all_interventions: Intervention.where(student_id: student_ids),
       all_summer_services: Service.where(student_id: student_ids)
