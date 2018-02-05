0701b929f765aa79109d3122be56520f18ea2aeb
Alex Soble <asoble@gmail.com>
2018-02-05 12:03:02 -0600

    Add new summary methods for inspecting exported assessments CSV

--- lib/analysis/analyze_assessments_table.rb
+++ lib/analysis/analyze_assessments_table.rb
@@ -24,6 +24,22 @@ class AnalyzeAssessmentsTable < Struct.new(:path)
     @parsed_csv ||= CSV.parse(contents, csv_options)
   end
 
+  def assessment_subject
+    data[:assessment_subject]
+  end
+
+  def assessment_subject_summary
+    assessment_subject.each_with_object(Hash.new(0)) { |subj, counts| counts[subj] += 1 }
+  end
+
+  def assessment_test
+    data[:assessment_test]
+  end
+
+  def assessment_test_summary
+    assessment_test.each_with_object(Hash.new(0)) { |subj, counts| counts[subj] += 1 }
+  end
+
   def nil_converter(value)
     value unless value == '\N'
   end
