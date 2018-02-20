9074396fdf627bcfad97057392cdf221836eb99f
Gabriela Voicu <gabrielavoicu16@gmail.com>
2018-02-19 19:14:54 -0600

    JS smoke test

---
+++ spec/javascripts/student_profile/NotesFeedPage.test.js
@@ -0,0 +1,14 @@
+import NotesFeedPage from '../../../app/assets/javascripts/notes_feed/NotesFeedPage.js';
+import SpecSugar from '../support/spec_sugar.jsx';
+
+SpecSugar.withTestEl('high-level integration tests', function(container) {
+  describe('NotesFeedPage', () => {
+    it('displays notes with student cards without crashing', () => {
+      const el = container.testEl;
+      window.ReactDOM.render(<NotesFeedPage
+        educatorsIndex={{}}
+        eventNotes={[]}
+        eventNoteTypesIndex={{}} />, el)
+    });
+  });
+});
