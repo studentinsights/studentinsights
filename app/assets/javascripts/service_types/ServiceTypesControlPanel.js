d9f7bcea243d85d139c0ece3f0d2dd88c58e1509
Alex Soble <asoble@gmail.com>
2018-02-20 11:26:20 -0600

    Sketch out UI following same model as Service Upload form

--- app/assets/javascripts/service_types/ServiceTypesControlPanel.js
+++ app/assets/javascripts/service_types/ServiceTypesControlPanel.js
@@ -2,6 +2,55 @@ import React from 'react';
 
 class ServiceTypesControlPanel extends React.Component {
 
+  renderServiceTypeDetail(serviceType) {
+    const boxStyle = {
+      marginTop: 10,
+      padding: 10,
+      fontFamily: "'Open Sans', sans-serif",
+      border: '1px solid #ccc'
+    };
+
+    const titleStyle = {
+      fontSize: 16
+    };
+
+    return (
+      <div style={boxStyle}>
+        <div style={titleStyle}>{serviceType.name}</div>
+      </div>
+    );
+  }
+
+  renderServiceTypes() {
+    const {serializedData} = this.props;
+    const {serviceTypes} = serializedData;
+
+    return serviceTypes.map(this.renderServiceTypeDetail);
+  }
+
+  renderNewServiceType() {
+    return (
+      <div>
+        <div style={{marginTop: 20}}>
+          Name
+        </div>
+        <input style={{fontSize: 18}} />
+        <div style={{marginTop: 20}}>
+          Educators can record in Student Profile?
+        </div>
+        <div>
+          <input type="radio" name="can_record_in_student_profile" /> Yes
+        </div>
+        <div>
+          <input type="radio" name="can_record_in_student_profile" /> No
+        </div>
+        <button className="btn" style={{fontSize: 18, marginTop: 20}}>
+          Add New Service Type
+        </button>
+      </div>
+    );
+  }
+
   render() {
     const panelOuterStyles = {
       width: '50%',
@@ -19,11 +68,13 @@ class ServiceTypesControlPanel extends React.Component {
         <div style={panelOuterStyles}>
           <div style={panelInnerStyles}>
             <h1>New Service Type</h1>
+            {this.renderNewServiceType()}
           </div>
         </div>
         <div style={panelOuterStyles}>
           <div style={panelInnerStyles}>
             <h1>Service Types</h1>
+            {this.renderServiceTypes()}
           </div>
         </div>
       </div>
