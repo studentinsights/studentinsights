073376cd93b2d6e5b3c9751cf381981daa528a50
Kevin Robinson <kevin.robinson.0@gmail.com>
2018-03-01 16:32:41 -0500

    Factor out IncidentCard from AttendanceDetails as a component

---
+++ app/assets/javascripts/components/IncidentCard.js
@@ -0,0 +1,86 @@
+import React from 'react';
+
+
+/*
+Shows a "card" of an incident that happened with a student.
+Note that this might contain sensitive data.
+*/
+class IncidentCard extends React.Component {
+  render() {
+    const {incident} = this.props;
+    return (
+      <div style={{paddingTop: 60}}>
+        <div style={styles.box} key={incident.id}>
+          <div style={styles.header}>
+            <div style={styles.item}>
+              <span style={styles.itemHead}>
+                {'Date: '}
+              </span>
+              <span>
+                {moment.utc(incident.occurred_at).format('MMM D, YYYY')}
+              </span>
+            </div>
+            <div style={styles.centerItem}>
+              <span style={styles.itemHead}>
+                {'Code: '}
+              </span>
+              <span>
+                {incident.incident_code}
+              </span>
+            </div>
+            <div style={styles.item}>
+              <span style={styles.itemHead}>
+                {'Location: '}
+              </span>
+              <span>
+                {incident.incident_location}
+              </span>
+            </div>
+          </div>
+          <div>
+            <span style={styles.desc}>
+              {'Description: '}
+            </span>
+          </div>
+          <div>
+            {incident.incident_description}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+const styles = {
+  box: {
+    border: '1px solid #ccc',
+    padding:15,
+    marginTop: 10,
+    marginBottom: 10,
+    width: '100%',
+    backgroundColor: '#f2f2f2'
+  },
+  item: {
+    paddingBottom: 10,
+    width: 160
+  },
+  itemHead: {
+    fontWeight: 'bold',
+  },
+  header: {
+    display: 'flex',
+    flexFlow: 'row',
+    justifyContent: 'space-between'
+  },
+  desc: {
+    fontWeight: 'bold',
+    paddingTop: 30
+  },
+  centerItem: {
+    paddingBottom: 10,
+    textAlign: 'center',
+    width: 75
+  }
+};
+
+export default IncidentCard;
