2c3b895a9820f31d3489a2512bdbec5d0bb1cbc5
Kevin Robinson <kevin.robinson.0@gmail.com>
2018-03-02 10:44:15 -0500

    Add controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,16 +1,13 @@
 class HomeController < ApplicationController
   def home
-    notes = EventNote.all
-    @serialized_data = 
-    {
-      educators_index: Educator.to_index,
-      event_note_types_index: EventNoteSerializer.event_note_types_index,
+    @serialized_data = {
       current_educator: current_educator
     }
     render 'shared/serialized_data'
   end
 
   def home_notes_json
+    notes = EventNote.all
     render json: {
       notes: notes.map {|event_note| EventNoteSerializer.new(event_note).serialize_event_note_with_student }
     }
