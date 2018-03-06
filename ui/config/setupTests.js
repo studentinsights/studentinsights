eec28d8b63ba5984ab0933edde85836f36fb876e
Kevin Robinson <kevin.robinson.0@gmail.com>
2018-03-06 13:27:18 -0500

    Add mocking for fetch in Jest

--- ui/config/setupTests.js
+++ ui/config/setupTests.js
@@ -1,7 +1,11 @@
 // This is a configuration file for Jest
 import './sprockets-shims.js';
 import '../legacy.js';
+
+// Enzyme support
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-15.4';
-
 Enzyme.configure({ adapter: new Adapter() });
+
+// https://github.com/jefflau/jest-fetch-mock
+global.fetch = require('jest-fetch-mock'); // eslint-disable-line no-undef
