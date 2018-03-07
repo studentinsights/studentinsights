622313fc0862916991a51eed7e3e9cef062a5df9
Alex Soble <asoble@gmail.com>
2018-03-07 17:04:54 -0600

    Test different mixpanel data contexts

--- spec/reports/research_matters_exporter_spec.rb
+++ spec/reports/research_matters_exporter_spec.rb
@@ -8,100 +8,158 @@ RSpec.describe ResearchMattersExporter do
   let!(:student) { FactoryGirl.create(:student, homeroom: homeroom, school: school) }
 
   class FakeMixpanelDownloader
-    def initialize(student_id)
-      @student_id = student_id
+    def initialize(event_data)
+      @event_data = event_data
     end
 
     def event_data
-      [{
-        "event"=>"PAGE_VISIT",
-        "properties"=>{
-          "time"=>1503905237,
-          "$current_url"=>"https://somerville-teacher-tool-demo.herokuapp.com/students/#{@student_id}",
-          "deployment_key"=>"production",
-          "educator_id"=>1,
-          "educator_is_admin"=>false,
-          "educator_school_id"=>1,
-          "isDemoSite"=>false,
-          "page_key"=>"STUDENT_PROFILE"
-        }
-      }]
+      @event_data
     end
   end
 
   let(:exporter) {
     described_class.new(options: {
-      mixpanel_downloader: FakeMixpanelDownloader.new('junk_id'),
+      mixpanel_downloader: FakeMixpanelDownloader.new(event_data),
       canonical_domain: 'somerville-teacher-tool-demo.herokuapp.com'
     })
   }
 
   describe '#student_file' do
-    context 'no notes' do
-      it 'outputs the right file' do
+    context 'no mixpanel page views' do
+      let(:event_data) {
+        [{
+          "event"=>"PAGE_VISIT",
+          "properties"=>{
+            "time"=>1503905237,
+            "$current_url"=>"https://somerville-teacher-tool-demo.herokuapp.com/students/junk_id",
+            "deployment_key"=>"production",
+            "educator_id"=>1,
+            "educator_is_admin"=>false,
+            "educator_school_id"=>1,
+            "isDemoSite"=>false,
+            "page_key"=>"STUDENT_PROFILE"
+          }
+        }]
+      }
+
+      context 'no notes' do
+        it 'outputs the right file' do
+          expect(exporter.student_file).to eq([
+            "student_id,school_id,absence_indicator,discipline_indicator,sst_indicator,notes_added,notes_revised,notes_total,educator_id,educator_count,pageview_count",
+            "#{student.id},HEA,0,0,0,0,0,0,#{educator.id},1,0"
+          ])
+        end
+      end
+
+      context 'notes during the focal period' do
+        let!(:event_note) {
+          FactoryGirl.create(:event_note, student: student, recorded_at: DateTime.new(2017, 12, 22), event_note_type_id: 300)
+        }
+        let!(:another_event_note) {
+          FactoryGirl.create(:event_note, student: student, recorded_at: DateTime.new(2017, 12, 20), event_note_type_id: 300)
+        }
+
+        it 'outputs the right file' do
+          expect(exporter.student_file).to eq([
+            "student_id,school_id,absence_indicator,discipline_indicator,sst_indicator,notes_added,notes_revised,notes_total,educator_id,educator_count,pageview_count",
+            "#{student.id},HEA,0,0,0,2,0,2,#{educator.id},1,0"
+          ])
+        end
+      end
+
+      context 'notes outside the focal period' do
+        let!(:event_note) {
+          FactoryGirl.create(:event_note, student: student, recorded_at: DateTime.new(2017, 12, 22), event_note_type_id: 300)
+        }
+        let!(:another_event_note) {
+          FactoryGirl.create(:event_note, student: student, recorded_at: DateTime.new(2017, 12, 20), event_note_type_id: 300)
+        }
+        let!(:outside_event_note) {
+          FactoryGirl.create(:event_note, student: student, recorded_at: DateTime.new(2016, 12, 20), event_note_type_id: 300)
+        }
+
+        it 'outputs the right file, does not count the notes outside the focal period' do
+          expect(exporter.student_file).to eq([
+            "student_id,school_id,absence_indicator,discipline_indicator,sst_indicator,notes_added,notes_revised,notes_total,educator_id,educator_count,pageview_count",
+            "#{student.id},HEA,0,0,0,2,0,2,#{educator.id},1,0"
+          ])
+        end
+      end
+
+      context 'notes and revision during the focal period' do
+        let!(:event_note) {
+          FactoryGirl.create(:event_note, student: student, recorded_at: DateTime.new(2017, 12, 18), event_note_type_id: 300)
+        }
+        let!(:another_event_note) {
+          FactoryGirl.create(:event_note, student: student, recorded_at: DateTime.new(2017, 12, 20), event_note_type_id: 300)
+        }
+        let!(:event_note_revision) {
+          FactoryGirl.create(:event_note_revision, event_note: event_note, student_id: student.id, created_at: DateTime.new(2017, 12, 21), event_note_type_id: 300)
+        }
+
+        it 'outputs the right file' do
+          expect(exporter.student_file).to eq([
+            "student_id,school_id,absence_indicator,discipline_indicator,sst_indicator,notes_added,notes_revised,notes_total,educator_id,educator_count,pageview_count",
+            "#{student.id},HEA,0,0,0,2,1,3,#{educator.id},1,0"
+          ])
+        end
+      end
+    end
+
+    context 'mixpanel page view: student profile' do
+      let(:event_data) {
+        [{
+          "event"=>"PAGE_VISIT",
+          "properties"=>{
+            "time"=>1503905237,
+            "$current_url"=>"https://somerville-teacher-tool-demo.herokuapp.com/students/#{student.id}",
+            "deployment_key"=>"production",
+            "educator_id"=>1,
+            "educator_is_admin"=>false,
+            "educator_school_id"=>1,
+            "isDemoSite"=>false,
+            "page_key"=>"STUDENT_PROFILE"
+          }
+        }]
+      }
+
+      it 'shows the pageview' do
+        expect(exporter.student_file).to eq([
+          "student_id,school_id,absence_indicator,discipline_indicator,sst_indicator,notes_added,notes_revised,notes_total,educator_id,educator_count,pageview_count",
+          "#{student.id},HEA,0,0,0,0,0,0,#{educator.id},1,1"
+        ])
+      end
+    end
+
+    context 'mixpanel page view: a different page' do
+      let(:event_data) {
+        [{
+          "event"=>"PAGE_VISIT",
+          "properties"=>{
+            "time"=>1503905237,
+            "$current_url"=>"https://somerville-teacher-tool-demo.herokuapp.com/students/#{student.id}",
+            "deployment_key"=>"production",
+            "educator_id"=>1,
+            "educator_is_admin"=>false,
+            "educator_school_id"=>1,
+            "isDemoSite"=>false,
+            "page_key"=>"DIFFERENT_PAGE"
+          }
+        }]
+      }
+
+      it 'shows the pageview' do
         expect(exporter.student_file).to eq([
           "student_id,school_id,absence_indicator,discipline_indicator,sst_indicator,notes_added,notes_revised,notes_total,educator_id,educator_count,pageview_count",
           "#{student.id},HEA,0,0,0,0,0,0,#{educator.id},1,0"
         ])
       end
     end
-
-    context 'notes during the focal period' do
-      let!(:event_note) {
-        FactoryGirl.create(:event_note, student: student, recorded_at: DateTime.new(2017, 12, 22), event_note_type_id: 300)
-      }
-      let!(:another_event_note) {
-        FactoryGirl.create(:event_note, student: student, recorded_at: DateTime.new(2017, 12, 20), event_note_type_id: 300)
-      }
-
-      it 'outputs the right file' do
-        expect(exporter.student_file).to eq([
-          "student_id,school_id,absence_indicator,discipline_indicator,sst_indicator,notes_added,notes_revised,notes_total,educator_id,educator_count,pageview_count",
-          "#{student.id},HEA,0,0,0,2,0,2,#{educator.id},1,0"
-        ])
-      end
-    end
-
-    context 'notes outside the focal period' do
-      let!(:event_note) {
-        FactoryGirl.create(:event_note, student: student, recorded_at: DateTime.new(2017, 12, 22), event_note_type_id: 300)
-      }
-      let!(:another_event_note) {
-        FactoryGirl.create(:event_note, student: student, recorded_at: DateTime.new(2017, 12, 20), event_note_type_id: 300)
-      }
-      let!(:outside_event_note) {
-        FactoryGirl.create(:event_note, student: student, recorded_at: DateTime.new(2016, 12, 20), event_note_type_id: 300)
-      }
-
-      it 'outputs the right file, does not count the notes outside the focal period' do
-        expect(exporter.student_file).to eq([
-          "student_id,school_id,absence_indicator,discipline_indicator,sst_indicator,notes_added,notes_revised,notes_total,educator_id,educator_count,pageview_count",
-          "#{student.id},HEA,0,0,0,2,0,2,#{educator.id},1,0"
-        ])
-      end
-    end
-
-    context 'notes and revision during the focal period' do
-      let!(:event_note) {
-        FactoryGirl.create(:event_note, student: student, recorded_at: DateTime.new(2017, 12, 18), event_note_type_id: 300)
-      }
-      let!(:another_event_note) {
-        FactoryGirl.create(:event_note, student: student, recorded_at: DateTime.new(2017, 12, 20), event_note_type_id: 300)
-      }
-      let!(:event_note_revision) {
-        FactoryGirl.create(:event_note_revision, event_note: event_note, student_id: student.id, created_at: DateTime.new(2017, 12, 21), event_note_type_id: 300)
-      }
-
-      it 'outputs the right file' do
-        expect(exporter.student_file).to eq([
-          "student_id,school_id,absence_indicator,discipline_indicator,sst_indicator,notes_added,notes_revised,notes_total,educator_id,educator_count,pageview_count",
-          "#{student.id},HEA,0,0,0,2,1,3,#{educator.id},1,0"
-        ])
-      end
-    end
   end
 
   describe '#teacher_file' do
+    let(:event_data) { [] }
+
     context 'teacher name present' do
       it 'outputs the right file' do
         expect(exporter.teacher_file).to eq([
