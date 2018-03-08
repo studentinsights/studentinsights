6eebe1d22c4e7360702177399b8a032bca39afe1
Kevin Robinson <kevin.robinson.0@gmail.com>
2018-03-08 10:03:52 -0500

    Validate discipline incident codes, improve local data

--- app/demo_data/fake_discipline_incident.rb
+++ app/demo_data/fake_discipline_incident.rb
@@ -2,8 +2,8 @@ module FakeDisciplineIncident
 
   def self.data
     {
-      incident_code: ["A", "B", "C", "D"].sample,
-      incident_location: ["Hallway", "Classroom", "Lunchroom", "Playground"].sample,
+      incident_code: DisciplineIncident.valid_incident_codes.sample,
+      incident_location: example_locations.sample,
       incident_description: [
         "Lorem ipsum dolor sit amet, elit purus, proin sed ut. Sed duis, id et, pede scelerisque. Neque magnis massa, arcu molestie.",
         "Cras rutrum, elit commodo. In amet, nec sed vel. Mattis condimentum. Natoque sit faucibus, vel dolor, semper mauris.",
@@ -11,5 +11,33 @@ module FakeDisciplineIncident
       ].sample
     }
   end
-
+  def self.example_locations
+    [
+      "Admin area",
+      "Auditorium",
+      "Bus stop",
+      "Cafeteria",
+      "Classroom",
+      "Computer lab",
+      "Hallway",
+      "Library/media center",
+      "Locker room/gym",
+      "MainOffice",
+      "Off campus",
+      "Off-campus District",
+      "Off-campus School",
+      "On campus",
+      "On-campus inside",
+      "On-campus outside",
+      "Online",
+      "Outside",
+      "Parking",
+      "Playground",
+      "Restroom",
+      "School bus",
+      "Stadium",
+      "Unknown",
+      "Walking School"
+    ]
+  end
 end
