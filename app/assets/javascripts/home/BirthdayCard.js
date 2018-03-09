daa0f4e05c18276d994cecb3e7f49bc9e6d0a89a
Kevin Robinson <kevin.robinson.0@gmail.com>
2018-03-09 08:28:24 -0500

    test

--- app/assets/javascripts/home/BirthdayCard.js
+++ app/assets/javascripts/home/BirthdayCard.js
@@ -4,18 +4,24 @@ import {toMomentFromTime} from '../helpers/toMoment';
 
 
 // Render a card in the feed for an EventNote
-function BirthdayCard({studentBirthdayCard, style = {}}) {
-  const thisYearBirthdateMoment = toMomentFromTime(studentBirthdayCard.date_of_birth).year(moment.utc().year());
-  const isWas = (thisYearBirthdateMoment.isAfter(moment.utc())) ? 'is' : 'was';
-  return (
-    <Card key={studentBirthdayCard.id} className="BirthdayCard" style={style}>
-      🎉<a style={styles.person} href={`/students/${studentBirthdayCard.id}`}>{studentBirthdayCard.first_name} {studentBirthdayCard.last_name}</a>
-      <span>'s birthday {isWas} on </span>
-      <span>{thisYearBirthdateMoment.format('dddd M/D')}!</span>
-    </Card>
-  );
+class BirthdayCard extends React.Component {
+  render() {
+    const {nowFn} = this.context;
+    const {studentBirthdayCard, style = {}} = this.props;
+    const thisYearBirthdateMoment = toMomentFromTime(studentBirthdayCard.date_of_birth).year(moment.utc().year());
+    const isWas = (thisYearBirthdateMoment.isSameOrAfter(nowFn())) ? 'is' : 'was';
+    return (
+      <Card key={studentBirthdayCard.id} className="BirthdayCard" style={style}>
+        🎉<a style={styles.person} href={`/students/${studentBirthdayCard.id}`}>{studentBirthdayCard.first_name} {studentBirthdayCard.last_name}</a>
+        <span>'s birthday {isWas} on </span>
+        <span>{thisYearBirthdateMoment.format('dddd M/D')}!</span>
+      </Card>
+    );
+  }
 }
-
+BirthdayCard.contextTypes = {
+  nowFn: React.PropTypes.func.isRequired
+};
 BirthdayCard.propTypes = {
   studentBirthdayCard: React.PropTypes.shape({
     id: React.PropTypes.number.isRequired,
