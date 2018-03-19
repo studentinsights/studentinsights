669a6493d26ba3e364777b364eb5ade643378d50
EricDS <edavidsonsawyer@gmail.com>
2018-03-19 10:12:59 -0400

    basic specs for discipline dash

---
+++ spec/javascripts/school_administrator_dashboard/SchoolDisciplineDashboard.test.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import SchoolDisciplineDashboard from '../../../app/assets/javascripts/school_administrator_dashboard/dashboard_components/discipline_dashboard/SchoolDisciplineDashboard';
+import * as Data from './DashboardTestData.js';
+
+  const dash = shallow(<SchoolDisciplineDashboard
+                        dashboardStudents={Data.Students}
+                        totalDisciplineIncidents={[]}
+                        disciplineIncidentsByLocation={{}}
+                        disciplineIncidentsByTime={{}}
+                        disciplineIncidentsByClassroomn={{}}
+                        disciplineIncidentsByGrade={{}}
+                        disciplineIncidentsByDay={{}}
+                        disciplineIncidentsByOffense={{}}
+                        disciplineIncidentsByRace={{}}/>);
+
+  it('renders at least one bar chart', () => {
+    expect(dash.find('DashboardBarChart').length > 1).toEqual(true);
+  });
+
+  it('renders a student list', () => {
+    expect(dash.find('StudentsTable').length).toEqual(1);
+  });
+
+  it('renders a date slider', () => {
+    expect(dash.find('DateSlider').length).toEqual(1);
+  });
