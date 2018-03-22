708e0bb27f62e0c04db8305055523f3098eb981b
Alex Soble <asoble@gmail.com>
2018-03-22 13:01:48 -0500

    Add code comment about filtering out :text

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,4 +1,7 @@
-# Be sure to restart your server when you modify this file.
-
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:password, :text]
+Rails.application.config.filter_parameters += [
+  :password,
+  :text, # Filtering this one out because the `text` field in the event_notes and
+         # event_note_revisions tables can contain sensitive information about
+         # students that we want to keep out of logs.
+]
