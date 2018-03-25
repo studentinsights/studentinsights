970a5b351e25d1490d68924f0e4cfe668f7c5833
EricDS <edavidsonsawyer@gmail.com>
2018-03-25 10:56:41 -0400

    unused _

--- app/assets/javascripts/school_administrator_dashboard/dashboard_components/discipline_dashboard/SchoolWideDisciplineIncidents.js
+++ app/assets/javascripts/school_administrator_dashboard/dashboard_components/discipline_dashboard/SchoolWideDisciplineIncidents.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import _ from 'lodash';
 
 import SchoolDisciplineDashboard from './SchoolDisciplineDashboard';
 import DashboardHelpers from '../DashboardHelpers';
