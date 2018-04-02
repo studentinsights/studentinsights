0ca507d88c48028c430b2ea38646f3c283b90d4a
Kevin Robinson <kevin.robinson.0@gmail.com>
2018-04-02 10:44:30 -0400

    Done JS specs

--- app/assets/javascripts/home/HomeInsights.test.js
+++ app/assets/javascripts/home/HomeInsights.test.js
@@ -1,16 +1,32 @@
 import React from 'react';
+import {shallow} from 'enzyme';
 import ReactDOM from 'react-dom';
 import HomeInsights from './HomeInsights';
+import CheckStudentsWithLowGrades from './CheckStudentsWithLowGrades';
 
-function testProps() {
+
+function testProps(props = {}) {
   return {
-    educatorId: 9999
+    educatorId: 9999,
+    inExperienceTeam: false,
+    ...props
   };
 }
 
-jest.mock('./CheckStudentsWithLowGrades');
 it('renders without crashing', () => {
   const props = testProps();
   const el = document.createElement('div');
   ReactDOM.render(<HomeInsights {...props} />, el);
+});
+
+it('shallow renders', () => {
+  const props = testProps();
+  const wrapper = shallow(<HomeInsights {...props} />);
+  expect(wrapper.find(CheckStudentsWithLowGrades).length).toEqual(0);
+});
+
+it('shallow renders box when inExperienceTeam: true', () => {
+  const props = testProps({ inExperienceTeam: true });
+  const wrapper = shallow(<HomeInsights {...props} />);
+  expect(wrapper.contains(<CheckStudentsWithLowGrades educatorId={props.educatorId} />)).toEqual(true);
 });
