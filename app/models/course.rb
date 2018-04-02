854fb112fd64540df9526de9e4edc2f68e9d3e84
Alex Soble <asoble@gmail.com>
2018-04-02 10:00:03 -0500

    Move uniqueness criteria for course and section to a new line
    + Make this code more prominent and easy-to-read when looking at the file

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,8 @@
 class Course < ActiveRecord::Base
-  validates :course_number, presence: true, uniqueness: { scope: [:course_number, :school_id] }
+  validates :course_number, presence: true, uniqueness: {
+    # Different courses at different schools can have the same course number
+    scope: [:course_number, :school_id]
+  }
   validates :school, presence: true
   has_many :sections
   belongs_to :school
