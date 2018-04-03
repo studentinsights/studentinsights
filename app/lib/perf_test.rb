fa0e9b57d4881e36f72aed6e0e33b46d726f9ff9
Kevin Robinson <kevin.robinson.0@gmail.com>
2018-04-03 14:18:51 -0400

    Add PerfTest, fixup bad use of contains_exactly

--- app/lib/perf_test.rb
+++ app/lib/perf_test.rb
@@ -1,4 +1,83 @@
 class PerfTest
+  # For testing the high absences insight
+  def self.high_absences(percentage, options = {})
+    timer = PerfTest.new.run(percentage) do |t, educator|
+      time_now = options[:time_now] || Time.at(1522779136)
+      time_threshold = time_now - 45.days
+      absences_threshold = 4
+      insight = InsightStudentsWithHighAbsences.new(educator)
+      insight.students_with_high_absences_json(time_now, time_threshold, absences_threshold)
+    end
+    pp timer.report
+    pp PerfTest::Reporter.new.report(timer.report.group_by {|tuple| tuple[0] })
+    timer
+  end
+
+  # Usage for testing the feed
+  def self.feed(percentage, options = {})
+    timer = PerfTest.new.run(percentage) do |t, educator|
+      time_now = options[:time_now] || Time.at(1521552855)
+      limit = options[:limit] || 10
+      feed = Feed.new(educator)
+      event_note_cards = t.measure('event_note_cards') do
+        feed.event_note_cards(time_now, limit)
+      end
+      incident_cards = t.measure('incident_cards') do
+        feed.incident_cards(time_now, limit)
+      end
+      birthday_cards = t.measure('birthday_cards') do
+        feed.birthday_cards(time_now, limit, {
+          limit: 3,
+          days_back: 3,
+          days_ahead: 0
+        })
+      end
+      t.measure('feed_cards') do
+        feed.merge_sort_and_limit_cards([
+          event_note_cards,
+          birthday_cards,
+          incident_cards
+        ], limit)
+      end
+    end
+    pp timer.report
+    timer
+  end
+
+  # Generic perftest usage:
+  # timer = perf_test.run(0.10) do |t, educator|
+  #   t.measure('whatever') { do_something_for(educator) }
+  #   t.measure('foo') { do_something_else_for(educator) }
+  # end;nil
+  # pp timer.report
+  def run(percentage, &block)
+    timer = Timer.new
+
+    puts
+    puts
+    puts 'Getting test set...'
+    educator_ids = test_educator_ids(percentage)
+    puts "Test set includes #{educator_ids.size} educators: [#{educator_ids.sort.join(',')}]"
+    puts 'Done.'
+    puts
+    puts
+    puts 'Starting test run...'
+    run_for_all_educators(timer, educator_ids, &block)
+    puts 'Done.'
+    puts
+    puts
+    timer
+  end
+
+  def test_educator_ids(percent, options = {})
+    seed = options[:seed] || 42
+    fixed_educator_ids = options[:fixed_educator_ids] || []
+    all_educator_ids = Educator.all.select(:id).map(&:id)
+    sample_size = percent * all_educator_ids.size
+    sample_educator_ids = all_educator_ids.sample(sample_size, random: Random.new(seed))
+    (sample_educator_ids + fixed_educator_ids).uniq
+  end
+
   # Helper class to print summary stats like p50, p95
   #
   # Usage:
@@ -61,71 +140,6 @@ class PerfTest
       end
       tuples.sort_by {|t| [t[0], t[1]] }
     end
-  end
-
-  # Usage for testing the feed
-  def self.feed(percentage, options = {})
-    timer = PerfTest.new.run(0.1) do |t, educator|
-      time_now = options[:time_now] || Time.at(1521552855)
-      limit = options[:limit] || 10
-      feed = Feed.new(educator)
-      event_note_cards = t.measure('event_note_cards') do
-        feed.event_note_cards(time_now, limit)
-      end
-      incident_cards = t.measure('incident_cards') do
-        feed.incident_cards(time_now, limit)
-      end
-      birthday_cards = t.measure('birthday_cards') do
-        feed.birthday_cards(time_now, limit, {
-          limit: 3,
-          days_back: 3,
-          days_ahead: 0
-        })
-      end
-      t.measure('feed_cards') do
-        feed.merge_sort_and_limit_cards([
-          event_note_cards,
-          birthday_cards,
-          incident_cards
-        ], limit)
-      end
-    end
-    pp timer.report
-    timer
-  end
-
-  # Generic perftest usage:
-  # timer = perf_test.run(0.10) do |t, educator|
-  #   t.measure('whatever') { do_something_for(educator) }
-  #   t.measure('foo') { do_something_else_for(educator) }
-  # end;nil
-  # pp timer.report
-  def run(percentage, &block)
-    timer = Timer.new
-
-    puts
-    puts
-    puts 'Getting test set...'
-    educator_ids = test_educator_ids(percentage)
-    puts "Test set includes #{educator_ids.size} educators: [#{educator_ids.sort.join(',')}]"
-    puts 'Done.'
-    puts
-    puts
-    puts 'Starting test run...'
-    run_for_all_educators(timer, educator_ids, &block)
-    puts 'Done.'
-    puts
-    puts
-    timer
-  end
-
-  def test_educator_ids(percent, options = {})
-    seed = options[:seed] || 42
-    fixed_educator_ids = options[:fixed_educator_ids] || []
-    all_educator_ids = Educator.all.select(:id).map(&:id)
-    sample_size = percent * all_educator_ids.size
-    sample_educator_ids = all_educator_ids.sample(sample_size, random: Random.new(seed))
-    (sample_educator_ids + fixed_educator_ids).uniq
   end
 
   private
