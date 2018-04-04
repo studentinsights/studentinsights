a01030de529d52c093dc64e7186dd90aa66af4d0
Kevin Robinson <kevin.robinson.0@gmail.com>
2018-04-03 20:34:00 -0400

    Fix redirects in controller specs
    update specs
    
    Remove comment

--- app/lib/paths_for_educator.rb
+++ app/lib/paths_for_educator.rb
@@ -4,48 +4,35 @@ class PathsForEducator
     @educator = educator
   end
 
-  # There are five types of entry experiences, depending on levels
-  # of access.
-  def homepage_type
-    begin
-      return :districtwide if @educator.districtwide_access?
-      return :school if @educator.schoolwide_access? || @educator.has_access_to_grade_levels?
-      return :section if @educator.school.is_high_school? && @educator.default_section
-      return :homeroom if @educator.homeroom.present? && !@educator.homeroom.school.is_high_school? && @educator.default_homeroom
-      return :nothing
-    rescue Exceptions::NoAssignedHomeroom, Exceptions::NoAssignedSections => _
-      :nothing
-    end
-  end
-
-  # Return the homepage path, depending on the educator's role
-  def homepage_path
-    type = homepage_type
-    if type == :districtwide
-      url_helpers.educators_districtwide_path
-    elsif type == :school
-      url_helpers.school_path(@educator.school)
-    elsif type == :section
-      url_helpers.section_path(@educator.default_section)
-    elsif type == :homeroom
-      url_helpers.homeroom_path(@educator.default_homeroom)
-    elsif type == :nothing
-      url_helpers.no_default_page_path
-    else
-      url_helpers.no_default_page_path
-    end
-  end
-
   # Links shown on the navbar, different depending on role
   def navbar_links
-    links = Set.new
-    links += [:district] if @educator.districtwide_access?
-    links += [:school] if @educator.school.present? && (@educator.schoolwide_access? || @educator.has_access_to_grade_levels?) && !@educator.districtwide_access?
-    links += [:sections] if @educator.school.present? && @educator.school.is_high_school? && @educator.sections.size > 0
-    links += [:homeroom] if @educator.homeroom.present? && !@educator.homeroom.school.is_high_school?
+    links = {}
+
+    if @educator.districtwide_access?
+      links[:district] = url_helpers.educators_districtwide_path
+    end
+
+    if @educator.school.present? && (@educator.schoolwide_access? || @educator.has_access_to_grade_levels?) && !@educator.districtwide_access?
+      links[:school] = url_helpers.school_path(@educator.school)
+    end
+
+    if @educator.school.present? && @educator.schoolwide_access? && !@educator.districtwide_access?
+      links[:absences] = url_helpers.school_administrator_dashboard_school_path(@educator.school) + '#/absences_dashboard'
+      links[:tardies] = url_helpers.school_administrator_dashboard_school_path(@educator.school) + '#/tardies_dashboard'
+    end
+
+    if @educator.school.present? && @educator.school.is_high_school? && @educator.sections.size > 0
+      links[:section] = url_helpers.section_path(@educator.default_section)
+    end
+
+    if @educator.homeroom.present? && !@educator.homeroom.school.is_high_school?
+      links[:homeroom] = url_helpers.homeroom_path(@educator.default_homeroom)
+    end
+
     links
   end
 
+  private
   def url_helpers
     Rails.application.routes.url_helpers
   end
