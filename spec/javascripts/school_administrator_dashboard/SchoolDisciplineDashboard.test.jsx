0bf7770594fa7a803c5899511d38110d6add9bcb
EricDS <edavidsonsawyer@gmail.com>
2018-04-09 15:34:18 -0400

    keepking specs in sync with component changes

--- spec/javascripts/school_administrator_dashboard/SchoolDisciplineDashboard.test.jsx
+++ spec/javascripts/school_administrator_dashboard/SchoolDisciplineDashboard.test.jsx
@@ -7,14 +7,7 @@ import { createStudents } from './DashboardTestData.js';
 describe('SchoolDisciplineDashboard', () => {
   const dash = shallow(<SchoolDisciplineDashboard
                         dashboardStudents={createStudents(moment.utc())}
-                        totalDisciplineIncidents={[]}
-                        disciplineIncidentsByLocation={{}}
-                        disciplineIncidentsByTime={{}}
-                        disciplineIncidentsByClassroomn={{}}
-                        disciplineIncidentsByGrade={{}}
-                        disciplineIncidentsByDay={{}}
-                        disciplineIncidentsByOffense={{}}
-                        disciplineIncidentsByRace={{}}/>);
+                        schoolWideDisciplineIncidents={}>);
 
   it('renders at least one bar chart', () => {
     expect(dash.find('DashboardBarChart').length > 0).toEqual(true);
