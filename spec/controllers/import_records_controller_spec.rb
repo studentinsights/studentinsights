8bb47b3d864c70f3973dd37c8e314021d157d35c
Alex Soble <asoble@gmail.com>
2018-04-09 09:53:08 -0500

    Add more examples to controller spec

--- spec/controllers/import_records_controller_spec.rb
+++ spec/controllers/import_records_controller_spec.rb
@@ -2,21 +2,45 @@ require 'rails_helper'
 
 RSpec.describe ImportRecordsController, type: :controller do
 
-  describe '#index' do
+  describe '#import_records_json' do
     def make_request
       request.env['HTTPS'] = 'on'
-      get :index
+      get :import_records_json
     end
 
     context 'educator signed in' do
 
       before { sign_in(educator) }
 
-      context 'educator w districtwide access' do
+      context 'educator w districtwide access, no import records' do
         let(:educator) { FactoryGirl.create(:educator, districtwide_access: true, admin: true) }
         it 'can access the page' do
           make_request
           expect(response).to be_success
+          expect(JSON.parse(response.body)).to eq({
+            "import_records" => [],
+            "queued_jobs" => [],
+          })
+        end
+      end
+
+      context 'educator w districtwide access, no import records' do
+        let(:educator) { FactoryGirl.create(:educator, districtwide_access: true, admin: true) }
+        let!(:import_record) {
+          ImportRecord.create!(
+            time_started: Time.now - 4.hours,
+            time_ended: Time.now - 2.hours,
+            importer_timing_json: "Super useful JSON...",
+            task_options_json: "Super useful JSON...",
+            log: "Super useful text..."
+          )
+        }
+
+        it 'can access the page' do
+          make_request
+          expect(response).to be_success
+          expect(JSON.parse(response.body)["import_records"].size).to eq 1
+          expect(JSON.parse(response.body)["queued_jobs"].size).to eq 0
         end
       end
 
