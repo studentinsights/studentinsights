4bab7b61f1ab5bb275953bd665207ad257eb8fa4
Kevin Robinson <kevin.robinson.0@gmail.com>
2018-04-09 12:29:24 -0400

    Remove legacyHelpers altogether

--- ui/legacyHelpers.js
+++
@@ -1,7 +0,0 @@
-// HELPERS
-import '../app/assets/javascripts/helpers/mixpanel_utils.jsx';
-
-// Export these so legacy components can use this, and
-// new components can pull in components
-window.shared || (window.shared = {});
-
