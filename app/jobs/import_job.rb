51214919f4fbbca2bde3d1709916c2f202d457fa
Kevin Robinson <kevin.robinson.0@gmail.com>
2018-04-12 10:09:07 -0400

    Update Delayed::Job failures modes

--- app/jobs/import_job.rb
+++ app/jobs/import_job.rb
@@ -1,3 +1,4 @@
+# These methods are all hooks for https://github.com/collectiveidea/delayed_job
 class ImportJob
 
   def initialize(options:)
@@ -16,4 +17,14 @@ class ImportJob
     6.hours
   end
 
+  def error(job, exception)
+    extra_info =  { "hook" => "error", "job" => job.as_json }
+    ErrorMailer.error_report(exception, extra_info).deliver_now if Rails.env.production?
+  end
+
+  def failure(job)
+    extra_info =  { "hook" => "failure", "job" => job.as_json }
+    exception = StandardError.new('ImportJob#failure')
+    ErrorMailer.error_report(exception, extra_info).deliver_now if Rails.env.production?
+  end
 end
