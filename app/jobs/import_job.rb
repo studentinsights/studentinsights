03392ee1425cdb800f812494326e7b57802c4f94
Kevin Robinson <kevin.robinson.0@gmail.com>
2018-04-12 10:17:20 -0400

    Change serialization

--- app/jobs/import_job.rb
+++ app/jobs/import_job.rb
@@ -18,12 +18,12 @@ class ImportJob
   end
 
   def error(job, exception)
-    extra_info =  { "hook" => "error", "job" => job.as_json }
+    extra_info =  { "hook" => "error", "job" => job.as_json(except: :log) }
     ErrorMailer.error_report(exception, extra_info).deliver_now if Rails.env.production?
   end
 
   def failure(job)
-    extra_info =  { "hook" => "failure", "job" => job.as_json }
+    extra_info =  { "hook" => "failure", "job" => job.as_json(except: :log) }
     exception = StandardError.new('ImportJob#failure')
     ErrorMailer.error_report(exception, extra_info).deliver_now if Rails.env.production?
   end
