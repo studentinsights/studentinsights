173c88d8a01f747d5039c532af1b82275e9fdaaa
EricDS <edavidsonsawyer@gmail.com>
2018-04-20 17:32:44 -0400

    apply date range to remaining tardies elements

--- app/assets/javascripts/school_administrator_dashboard/dashboard_components/tardies_dashboard/SchoolwideTardies.js
+++ app/assets/javascripts/school_administrator_dashboard/dashboard_components/tardies_dashboard/SchoolwideTardies.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import _ from 'lodash';
 
 import DashboardHelpers from '../DashboardHelpers';
 import SchoolTardiesDashboard from './SchoolTardiesDashboard';
@@ -12,29 +11,10 @@ class SchoolwideTardies extends React.Component {
     return DashboardHelpers.tardyEventsByDay(studentRecords);
   }
 
-  homeroomTardyEvents() {
-    let homeroomTardyEvents = {};
-    const studentRecords = this.props.dashboardStudents;
-    const studentsByHomeroom = DashboardHelpers.groupByHomeroom(studentRecords);
-    const schoolYearStart = DashboardHelpers.schoolYearStart();
-    Object.keys(studentsByHomeroom).forEach((homeroom) => {
-      homeroomTardyEvents[homeroom] = 0;
-      _.each(studentsByHomeroom[homeroom], (student) => {
-        student.tardies.forEach((tardy) => {
-          if (moment.utc(tardy.occurred_at).isSameOrAfter(schoolYearStart)) {
-            homeroomTardyEvents[homeroom]++;
-          }
-        });
-      });
-    });
-    return homeroomTardyEvents;
-  }
-
   render() {
     return (
         <SchoolTardiesDashboard
           schoolTardyEvents = {this.schoolTardyEvents()}
-          homeroomTardyEvents = {this.homeroomTardyEvents()}
           dashboardStudents = {this.props.dashboardStudents}/>);
   }
 }
