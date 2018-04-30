1d277f274abbd96420ef1da4738a0cc8af2c199d
Kevin Robinson <kevin.robinson.0@gmail.com>
2018-04-30 09:10:02 -0400

    Add story for react-select

--- ui/config/.storybook/config.js
+++ ui/config/.storybook/config.js
@@ -4,8 +4,10 @@ import '../../legacy.js';
 
 /* eslint-disable no-undef */
 function loadStories() {
-  require('../../../app/assets/javascripts/student_profile/RiskBubble.story.js');
   require('../../../app/assets/javascripts/components/BoxAndWhisker.story.js');
+  require('../../../app/assets/javascripts/components/ReactSelect.story.js');
+  require('../../../app/assets/javascripts/student_profile/RiskBubble.story.js');
+  
   require('../../../app/assets/javascripts/equity/HorizontalStepper.story.js');
   require('../../../app/assets/javascripts/equity/CreateYourClassroomsView.story.js');
   // add more here!
