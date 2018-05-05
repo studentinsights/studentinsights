653acc181fd933cc82b4378fc117db7a1f5a2150
Kevin Robinson <kevin.robinson.0@gmail.com>
2018-05-05 07:42:19 -0400

    Add labels to each DibelsBreakDown, refactor

--- app/assets/javascripts/components/DibelsBreakdownBar.js
+++ app/assets/javascripts/components/DibelsBreakdownBar.js
@@ -2,60 +2,77 @@ import React from 'react';
 
 // Visual component showing a horizontal bar broken down into three colors
 // showing percent of students at different DIBELS levels.
-export default function DibelsBreakdownBar(props) {
-  const {height, coreCount, strategicCount, intensiveCount, style = {}} = props;
-  const totalCount = coreCount + strategicCount + intensiveCount;
-  const opacity = 0.5;
-  const fontSize = 10;
-  const scale = (value) => `${Math.ceil(100 * value)}%`;
+export default class DibelsBreakdownBar extends React.Component {
+  totalCount() {
+    const {coreCount, strategicCount, intensiveCount} = this.props;
+    return coreCount + strategicCount + intensiveCount;
+  }
 
-  if (totalCount === 0) return null;
-  return (
-    <div className="DibelsBreakdownBar" style={{height, ...style}}>
-      <div style={{position: 'relative', width: '100%', height}}>
-        <div style={{
-          position: 'absolute',
-          background: 'green',
-          opacity,
-          fontSize,
-          left: scale(0),
-          width: scale(coreCount / totalCount),
-          height
-        }}>{'\u00A0'}</div>
-        <div style={{
-          position: 'absolute',
-          background: 'orange',
-          opacity,
-          fontSize,
-          left: scale(coreCount / totalCount),
-          width: scale(strategicCount / totalCount),
-          height
-        }}>{'\u00A0'}</div>
-        <div style={{
-          position: 'absolute',
-          background: 'red',
-          opacity,
-          fontSize,
-          left: scale((coreCount + strategicCount) / totalCount),
-          width: scale(intensiveCount / totalCount),
-          height
-        }}>{'\u00A0'}</div>
+  scale(count) {
+    const totalCount = this.totalCount();
+    return `${Math.ceil(100 * count / totalCount)}%`;
+  }
 
-        {coreCount > 0 && 
+  render() {
+    const {height, coreCount, strategicCount, intensiveCount, style = {}} = this.props;    
+    return (
+      <div className="DibelsBreakdownBar" style={{height, ...style}}>
+        <div style={{position: 'relative', width: '100%', height}}>
+          {this.renderBarAndLabel({
+            left: 0,
+            width: coreCount,
+            color: 'green'
+          })}
+          {this.renderBarAndLabel({
+            left: coreCount,
+            width: strategicCount,
+            color: 'orange'
+          })}
+          {this.renderBarAndLabel({
+            left: coreCount + strategicCount,
+            width: intensiveCount,
+            color: 'red'
+          })}
+        </div>
+      </div>
+    );
+  }
+
+
+  renderBarAndLabel({left, width, color}) {
+    const {height} = this.props;
+    const opacity = 0.5;
+    const fontSize = 10;
+
+    if (width === 0) return;
+    return (
+      <div>
+        <div style={{
+          opacity,
+          fontSize,
+          position: 'absolute',
+          background: color,
+          left: this.scale(left),
+          width: this.scale(width),
+          height
+        }}>{'\u00A0'}</div>
+        {width > 0 && 
           <div style={{
-            position: 'absolute',
-            textAlign: 'right',
             opacity,
             fontSize,
-            left: scale(0),
-            width: scale(coreCount / totalCount),
+            color,
+            position: 'absolute',
+            textAlign: 'right',
+            left: this.scale(left),
+            width: this.scale(width),
             top: height/2  + 1, // padding
-            color: 'green'
-          }}>{coreCount}</div>}
+            paddingRight: 1
+          }}>{width}</div>}
       </div>
-    </div>
-  );
+    );
+  }
 }
+
 DibelsBreakdownBar.propTypes = {
   coreCount: React.PropTypes.number.isRequired,
   strategicCount: React.PropTypes.number.isRequired,
