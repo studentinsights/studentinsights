06746bef3dbd528a4fd3c61f1a4f19964c6d5e85
EricDS <edavidsonsawyer@gmail.com>
2018-05-09 21:38:42 -0400

    fixing includes for discipline dashboard

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -50,7 +50,7 @@ class SchoolsController < ApplicationController
 
   def discipline_dashboard_data
     student_discipline_data = active_students(@school)
-      .includes([homeroom: :educator], :discipline_incidents, :event_notes)
+      .includes([homeroom: :educator], :discipline_incidents, :sst_notes)
       .where('occurred_at >= ?', 1.year.ago).references(:discipline_incidents)
     student_discipline_data_json = student_discipline_data.map do |student|
       individual_student_discipline_data(student)
