2c31b842c4ed399ba385bb0a41a9f8a36f75a5da
Kevin Robinson <kevin.robinson.0@gmail.com>
2018-05-14 09:03:56 -0400

    Simplify legacy grouping

--- ui/legacy.js
+++ ui/legacy.js
@@ -14,13 +14,8 @@ import '../app/assets/javascripts/student_profile/record_service.jsx';
 import '../app/assets/javascripts/student_profile/student_profile_header.jsx';
 import '../app/assets/javascripts/student_profile/student_sections_roster.jsx';
 import '../app/assets/javascripts/student_profile/pdf/student_profile_pdf.js';
-
-  // details:
 import '../app/assets/javascripts/student_profile/services_details.jsx';
 import '../app/assets/javascripts/student_profile/profile_details.jsx';
-
-  // page:
 import '../app/assets/javascripts/student_profile/student_profile_page.jsx';
-  // (unordered)
 import '../app/assets/javascripts/student_profile/page_container.jsx';
 
