d7b41d9957a3f3641aa2fcec767390315bd2c67a
Alex Soble <asoble@gmail.com>
2018-05-17 12:31:27 -0500

    Read educator emails in from database

--- app/lib/test_pals_mock_ldap.rb
+++ app/lib/test_pals_mock_ldap.rb
@@ -1,23 +1,5 @@
 class TestPalsMockLDAP
 
-  TEST_PAL_NAMES = %w[
-    uri
-    rich
-    vivian
-    alonso
-    silva
-    laura
-    sarah
-    marcus
-    sofia
-    jodi
-    bill
-    hugo
-    fatima
-  ]
-
-  TEST_PAL_EMAILS = TEST_PAL_NAMES.map { |n| "#{n}@demo.studentinsights.org" }
-
   def initialize(options)
     @options = options
     @email = options[:auth][:username]
@@ -27,7 +9,7 @@ class TestPalsMockLDAP
   def bind
     raise 'LDAP error' unless ::EnvironmentVariable.is_true('USE_TEST_PALS_LDAP')
 
-    return TEST_PAL_EMAILS.include?(@email) && @password == 'demo-password'
+    return Educator.find_by_email(@email).present? && @password == 'demo-password'
   end
 
   def get_operation_result
