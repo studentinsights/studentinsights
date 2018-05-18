031197e23cfa8d139dcc638d75fa909a07b1c4d7
Alex Soble <asoble@gmail.com>
2018-05-18 10:07:32 -0500

    Mock insecure "unauthenticated bind" setting in mock LDAP server

--- app/lib/test_pals_mock_ldap.rb
+++ app/lib/test_pals_mock_ldap.rb
@@ -11,6 +11,8 @@ class TestPalsMockLDAP
 
     return false unless email_present?
 
+    return true if unauthenticated_bind?
+
     return false unless password_correct?
 
     return true
@@ -23,6 +25,19 @@ class TestPalsMockLDAP
 
   def email_present?
     Educator.find_by_email(@email).present?
+  end
+
+  def unauthenticated_bind?
+    # This mocks the worst-case scenario for LDAP setup: the unauthenticated
+    # bind setting, which responds to a correct user email and a blank password
+    # with a "success" response that establishes an "anonymous authorization state."
+
+    # The LDAP spec states that "Servers SHOULD by default fail Unauthenticated
+    # Bind requests with a resultCode of unwillingToPerform."
+    # (https://tools.ietf.org/rfc/rfc4513.txt, 5.1.2)
+
+    # But we can't count on all servers to have this configuration.
+    @password == nil || @password == ''
   end
 
   def password_correct?
