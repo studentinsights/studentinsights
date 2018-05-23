3e6d525f6c38f2a0474f1096fad82134bf7cc593
Kevin Robinson <kevin.robinson.0@gmail.com>
2018-05-23 01:01:47 -0400

    WIP, improving perf on validation

--- app/models/class_list.rb
+++ app/models/class_list.rb
@@ -19,32 +19,52 @@ class ClassList < ActiveRecord::Base
   end
 
   private
-  # These shouldn't change over the life of a workspace
+  # These shouldn't change over the life of a workspace, so if we find
+  # any workspace_id records with different grade or school, fail the validation.
   def validate_consistent_workspace_grade_school
-    class_lists = ClassList.where(workspace_id: workspace_id)
+    # Optimized SQL group by for many workspace_id records
+    groups_by_school_and_grade = ClassList
+      .where(workspace_id: workspace_id)
+      .group(:school_id, :grade_level_next_year)
+      .count
 
-    grade_conflicts = []
-    school_conflicts = []
-    class_lists.each do |class_list|
-      grade_conflicts << class_list if class_list.grade_level_next_year != grade_level_next_year
-      school_conflicts << class_list if class_list.school_id != school_id
+    # There already is a validation violation, abort
+    if groups_by_school_and_grade.keys.size > 1
+      errors.add(:grade_level_next_year, "preexisting violation, different grade_level_next_year values for same workspace_id")
+      errors.add(:school_id, "preexisting violation, different school_id values for same workspace_id")
     end
-    if grade_conflicts.size > 0
-      errors.add(:grade_level_next_year, "cannot add different grade_level_next_year to existing workspace_id")
-    end
-    if school_conflicts.size > 0
+
+    # We're trying to write something different that what is already there
+    school_id, grade_level_next_year = groups_by_school_and_grade.values
+    if school_id != self.school_id
       errors.add(:school_id, "cannot add different school_id to existing workspace_id")
+    end
+    if grade_level_next_year != self.grade_level_next_year
+      errors.add(:grade_level_next_year, "cannot add different grade_level_next_year to existing workspace_id")
     end
   end
 
   # Only one writer can write to a workspace
   def validate_single_writer_in_workspace
-    latest_class_list = ClassList.latest_class_list_for_workspace(workspace_id)
-    if latest_class_list.present?
-      owner = latest_class_list.created_by_educator
-      if owner.present? && owner.id != created_by_educator_id
-        errors.add(:created_by_educator_id, "cannot add record with different created_by_educator_id for existing workspace_id")
-      end
+    puts '  validate_single_writer_in_workspace'
+    # Optimized SQL group by for many workspace_id records
+    groups_by_created_by_educator_id = ClassList
+      .where(workspace_id: workspace_id)
+      .group(:created_by_educator_id)
+      .count
+
+    puts "  ClassList.all.as_json: #{ClassList.all.as_json}"
+    puts "  groups_by_created_by_educator_id: #{groups_by_created_by_educator_id}"
+    puts "  groups_by_created_by_educator_id.values: #{groups_by_created_by_educator_id.values}"
+    # There already is a violation
+    if groups_by_created_by_educator_id.keys.size > 1
+      errors.add(:created_by_educator_id, "preexisting violation, different created_by_educator_id for existing workspace_id")
+    end
+
+    # We are about to violate
+    other_educator_id = groups_by_created_by_educator_id.values.first
+    if other_educator_id.present? && other_educator_id != self.created_by_educator_id
+      errors.add(:created_by_educator_id, "cannot add record with different created_by_educator_id for existing workspace_id")
     end
   end
 end
