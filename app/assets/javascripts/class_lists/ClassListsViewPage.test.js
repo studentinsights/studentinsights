dd70854dfd679cb2156f436c1066b7741c7ebd2f
Kevin Robinson <kevin.robinson.0@gmail.com>
2018-05-23 17:44:53 -0400

    Update columns on class lists, improve sort order

--- app/assets/javascripts/class_lists/ClassListsViewPage.test.js
+++ app/assets/javascripts/class_lists/ClassListsViewPage.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
+import {withDefaultNowContext} from '../../../../spec/javascripts/support/NowContainer';
 import mockWithFixtures from './fixtures/mockWithFixtures';
 import ClassListsViewPage, {ClassListsViewPageView} from './ClassListsViewPage';
 import workspaces_json from './fixtures/workspaces_json';
@@ -17,14 +18,14 @@ export function testProps(props = {}) {
 it('renders without crashing', () => {
   const el = document.createElement('div');
   const props = testProps();
-  ReactDOM.render(<ClassListsViewPage {...props} />, el);
+  ReactDOM.render(withDefaultNowContext(<ClassListsViewPage {...props} />), el);
 });
 
 
 it('snapshots view', () => {
   const props = testProps({...workspaces_json});
   const tree = renderer
-    .create(<ClassListsViewPageView {...props} />)
+    .create(withDefaultNowContext(<ClassListsViewPageView {...props} />))
     .toJSON();
   expect(tree).toMatchSnapshot();
 });
