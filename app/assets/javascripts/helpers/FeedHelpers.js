b55c894b557e7ed9b4a0ece1740bdf1f4e279782
Alex Soble <asoble@gmail.com>
2018-05-23 14:12:14 -0500

    WIP - pin HS Transition notes to the top

--- app/assets/javascripts/helpers/FeedHelpers.js
+++ app/assets/javascripts/helpers/FeedHelpers.js
@@ -5,16 +5,22 @@ Functions for transforming the feed data structure that holds
 all notes and services for a student.
 */
 
+function highSchoolTransition(note) {
+  if (note.type !== 'event_notes') return false;
+  if (note.event_note_type_id !== 307) return false;
+  return true;
+}
+
 // Merges data from event_notes and deprecated tables (notes, interventions).
 export function mergedNotes(feed) {
-  const deprecatedInterventions = feed.deprecated.interventions.map(function(intervention) {
+  const deprecatedInterventions = feed.deprecated.interventions.map((intervention) => {
     return {
       ...intervention,
       type: 'deprecated_interventions',
       sort_timestamp: intervention.start_date_timestamp
     };
   });
-  const eventNotes = feed.event_notes.map(function(eventNote) {
+  const eventNotes = feed.event_notes.map((eventNote) => {
     return {
       ...eventNote,
       type: 'event_notes',
@@ -23,7 +29,15 @@ export function mergedNotes(feed) {
   });
 
   const mergedNotes = eventNotes.concat.apply(eventNotes, [deprecatedInterventions]);
-  return _.sortBy(mergedNotes, 'sort_timestamp').reverse();
+
+  return mergedNotes.sort((note_a, note_b) => {
+    if (highSchoolTransition(note_a) && !highSchoolTransition(note_b)) return -1;
+    if (!highSchoolTransition(note_a) && highSchoolTransition(note_b)) return 1;
+
+    if (note_a.sort_timestamp > note_b.sort_timestamp) return -1;
+    if (note_a.sort_timestamp < note_b.sort_timestamp) return 1;
+    return 0;
+  });
 }
 
 export function matchesMergedNoteType(mergedNote, mergedNoteType, mergedNoteTypeId) {
