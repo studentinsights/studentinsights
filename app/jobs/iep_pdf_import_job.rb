5ce0c0bab56c127c898815e4bb856d079ceaae7f
Alex Soble <asoble@gmail.com>
2018-05-29 14:27:40 -0500

    Add code comments for IEP PDF import job filenames

--- app/jobs/iep_pdf_import_job.rb
+++ app/jobs/iep_pdf_import_job.rb
@@ -25,6 +25,17 @@ class IepPdfImportJob
     import_ieps!(remote_filenames)
   end
 
+  # Each ZIP file sent down from EasyIEP has a number at the end, the number
+  # indicates that the ZIP contains a set of changed IEPs from `n` days ago.
+  #
+  # If there are no IEPs on any given day, that number may be missing from the
+  # sequence.
+  #
+  # The most recent change set (student-documents-1.zip), is the only one we really
+  # need to import, but the rest are sent down in case the import job fails
+  # and we need to catch up on missed change sets.
+  #
+  # Order is important here.
   def nightly_import!
     remote_filenames = [
       'student-documents-6.zip',
