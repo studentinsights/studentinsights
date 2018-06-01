8bc0c2d30c1bf9a2d9b5d176c72fc180c1410203
Kevin Robinson <kevin.robinson.0@gmail.com>
2018-06-01 18:08:04 -0400

    Back out jsonb

--- db/migrate/20180601193438_json_to_jsonb.rb
+++ db/migrate/20180601193438_json_to_jsonb.rb
@@ -1,6 +1,5 @@
 class JsonToJsonb < ActiveRecord::Migration[5.1]
   def change
-    change_column :class_lists, :json, :jsonb
-    change_column :class_lists, :principal_revisions_json, :jsonb
+    change_column :class_lists, :principal_revisions_json, :json
   end
 end
