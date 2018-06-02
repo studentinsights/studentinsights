6fdfe278d212fe308f5d2fc67dfc87e4ca04b742
Kevin Robinson <kevin.robinson.0@gmail.com>
2018-06-02 11:58:07 -0400

    Use now from context in ExportList, update test snapshot and story

--- app/assets/javascripts/class_lists/ExportList.story.js
+++ app/assets/javascripts/class_lists/ExportList.story.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import {storiesOf} from '@storybook/react';
 import ExportList from './ExportList';
-import {testProps} from './ExportList.test';
+import {testProps, testEl} from './ExportList.test';
 
 
 storiesOf('classlists/ExportList', module) // eslint-disable-line no-undef
-  .add('normal', () => {
-    const props = testProps();
-    return <ExportList {...props} />;
-  });
+  .add('normal', () => testEl(testProps()));
