0debdc7e878d62aae7823245d3218b1b9e6388d6
Kevin Robinson <kevin.robinson.0@gmail.com>
2018-06-04 16:16:22 -0400

    Remove legacy.js import from PDF module

--- ui/student_report_pdf.js
+++ ui/student_report_pdf.js
@@ -1,6 +1,5 @@
 import 'es5-shim'; // This has to be first, wkhtmltopdf uses QtWebKit
 import './config/sprockets-shims.js';
-import './legacy.js';
 import {load} from '../app/assets/javascripts/student_profile/pdf/studentProfilePdf';
 
 $(document).ready(() => {
