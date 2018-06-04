9274a060a04dd736d302e187ab5d7cbdb5e252e9
Kevin Robinson <kevin.robinson.0@gmail.com>
2018-06-04 13:46:42 -0400

    Remove spyOn global

--- app/assets/javascripts/student_profile/Api.test.js
+++ app/assets/javascripts/student_profile/Api.test.js
@@ -1,42 +1,36 @@
 import Api from './Api';
 
-describe('Api', function() {
+function createTestApi() {
+  const api = new Api();
+  const createNoteSpy = jest.spyOn(api, '_createNote');
+  const updateNoteSpy = jest.spyOn(api, '_updateNote');
+  return {api, createNoteSpy, updateNoteSpy};
+}
 
-  describe('#saveNotes', function() {
-
-    describe('no eventNoteParams id', function() {
-      it('calls _createNote', function() {
-        const api = new Api();
-
-        spyOn(api, '_createNote');
-        spyOn(api, '_updateNote');
-
-        api.saveNotes(1, {
-          text: 'Student loves writing specs for JS code, just cannot get enough.'
-        });
-
-        expect(api._createNote.calls.any()).toEqual(true);
-        expect(api._updateNote.calls.any()).toEqual(false);
+describe('#saveNotes', () => {
+  describe('no eventNoteParams id', () => {
+    it('calls _createNote', () => {
+      const {api, createNoteSpy, updateNoteSpy} = createTestApi();
+      api.saveNotes(1, {
+        text: 'Student loves writing specs for JS code, just cannot get enough.'
       });
+
+      expect(createNoteSpy).toHaveBeenCalled();
+      expect(updateNoteSpy).not.toHaveBeenCalled();
     });
+  });
 
-    describe('with eventNoteParams id', function() {
-      it('calls _updateNote', function() {
-        const api = new Api();
-
-        spyOn(api, '_createNote');
-        spyOn(api, '_updateNote');
-
-        api.saveNotes(1, {
-          id: 1,
-          text: 'Student loves writing specs for JS code, just cannot get enough.'
-        });
-
-        expect(api._createNote.calls.any()).toEqual(false);
-        expect(api._updateNote.calls.any()).toEqual(true);
+  describe('with eventNoteParams id', () => {
+    it('calls _updateNote', () => {
+      const {api, createNoteSpy, updateNoteSpy} = createTestApi();
+      api.saveNotes(1, {
+        id: 1,
+        text: 'Student loves writing specs for JS code, just cannot get enough.'
       });
-    });
 
+      expect(createNoteSpy).not.toHaveBeenCalled();
+      expect(updateNoteSpy).toHaveBeenCalled();
+    });
   });
 
 });
