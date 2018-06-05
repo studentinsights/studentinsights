40f94c24358f676cd9409421f75277a4f7cb1de1
Alex Soble <asoble@gmail.com>
2018-06-05 19:45:50 -0400

    Test update scenario

--- spec/controllers/transition_notes_controller_spec.rb
+++ spec/controllers/transition_notes_controller_spec.rb
@@ -20,13 +20,30 @@ RSpec.describe TransitionNotesController, type: :controller do
       post :update, params: params
     end
 
-    context 'good params, authorized educator' do
+    context 'good params, authorized educator, new note' do
       it 'works' do
         sign_in(pals.west_counselor)
         update(params)
         expect(response.status).to eq 200
         json = JSON.parse(response.body)
         expect(json['result']).to eq 'ok'
+        expect(TransitionNote.count).to eq 1
+        expect(TransitionNote.last.text).to eq 'foo'
+      end
+    end
+
+    context 'good params, authorized educator, updating note' do
+      it 'works' do
+        sign_in(pals.west_counselor)
+
+        update(params)
+        expect(TransitionNote.count).to eq 1
+        expect(TransitionNote.last.text).to eq 'foo'
+
+        update(params.merge({text: 'foofoofoo'}))
+        expect(response.status).to eq 200
+        expect(TransitionNote.count).to eq 1
+        expect(TransitionNote.last.text).to eq 'foofoofoo'
       end
     end
 
