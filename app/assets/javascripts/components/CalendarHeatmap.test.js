26df3dc708958ab93ada897f5c9feceee7717aaa
Kevin Robinson <kevin.robinson.0@gmail.com>
2018-06-08 13:05:48 -0400

    Update JS tests

--- app/assets/javascripts/components/CalendarHeatmap.test.js
+++ app/assets/javascripts/components/CalendarHeatmap.test.js
@@ -9,7 +9,7 @@ function testProps(props) {
       '2017-12-19': 5,
       '2017-12-20': 1
     },
-    years: [2017, 2018],
+    year: 2017,
     ...props
   };
 }
@@ -17,16 +17,7 @@ function testProps(props) {
 it('renders boxes, with some colored in', () => {
   const div = document.createElement('div');
   ReactDOM.render(<CalendarHeatmap {...testProps()} />, div);
-  expect(div.querySelectorAll('rect').length).toEqual(365);
+  expect(div.querySelectorAll('rect').length).toEqual(364);
   expect(div.querySelectorAll('rect[fill]').length).toEqual(3);
   expect(div.querySelectorAll('title').length).toEqual(3);
-});
-
-it('can render multiple years', () => {
-  const div = document.createElement('div');
-  const props = {...testProps(), years: [2017, 2019]};
-  ReactDOM.render(<CalendarHeatmap {...props} />, div);
-  expect(div.innerHTML).toContain('2017');
-  expect(div.innerHTML).toContain('2018');
-  expect(div.querySelectorAll('rect').length).toEqual(365 * 2);
 });
--- app/assets/javascripts/district_notes/NotesHeatmapPage.test.js
+++ app/assets/javascripts/district_notes/NotesHeatmapPage.test.js
@@ -5,10 +5,14 @@ import NotesHeatmap from './NotesHeatmap';
 function testProps(props) {
   return {
     heatmapNotes: [
-      {"id":1,"recorded_at":"2010-10-13T00:00:00.000Z","student_id":1,"event_note_type_id":300,"grade":"3"},
-      {"id":2,"recorded_at":"2010-10-13T00:00:00.000Z","student_id":2,"event_note_type_id":302,"grade":"3"},
-      {"id":3,"recorded_at":"2010-12-27T00:00:00.000Z","student_id":1,"event_note_type_id":301,"grade":"3"},
-      {"id":4,"recorded_at":"2010-12-23T00:00:00.000Z","student_id":1,"event_note_type_id":300,"grade":"3"}
+      {"id":1,"recorded_at":"2010-10-13T00:00:00.000Z","event_note_type_id":300,"student": { "id":1, "grade":"3", "school_id": 2 }},
+      {"id":2,"recorded_at":"2010-10-13T00:00:00.000Z","event_note_type_id":302,"student": { "id":2, "grade":"3", "school_id": 2 }},
+      {"id":3,"recorded_at":"2010-12-27T00:00:00.000Z","event_note_type_id":301,"student": { "id":1, "grade":"3", "school_id": 3 }},
+      {"id":4,"recorded_at":"2010-12-23T00:00:00.000Z","event_note_type_id":300,"student": { "id":1, "grade":"3", "school_id": 2 }}
+    ],
+    schools: [
+      {"id": 2, "name": "Parker" }, 
+      {"id": 3, "name": "Pulaski" }
     ],
     ...props
   };
@@ -20,11 +24,11 @@ it('renders UI elements', () => {
   const text = $(div).text();
   expect(text).toContain('Notes over time');
   expect(text).toContain('Legend');
-  expect(text).toContain('Year: 2010');
+  expect(text).toContain('2010 - 2011');
   expect(text).toContain('District');
   expect(text).toContain('SST: 50%');
   expect(text).toContain('MTSS: 25%');
   expect(div.querySelector('.btn').innerHTML).toContain('Show each grade');
-  expect(div.querySelectorAll('rect').length).toEqual(365);
+  expect(div.querySelectorAll('rect').length).toEqual(364);
   expect(div.querySelectorAll('rect[fill]').length).toEqual(3);
 });
