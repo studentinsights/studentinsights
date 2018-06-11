5462672bac3a9acf7cab701780e33e60166b7d0c
Alex Soble <asoble@gmail.com>
2018-06-11 17:18:22 -0500

    Restore prod query path

--- app/controllers/service_types_controller.rb
+++ app/controllers/service_types_controller.rb
@@ -18,19 +18,17 @@ class ServiceTypesController < ApplicationController
   def is_service_working_json
     service_type_id = params[:service_type_id]
 
-    # service_type = ServiceType.find(service_type_id)
-    # student_ids = service_type.services.where('date_started > ?', Time.current - 1.year).map(&:student_id)
+    service_type = ServiceType.find(service_type_id)
+    student_ids = service_type.services.where('date_started > ?', Time.current - 1.year).map(&:student_id)
 
-    # chart_data = Student.active.where(id: student_ids).map do |student|
-    #   {
-    #     student: student,
-    #     school: student.school.local_id,
-    #     services: student.services.where(service_type_id: service_type_id),
-    #     absences: student.absences.order(occurred_at: :desc)
-    #   }
-    # end
-
-    chart_data = JSON.parse(IO.read("/Users/alexsoble/DANGER_DATA/#{service_type_id}.json"))
+    chart_data = Student.active.where(id: student_ids).map do |student|
+      {
+        student: student,
+        school: student.school.local_id,
+        services: student.services.where(service_type_id: service_type_id),
+        absences: student.absences.order(occurred_at: :desc)
+      }
+    end
 
     render json: { chart_data: chart_data }
   end
