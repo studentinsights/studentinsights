c3ef5be670bdf9b862f81d8071bc76c6bf4347d5
Alex Soble <asoble@gmail.com>
2018-06-12 10:49:16 -0500

    Fix up linter issues with main.js

--- app/assets/javascripts/service_types/main.js
+++ app/assets/javascripts/service_types/main.js
@@ -1,8 +1,8 @@
-import MixpanelUtils from '../helpers/mixpanel_utils.jsx';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import IsServiceWorking from '../service_types/IsServiceWorking.js';
 
 export default function renderIsServiceWorking() {
-  const ReactDOM = window.ReactDOM;
   const serializedData = $('#serialized-data').data();
 
   ReactDOM.render(
