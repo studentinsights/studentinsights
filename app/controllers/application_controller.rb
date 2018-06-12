7b11dcd2d2c9f4866ab3ee13a831888a21b1081e
Kevin Robinson <kevin.robinson.0@gmail.com>
2018-06-12 09:03:16 -0400

    Revert "Revert "Allow masquerading as different users""

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -67,6 +67,28 @@ class ApplicationController < ActionController::Base
     return_value
   end
 
+  # override
+  # This overrides the Devise method in order to allow masquerading
+  # as another user.
+  def current_educator(options = {})
+    # `super: true` allows callers to get the real underlying Devise user even
+    # when there is masquerading.  This should only be used in limited
+    # places (eg, the code involved in setting and clearing the masquerade).
+    if options.has_key?(:super) && options[:super]
+      super()
+    elsif masquerade.authorized? && masquerade.is_masquerading?
+      masquerade.current_educator
+    else
+      super()
+    end
+  end
+
+  # Controls masquerading as another user
+  helper_method :masquerade
+  def masquerade
+    @masquerade ||= Masquerade.new(session, -> { current_educator(super: true) })
+  end
+
   private
   def authorizer
     @authorizer ||= Authorizer.new(current_educator)
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :educators
     get '/authorization' => 'educators#authorization'
+    post '/masquerade/become' => 'masquerade#become'
+    post '/masquerade/clear' => 'masquerade#clear'
     root to: "educators#index"
   end
 
