5bdb748942e4fe41503380e5571a445534de3179
Alex Soble <asoble@gmail.com>
2018-06-18 11:30:41 -0500

    Remove StudentIdDoesNotMatch exception

--- app/lib/exceptions.rb
+++ app/lib/exceptions.rb
@@ -3,5 +3,4 @@ module Exceptions
   class NoAssignedSections < StandardError; end
   class EducatorNotAuthorized < StandardError; end
   class DistrictKeyNotHandledError < StandardError; end
-  class StudentIdDoesNotMatch < StandardError; end
 end
