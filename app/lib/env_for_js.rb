45dc6e65835513f56baddf9d196488712b6f7148
Kevin Robinson <kevin.robinson.0@gmail.com>
2018-06-19 16:19:52 -0400

    Add test for EnvForJs

--- app/lib/env_for_js.rb
+++ app/lib/env_for_js.rb
@@ -1,13 +1,15 @@
+# This is the data sent down to the JS code in the UI
+# Keep in mind that anything here is insecure.
 class EnvForJs
   def as_json
     {
       railsEnvironment: Rails.env,
+      sessionTimeoutInSeconds: Devise.timeout_in.to_i,
       districtKey: EnvironmentVariable.value('DISTRICT_KEY'),
       deploymentKey: EnvironmentVariable.value('DEPLOYMENT_KEY'),
+      isDemoSite: EnvironmentVariable.is_true('DEMO_SITE'),
       shouldReportAnalytics: EnvironmentVariable.is_true('SHOULD_REPORT_ANALYTICS'),
       shouldReportErrors: EnvironmentVariable.is_true('SHOULD_REPORT_ERRORS'),
-      isDemoSite: EnvironmentVariable.is_true('DEMO_SITE'),
-      sessionTimeoutInSeconds: Devise.timeout_in.to_i,
       mixpanelToken: EnvironmentVariable.value('MIXPANEL_TOKEN'),
       rollbarJsAccessToken: EnvironmentVariable.value('ROLLBAR_JS_ACCESS_TOKEN'),
     }
