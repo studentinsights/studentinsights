79821f96931fe2d14ba0fc57668594b68314c603
Kevin Robinson <kevin.robinson.0@gmail.com>
2018-06-19 14:10:03 -0400

    Add react-select inline styles to CSP whitelist

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -13,7 +13,19 @@ SecureHeaders::Configuration.default do |config|
       connect_src: %w('self' https:),
       form_action: %w('self' https:),
       script_src: %w('unsafe-inline' https: api.mixpanel.com cdn.mxpnl.com https://cdnjs.cloudflare.com/ajax/libs/rollbar.js/),
-      style_src: %w('self' https: fonts.googleapis.com),
+
+      # most of these are from react-select
+      # see https://github.com/JedWatson/react-select/issues/2030
+      # and https://github.com/JedWatson/react-input-autosize#csp-and-the-ie-clear-indicator
+      style_src: %w(
+        'self'
+        'sha256-0nyLRBz+flfRx09c4cmwCuyAActw00JI7EotsctBGyE='
+        'sha256-47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU='
+        'sha256-0A52LJhsO5+DZbOwJT8O6MYgDV+bXVxvgzJm0x3jxnI='
+        'sha256-Ns85mrVfDtTI+AJP+HXSBWflADJ/6n0q5JKJC7KktSQ='
+        https:
+        fonts.googleapis.com
+        ),
       font_src: %w('self' https: data: fonts.gstatic.com),
       img_src: %w('self' https: data:),
       report_uri: %w(https://studentinsights-csp-logger.herokuapp.com/csp),
