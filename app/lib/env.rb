89248c614f12b807a7eaf0a0106a4ab118151ee8
Kevin Robinson <kevin.robinson.0@gmail.com>
2018-06-19 16:37:41 -0400

    WIP

---
+++ app/lib/env.rb
@@ -0,0 +1,22 @@
+class Env
+  def self.set_for_development_and_test!
+    return unless Rails.env.development? || Rails.env.test?
+
+    # instance config
+    ENV['DISTRICT_KEY'] = 'somerville'
+    ENV['DISTRICT_NAME'] = 'Localhost Public Schools'
+
+    # service config
+    ENV['USE_MOCK_LDAP'] = 'true'
+    ENV['MOCK_LDAP_PASSWORD'] = 'demo-password'
+    ENV['AWS_REGION'] = 'us-west-2'
+    ENV['MIXPANEL_TOKEN'] = 'foo';
+    ENV['ROLLBAR_JS_ACCESS_TOKEN'] = 'foo';
+
+    # feature switches
+    ENV['ENABLE_CLASS_LISTS'] = 'true'
+    ENV['ENABLE_COUNSELOR_BASED_FEED'] = 'true'
+    ENV['HOUSEMASTERS_AUTHORIZED_FOR_GRADE_8'] = 'true'
+    ENV['USE_PLACEHOLDER_STUDENT_PHOTO'] = 'true'
+  end
+end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,12 +1,6 @@
 Rails.application.configure do
-  ENV['DISTRICT_KEY'] = 'somerville'
-  ENV['DISTRICT_NAME'] = 'Localhost Public Schools'
-  ENV['USE_MOCK_LDAP'] = 'true'
-  ENV['MOCK_LDAP_PASSWORD'] = 'demo-password'
-  ENV['AWS_REGION'] = 'us-west-2'
-  ENV['ENABLE_CLASS_LISTS'] = 'true'
-  ENV['ENABLE_COUNSELOR_BASED_FEED'] = 'true'
-  ENV['HOUSEMASTERS_AUTHORIZED_FOR_GRADE_8'] = 'true'
+  # Set env variables
+  Env.set_for_development_and_test!
 
   # The test environment is used exclusively to run your application's
   # test suite. You never need to work with it otherwise. Remember that
