de5b340a79295c8119459f59058725155f814f81
Kevin Robinson <kevin.robinson.0@gmail.com>
2018-06-19 15:40:18 -0400

    Restore production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,23 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+  # ---- Student Insights additions ----
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.default_url_options = {
+    host: "https://#{LoadDistrictConfig.new.canonical_domain}/"
+  }
+
+  ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => "https://#{LoadDistrictConfig.new.canonical_domain}/",
+    :authentication => :plain,
+  }
+
+  ActionMailer::Base.delivery_method = :smtp
 end
