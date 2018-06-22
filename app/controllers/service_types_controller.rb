d88f2f08378fbf7123b732c98f941d497e82de90
Alex Soble <asoble@gmail.com>
2018-06-22 10:31:01 -0500

    Bye whitspace

--- app/controllers/service_types_controller.rb
+++ app/controllers/service_types_controller.rb
@@ -1,5 +1,6 @@
 class ServiceTypesController < ApplicationController
   # Used to supply valid service type names to the service upload page.
+
   # Authentication by default inherited from ApplicationController.
 
   def index
--- ui/legacyRouteHandler.js
+++ ui/legacyRouteHandler.js
@@ -7,6 +7,7 @@ import renderSectionMain from '../app/assets/javascripts/section/main';
 import renderServiceUploadsMain from '../app/assets/javascripts/service_uploads/main';
 import renderSchoolAdminDashboardMain from '../app/assets/javascripts/school_administrator_dashboard/main';
 
+
 // Placeholder routing (not fully client-side, just on page load).
 // Clicking links still reloads the whole page from the server.
 //
