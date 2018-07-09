3a789b850642b2b5dcc574c2bf32a4e292c5a827
Kevin Robinson <kevin.robinson.0@gmail.com>
2018-07-09 16:51:35 -0400

    Log cleaning output

--- app/importers/file_importers/attendance_importer.rb
+++ app/importers/file_importers/attendance_importer.rb
@@ -39,6 +39,7 @@ class AttendanceImporter
     end
 
     log('Done loop.')
+    log("StreamingCsvTransformer#stats: #{streaming_csv.stats}")
     log("@skipped_from_school_filter: #{@skipped_from_school_filter}")
     log("@skipped_old_rows_count: #{@skipped_old_rows_count}")
     log("@skipped_other_rows_count: #{@skipped_other_rows_count}")
