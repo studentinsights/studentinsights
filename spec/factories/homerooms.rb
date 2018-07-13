82ace185ffc15f0b17e4d800cdfe35c286a4f97d
Alex Soble <asoble@gmail.com>
2018-07-13 18:29:31 -0500

    Remove 'with_risk_level'

--- spec/factories/homerooms.rb
+++ spec/factories/homerooms.rb
@@ -12,19 +12,19 @@ FactoryBot.define do
 
     factory :homeroom_with_student do
       after(:create) do |homeroom|
-        homeroom.students << FactoryBot.create(:student, :with_risk_level, :registered_last_year, homeroom: homeroom)
+        homeroom.students << FactoryBot.create(:student, :registered_last_year, homeroom: homeroom)
       end
     end
 
     factory :homeroom_with_second_grader do
       after(:create) do |homeroom|
-        homeroom.students << FactoryBot.create(:second_grade_student, :with_risk_level, :registered_last_year, homeroom: homeroom)
+        homeroom.students << FactoryBot.create(:second_grade_student, :registered_last_year, homeroom: homeroom)
       end
     end
 
     factory :homeroom_with_pre_k_student do
       after(:create) do |homeroom|
-        homeroom.students << FactoryBot.create(:pre_k_student, :with_risk_level, :registered_last_year, homeroom: homeroom)
+        homeroom.students << FactoryBot.create(:pre_k_student, :registered_last_year, homeroom: homeroom)
       end
     end
 
