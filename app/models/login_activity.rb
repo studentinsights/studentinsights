999081e0eb72529a090a06b4ecaf88adbd254c9b
Alex Soble <asoble@gmail.com>
2018-07-16 14:28:09 -0500

    Add polymorphic association (for successful logins only)

--- app/models/login_activity.rb
+++ app/models/login_activity.rb
@@ -1,5 +1,6 @@
 class LoginActivity < ApplicationRecord
   belongs_to :user,
+             polymorphic: true,
              optional: true   # Optional because an attacker might try logging
                               # in with an email that doesn't belong to any educator.
                               # (Or more generously, because someone might typo their email.)
