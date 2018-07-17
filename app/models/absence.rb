349fd3924d428e5d97ef601422af247868681603
Kevin Robinson <kevin.robinson.0@gmail.com>
2018-07-17 18:44:08 -0400

    Remove model validations on Absence and Tardy, since they regress Importer performance

--- app/models/absence.rb
+++ app/models/absence.rb
@@ -1,6 +1,12 @@
 class Absence < ActiveRecord::Base
   belongs_to :student
 
-  validates :student, presence: true
-  validates :occurred_at, presence: true, uniqueness: { scope: [:student_id] }
+  # The database enforces more constraints, since adding them
+  # as validations here impacts the performance of the import task.
+  # With validations on associations, each call to check whether a record
+  # is valid triggers additional queries to other tables to validate the foreign keys
+  # In most cases, this in unnecessary, and when there is a violation the database
+  # will enforce it.
+  validates :student_id, presence: true
+  validates :occurred_at, presence: true
 end
