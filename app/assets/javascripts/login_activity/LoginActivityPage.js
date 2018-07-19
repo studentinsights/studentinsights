432161ef97f13b56d8d8cfedc8742e47f1e50d02
Alex Soble <asoble@gmail.com>
2018-07-19 10:23:35 -0500

    Send down the right query

--- app/assets/javascripts/login_activity/LoginActivityPage.js
+++ app/assets/javascripts/login_activity/LoginActivityPage.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import GenericLoader from '../components/GenericLoader';
 import {apiFetchJson} from '../helpers/apiFetchJson';
+import moment from 'moment';
 
 export default class LoginActivityPage extends React.Component {
 
   fetchLoginActivities() {
-    const createdAtOrBefore = 0;  // todo
-    const createdAfter = 0;  // todo
-    const url = `/api/login_activity?created_at_or_before=${createdAtOrBefore}&created_after=${createdAfter}`;
+    const nowTimestamp = moment().unix();
+    const thirtyDaysAgoTimestamp = moment().subtract(30, 'days').unix();
+
+    const endpoint = '/api/login_activity';
+    const params = `?created_at_or_before=${nowTimestamp}&created_after=${thirtyDaysAgoTimestamp}`;
+    const url = endpoint + params;
+
     return apiFetchJson(url);
   }
 
