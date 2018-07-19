1525b40005b5e09f7a2ea6cabaf0f300e9f22b4c
Kevin Robinson <kevin.robinson.0@gmail.com>
2018-07-19 09:17:53 -0400

    Fixup other call sites for FakeStudent

--- spec/demo_data/fake_student_spec.rb
+++ spec/demo_data/fake_student_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe FakeStudent do
   let!(:homeroom) { FactoryBot.create(:homeroom, grade: '5') }
   before { FactoryBot.create(:educator, :admin) }
 
-  let(:student) { described_class.new(school, homeroom).student }
+  let(:student) { FakeStudent.create!(school, homeroom) }
 
   it 'sets student name' do
     expect(student.first_name).not_to be_nil
