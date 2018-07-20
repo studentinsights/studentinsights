2f958e57487691b3b66122123a8e2c0268938e98
Kevin Robinson <kevin.robinson.0@gmail.com>
2018-07-20 12:00:13 -0400

    Done

---
+++ app/assets/javascripts/school_administrator_dashboard/absences_dashboard/SchoolAbsenceDashboard.js
@@ -0,0 +1,263 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import _ from 'lodash';
+import moment from 'moment';
+import {supportsExcusedAbsences} from '../../helpers/PerDistrict';
+import SectionHeading from '../../components/SectionHeading';
+import DashboardHelpers from '../DashboardHelpers';
+import StudentsTable from '../StudentsTable';
+import DashboardBarChart from '../DashboardBarChart';
+import DashRangeButtons from '../DashRangeButtons';
+import DashButton from '../DashButton';
+
+
+export default class SchoolAbsenceDashboard extends React.Component {
+
+  constructor(props, context) {
+    super(props);
+
+    const {dateRange} = props;
+    const {districtKey} = context;
+    const showExcused = supportsExcusedAbsences(districtKey) ? false : true;
+    this.state = {
+      showExcused,
+      displayDates: dateRange,
+      selectedHomeroom: null,
+      selectedRange: 'This School Year'
+    };
+    this.setStudentList = (highchartsEvent) => {
+      this.setState({selectedHomeroom: highchartsEvent.point.category});
+    };
+    this.resetStudentList = () => {
+      this.setState({selectedHomeroom: null});
+    };
+  }
+
+  //Monthly attendance for the school must be calculated after the range filter is applied
+  monthlySchoolAttendance(schoolAverageDailyAttendance) {
+    let monthlySchoolAttendance = {};
+    //Use the filtered daterange to find the days to include
+    this.state.displayDates.forEach((day) => {
+      const date = moment.utc(day).date(1).format("YYYY-MM"); //first day of the month in which 'day' occurs
+      if (schoolAverageDailyAttendance[day] !== undefined) { //school days are based on all absences so it's possible this is undefined when getting the total for unexcused absences
+        (monthlySchoolAttendance[date] === undefined) ? //if there's nothing for this month yet
+        monthlySchoolAttendance[date] = [schoolAverageDailyAttendance[day]] :
+        monthlySchoolAttendance[date] = monthlySchoolAttendance[date].concat(schoolAverageDailyAttendance[day]);
+      }
+    });
+    return monthlySchoolAttendance;
+  }
+
+  monthlyHomeroomAttendance(dailyHomeroomAttendance) {
+    let monthlyHomeroomAttendance = {};
+    Object.keys(dailyHomeroomAttendance).forEach((homeroom) => {
+      const rawAvg = _.sum(dailyHomeroomAttendance[homeroom])/dailyHomeroomAttendance[homeroom].length;
+      const monthlyAverage = Math.round(rawAvg*10)/10;
+      monthlyHomeroomAttendance[homeroom] = monthlyAverage;
+    });
+
+    return monthlyHomeroomAttendance;
+  }
+
+  filteredHomeroomAttendance(dailyHomeroomAttendance) {
+    let filteredHomeroomAttendance = {};
+    Object.keys(dailyHomeroomAttendance).forEach((homeroom) => {
+      filteredHomeroomAttendance[homeroom] = this.state.displayDates.map((date) => {
+        return dailyHomeroomAttendance[homeroom][date];
+      });
+    });
+    return filteredHomeroomAttendance;
+  }
+
+  studentAbsenceCounts() {
+    let studentAbsenceCounts = {};
+    const eventsByDay = this.state.showExcused ? this.props.schoolAbsenceEventsByDay : this.props.schoolUnexcusedAbsenceEventsByDay;
+    this.state.displayDates.forEach((day) => {
+      _.each(eventsByDay[day], (absence) => {
+        studentAbsenceCounts[absence.student_id] = studentAbsenceCounts[absence.student_id] || 0;
+        studentAbsenceCounts[absence.student_id]++;
+      });
+    });
+    return studentAbsenceCounts;
+  }
+
+  render() {
+    const {school} = this.props;
+    return (
+      <div className="SchoolAbsenceDashboard" style={styles.root}>
+        <SectionHeading>Absences at {school.name}</SectionHeading>
+        <div className="SchoolDashboard-filter-bar">
+          {this.renderRangeSelector()}
+          {this.renderExcusedAbsencesSelect()}
+        </div>
+        <div className="SchoolDashboard-columns">
+          <div className="SchoolDashboard-roster-column">
+            {this.renderStudentAbsenceTable()}
+          </div>
+          <div className="SchoolDashboard-charts-column">
+            {this.renderMonthlyAbsenceChart()}
+            {this.renderHomeroomAbsenceChart()}
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  renderRangeSelector() {
+    const {dateRange} = this.props;
+    const today = moment.utc().format("YYYY-MM-DD");
+    const ninetyDaysAgo = moment.utc().subtract(90, 'days').format("YYYY-MM-DD");
+    const fortyFiveDaysAgo = moment.utc().subtract(45, 'days').format("YYYY-MM-DD");
+    const schoolYearStart = DashboardHelpers.schoolYearStart();
+    return (
+      <DashRangeButtons
+        schoolYearFilter={() => this.setState({
+          displayDates: DashboardHelpers.filterDates(dateRange, schoolYearStart, today),
+          selectedRange: 'This School Year'})}
+        ninetyDayFilter={() => this.setState({
+          displayDates: DashboardHelpers.filterDates(dateRange, ninetyDaysAgo, today),
+          selectedRange: 'Past 90 Days'})}
+        fortyFiveDayFilter={() => this.setState({
+          displayDates: DashboardHelpers.filterDates(dateRange, fortyFiveDaysAgo, today),
+          selectedRange: 'Past 45 Days'})}/>
+    );
+  }
+
+  renderStudentAbsenceTable() {
+    const studentAbsenceCounts = this.studentAbsenceCounts();
+    const studentsByHomeroom = DashboardHelpers.groupByHomeroom(this.props.dashboardStudents);
+    const students = studentsByHomeroom[this.state.selectedHomeroom] || this.props.dashboardStudents;
+    let rows =[];
+    students.forEach((student) => {
+      rows.push({
+        id: student.id,
+        first_name: student.first_name,
+        last_name: student.last_name,
+        latest_note: student.latest_note,
+        events: studentAbsenceCounts[student.id] || 0,
+        grade: student.grade,
+      });
+    });
+
+    const {selectedRange} = this.state;
+
+    return (
+      <StudentsTable
+        rows={rows}
+        selectedCategory={this.state.selectedHomeroom}
+        incidentType='Absences'
+        incidentSubtitle={selectedRange}
+        resetFn={this.resetStudentList}/>
+    );
+  }
+
+  renderExcusedAbsencesSelect() {
+    const {districtKey} = this.context;
+    if (!supportsExcusedAbsences(districtKey)) return null;
+
+    return(
+      <div style={styles.excusedFilter} className="SchoolAbsenceDashboard-excused-absences-select">
+        <DashButton
+            buttonText={"Unexcused Absences Only"}
+            onClick={() => this.setState({showExcused: false})}
+            isSelected={!this.state.showExcused}/>
+        <DashButton
+          buttonText={"All Absences"}
+          onClick={() => this.setState({showExcused: true})}
+          isSelected={this.state.showExcused}/>
+      </div>
+    );
+  }
+
+  renderMonthlyAbsenceChart() {
+    const dailyAttendance = this.state.showExcused ?
+                            this.props.schoolAverageDailyAttendance :
+                            this.props.schoolAverageDailyAttendanceUnexcused;
+    const monthlyAttendance = this.monthlySchoolAttendance(dailyAttendance);
+    const filteredAttendanceSeries = Object.keys(monthlyAttendance).map( (month) => {
+      const rawAvg = _.sum(monthlyAttendance[month])/monthlyAttendance[month].length;
+      return Math.round(rawAvg*10)/10;
+    });
+    const categories = Object.keys(monthlyAttendance);
+
+    return (
+        <DashboardBarChart
+          id = {'string'}
+          categories = {{categories: categories}}
+          seriesData = {filteredAttendanceSeries}
+          yAxisMin = {80}
+          yAxisMax = {100}
+          titleText = {`Average Attendance By Month (${this.state.selectedRange})`}
+          measureText = {'Attendance (Percent)'}
+          tooltip = {{
+            pointFormat: 'Average Daily Attendance: <b>{point.y}</b>',
+            valueSuffix: '%'}}
+          onColumnClick = {this.resetStudentList}
+          onBackgroundClick = {this.resetStudentList}/>
+    );
+  }
+
+  renderHomeroomAbsenceChart() {
+    const homeroomAverageDailyAttendance =  this.state.showExcused ?
+                                            this.props.homeroomAverageDailyAttendance :
+                                            this.props.homeroomAverageDailyAttendanceUnexcused;
+    const filteredHomeroomAttendance = this.filteredHomeroomAttendance(homeroomAverageDailyAttendance); //remove dates outside of selected range
+    const monthlyHomeroomAttendance = this.monthlyHomeroomAttendance(filteredHomeroomAttendance); //Average homeroom attendance by month
+    const homerooms = Object.keys(monthlyHomeroomAttendance).sort((a,b) => { //sort homerooms by attendance, low to high
+      return monthlyHomeroomAttendance[a] - monthlyHomeroomAttendance[b];
+    });
+    const homeroomSeries = homerooms.map((homeroom) => {
+      return monthlyHomeroomAttendance[homeroom];
+    });
+
+    return (
+      <DashboardBarChart
+        id = {'string'}
+        categories = {{categories: homerooms}}
+        seriesData = {homeroomSeries}
+        yAxisMin = {80}
+        yAxisMax = {100}
+        titleText = {`Average Attendance By Homeroom (${this.state.selectedRange})`}
+        measureText = {'Attendance (Percent)'}
+        tooltip = {{
+          pointFormat: 'Average Daily Attendance: <b>{point.y}</b>',
+          valueSuffix: '%'}}
+        onColumnClick = {this.setStudentList}
+        onBackgroundClick = {this.resetStudentList}/>
+    );
+  }
+}
+SchoolAbsenceDashboard.contextTypes = {
+  districtKey: PropTypes.string.isRequired
+};
+SchoolAbsenceDashboard.propTypes = {
+  schoolAverageDailyAttendance: PropTypes.object.isRequired,
+  schoolAverageDailyAttendanceUnexcused: PropTypes.object.isRequired,
+  homeroomAverageDailyAttendance: PropTypes.object.isRequired,
+  homeroomAverageDailyAttendanceUnexcused: PropTypes.object.isRequired,
+  dashboardStudents: PropTypes.array.isRequired,
+  schoolAbsenceEventsByDay: PropTypes.object.isRequired,
+  schoolUnexcusedAbsenceEventsByDay: PropTypes.object.isRequired,
+  dateRange: PropTypes.array.isRequired,
+  school: PropTypes.shape({
+    name: PropTypes.string.isRequired
+  }).isRequired
+};
+
+const styles = {
+  root: {
+    flex: 1,
+    width: '100%',
+    paddingLeft: 10,
+    paddingRight: 10,
+    display: 'flex',
+    flexDirection: 'column'
+  },
+  excusedFilter: {
+    display: 'flex',
+    alignItems: 'flex-end',
+    paddingLeft: 20,
+    marginLeft: 20,
+    borderLeft: 'thin solid #ccc'
+  }
+};
---
+++ app/assets/javascripts/school_administrator_dashboard/absences_dashboard/SchoolAbsenceDashboard.test.js
@@ -0,0 +1,105 @@
+import React from 'react';
+import moment from 'moment';
+import {mount, shallow} from 'enzyme';
+import {withNowContext} from '../../testing/NowContainer';
+import PerDistrictContainer from '../../components/PerDistrictContainer';
+import {
+  createTestEvents,
+  createStudents,
+  testSchool
+} from '../DashboardTestData';
+import SchoolAbsenceDashboard from './SchoolAbsenceDashboard';
+
+
+function testSetup(options = {}) {
+  const districtKey = options.districtKey || 'somerville';
+  const nowMoment = moment.utc();
+  const attendance = {'2001-01-01': 50, '2001-01-02': 25, '2001-01-03': 75, '2001-01-04': 100, '2001-02-01': 10};
+  const attendanceWithExcused = {'2001-01-01': 50, '2001-01-02': 25, '2001-01-03': 50, '2001-01-04': 100, '2001-02-01': 10};
+  const homeroom = {'HR1': attendance, 'No Homeroom': {'2001-01-01': 50, '2001-01-02': 100, '2001-01-03': 100, '2001-01-04': 100, '2001-02-01': 50}};
+  const homeroomWithExcused = {'HR1': attendanceWithExcused, 'No Homeroom': {'2001-01-01': 50, '2001-01-02': 100, '2001-01-03': 100, '2001-01-04': 100, '2001-02-01': 50}};
+  const students = createStudents(nowMoment);
+  const events = createTestEvents(nowMoment);
+  const dateRange = ['2001-01-01', '2001-01-02', '2001-01-03', '2001-01-04', '2001-02-01'];
+  const context = {
+    districtKey,
+    nowFn() { return nowMoment; }
+  };
+  const el = (
+    <SchoolAbsenceDashboard
+     school={testSchool()} 
+     schoolAverageDailyAttendance={attendanceWithExcused}
+     schoolAverageDailyAttendanceUnexcused={attendance}
+     homeroomAverageDailyAttendance={homeroomWithExcused}
+     homeroomAverageDailyAttendanceUnexcused={homeroom}
+     dashboardStudents={students}
+     schoolAbsenceEventsByDay={events}
+     schoolUnexcusedAbsenceEventsByDay={events}
+     dateRange={dateRange}/>
+  );
+  return {el, context, attendance};
+}
+
+// mount doesn't support passing context directly, and shallow doesn't
+// work with wrapper components, that's why there's extra setup to translate
+// between
+function elWrappedInContext(el, context) {
+  const {districtKey, nowFn} = context;
+  const nowTimeString = nowFn().format('YYYY-MM-DDTHH:mm:ss.SSSZ');
+  return withNowContext(nowTimeString, (
+    <PerDistrictContainer districtKey={districtKey}>{el}</PerDistrictContainer>
+  ));
+}
+
+it('does not show excused absence option for New Bedford', () => {
+  const {el, context} = testSetup({districtKey: 'new_bedford'});
+  const dash = mount(elWrappedInContext(el, context));
+  expect(dash.html()).not.toContain('Unexcused Absences Only');
+  expect(dash.html()).not.toContain('All Absences');
+  expect(dash.find('.SchoolAbsenceDashboard-excused-absences-select').length).toEqual(0);
+});
+
+describe('with testSetup', () => {
+  const {el, context, attendance} = testSetup();
+  const dash = shallow(el, {context});
+
+  it('renders two bar charts', () => {
+    expect(dash.find('DashboardBarChart').length).toEqual(2);
+  });
+
+  it('renders a student list', () => {
+    expect(dash.find('StudentsTable').length).toEqual(1);
+  });
+
+  it('renders range selection buttons', () => {
+    expect(dash.find('DashRangeButtons').length).toEqual(1);
+  });
+
+  it('groups the daily attendance within a month', () => {
+    const monthly = dash.instance().monthlySchoolAttendance(attendance);
+    expect(monthly).toEqual({'2001-01': [50, 25, 75, 100], '2001-02': [10]});
+  });
+
+  it('averages the unexcused monthly attendance for the school', () => {
+    expect(dash.find('DashboardBarChart').first().props().seriesData).toEqual([62.5,10]);
+  });
+
+  it('averages the attendance by homeroom', () => {
+    expect(dash.find('DashboardBarChart').last().props().seriesData).toEqual([52,80]);
+  });
+
+  it('includes the excused attendance when filter is set', () => {
+    dash.setState({showExcused: true});
+    expect(dash.find('DashboardBarChart').first().props().seriesData).toEqual([56.3,10]);
+    expect(dash.find('DashboardBarChart').last().props().seriesData).toEqual([47,80]);
+  });
+
+  it('filters dates outside the range for the school', () => {
+    dash.setState({showExcused: false, displayDates:['2001-01-02', '2001-01-03', '2001-01-04']});
+    expect(dash.find('DashboardBarChart').first().props().seriesData).toEqual([66.7]);
+  });
+
+  it('filters dates outside the range for homerooms', () => {
+    expect(dash.find('DashboardBarChart').last().props().seriesData).toEqual([66.7,100]);
+  });
+});
