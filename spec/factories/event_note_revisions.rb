edf9fe6d8d8ec209b38af22720c8b1e0fc841a53
Kevin Robinson <kevin.robinson.0@gmail.com>
2018-07-24 10:49:55 -0400

    Update spec and factory to produce valid data

--- spec/factories/event_note_revisions.rb
+++ spec/factories/event_note_revisions.rb
@@ -2,8 +2,8 @@ FactoryBot.define do
   factory :event_note_revision do
     association :event_note
     association :educator
-    student_id 1
-    event_note_type_id 1
+    association :student
+    event_note_type { EventNoteType.all.sample }
     text "MyText"
     created_at "2016-04-11 01:41:48"
     updated_at "2016-04-11 01:41:48"
