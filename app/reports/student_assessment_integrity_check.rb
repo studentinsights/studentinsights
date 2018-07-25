2131262a884a38b2adf876d4d7c399667353aa60
Alex Soble <asoble@gmail.com>
2018-07-25 08:46:53 -0500

    Add integrity checks for STAR-specific fields

--- app/reports/student_assessment_integrity_check.rb
+++ app/reports/student_assessment_integrity_check.rb
@@ -16,6 +16,7 @@ class StudentAssessmentIntegrityCheck
     total_records = records.count
 
     percentile_rank_present = records.where.not(percentile_rank: nil)
+    grade_equivalent_present = records.where.not(grade_equivalent: nil)
     scale_score_nil = records.where(scale_score: nil)
     growth_percentile_nil = records.where(growth_percentile: nil)
     performance_level_nil = records.where(performance_level: nil)
@@ -24,9 +25,18 @@ class StudentAssessmentIntegrityCheck
     puts "Total records: #{total_records}"
 
     puts "percentile_rank_present: #{percentile_rank_present.count}/#{total_records}"
+    puts "grade_equivalent_present: #{grade_equivalent_present.count}/#{total_records}"
     puts "scale_score_nil: #{scale_score_nil.count}/#{total_records}"
     puts "growth_percentile_nil: #{growth_percentile_nil.count}/#{total_records}"
     puts "performance_level_ni: #{performance_level_nil.count}/#{total_records}"
+    puts
+
+    star_reading_id = Assessment.where(subject: 'Reading', family: 'STAR').pluck(:id)
+    star_reading_records = StudentAssessment.where(assessment_id: star_reading_id)
+    irl_present = star_reading_records.where.not(instructional_reading_level: nil)
+
+    puts "STAR Reading:"
+    puts "instructional_reading_level_present: #{irl_present.count}/#{total_records}"
     puts
   end
 
