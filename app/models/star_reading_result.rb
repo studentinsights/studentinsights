beae5fa73df11bc2166e871e75e10e4070859f93
Alex Soble <asoble@gmail.com>
2018-07-30 16:46:30 -0500

    Add code comments on migration strategy

--- app/models/star_reading_result.rb
+++ app/models/star_reading_result.rb
@@ -1,3 +1,8 @@
+# This is a new model that forks STAR Math off the monolith student_assessment model.
+# Migrating over in stages:
+#   - First, dump data from the import process into the new model's table.
+#   - Next, point endpoints in the app over to read from the new table.
+#   - Finally, remove STAR results from the student_assessment model/table.
 class StarReadingResult < ApplicationRecord
   belongs_to :student
   validates :percentile_rank, :grade_equivalent, :total_time, :student,
