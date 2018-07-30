f9a253b775cf38ffc5605d3bd699c23e5d1737b0
Alex Soble <asoble@gmail.com>
2018-07-30 17:36:30 -0500

    Add notes on what's different between old/new models

--- app/models/star_math_result.rb
+++ app/models/star_math_result.rb
@@ -1,9 +1,17 @@
 # This is a new model that forks STAR Math off the monolith student_assessment model.
+#
 # Migrating over in stages:
-#   - First, dump data from the import process into the new model's table.
-#   - Next, point endpoints in the app over to read from the new table.
+#   * First, dump data from the import process into the new model's table.
+#   * Next, point endpoints in the app over to read from the new table.
 #     (The app may be showing slightly stale data until this step is shipped.)
-#   - Finally, remove STAR results from the student_assessment model/table.
+#   * Finally, remove STAR results from the student_assessment model/table.
+#
+# Key differences from older model:
+#   * Enforce `null: false` validations at the database level
+#   * Validate percentile rank
+#   * Import :total_time field
+#   * Import time as well as date for date_taken field (should probably rename field)
+
 class StarMathResult < ApplicationRecord
   belongs_to :student
   validates :percentile_rank, :grade_equivalent, :total_time, :student, presence: true
