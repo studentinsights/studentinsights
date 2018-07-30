d4be024436f246a839248a38ae7dc43b5cf43bf6
Alex Soble <asoble@gmail.com>
2018-07-30 14:55:43 -0500

    Start sketching out migrations to add new STAR tables

---
+++ db/migrate/20180730195020_add_separate_star_tables.rb
@@ -0,0 +1,18 @@
+class AddSeparateStarTables < ActiveRecord::Migration[5.2]
+  def change
+    create_table :star_math_test_results do |t|
+      t.integer :percentile_rank, null: false
+      t.string :grade_equivalent, null: false
+      t.references :student
+      t.timestamps
+    end
+
+    create_table :star_reading_test_results do |t|
+      t.integer :percentile_rank, null: false
+      t.string :grade_equivalent, null: false
+      t.decimal :instructional_reading_level, null: false
+      t.references :student
+      t.timestamps
+    end
+  end
+end
