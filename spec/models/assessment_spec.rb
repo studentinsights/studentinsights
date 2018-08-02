9d64dcf91533b88e2eda755bd18a1fb6181b2404
Alex Soble <asoble@gmail.com>
2018-08-02 12:12:37 -0500

    Remove STAR from assessment spec

--- spec/models/assessment_spec.rb
+++ spec/models/assessment_spec.rb
@@ -21,19 +21,4 @@ RSpec.describe Assessment, type: :model do
     end
   end
 
-  context 'STAR' do
-    context 'valid subject' do
-      let(:assessment) { FactoryBot.build(:assessment, family: 'STAR', subject: 'Mathematics') }
-      it 'is valid' do
-        expect(assessment).to be_valid
-      end
-    end
-    context 'invalid subject' do
-      let(:assessment) { FactoryBot.build(:assessment, family: 'STAR', subject: 'Math') }
-      it 'is invalid' do
-        expect(assessment).to be_invalid
-      end
-    end
-  end
-
 end
