9ebfe633d9b8e5d3817b08b8769d3bb765571a84
Alex Soble <asoble@gmail.com>
2018-08-03 12:14:09 -0500

    Add JS test

--- app/assets/javascripts/helpers/PerDistrict.test.js
+++ app/assets/javascripts/helpers/PerDistrict.test.js
@@ -1,6 +1,7 @@
 import {
   inExperienceTeam,
-  sortSchoolSlugsByGrade
+  sortSchoolSlugsByGrade,
+  studentTableEventNoteTypeIds
 } from './PerDistrict';
 
 it('#inExperienceTeam', () => {
@@ -12,4 +13,22 @@ it('#inExperienceTeam', () => {
 it('#sortSchoolSlugsByGrade', () => {
   expect(['shs', 'whcs'].sort(sortSchoolSlugsByGrade.bind(null, 'somerville'))).toEqual(['whcs', 'shs']);
   expect(['shs', 'whcs'].sort(sortSchoolSlugsByGrade.bind(null, 'other'))).toEqual(['shs', 'whcs']);
+});
+
+describe('#studentTableEventNoteTypeIds', () => {
+  it('handles somerville HS correctly', () => {
+    const eventNoteTypeIds = studentTableEventNoteTypeIds('somerville', 'HS');
+
+    expect(eventNoteTypeIds).toEqual([300, 305, 306]);
+  });
+  it('handles somerville elementary school correctly', () => {
+    const eventNoteTypeIds = studentTableEventNoteTypeIds('somerville', 'ESMS');
+
+    expect(eventNoteTypeIds).toEqual([300, 301]);
+  });
+  it('handles somerville Capuano early childhood center correctly', () => {
+    const eventNoteTypeIds = studentTableEventNoteTypeIds('somerville', null);
+
+    expect(eventNoteTypeIds).toEqual([300, 301]);
+  });
 });
