f07e62f72eb9d98e5ecda2175397cc05cc1417cd
Alex Soble <asoble@gmail.com>
2018-08-10 16:15:49 -0500

    Add code comments about migration path

--- app/models/dibels_result.rb
+++ app/models/dibels_result.rb
@@ -1,6 +1,12 @@
 # This model stores data for the DIBELS K-6 literacy assessment:
 # https://dibels.uoregon.edu/assessment/dibels
 
+# NOTE: Right now DIBELS data is stored in both this table (where we want to
+# read from eventually) and in the student_assessments table (where we want to
+# move away from). The next move is to change the controller and client code
+# to read data from this table. After that we can safely delete DIBELS
+# rows in the student_assessments table.
+
 class DibelsResult < ApplicationRecord
   VALID_BENCHMARK_SCORES = ['CORE', 'INTENSIVE', 'STRATEGIC']
 
