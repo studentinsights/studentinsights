90a37ba98d7314a8578db290a638412baffa41f9
Alex Soble <asoble@gmail.com>
2018-08-16 14:16:19 -0500

    Add name and color for bedford to deploy-all script

--- scripts/deploy/all.sh
+++ scripts/deploy/all.sh
@@ -3,10 +3,11 @@ echo "🚢  Fetching from GitHub..."
 git fetch origin
 
 # Double quotes since the shell and yarn both are escaping.
+# Add a name and a color for any new remotes.
 echo "🚢  Deploying..."
 yarn concurrently \
-  --names "demo,somerville,new-bedford" \
-  -c "yellow.bold,blue.bold,magenta.bold" \
+  --names "demo,somerville,bedford,new-bedford" \
+  -c "yellow.bold,blue.bold,magenta.bold,cyan.bold" \
   "'scripts/deploy/deploy.sh demo'" \
   "'scripts/deploy/deploy.sh somerville'" \
   "'scripts/deploy/deploy.sh bedford'" \
