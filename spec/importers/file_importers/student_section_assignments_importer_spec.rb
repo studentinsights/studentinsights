18837d61d2924e9d09286b40f42c4f8010470033
Kevin Robinson <kevin.robinson.0@gmail.com>
2018-08-21 13:01:03 -0400

    Update row spec for matching_insights_record_for_row

--- spec/importers/file_importers/student_section_assignments_importer_spec.rb
+++ spec/importers/file_importers/student_section_assignments_importer_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe StudentSectionAssignmentsImporter do
   def make_importer_with_initialization(options = {})
-    importer = described_class.new(options: {
+    importer = StudentSectionAssignmentsImporter.new(options: {
       school_scope: nil,
       log: LogHelper::Redirect.instance.file
     }.merge(options))
@@ -258,6 +258,93 @@ RSpec.describe StudentSectionAssignmentsImporter do
 
       it 'deletes all student section assignments for that school except the recently imported one' do
         expect(StudentSectionAssignment.count).to eq(21)
+      end
+    end
+  end
+
+  describe '#matching_insights_record_for_row' do
+    let(:importer) { make_importer_with_initialization }
+    let(:student_section_assignment) { importer.send(:matching_insights_record_for_row, row) }
+    let(:healey_school) { School.find_by_local_id('HEA') }
+    let(:brown_school) { School.find_by_local_id('BRN') }
+
+    context 'happy path' do
+      let!(:student) { FactoryBot.create(:high_school_student) }
+      let!(:course) {
+        FactoryBot.create(
+          :course, course_number: 'F100', school: healey_school
+        )
+      }
+      let!(:section) {
+        FactoryBot.create(
+          :section, course: course, section_number: 'MUSIC-005', term_local_id: 'FY'
+        )
+      }
+
+      let(:row) {
+        {
+          local_id: student.local_id,
+          section_number: 'MUSIC-005',
+          course_number: 'F100',
+          school_local_id: 'HEA',
+          term_local_id: 'FY'
+        }
+      }
+
+      it 'assigns the correct values' do
+        expect(student_section_assignment.student).to eq(student)
+        expect(student_section_assignment.section).to eq(section)
+      end
+    end
+
+    context 'section with same section_number at different school' do
+      let!(:another_course) {
+        FactoryBot.create(:course, course_number: 'F100', school: brown_school)
+      }
+      let!(:another_section) {
+        FactoryBot.create(
+          :section, course: another_course, section_number: 'MUSIC-005', term_local_id: 'FY'
+        )
+      }
+
+      let!(:student) { FactoryBot.create(:high_school_student) }
+      let!(:course) {
+        FactoryBot.create(:course, course_number: 'F100', school: healey_school)
+      }
+      let!(:section) {
+        FactoryBot.create(
+          :section, course: course, section_number: 'MUSIC-005', term_local_id: 'FY'
+        )
+      }
+
+      let(:row) {
+        {
+          local_id: student.local_id,
+          section_number: 'MUSIC-005',
+          course_number: 'F100',
+          school_local_id: 'HEA',
+          term_local_id: 'FY'
+        }
+      }
+
+      it 'assigns the correct values' do
+        expect(student_section_assignment.student).to eq(student)
+        expect(student_section_assignment.section).to eq(section)
+      end
+    end
+
+    context 'no course info or school_local_id' do
+      let!(:section) { FactoryBot.create(:section) }
+      let!(:student) { FactoryBot.create(:high_school_student) }
+      let(:row) {
+        {
+          local_id: student.local_id,
+          section_number: section.section_number,
+        }
+      }
+
+      it 'returns nil' do
+        expect(student_section_assignment).to be_nil
       end
     end
   end
