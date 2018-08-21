2ffe7ee3568dfa8dcca17dad39e8b6fae7ec37f4
Kevin Robinson <kevin.robinson.0@gmail.com>
2018-08-21 09:42:55 -0400

    Progress on integration test

--- app/importers/rows/student_section_assignment_row.rb
+++ app/importers/rows/student_section_assignment_row.rb
@@ -1,47 +1,62 @@
-class StudentSectionAssignmentRow < Struct.new(:row, :school_ids_dictionary)
-  # Represents a row in a CSV export from Somerville's Aspen X2 student information system.
-  # This structure represents student section assignments.
-  #
-  # Expects the following headers:
-  #
-  #   :local_id, :course_number, :school_local_id, :section_number,
-  #   :term_local_id
-  #
-  # Eventually this will also include the letter grade for graded courses
+# class StudentSectionAssignmentRow < Struct.new(:row, :school_ids_dictionary, :student_ids_map)
+#   # Represents a row in a CSV export from Somerville's Aspen X2 student information system.
+#   # This structure represents student section assignments.
+#   #
+#   # Expects the following headers:
+#   #
+#   #   :local_id, :course_number, :school_local_id, :section_number,
+#   #   :term_local_id
+#   #
+#   # Eventually this will also include the letter grade for graded courses
 
-  def self.build(row)
-    new(row).build
-  end
+#   # Matches a row from a CSV export with an existing or new (unsaved) Insights record
+#   # Returns nil if something about the CSV row is invalid and it can't process the row.
+#   def matching_insights_record_for_row
+#     student_id = find_student_id
+#     if student_id.nil?
+#       @invalid_studentcount += 1
+#       return nil
+#     end
 
-  def build
-    return if course.nil? || student.nil? || section.nil?
+#     course = find_course
+#     if course.nil?
+#       @invalid_course_count +=1
+#       return nil
+#     end
 
-    StudentSectionAssignment.find_or_initialize_by(student: student, section: section)
-  end
+#     section = find_section
+#     if section.nil?
+#       @invalid_section_count +=1
+#       return nil
+#     end
 
-  def student
-    return unless row[:local_id]
+#     StudentSectionAssignment.find_or_initialize_by({
+#       student_id: student_id,
+#       section: section
+#     })
+#   end
 
-    Student.find_by_local_id(row[:local_id])
-  end
+#   def find_student_id
+#     return nil unless row[:local_id]
+#     student_ids_map.lookup_student_id(row[:local_id])
+#   end
 
-  def course
-    return unless row[:course_number]
+#   def find_course
+#     return nil unless row[:course_number]
 
-    school_local_id = row[:school_local_id]
-    school_id = school_ids_dictionary[school_local_id]
+#     school_local_id = row[:school_local_id]
+#     school_id = school_ids_dictionary[school_local_id]
 
-    Course.find_by(course_number: row[:course_number], school_id: school_id)
-  end
+#     Course.find_by(course_number: row[:course_number], school_id: school_id)
+#   end
 
-  def section
-    return unless row[:section_number]
+#   def find_section
+#     return nil unless row[:section_number]
 
-    Section.find_by(
-      section_number: row[:section_number],
-      course: course,
-      term_local_id: row[:term_local_id]
-    )
-  end
-
-end
+#     Section.find_by(
+#       section_number: row[:section_number],
+#       course: course,
+#       term_local_id: row[:term_local_id]
+#     )
+#   end
+# end
--- spec/importers/file_importers/student_section_assignments_importer_spec.rb
+++ spec/importers/file_importers/student_section_assignments_importer_spec.rb
@@ -10,167 +10,244 @@ RSpec.describe StudentSectionAssignmentsImporter do
     })
   }
 
-  describe '#import_row' do
-    let!(:course) { FactoryBot.create(:course, school: high_school) }
-    let!(:section) { FactoryBot.create(:section, course: course) }
-    let!(:student) { FactoryBot.create(:student) }
-
-    context 'happy path' do
-      let(:row) {
-        {
-          local_id: student.local_id,
-          course_number: section.course.course_number,
-          school_local_id: 'SHS',
-          section_number: section.section_number,
-          term_local_id: 'FY'
-        }
-      }
-
-        before do
-          student_section_assignments_importer.import_row(row)
-        end
-
-        it 'creates a student section assignment' do
-          expect(StudentSectionAssignment.count).to eq(1)
-        end
-
-        it 'assigns the proper student to the proper section' do
-          expect(StudentSectionAssignment.first.student).to eq(student)
-          expect(StudentSectionAssignment.first.section).to eq(section)
-        end
+  describe 'integration tests' do
+    def make_importer(options = {})
+      StudentSectionAssignmentsImporter.new(options: {
+        school_scope: nil,
+        log: LogHelper::FakeLog.new
+      }.merge(options))
     end
 
-    context 'student lasid is missing' do
-      let(:row) {
-        {
-          course_number: section.course.course_number,
-          school_local_id: 'SHS',
-          section_number: section.section_number,
-          term_local_id: 'FY'
-        }
-      }
-
-      before do
-        student_section_assignments_importer.import_row(row)
-      end
-
-      it 'does not create a student section assignment' do
-        expect(StudentSectionAssignment.count).to eq(0)
-      end
+    def mock_importer_with_csv(importer, filename)
+      csv = test_csv_from_file(filename)
+      allow(importer).to receive(:download_csv).and_return(csv)
+      importer
     end
 
-    context 'section is missing' do
-      let(:row) {
-        {
-          local_id: student.local_id,
-          course_number: section.course.course_number,
-          school_local_id: 'SHS',
-          term_local_id: 'FY'
-        }
-      }
-
-      before do
-        student_section_assignments_importer.import_row(row)
-      end
-
-      it 'does not create a student section assignment' do
-        expect(StudentSectionAssignment.count).to eq(0)
-      end
+    def test_csv_from_file(filename)
+      file = File.read(filename)
+      transformer = StreamingCsvTransformer.new(LogHelper::FakeLog.new)
+      transformer.transform(file)
     end
 
-    context 'student does not exist' do
-      let(:row) {
-        {
-          local_id: 'NO EXIST',
-          course_number: section.course.course_number,
-          school_local_id: 'SHS',
-          section_number: section.section_number,
-          term_local_id: 'FY'
-        }
-      }
+    let!(:east) { School.find_by_local_id('ESCS') }
+    let!(:log) { LogHelper::FakeLog.new }
+    let!(:importer) { make_importer(log: log) }
+    before { mock_importer_with_csv(importer, "#{Rails.root}/spec/fixtures/student_section_assignment_export.txt") }
 
-      before do
-        student_section_assignments_importer.import_row(row)
-      end
-
-      it 'does not create a student section assignment' do
-        expect(StudentSectionAssignment.count).to eq(0)
-      end
+    # These match the fixture file
+    before do
+      FactoryBot.create(:student, local_id: '111')
+      FactoryBot.create(:student, local_id: '222')
+      FactoryBot.create(:student, local_id: '333')
     end
 
-    context 'section does not exist' do
-      let(:row) {
-        {
-          local_id: student.local_id,
-          course_number: section.course.course_number,
-          school_local_id: 'SHS',
-          section_number: 'NO EXIST',
-          term_local_id: 'FY'
-        }
-      }
+    let!(:east_history) { FactoryBot.create(:course, course_number: 'SOC6', school: east) }
+    let!(:shs_history) { FactoryBot.create(:course, course_number: 'SOC6', school: high_school) }
+    let!(:shs_ela) { FactoryBot.create(:course, course_number: 'ELA6', school: high_school) }
+    let!(:shs_math) { FactoryBot.create(:course, course_number: 'ALG2', school: high_school) }
+    let!(:east_history_section) { FactoryBot.create(:section, course: east_history, section_number: 'SOC6-001') }
+    let!(:shs_history_section) { FactoryBot.create(:section, course: shs_history, section_number: 'SOC6-001') }
+    let!(:shs_ela_section) { FactoryBot.create(:section, course: shs_ela, section_number: 'ELA6-002') }
+    let!(:shs_math_section) { FactoryBot.create(:section, course: shs_math, section_number: 'ALG2-004') }
 
-      before do
-        student_section_assignments_importer.import_row(row)
-      end
-
-      it 'does not create a student section assignment' do
-        expect(StudentSectionAssignment.count).to eq(0)
-      end
+    it 'works' do
+      importer.import
+      expect(StudentSectionAssignment.count).to eq 4
+      expect(log.output).to include '@invalid_student_count: 2'
+      expect(log.output).to include '@invalid_course_count: 1'
+      expect(log.output).to include '@invalid_section_count: 1'
+      expect(log.output).to include ':passed_nil_record_count=>4'
+      expect(log.output).to include ':created_rows_count=>4'
     end
+
+    it 'syncs when existing records' do
+      # will be unchanged
+      StudentSectionAssignment.create!({
+        student: Student.find_by_local_id('111'),
+        section: east_history_section
+      })
+      # will be deleted
+      StudentSectionAssignment.create!({
+        student: Student.find_by_local_id('333'),
+        section: shs_math_section
+      })
+      expect(StudentSectionAssignment.count).to eq 2
+
+      importer.import
+      expect(log.output).to include '@invalid_student_count: 2'
+      expect(log.output).to include '@invalid_course_count: 1'
+      expect(log.output).to include '@invalid_section_count: 1'
+      expect(log.output).to include ':passed_nil_record_count=>4'
+      expect(log.output).to include ':unchanged_rows_count=>1'
+      expect(log.output).to include ':created_rows_count=>3'
+      expect(log.output).to include ':destroyed_records_count=>1'
+      expect(StudentSectionAssignment.count).to eq 4
+    end
+
   end
 
-  describe '#delete_rows' do
-    let(:log) { LogHelper::Redirect.instance.file }
-    let!(:section) { FactoryBot.create(:section) }
-    let!(:student) { FactoryBot.create(:student) }
-    let(:row) {
-      {
-        local_id: student.local_id,
-        course_number: section.course.course_number,
-        school_local_id: section.course.school.local_id,
-        section_number: section.section_number,
-        term_local_id: section.term_local_id
-      }
-    }
+  # describe '#import_row' do
+  #   let!(:course) { FactoryBot.create(:course, school: high_school) }
+  #   let!(:section) { FactoryBot.create(:section, course: course) }
+  #   let!(:student) { FactoryBot.create(:student) }
 
-    context 'happy path' do
-      let(:student_section_assignments_importer) {
-        described_class.new(options: {
-          school_scope: School.pluck(:local_id), log: LogHelper::Redirect.instance.file
-        })
-      }
+  #   context 'happy path' do
+  #     let(:row) {
+  #       {
+  #         local_id: student.local_id,
+  #         course_number: section.course.course_number,
+  #         school_local_id: 'SHS',
+  #         section_number: section.section_number,
+  #         term_local_id: 'FY'
+  #       }
+  #     }
 
-      before do
-        FactoryBot.create_list(:student_section_assignment, 20)
-        FactoryBot.create(:student_section_assignment, student_id: student.id, section_id: section.id)
+  #       before do
+  #         student_section_assignments_importer.import_row(row)
+  #       end
 
-        student_section_assignments_importer.import_row(row)
-        student_section_assignments_importer.delete_rows
-      end
+  #       it 'creates a student section assignment' do
+  #         expect(StudentSectionAssignment.count).to eq(1)
+  #       end
 
-      it 'deletes all student section assignments except the recently imported one' do
-        expect(StudentSectionAssignment.count).to eq(1)
-      end
-    end
+  #       it 'assigns the proper student to the proper section' do
+  #         expect(StudentSectionAssignment.first.student).to eq(student)
+  #         expect(StudentSectionAssignment.first.section).to eq(section)
+  #       end
+  #   end
 
-    context 'delete only stale assignments from schools being imported' do
-      let(:student_section_assignments_importer) {
-        described_class.new(options: {
-          school_scope: ['SHS'], log: LogHelper::Redirect.instance.file
-        })
-      }
+  #   context 'student lasid is missing' do
+  #     let(:row) {
+  #       {
+  #         course_number: section.course.course_number,
+  #         school_local_id: 'SHS',
+  #         section_number: section.section_number,
+  #         term_local_id: 'FY'
+  #       }
+  #     }
 
-      before do
-        FactoryBot.create_list(:student_section_assignment, 20)
-        FactoryBot.create(:student_section_assignment, student_id: student.id, section_id: section.id)
+  #     before do
+  #       student_section_assignments_importer.import_row(row)
+  #     end
 
-        student_section_assignments_importer.import_row(row)
-        student_section_assignments_importer.delete_rows
-      end
+  #     it 'does not create a student section assignment' do
+  #       expect(StudentSectionAssignment.count).to eq(0)
+  #     end
+  #   end
 
-      it 'deletes all student section assignments for that school except the recently imported one' do
-        expect(StudentSectionAssignment.count).to eq(21)
-      end
-    end
-  end
+  #   context 'section is missing' do
+  #     let(:row) {
+  #       {
+  #         local_id: student.local_id,
+  #         course_number: section.course.course_number,
+  #         school_local_id: 'SHS',
+  #         term_local_id: 'FY'
+  #       }
+  #     }
+
+  #     before do
+  #       student_section_assignments_importer.import_row(row)
+  #     end
+
+  #     it 'does not create a student section assignment' do
+  #       expect(StudentSectionAssignment.count).to eq(0)
+  #     end
+  #   end
+
+  #   context 'student does not exist' do
+  #     let(:row) {
+  #       {
+  #         local_id: 'NO EXIST',
+  #         course_number: section.course.course_number,
+  #         school_local_id: 'SHS',
+  #         section_number: section.section_number,
+  #         term_local_id: 'FY'
+  #       }
+  #     }
+
+  #     before do
+  #       student_section_assignments_importer.import_row(row)
+  #     end
+
+  #     it 'does not create a student section assignment' do
+  #       expect(StudentSectionAssignment.count).to eq(0)
+  #     end
+  #   end
+
+  #   context 'section does not exist' do
+  #     let(:row) {
+  #       {
+  #         local_id: student.local_id,
+  #         course_number: section.course.course_number,
+  #         school_local_id: 'SHS',
+  #         section_number: 'NO EXIST',
+  #         term_local_id: 'FY'
+  #       }
+  #     }
+
+  #     before do
+  #       student_section_assignments_importer.import_row(row)
+  #     end
+
+  #     it 'does not create a student section assignment' do
+  #       expect(StudentSectionAssignment.count).to eq(0)
+  #     end
+  #   end
+  # end
+
+  # describe '#delete_rows' do
+  #   let(:log) { LogHelper::Redirect.instance.file }
+  #   let!(:section) { FactoryBot.create(:section) }
+  #   let!(:student) { FactoryBot.create(:student) }
+  #   let(:row) {
+  #     {
+  #       local_id: student.local_id,
+  #       course_number: section.course.course_number,
+  #       school_local_id: section.course.school.local_id,
+  #       section_number: section.section_number,
+  #       term_local_id: section.term_local_id
+  #     }
+  #   }
+
+  #   context 'happy path' do
+  #     let(:student_section_assignments_importer) {
+  #       described_class.new(options: {
+  #         school_scope: School.pluck(:local_id), log: LogHelper::Redirect.instance.file
+  #       })
+  #     }
+
+  #     before do
+  #       FactoryBot.create_list(:student_section_assignment, 20)
+  #       FactoryBot.create(:student_section_assignment, student_id: student.id, section_id: section.id)
+
+  #       student_section_assignments_importer.import_row(row)
+  #       student_section_assignments_importer.delete_rows
+  #     end
+
+  #     it 'deletes all student section assignments except the recently imported one' do
+  #       expect(StudentSectionAssignment.count).to eq(1)
+  #     end
+  #   end
+
+  #   context 'delete only stale assignments from schools being imported' do
+  #     let(:student_section_assignments_importer) {
+  #       described_class.new(options: {
+  #         school_scope: ['SHS'], log: LogHelper::Redirect.instance.file
+  #       })
+  #     }
+
+  #     before do
+  #       FactoryBot.create_list(:student_section_assignment, 20)
+  #       FactoryBot.create(:student_section_assignment, student_id: student.id, section_id: section.id)
+
+  #       student_section_assignments_importer.import_row(row)
+  #       student_section_assignments_importer.delete_rows
+  #     end
+
+  #     it 'deletes all student section assignments for that school except the recently imported one' do
+  #       expect(StudentSectionAssignment.count).to eq(21)
+  #     end
+  #   end
+  # end
 end
