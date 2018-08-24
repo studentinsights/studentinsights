c64792e8a0dbf90da3f1a38c90c68e60a2fb86b0
Alex Soble <asoble@gmail.com>
2018-08-24 12:17:45 -0500

    Fix linter errors

--- app/assets/javascripts/homeroom/HomeroomTable.test.js
+++ app/assets/javascripts/homeroom/HomeroomTable.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ReactTestUtils from 'react-addons-test-utils';
 import {SOMERVILLE, NEW_BEDFORD} from '../helpers/PerDistrict';
 import PerDistrictContainer from '../components/PerDistrictContainer';
 import HomeroomTable from './HomeroomTable';
