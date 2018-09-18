f5467b0b4bec06077b333c36442e79c87f6c8c81
Kevin Robinson <kevin.robinson.0@gmail.com>
2018-09-18 12:37:06 -0400

    Update school definitions

--- app/models/school.rb
+++ app/models/school.rb
@@ -7,26 +7,33 @@ class School < ActiveRecord::Base
   has_many :educators
   has_many :homerooms
 
-  def self.with_students
-    School.all.select { |s| s.students.count > 0 }
-  end
-
-  def educators_without_test_account
-    educators.where.not(local_id: 'LDAP')
-  end
-
-  def educator_names_for_services
-    educators_without_test_account.pluck(:full_name)
-  end
+  # These are internal to Insights, not from the SIS
+  SCHOOL_TYPES = [
+    'ECS', # early childhood
+    'ES', # elementary
+    'ESMS', # elementary and middle
+    'MS', # middle
+    'HS', # high
+    'OTHER' # anything else
+  ]
 
   def is_high_school?
     school_type == 'HS'
   end
 
+  # deprecated
+  def educators_without_test_account
+    educators.where.not(local_id: 'LDAP')
+  end
+
+  # deprecated
+  def educator_names_for_services
+    educators_without_test_account.pluck(:full_name)
+  end
+
   private
   def validate_school_type
-    whitelist = ['ES', 'MS', 'ESMS', 'HS', nil]
-    if !whitelist.include?(school_type)
+    if !SCHOOL_TYPES.include?(school_type)
       errors.add(:school_type, 'invalid school_type; use nil for unknown values or add to validation whitelist')
     end
   end
