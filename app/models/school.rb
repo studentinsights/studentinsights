41d34d353a567b61bb2fb6a72b52a39225cebf6c
Kevin Robinson <kevin.robinson.0@gmail.com>
2018-09-18 13:55:44 -0400

    Remove School state_id, work towards tightening validations

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,11 +1,22 @@
 class School < ActiveRecord::Base
   extend FriendlyId
   friendly_id :local_id, use: :slugged
-  validates :local_id, presence: true, uniqueness: true
-  validate :validate_school_type
+
+  # These are internal to Insights, not from the SIS
+  VALID_SCHOOL_TYPES = [
+    'ECS', # early childhood
+    'ES', # elementary
+    'ESMS', # elementary and middle
+    'MS', # middle
+    'HS', # high
+    'OTHER' # anything else
+  ]
+
   has_many :students
   has_many :educators
   has_many :homerooms
+
+  validates :local_id, presence: true, uniqueness: true
 
   def self.with_students
     School.all.select { |s| s.students.count > 0 }
@@ -37,13 +48,5 @@ class School < ActiveRecord::Base
 
   def is_high_school?
     school_type == 'HS'
-  end
-
-  private
-  def validate_school_type
-    whitelist = ['ES', 'MS', 'ESMS', 'HS', nil]
-    if !whitelist.include?(school_type)
-      errors.add(:school_type, 'invalid school_type; use nil for unknown values or add to validation whitelist')
-    end
   end
 end
