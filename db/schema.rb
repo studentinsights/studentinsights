3503902225b96e894e7d70b94548d52c50f2b678
Kevin Robinson <kevin.robinson.0@gmail.com>
2018-09-24 11:44:44 -0400

    Undo auto migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_24_145257) do
+ActiveRecord::Schema.define(version: 2018_09_24_143156) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
