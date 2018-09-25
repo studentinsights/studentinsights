8702f2efc0f96675983c0bea2ed7ceaa75933610
Kevin Robinson <kevin.robinson.0@gmail.com>
2018-09-25 17:21:40 -0400

    Update snapshots

--- app/assets/javascripts/tiering/SupportGaps.js
+++ app/assets/javascripts/tiering/SupportGaps.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import _ from 'lodash';
 import Button from '../components/Button';
 import StudentLevelsTable from './StudentLevelsTable';
 
