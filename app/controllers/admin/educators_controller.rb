abdfc44ec7e3ae8f3a1fa7f4edbe29fbcd4ddcd7
Kevin Robinson <kevin.robinson.0@gmail.com>
2018-09-25 07:55:17 -0400

    Update all uses for GradeLevels

--- app/controllers/admin/educators_controller.rb
+++ app/controllers/admin/educators_controller.rb
@@ -10,7 +10,7 @@ module Admin
     end
 
     def edit
-      @valid_grades = %w(PK KF SP 1 2 3 4 5 6 7 8 9 10 11 12)
+      @valid_grades = GradeLevels::ORDERED_GRADE_LEVELS
       super
     end
 
