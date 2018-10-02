2882ff8c0e2d9fa64299cd1e5ec25ae6d5f32ddd
EricDS <edavidsonsawyer@gmail.com>
2018-10-02 13:10:45 -0400

    handle 0 incidents case

--- app/assets/javascripts/school_administrator_dashboard/discipline_dashboard/SchoolDisciplineDashboard.js
+++ app/assets/javascripts/school_administrator_dashboard/discipline_dashboard/SchoolDisciplineDashboard.js
@@ -149,31 +149,31 @@ export default class SchoolDisciplineDashboard extends React.Component {
   sortedTimes(chartKeys) {
     //chartKeys will either contain a time like "4:00 pm", "10:00 am", or "Not Logged"
     return [
-    "Not Logged",
-    "12:00 am",
-    "1:00 am",
-    "2:00 am",
-    "3:00 am",
-    "4:00 am",
-    "5:00 am",
-    "6:00 am",
-    "7:00 am",
-    "8:00 am",
-    "9:00 am",
-    "10:00 am",
-    "11:00 am",
-    "12:00 pm",
-    "1:00 pm",
-    "2:00 pm",
-    "3:00 pm",
-    "4:00 pm",
-    "5:00 pm",
-    "6:00 pm",
-    "7:00 pm",
-    "8:00 pm",
-    "9:00 pm",
-    "10:00 pm",
-    "11:00 pm"];
+      "Not Logged",
+      "12:00 am",
+      "1:00 am",
+      "2:00 am",
+      "3:00 am",
+      "4:00 am",
+      "5:00 am",
+      "6:00 am",
+      "7:00 am",
+      "8:00 am",
+      "9:00 am",
+      "10:00 am",
+      "11:00 am",
+      "12:00 pm",
+      "1:00 pm",
+      "2:00 pm",
+      "3:00 pm",
+      "4:00 pm",
+      "5:00 pm",
+      "6:00 pm",
+      "7:00 pm",
+      "8:00 pm",
+      "9:00 pm",
+      "10:00 pm",
+      "11:00 pm"];
   }
 
   sortedGrades(chartKeys) {
@@ -209,7 +209,10 @@ export default class SchoolDisciplineDashboard extends React.Component {
   }
 
   allIncidentTypes(students) {
-    return _.uniqBy(_.flatten(students.map(student => student.discipline_incidents)), 'incident_code')
+    const incidents = students.map(student => {
+      return student.discipline_incidents ? student.discipline_incidents : []
+    });
+    return _.uniqBy(_.flatten(incidents), 'incident_code')
     .map(incident => incident.incident_code);
   }
 
