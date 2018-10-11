a3e9827d856c214bb2fd8e3a567bbeea5df55b4d
EricDS <edavidsonsawyer@gmail.com>
2018-10-11 06:51:29 -0400

    remove lodash from bar chart

--- app/assets/javascripts/school_administrator_dashboard/DashboardBarChart.js
+++ app/assets/javascripts/school_administrator_dashboard/DashboardBarChart.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import _ from 'lodash';
 import HighchartsWrapper from '../components/HighchartsWrapper';
 
 // Component for all charts in the dashboard page.
