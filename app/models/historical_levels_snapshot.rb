6385f4154ce7d4c758dc60b8991dac6212f77910
Kevin Robinson <kevin.robinson.0@gmail.com>
2018-10-16 18:06:28 -0400

    Fix Rubocop

--- app/models/historical_levels_snapshot.rb
+++ app/models/historical_levels_snapshot.rb
@@ -1,11 +1,10 @@
 # Store a historical data point about levels for SHS students
 class HistoricalLevelsSnapshot < ApplicationRecord
   def self.snapshot!(options = {})
-    raise Exceptions::EducatorNotAuthorized unless PerDistrict.new.enabled_high_school_levels?    
+    raise Exceptions::EducatorNotAuthorized unless PerDistrict.new.enabled_high_school_levels?
     school_id = options.fetch(:school_id, 'SHS')
     time_now = options.fetch(:time_now, Time.now)
     log = options.fetch(:log, STDOUT)
-    
 
     levels = SomervilleHighLevels.new
     students = Student.active.where(school_id: school_ids)
