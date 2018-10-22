05afdd4edfbc8697c4338a0e678777226c578b37
Kevin Robinson <kevin.robinson.0@gmail.com>
2018-10-22 11:53:54 -0400

    Update for changes upstream

--- app/reports/notes_review.rb
+++ app/reports/notes_review.rb
@@ -1,7 +1,9 @@
+# For groups of educators to reflect as a team.
 # Aimed at not identifying individual teachers, avoiding accountability,
-# thinking about groups and support systems.
+# thinking about groups and support systems or reflecting as a group.
 class NotesReview
   def high_school_teachers
+    ensure_somerville_only!
     compute_and_printout({
       educator_id: Educator.where(school_id: 9, schoolwide_access: false),
       student_id: Student.active.where(school_id: 9)
@@ -9,18 +11,24 @@ class NotesReview
   end
 
   def high_school_counselors
+    ensure_somerville_only!
     compute_and_printout({
       educator_id: EducatorLabel.where(label_key: 'use_counselor_based_feed').map(&:educator_id)
     })
   end
 
   def high_school_housemasters
+    ensure_somerville_only!
     compute_and_printout({
       educator_id: EducatorLabel.where(label_key: 'use_housemaster_based_feed').map(&:educator_id)
     })
   end
 
   private
+  def ensure_somerville_only!
+    raise 'somerville only!' unless PerDistrict.new.district_key == PerDistrict::SOMERVILLE
+  end
+
   def section(title)
     "\n\n#{title}\n--------------------------"
   end
@@ -30,7 +38,7 @@ class NotesReview
     n_days_back = options.fetch(:n_days_back, 45)
     time_interval = n_days_back.days
     event_notes = EventNote.where(where_event_notes).where('created_at > ?', time_now - time_interval)
-    tiers = ExperimentalSomervilleHighTiers.new(time_interval: time_interval)
+    levels = SomervilleHighLevels.new(time_interval: time_interval)
 
     lines = []
     lines << "In the last #{n_days_back} days since #{time_now}..."
@@ -45,13 +53,13 @@ class NotesReview
     lines << event_notes.includes(:student).group_by {|n| n.student.house }.sort_by {|key, values| key.to_i }.map {|key, values| [key, values.size].join("\t")}
 
     lines << section("About whom (levels)?")
-    lines << tiers.send(:unsafe_tiering_json, event_notes.includes(:student).map(&:student).uniq, time_now).group_by {|json| json['tier']['level'] }.sort_by {|key, values| key.to_i }.map {|key, values| [key, values.size].join("\t")}
+    lines << levels.unsafe_students_with_levels_json(event_notes.includes(:student).map(&:student).uniq, time_now).group_by {|json| json['level']['level_number'] }.sort_by {|key, values| key.to_i }.map {|key, values| [key, values.size].join("\t")}
 
     lines << section("By whom (educator names hidden)?")
     lines << event_notes.includes(:educator).group_by {|n| Digest::SHA256.hexdigest(n.educator.login_name) }.sort_by {|key, values| -1 * values.size }.map {|key, values| [key, values.size].join("\t")}
 
     lines << section("What does the quality look like?")
-    lines << event_notes.map(&:text).join("\n\n")
+    lines << event_notes.map(&:text).join("\n\n---------------\n\n")
 
     puts lines.join("\n")
     nil
