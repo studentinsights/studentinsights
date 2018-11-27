295480047029dc385e246e1f5f47f531215f0ac9
Kevin Robinson <kevin.robinson.0@gmail.com>
2018-11-27 17:19:52 -0500

    Factor out login failure message

--- spec/features/rack_attack_feature_spec.rb
+++ spec/features/rack_attack_feature_spec.rb
@@ -97,7 +97,7 @@ describe 'Rack::Attack respects example development config', type: :feature do
       expect(Rollbar).not_to receive(:warn)
       6.times do
         feature_plain_sign_in(rand().to_s, 'password')
-        expect(page).to have_content 'Invalid login, password, or code.'
+        expect(page).to have_content LoginTests.failed_login_message
         expect(page).not_to have_content 'Hello! This request has been blocked.'
       end
     end
