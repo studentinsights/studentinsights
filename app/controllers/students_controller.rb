502a0c5d21ec73f706c4643ea7b33dee72b9e095
Kevin Robinson <kevin.robinson.0@gmail.com>
2018-12-03 15:56:23 -0500

    Done controller cleanup

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,8 +2,7 @@ class StudentsController < ApplicationController
   include ApplicationHelper
 
   before_action :authorize!, except: [
-    :names,
-    :sample_students_json
+    :names
   ]
 
   def photo
@@ -71,28 +70,6 @@ class StudentsController < ApplicationController
     else
       render json: SearchbarHelper.names_for(current_educator)
     end
-  end
-
-  # Admin only; get a sample of students for looking at data across the site
-  def sample_students_json
-    raise Exceptions::EducatorNotAuthorized unless current_educator.can_set_districtwide_access?
-
-    seed = params.fetch(:seed, '42').to_i
-    n = params.fetch(:n, '40').to_i
-    authorized_sample_students = authorized do
-      Student.active.sample(n, random: Random.new(seed))
-    end
-    sample_students_json = authorized_sample_students.as_json({
-      only: [:id, :grade, :first_name, :last_name],
-      include: {
-        school: {
-          only: [:id, :name, :school_type]
-        }
-      }
-    })
-    render json: {
-      sample_students: sample_students_json
-    }
   end
 
   private
--- spec/controllers/sample_students_controller_spec.rb
+++ spec/controllers/sample_students_controller_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 describe SampleStudentsController, :type => :controller do
   def get_sample_students_json(params = {})
+    request.env['HTTPS'] = 'on'
     request.env['HTTP_ACCEPT'] = 'application/json'
     get :sample_students_json, params: params
     response
@@ -39,6 +40,35 @@ describe SampleStudentsController, :type => :controller do
       sign_in(pals.rich_districtwide)
       response = get_sample_students_json
       expect(response.status).to eq 403
+    end
+
+    it 'returns students for Uri' do
+      sign_in(pals.uri)
+      get_sample_students_json
+      expect(response.status).to eq 200
+      json = JSON.parse(response.body)
+      expect(json.keys).to contain_exactly('sample_students')
+      expect(json['sample_students'].size).to eq Student.all.size
+      expect(json['sample_students']).to include({
+        "id"=>pals.shs_freshman_mari.id,
+        "grade"=>"9",
+        "first_name"=>"Mari",
+        "last_name"=>"Kenobi",
+        "school"=>{
+          "id"=>pals.shs.id,
+          "school_type"=>"HS",
+          "name"=>"Somerville High"
+        }
+      })
+    end
+
+    it 'guards access for everyone else' do
+      (Educator.all - [pals.uri]).each do |educator|
+        sign_in(educator)
+        get_sample_students_json
+        expect(response.status).to eq 403
+        sign_out(educator)
+      end
     end
   end
 end
