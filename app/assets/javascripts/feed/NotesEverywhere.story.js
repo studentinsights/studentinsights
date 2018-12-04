54c0e0ad6f0803b1522c220e5ec6e404374eeb8b
Kevin Robinson <kevin.robinson.0@gmail.com>
2018-12-04 14:57:26 -0500

    Remove file not used

--- app/assets/javascripts/feed/NotesEverywhere.story.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {storiesOf} from '@storybook/react';
-import {withDefaultNowContext} from '../testing/NowContainer';
-import IncidentCard from './IncidentCard';
-import {testProps} from './IncidentCard.test';
-
-storiesOf('feed/Notes (everywhere)', module) // eslint-disable-line no-undef
-  .add('all', () => withDefaultNowContext(<IncidentCard {...testProps()} />));
