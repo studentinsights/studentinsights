e04007d9941709d395a50b79f8988cf8b701b628
Kevin Robinson <kevin.robinson.0@gmail.com>
2018-12-04 15:04:09 -0500

    Remove two unused files

--- app/assets/javascripts/student_profile/parseQueryString.test.js
+++
@@ -1,9 +0,0 @@
-import parseQueryString from './parseQueryString';
-
-describe('parseQueryString', () => {
-  it('parses a string without crashing', () => {
-    const result = parseQueryString('color=red&shape=round');
-
-    expect(result).toEqual({"color": "red", "shape": "round"});
-  });
-});
