0fac9d5077ed1cd4ef16b884fd1fbe54660b8b1f
kevinrobinson <kevin.robinson.0@gmail.com>
2018-12-10 07:29:26 -0500

    Refactoring note update path

--- app/controllers/event_notes_controller.rb
+++ app/controllers/event_notes_controller.rb
@@ -49,24 +49,17 @@ class EventNotesController < ApplicationController
     event_note = authorized_or_raise! { EventNote.find(safe_params[:id]) }
     raise Exceptions::EducatorNotAuthorized unless event_note.educator_id == current_educator.id
 
-    # First store the current state of the existing event note
-    event_note_revision = create_event_note_revision(event_note)
-    unless event_note_revision
-      return render json: { errors: event_note_revision.errors.full_messages }, status: 422
-    end
+    # Update and also store the revision history
+    update_succeeded, event_note_revision = update_note_with_revision(event_note, :text, safe_params[:event_note][:text])
 
-    # Update the EventNote, but don't update `recorded_at` - keep that at the original
-    # recording time.  The assumption is that these "updates" are from from the user continually
-    # editing during a meeting (the "start time" is fine) or from typo fixes (we don't care about
-    # those).
-    safe_params[:event_note]
-    if event_note.update({
-      text: safe_params[:event_note][:text]
-    })
+    # Error types
+    if event_note_revision.nil?
+      render json: { errors: event_note_revision.errors.full_messages }, status: 422
+    elsif !update_succeeded
+      render json: { errors: event_note.errors.full_messages }, status: 422
+    else
       serializer = EventNoteSerializer.dangerously_include_restricted_note_text(event_note)
       render json: serializer.serialize_event_note
-    else
-      render json: { errors: event_note.errors.full_messages }, status: 422
     end
   end
 
@@ -87,6 +80,21 @@ class EventNotesController < ApplicationController
     end
   end
 
+  # put
+  def mark_as_restricted
+    safe_params = params.permit(:id)
+    event_note = authorized_or_raise! { EventNote.find(safe_params[:id]) }
+    raise Exceptions::EducatorNotAuthorized if event_note.is_restricted
+    raise Exceptions::EducatorNotAuthorized unless current_educator.labels.include?('can_mark_notes_as_restricted')
+
+    event_note_revision, update_succeeded = update_note_with_revision(event_note, :is_restricted, true)
+    if event_note_revision.nil? || !update_succeeded
+      render json: { errors: 'request failed' }, status: 422
+    else
+      render json: { status: 'ok'}
+    end
+  end
+
   private
   # Guard what values can be set by the current educator
   def safe_is_restricted_value_for_create(safe_params)
@@ -95,6 +103,20 @@ class EventNotesController < ApplicationController
     else
       false
     end
+  end
+
+  # Update a single field on a note, storing revision history
+  def update_note_with_revision(event_note, attr_key, attr_value)
+    # First store the current state of the existing event note
+    event_note_revision = create_event_note_revision(event_note)
+
+    # Update the EventNote, but don't update `recorded_at` - keep that at the original
+    # recording time.  The assumption is that these "updates" are from from the user continually
+    # editing during a meeting (the "start time" is fine) or from typo fixes (we don't care about
+    # those).  Or they are from some meta change like "set this as restricted."
+    update_succeeded = event_note.update(attr_key => attr_value)
+
+    [update_succeeded, event_note_revision]
   end
 
   def create_event_note_revision(event_note)
