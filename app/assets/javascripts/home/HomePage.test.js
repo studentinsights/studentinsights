03e26f59c5c09dc881127b4f2c3b1ff9a5297a74
kevinrobinson <kevin.robinson.0@gmail.com>
2018-12-10 09:13:49 -0500

    Add link to HomePage, with spec

--- app/assets/javascripts/home/HomePage.test.js
+++ app/assets/javascripts/home/HomePage.test.js
@@ -6,10 +6,11 @@ import HomeInsights from './HomeInsights';
 import SectionHeading from '../components/SectionHeading';
 import {testContext} from '../testing/NowContainer';
 
-function testProps() {
+function testProps(props = {}) {
   return {
     educatorId: 9999,
-    educatorLabels: ['foo']
+    educatorLabels: ['foo'],
+    ...props
   };
 }
 
@@ -21,4 +22,24 @@ it('shallow renders without crashing', () => {
   expect(wrapper.find(SectionHeading).length).toEqual(2);
   expect(wrapper.contains(<HomeFeed educatorId={9999} />)).toEqual(true);
   expect(wrapper.contains(<HomeInsights educatorId={9999} educatorLabels={['foo']} />)).toEqual(true);
+});
+
+
+it('does not show search', () => {
+  const props = testProps();
+  const context = testContext();
+  const wrapper = shallow(<HomePage {...props} />, {context});
+  expect(wrapper.find('.HomePage').length).toEqual(1);
+  expect(wrapper.find(SectionHeading).length).toEqual(2);
+  expect(wrapper.contains('Search notes')).toEqual(false);
+});
+
+
+it('can show search for `enable_searching_notes`', () => {
+  const props = testProps({educatorLabels: ['enable_searching_notes']});
+  const context = testContext();
+  const wrapper = shallow(<HomePage {...props} />, {context});
+  expect(wrapper.find('.HomePage').length).toEqual(1);
+  expect(wrapper.find(SectionHeading).length).toEqual(2);
+  expect(wrapper.contains('Search notes')).toEqual(true);
 });
