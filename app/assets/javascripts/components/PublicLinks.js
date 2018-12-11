b760c3b781f75e3b1a8fec4c616aa13d6ec4a4ae
kevinrobinson <kevin.robinson.0@gmail.com>
2018-12-11 13:27:47 -0500

    Update test case

--- app/assets/javascripts/components/PublicLinks.js
+++ app/assets/javascripts/components/PublicLinks.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 
 export function Website(props) {
-  return <a {...props} href="https://www.studentinsights.org">studentinsights.org</a>;
+  return <a {...props} href="https://www.studentinsights.org">www.studentinsights.org</a>;
 }
 
 export function Email(props) {
   return <a {...props} href="mailto://ideas@studentinsights.org">ideas@studentinsights.org</a>;
 }
 
-export function WorkBoardIFrame(props) {
-  return <iframe {...props} src="https://www.studentinsights.org/work-board.html" />;
+
+export function WorkBoardIFrame(props) {  
+  const workBoardUrl = 'https://www.studentinsights.org/work-board.html';
+  if (process.env.NODE_ENV === 'test' && !props.forceIFrameDuringTest) return <div>(placeholder iframe to {workBoardUrl})</div>; // eslint-disable-line
+  return <iframe {...props} src={workBoardUrl} />;
 }
+WorkBoardIFrame.propTypes = {
+  forceIFrameDuringTest: PropTypes.bool
+};
