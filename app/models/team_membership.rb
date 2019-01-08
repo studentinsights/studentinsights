fc0ca440ed409645f9e4a20298852e837f759785
kevinrobinson <kevin.robinson.0@gmail.com>
2019-01-08 16:12:48 -0500

    progress

--- app/models/team_membership.rb
+++ app/models/team_membership.rb
@@ -10,12 +10,25 @@ class TeamMembership < ApplicationRecord
   }
 
   def active(options = {})
+    this_season_key, school_year_text = TeamMembership.this_season_and_year(options)
+    self.school_year_text == school_year_text && self.season_key == this_season_key.to_s
+  end
+
+  def self.active(options = {})
+    this_season_key, school_year_text = self.this_season_and_year(options)
+    TeamMembership.where({
+      school_year_text: school_year_text,
+      season_key: this_season_key
+    })
+  end
+
+  def self.this_season_and_year(options = {})
     time_now = options.fetch(:time_now, Time.now)
-    this_school_year = SchoolYear.to_school_year(time_now)
+    school_year_text = SchoolYear.to_school_year(time_now)
     short_next_school_year = (this_school_year + 1).to_s.last(2)
     school_year_text = "#{this_school_year}-#{short_next_school_year}"
     this_season_key = PerDistrict.new.sports_season_key(time_now)
 
-    self.school_year_text == school_year_text && self.season_key == this_season_key.to_s
+    [this_season_key, school_year_text]
   end
 end
