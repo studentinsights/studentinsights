ce2b26757bf0dc1574a797a3801da7f030c75848
EricDS <edavidsonsawyer@gmail.com>
2019-01-10 15:31:27 -0500

    removing for now

--- app/assets/javascripts/helpers/schoolHours.js
+++
@@ -1,9 +0,0 @@
-//returns the school start time as minutes past midnight
-export function schoolStartTime() {
-  return 420; // 7AM
-}
-
-//returns the school end time as minutes past midnight
-export function schoolEndtTime() {
-  return 900; // 3PM
-}
