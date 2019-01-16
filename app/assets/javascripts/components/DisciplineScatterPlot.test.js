02aee7c852c0996ef7d2e5e2ddf8b247bf43e85a
EricDS <edavidsonsawyer@gmail.com>
2019-01-16 14:38:31 -0500

    adding missing heatmap spec

---
+++ app/assets/javascripts/components/DisciplineScatterPlot.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import DisciplineScatterPlot from './DisciplineScatterPlot';
+import HighchartsWrapper from '../components/HighchartsWrapper';
+
+describe('DisciplineScatterPlot', () => {
+  const chart = shallow(<DisciplineScatterPlot
+                        id="1" categories={{}} seriesData={[]} titleText="Title" measureText="Measure" tooltip={{}} studentChart={true}
+                        />);
+
+  it('should render a highcharts wrapper', () => {
+    expect(chart.find(HighchartsWrapper).length).toEqual(1);
+  });
+});
