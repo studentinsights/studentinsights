dc3da8eab60d38d4d111b25fc99b75631d7815a5
kevinrobinson <kevin.robinson.0@gmail.com>
2019-01-25 18:52:52 -0500

    WIP, revising to generalize and ship

--- app/controllers/educators/sessions_controller.rb
+++ app/controllers/educators/sessions_controller.rb
@@ -2,9 +2,12 @@
 
 class Educators::SessionsController < Devise::SessionsController
   # GET /educator/sign_in
-  # def new
-  #   super
-  # end
+  def new
+    super
+    @district_url = 'http://www.somerville.k12.ma.us'
+    @district_logo_src = '/somerville-public-schools.jpg'
+    @district_logo_alt = 'Somerville public schools logo'
+  end
 
   # changes here impact Rack::Attack setup
   # POST /educator/sign_in
