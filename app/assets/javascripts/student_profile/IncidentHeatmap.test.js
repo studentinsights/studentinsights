a8a67ff2e2a7f56c27e10fc8f151dfdf6dfa36cf
EricDS <edavidsonsawyer@gmail.com>
2019-01-27 23:28:10 -0500

    remove renderer

--- app/assets/javascripts/student_profile/IncidentHeatmap.test.js
+++ app/assets/javascripts/student_profile/IncidentHeatmap.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import renderer from 'react-test-renderer';
 import {withDefaultNowContext} from '../testing/NowContainer';
 import IncidentHeatmap from './IncidentHeatmap';
 
