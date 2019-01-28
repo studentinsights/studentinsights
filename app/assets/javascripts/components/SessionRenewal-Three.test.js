89f104dbe858031adf47167db9aba2401adc6666
kevinrobinson <kevin.robinson.0@gmail.com>
2019-01-28 16:19:58 -0500

    Rewrite SessionRenewal to remove UI-side heuristic
    Fixup SessionRenewal JS specs

--- app/assets/javascripts/components/SessionRenewal-Three.test.js
+++ app/assets/javascripts/components/SessionRenewal-Three.test.js
@@ -1,17 +1,24 @@
-import {testProps, testRender, resetFetchMock} from './SessionRenewal-One.test';
+import fetchMock from 'fetch-mock/es5/client';
+import {TEST_DELAY, testProps, testRender} from './SessionRenewal-testHelpers';
 
-// TODO(kr)
-function disabledTests() { // eslint-disable-line
-  beforeEach(resetFetchMock);
+beforeEach(() => {
+  fetchMock.reset();
+  fetchMock.restore();
+  fetchMock.get('/educators/reset', {});
+  fetchMock.get('/educators/probe', new Response('{}', {
+    status: 302,
+    redirectUrl: '/educators/probe'
+  }));
+});
 
-  it('after TIMED_OUT, shows message, signs out, and calls forceReload', done => {
-    const props = testProps();
-    const el = testRender(props);
+it('if probe fails, calls forciblyClearPage', done => {
+  const props = testProps();
+  const el = testRender(props);
 
-    setTimeout(() => {
-      expect($(el).text()).toEqual('Your session has timed out due to inactivity.');
-      expect(props.forceReload).toHaveBeenCalled();
-      done();
-    }, 2500);
-  });
-}
+  setTimeout(() => {
+    expect(props.forciblyClearPage).toHaveBeenCalled();
+    expect(props.warnFn).toHaveBeenCalledWith('SessionRenewal#onProbed-TIMED_OUT');
+    expect($(el).text()).toEqual('');
+    done();
+  }, TEST_DELAY);
+});
