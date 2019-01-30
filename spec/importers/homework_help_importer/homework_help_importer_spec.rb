208cbf6877679d9120833e1e7d44ca88533033d1
kevinrobinson <kevin.robinson.0@gmail.com>
2019-01-30 08:15:17 -0500

    Finish timezone fixes

--- spec/importers/homework_help_importer/homework_help_importer_spec.rb
+++ spec/importers/homework_help_importer/homework_help_importer_spec.rb
@@ -15,21 +15,27 @@ RSpec.describe HomeworkHelpImporter do
     it 'works for importing notes' do
       log = LogHelper::FakeLog.new
       importer = HomeworkHelpImporter.new(pals.shs_jodi.id, log: log)
-      homework_help_sessions = importer.import(fixture_file_text)
+
+      # freeze test, since importing sheets timestamps depends on EST/EDT
+      test_time = Time.parse('2019-01-30 12:22:02 -0400')
+      homework_help_sessions = Timecop.freeze(test_time) do
+        importer.import(fixture_file_text)
+      end
+
       expect(HomeworkHelpSession.all.size).to eq 3
       expect(homework_help_sessions.as_json(except: [:id, :created_at, :updated_at])).to contain_exactly(*[{
         'student_id' => pals.shs_freshman_mari.id,
-        'form_timestamp' => Time.parse('Tue, 25 Sep 2018 13:41:43.000000000 +0000'),
+        'form_timestamp' => Time.parse('Tue, 25 Sep 2018 18:41:43.000000000 +0000'),
         'course_ids' => [history.id, algebra.id],
         'recorded_by_educator_id' => pals.shs_jodi.id
       }, {
         'student_id' => pals.shs_freshman_amir.id,
-        'form_timestamp' => Time.parse('Mon, 01 Oct 2018 13:25:23.000000000 +0000'),
+        'form_timestamp' => Time.parse('Mon, 01 Oct 2018 18:25:23.000000000 +0000'),
         'course_ids' => [english.id, history.id, algebra.id],
         'recorded_by_educator_id' => pals.shs_jodi.id
       }, {
         'student_id' => pals.shs_senior_kylo.id,
-        'form_timestamp' => Time.parse('Mon, 01 Oct 2018 13:45:12.000000000 +0000'),
+        'form_timestamp' => Time.parse('Mon, 01 Oct 2018 18:45:12.000000000 +0000'),
         'course_ids' => [geometry.id],
         'recorded_by_educator_id' => pals.shs_jodi.id
       }])
