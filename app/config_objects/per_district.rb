8b674fc76d142eacd949160ba02ce44beb706d86
kevinrobinson <kevin.robinson.0@gmail.com>
2019-02-08 14:44:16 -0500

    Update copy and typo

--- app/config_objects/per_district.rb
+++ app/config_objects/per_district.rb
@@ -471,7 +471,7 @@ class PerDistrict
   def sign_in_params
     if @district_key == SOMERVILLE
       { preview_image_src: 'sign_in/somerville-preview.png' }
-    elsif @district_key == BEDORD
+    elsif @district_key == BEDFORD
       { preview_image_src: 'sign_in/bedford-preview.png' }
     elsif @district_key == NEW_BEDFORD
       { preview_image_src: 'sign_in/new-bedford-preview.png' }
