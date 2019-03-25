42a7e9a9b0dc30286800c5a3bd82c656da236ea5
kevinrobinson <kevin.robinson.0@gmail.com>
2019-03-25 17:17:40 -0400

    Update FeedView test

--- app/assets/javascripts/feed/FeedView.test.js
+++ app/assets/javascripts/feed/FeedView.test.js
@@ -2,13 +2,18 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import FeedView from './FeedView';
 import {withDefaultNowContext} from '../testing/NowContainer';
+import PerDistrictContainer from '../components/PerDistrictContainer';
 import homeFeedJson from '../testing/fixtures/home_feed_json';
 
 
 describe('FeedView', () => {
   it('pure component matches snapshot', () => {
     const tree = renderer
-      .create(withDefaultNowContext(<FeedView feedCards={homeFeedJson.feed_cards} />))
+      .create(withDefaultNowContext(
+        <PerDistrictContainer districtKey="somerville">
+          <FeedView feedCards={homeFeedJson.feed_cards} />
+        </PerDistrictContainer>
+      ))
       .toJSON();
     expect(tree).toMatchSnapshot();
   });
