47f28197d3a82be597141071a056f0f63be4750f
kevinrobinson <kevin.robinson.0@gmail.com>
2019-04-25 15:22:46 -0400

    Update courses and sections to include district_school_year to unique across years

--- x2_export/sql/student_section_assignment_export.sql
+++ x2_export/sql/student_section_assignment_export.sql
@@ -4,7 +4,8 @@ SELECT
   'course_number',
   'school_local_id',
   'section_number',
-  'term_local_id'
+  'term_local_id',
+  'district_school_year'
 UNION ALL
 SELECT
   STD_ID_LOCAL,
@@ -12,6 +13,7 @@ SELECT
   SKL_SCHOOL_ID,
   MST_COURSE_VIEW,
   MST_TERM_VIEW
+  CTX_SCHOOL_YEAR
 FROM district_school_year_context
 LEFT JOIN SCHEDULE
   ON district_school_year_context.CTX_OID = schedule.SCH_CTX_OID
