807ab5184a039bdada86c40298d11ec54cafe1e1
kevinrobinson <kevin.robinson.0@gmail.com>
2019-05-01 13:21:06 -0400

    Update StudentCard snapshot with UI changes

--- app/assets/javascripts/class_lists/StudentCard.test.js
+++ app/assets/javascripts/class_lists/StudentCard.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
 import {withDefaultNowContext} from '../testing/NowContainer';
+import PerDistrictContainer from '../components/PerDistrictContainer';
 import StudentCard from './StudentCard';
 import students_for_grade_level_next_year_json from './fixtures/students_for_grade_level_next_year_json';
 
@@ -18,17 +19,25 @@ export function testProps(props = {}) {
   };
 }
 
+function testEl(props = {}) {
+  return withDefaultNowContext(
+    <PerDistrictContainer districtKey="somerville">
+      <StudentCard {...props} />
+    </PerDistrictContainer>
+  );
+}
+
 it('renders without crashing', () => {
   const el = document.createElement('div');
   const props = testProps();
-  ReactDOM.render(withDefaultNowContext(<StudentCard {...props} />), el);
+  ReactDOM.render(testEl(props), el);
 });
 
 
 it('snapshots', () => {
   const props = testProps();
   const tree = renderer
-    .create(withDefaultNowContext(<StudentCard {...props} />))
+    .create(testEl(props))
     .toJSON();
   expect(tree).toMatchSnapshot();
 });
