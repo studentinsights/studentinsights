90769a06c74be4f1d893d5f8721dc84e2ac6c05f
EricDS <edavidsonsawyer@gmail.com>
2019-05-03 11:32:29 -0400

    updated to use service account

--- lib/tasks/reading/reading_sheets_fetcher.rb
+++ lib/tasks/reading/reading_sheets_fetcher.rb
@@ -1,53 +1,24 @@
 require 'google/apis/drive_v3'
 require 'google/apis/sheets_v4'
 require 'googleauth'
-require 'googleauth/stores/file_token_store'
 require 'fileutils'
 require 'csv'
 
-OOB_URI = 'urn:ietf:wg:oauth:2.0:oob'.freeze
 APPLICATION_NAME = 'Student Insights'.freeze
-CREDENTIALS_PATH = File.expand_path('credentials.json', File.dirname(__FILE__)).freeze.freeze
-# The file token.yaml stores the user's access and refresh tokens, and is
-# created automatically when the authorization flow completes for the first
-# time.
-TOKEN_PATH = 'token.yaml'.freeze
+GOOGLE_APPLICATION_CREDENTIALS = File.expand_path('credentials.json', File.dirname(__FILE__)).freeze.freeze
 SCOPE = [Google::Apis::DriveV3::AUTH_DRIVE_READONLY, Google::Apis::SheetsV4::AUTH_SPREADSHEETS_READONLY]
-
-##
-# Ensure valid credentials, either by restoring from the saved credentials
-# files or intitiating an OAuth2 authorization. If authorization is required,
-# the user's default browser will be launched to approve the request.
-#
-# @return [Google::Auth::UserRefreshCredentials] OAuth2 credentials
-def authorize
-  client_id = Google::Auth::ClientId.from_file(CREDENTIALS_PATH)
-  token_store = Google::Auth::Stores::FileTokenStore.new(file: TOKEN_PATH)
-  authorizer = Google::Auth::UserAuthorizer.new(client_id, SCOPE, token_store)
-  user_id = 'default'
-  credentials = authorizer.get_credentials(user_id)
-  if credentials.nil?
-    url = authorizer.get_authorization_url(base_url: OOB_URI)
-    puts 'Open the following URL in the browser and enter the ' \
-         "resulting code after authorization:\n" + url
-    code = gets
-    credentials = authorizer.get_and_store_credentials_from_code(
-      user_id: user_id, code: code, base_url: OOB_URI
-    )
-  end
-  credentials
-end
 
 # Initialize the APIs
 
 drive_service = Google::Apis::DriveV3::DriveService.new
 drive_service.client_options.application_name = APPLICATION_NAME
-drive_service.authorization = authorize
+drive_service.authorization = Google::Auth::ServiceAccountCredentials.make_creds(json_key_io: File.open(GOOGLE_APPLICATION_CREDENTIALS),
+                                                                                  scope: SCOPE)
 
 sheet_service = Google::Apis::SheetsV4::SheetsService.new
 sheet_service.client_options.application_name = APPLICATION_NAME
-sheet_service.authorization = authorize
-
+sheet_service.authorization = Google::Auth::ServiceAccountCredentials.make_creds(json_key_io: File.open(GOOGLE_APPLICATION_CREDENTIALS),
+                                                                                  scope: SCOPE)
 # Get folder ID
 
 folder_name = "Sheets imports"
