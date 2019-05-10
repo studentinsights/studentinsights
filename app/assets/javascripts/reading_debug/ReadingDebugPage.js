55f85f333b6334ca173929731d8550c90a91a7bf
kevinrobinson <kevin.robinson.0@gmail.com>
2019-05-10 10:43:59 -0400

    Working prototype

--- app/assets/javascripts/reading_debug/ReadingDebugPage.js
+++ app/assets/javascripts/reading_debug/ReadingDebugPage.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import _ from 'lodash';
+import {Table, Column, AutoSizer} from 'react-virtualized';
 import {apiFetchJson} from '../helpers/apiFetchJson';
 import {updateGlobalStylesToTakeFullHeight} from '../helpers/globalStylingWorkarounds';
 import GenericLoader from '../components/GenericLoader';
 import SectionHeading from '../components/SectionHeading';
+import ExperimentalBanner from '../components/ExperimentalBanner';
+import StudentPhotoCropped from '../components/StudentPhotoCropped';
 import HighchartsWrapper from '../components/HighchartsWrapper';
 import {gradeText} from '../helpers/gradeText';
-import {orderedFAndPLevels} from '../reading/readingData';
-
+import {orderedFAndPLevels, classifyFAndPEnglish, interpretFAndPEnglish} from '../reading/readingData';
+import FountasAndPinellBreakdown from '../reading/FountasAndPinellBreakdown';
+import {
+  high,
+  medium,
+  low,
+  missing
+} from '../helpers/colors';
 
 // For reviewing, debugging and developing new ways to make use of
 // or revise reading data.
@@ -30,6 +39,8 @@ export default class ReadingDebugPage extends React.Component {
   render() {
     return (
       <div className="ReadingDebugPage" style={styles.flexVertical}>
+        <ExperimentalBanner />
+        <SectionHeading>Benchmark Reading Data (DEBUG)</SectionHeading>
         <GenericLoader
           promiseFn={this.fetchJson}
           style={styles.flexVertical}
@@ -53,22 +64,29 @@ export class ReadingDebugView extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      benchmarkAssessmentKey: 'f_and_p_english'
+      benchmarkAssessmentKey: 'f_and_p_english',
+      selection: null
     };
+
+    this.renderName = this.renderName.bind(this);
+  }
+
+  onCellClicked(selection) {
+    this.setState({selection});
   }
 
   render() {
     return (
       <div style={{...styles.flexVertical, margin: 10}}>
-        <SectionHeading>Benchmark Reading Data (DEBUG)</SectionHeading>
         {this.renderList()}
+        <div style={{flex: 1}}>{this.renderTable()}</div>
       </div>
     );
   }
 
   renderList() {
     const {students, readingBenchmarkDataPoints} = this.props;
-    const {benchmarkAssessmentKey} = this.state;
+    const {benchmarkAssessmentKey, selection} = this.state;
     const studentsById = students.reduce((map, student) => {
       return {...map, [student.id]: student};
     }, {});
@@ -84,9 +102,18 @@ export class ReadingDebugView extends React.Component {
       ].join('-');
     });
 
-    const years = [2016, 2017, 2018, 2019];
-    const periods = ['fall', 'winter', 'spring'];
-    const intervals = _.flatMap(years, year => periods.map(period => [year, period]));
+    // const years = [2017, 2018, 2019];
+    // const periods = ['fall', 'winter', 'spring'];
+    // const intervals = _.flatMap(years, year => periods.map(period => [year, period]));
+    const intervals = [
+      [2017, 'winter'],
+      [2017, 'spring'],
+      [2018, 'fall'],
+      [2018, 'winter'],
+      [2018, 'spring'],
+      [2019, 'fall'],
+      [2019, 'winter']
+    ];
     const grades = ['KF', '1', '2'];
     return (
       <div>
@@ -95,7 +122,7 @@ export class ReadingDebugView extends React.Component {
           dataPoints={filteredDataPoints}
           studentsById={studentsById}
         />*/}
-        <table>
+        <table style={{width: '100%'}}>
           <thead>
             <tr>
               <th>Grade</th>
@@ -118,10 +145,26 @@ export class ReadingDebugView extends React.Component {
                   const [year, period] = interval;
                   const key = [year, period, grade].join('-');
                   const intervalDataPoints = groups[key] || [];
+                  const fAndPValuesWithNulls = intervalDataPoints.map(dataPoint => dataPoint.json.value);
                   return (
-                    <td key={interval.join('-')}>
-                      <div title={JSON.stringify(intervalDataPoints, null, 2)}>
-                        {intervalDataPoints.length}
+                    <td
+                      key={interval.join('-')}>
+                      <div 
+                        style={_.isEqual(selection, {year, period, grade})
+                          ? {cursor: 'pointer', padding: 10, border: `2px solid ${selection}`}
+                          : {cursor: 'pointer', padding: 10, border: `2px solid white`}
+                        }
+                        onClick={this.onCellClicked.bind(this, {year, period, grade})}>
+                        <FountasAndPinellBreakdown
+                          grade={grade}
+                          benchmarkPeriodKey={period}
+                          fAndPValuesWithNulls={fAndPValuesWithNulls}
+                          includeMissing={true}
+                          // style={styles.breakdownBar}
+                          // innerStyle={styles.breakdownBarInner}
+                          height={5}
+                          labelTop={5}
+                        />
                       </div>
                     </td>
                   );
@@ -130,7 +173,6 @@ export class ReadingDebugView extends React.Component {
             ))}
           </tbody>
         </table>
-        <pre>{JSON.stringify(groups, null, 2)}</pre>
       </div>
     );
   }
@@ -168,49 +210,116 @@ export class ReadingDebugView extends React.Component {
   //   );
   // }
 
-  // renderTable(students, columns, doc) {
-  //   const {districtKey, nowFn} = this.context;
-  //   const nowMoment = nowFn();
-  //   const rowHeight = 40; // for two lines of student names
+  renderTable() {
+    const {districtKey, nowFn} = this.context;
+    const {students, readingBenchmarkDataPoints} = this.props;
+    const {benchmarkAssessmentKey, selection} = this.state;
+    if (selection === null) {
+      return <div>Click a cell to see the list of students.</div>;
+    }
 
-  //   // In conjuction with the filtering, this can lead to a warning in development.
-  //   // See https://github.com/bvaughn/react-virtualized/issues/1119 for more.
-  //   return (
-  //     <AutoSizer style={{marginTop: 20, margin: 10}}>
-  //       {({width, height}) => (
-  //         <Sortable>
-  //           {({sortDirection, sortBy, ordered, onTableSort}) => {
-  //             const sortFns = sortFnsMap(doc, districtKey, nowMoment);
-  //             const sortedStudents = ordered(this.filteredStudents(students), sortFns);
-  //             return (
-  //               <Table
-  //                 width={width}
-  //                 height={height}
-  //                 headerHeight={rowHeight}
-  //                 headerStyle={{display: 'flex', fontWeight: 'bold', cursor: 'pointer'}}
-  //                 rowStyle={{display: 'flex'}}
-  //                 style={{fontSize: 14}}
-  //                 rowHeight={rowHeight}
-  //                 rowCount={sortedStudents.length}
-  //                 rowGetter={({index}) => sortedStudents[index]}
-  //                 sort={onTableSort}
-  //                 sortBy={sortBy}
-  //                 sortDirection={sortDirection}
-  //                 >{columns.map(column => (
-  //                   <Column
-  //                     key={column.dataKey}
-  //                     headerRenderer={this.renderHeaderCell}
-  //                     {...column}
-  //                   />
-  //                 ))}
-  //               </Table>
-  //             );
-  //           }}
-  //         </Sortable>
-  //       )}
-  //     </AutoSizer>
-  //   );
-  // }
+    if (!selection) return null;
+
+    const {year, period, grade} = selection;
+    const dataPointsByStudentId = {};
+    readingBenchmarkDataPoints.forEach(d => {
+      if (benchmarkAssessmentKey !== null && benchmarkAssessmentKey !== d.benchmark_assessment_key) return;
+      if (d.benchmark_school_year !== year) return;
+      if (d.benchmark_period_key !== period) return;
+      dataPointsByStudentId[d.student_id] = (dataPointsByStudentId[d.student_id] || []).concat(d);
+    });
+
+    const filteredStudents = students.filter(student => {
+      if (!dataPointsByStudentId[student.id]) return false;
+      if (dataPointsByStudentId.length === 0) return false;
+      if (student.grade !== grade) return false;
+      return true;
+    });
+
+    const nowMoment = nowFn();
+    const rowHeight = 40; // for two lines of student names
+
+    // return <pre>{JSON.stringify(filteredStudents, null, 2)}</pre>;
+
+    // In conjuction with the filtering, this can lead to a warning in development.
+    // See https://github.com/bvaughn/react-virtualized/issues/1119 for more.
+    return (
+      <AutoSizer style={{marginTop: 20, margin: 10}}>
+        {({width, height}) => (
+          <Table
+            width={width}
+            height={height}
+            headerHeight={rowHeight}
+            headerStyle={{display: 'flex', fontWeight: 'bold', cursor: 'pointer'}}
+            rowStyle={{display: 'flex'}}
+            style={{fontSize: 14}}
+            rowHeight={rowHeight}
+            rowCount={filteredStudents.length}
+            rowGetter={({index}) => filteredStudents[index]}
+            // sort={onTableSort}
+            // sortBy={sortBy}
+            // sortDirection={sortDirection}
+            >
+              <Column
+                label='Name'
+                dataKey='name'
+                cellRenderer={this.renderName}
+                width={260}
+              />
+              <Column
+                label='Grade'
+                dataKey='grade'
+                width={100}
+              />
+              <Column
+                label='F&P level'
+                dataKey='f_and_p_english'
+                cellRenderer={this.renderFAndP.bind(this, dataPointsByStudentId, period)}
+                width={100}
+              />
+          </Table>
+        )}
+      </AutoSizer>
+    );
+  }
+
+  renderName(cellProps) {
+    const student = cellProps.rowData;
+    // return (
+    //   <div style={styles.nameBlock}>
+    //     <a style={{fontSize: 14}} href={`/students/${student.id}`} target="_blank" rel="noopener noreferrer">{student.first_name} {student.last_name}</a>
+    //     {student.has_photo && (
+    //       <StudentPhotoCropped
+    //         studentId={student.id}
+    //         style={styles.photo}
+    //       />
+    //     )}
+    //   </div>
+    // );
+    return <div>{student.id}</div>;
+  }
+
+  renderFAndP(dataPointsByStudentId, benchmarkPeriodKey, cellProps) {
+    const student = cellProps.rowData;
+    // if (student.grade !== grade) return null;
+    const dataPoints = dataPointsByStudentId[student.id] || [];
+    if (dataPoints.length === 0) return null;
+
+    // const dataPoints = readingBenchmarkDataPoints.filter(d => {
+    //   if (d.student_id !== student.id) return false;
+    //   if (benchmarkAssessmentKey !== null && benchmarkAssessmentKey !== d.benchmark_assessment_key) return false;
+    //   if (d.benchmark_school_year !== year) return false;
+    //   if (d.benchmark_period_key !== period) return false;
+    //   return true;
+    // });
+    return (
+      // <pre>{dataPoints.length}</pre>
+      dataPoints.map(d => <div key={d.id}>
+        {renderFAndPLevel(d.json.value, student.grade, benchmarkPeriodKey)}
+      </div>)
+      // <pre title={JSON.stringify(dataPoints, null, 2)}>{JSON.stringify(dataPoints)}</pre>
+    );
+  }
 }
 ReadingDebugView.contextTypes = {
   districtKey: PropTypes.string.isRequired,
@@ -345,5 +454,46 @@ function Scatterplot(props) {
           }
         ]} />
     </div>
+  );
+}
+
+function renderFAndPLevel(text, grade, benchmarkPeriodKey) {
+  if (!text) return none();
+  const level = interpretFAndPEnglish(text);
+  if (!level) return none();
+
+  // return <div>{level}</div>;
+  const category = classifyFAndPEnglish(level, grade, benchmarkPeriodKey);
+  const color = {
+    high,
+    medium,
+    low
+  }[category] || missing;
+  return (
+    <div style={{paddingLeft: 5, display: 'flex', flexDirection: 'row'}}>
+      <div style={{display: 'inline-block', marginRight: 5}}>
+        {coloredBadge(level, color)}
+      </div>
+    </div>
+  );
+}
+
+function none() {
+  return <span style={{paddingLeft: 5}}>(none)</span>;
+}
+
+
+function coloredBadge(value, color) {
+  return (
+    <div style={{
+      display: 'flex',
+      width: 32,
+      height: 32,
+      justifyContent: 'center',
+      alignItems: 'center',
+      color: 'white',
+      fontSize: 14,
+      backgroundColor: color
+    }}>{value}</div>
   );
 }
