4f5ed8e4464d4042e75cd613500cab1149be974d
kevinrobinson <kevin.robinson.0@gmail.com>
2019-05-29 11:04:07 -0400

    Student searchbar: Migrate to React component, optimize for larger lists

--- app/assets/javascripts/components/StudentSearchbar.js
+++ app/assets/javascripts/components/StudentSearchbar.js
@@ -2,11 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import _ from 'lodash';
 import Autocomplete from 'react-autocomplete';
-import MixpanelUtils from '../helpers/MixpanelUtils';
+import * as Routes from '../helpers/Routes';
+import memoizer from '../helpers/memoizer';
 import {apiFetchJson} from '../helpers/apiFetchJson';
 import * as colors from '../helpers/colors';
 
-
+// Optimized for feeling responsive with large lists.
 export default class StudentSearchbar extends React.Component {
   constructor(props) {
     super(props);
@@ -20,10 +21,14 @@ export default class StudentSearchbar extends React.Component {
     this.fetchStudentsJsonFromServer = this.fetchStudentsJsonFromServer.bind(this);
     this.onStudentSelected = this.onStudentSelected.bind(this);
     this.renderMenuContent = this.renderMenuContent.bind(this);
+
+    this.memoize = memoizer();
   }
 
   componentDidMount() {
-    this.fetchStudentsJson().then(students => this.setState({students}));
+    this.fetchStudentsJson()
+      .then(students => this.setState({students}))
+      .catch(err => reportToRollbar('StudentSearchbar#catch', err));
   }
 
   fetchStudentsJson() {
@@ -40,40 +45,110 @@ export default class StudentSearchbar extends React.Component {
 
   // Fetch and also update the sessionStorage cache
   fetchStudentsJsonFromServer() {
-    return apiFetchJson('/students/names').then(json => {
+    const url = '/api/educators/student_searchbar_json';
+    return apiFetchJson(url).then(json => {
       window.sessionStorage.student_names_cache = JSON.stringify(json);
+      return json;
+    });
+  }
+
+  // Performance optimizations for districtwide users in particular.
+  // This can be more than 10k items, and rendering them all is
+  // expensive and not useful.  This caches 
+  filteredAndTruncated() {
+    return this.memoize(['filteredAndTruncated', this.state], () => {
+      const {students, text} = this.state;
+
+      // don't popup all students
+      if (text === '') {
+        return {
+          studentsForList: [],
+          studentsForListById: [],
+          countOverLimit: 0
+        };
+      }
+
+      // Filtering for search, limiting max results since it's too
+      // expensive to render 6000 matches (eg, after typing one
+      // character).
+      const tokens = text.split(' ').map(token => token.toLowerCase());
+      const studentsForList = [];
+      const MATCHES_LIMIT = 500;
+      var countOverLimit = 0; // eslint-disable-line no-var
+      students.forEach(student => {
+        if (studentsForList.length > MATCHES_LIMIT) {
+          countOverLimit = countOverLimit + 1;
+          return;
+        }
+        if (matchStateToTerm(student, tokens)) {
+          studentsForList.push(student);
+        }
+      });
+      const studentsForListById = studentsForList.reduce((map, student) => {
+        return {...map, [student.id]: true};
+      }, {});
+      return {
+        studentsForList,
+        studentsForListById,
+        countOverLimit: countOverLimit
+      };
     });
   }
 
   onStudentSelected(studentId) {
     if (this.props.onStudentSelected) return this.props.onStudentSelected();
-
-    MixpanelUtils.track('SEARCHBAR_SELECTED_STUDENT', {});
-    window.location.pathname = `/students/${studentId}`;
+    const studentProfileUrl = Routes.studentProfile(studentId);
+    window.location.pathname = studentProfileUrl;
   }
 
   render() {
-    const {students, text} = this.state;
+    const {text} = this.state;
     const {autocompleteProps, inputStyles} = this.props;
     
+    // Optimization, since <Autocomplete /> doesn't support limiting
+    // the number of suggestions and we need to do that for large lists.
+    const {studentsForList, studentsForListById, countOverLimit} = this.filteredAndTruncated();
+    const maybeMoreItemsEl = (countOverLimit === 0) ? null : (
+      <div style={styles.more}>
+        <span>+ {countOverLimit} more {countOverLimit === 1 ? 'student' : 'students'}</span>
+        <a style={{marginLeft: 10}} href="/educators/my_students">view all</a>
+      </div>
+    );
     return (
       <Autocomplete
         className="StudentSearchbar-autocomplete"
         value={text}
-        items={students}
+        items={studentsForList}
         getItemValue={item => item.label}
         autoHighlight={true}
-        shouldItemRender={matchStateToTerm}
         onChange={(event, text) => this.setState({text})}
         onSelect={(text, item) => this.onStudentSelected(item.id)}
         renderInput={props => <input style={{...styles.input, ...inputStyles}} placeholder="Find student..." {...props} />}
-        menuStyle={styles.menu}
+        shouldItemRender={(item, value) => studentsForListById[item.id]}
         renderItem={(item, isHighlighted) => (
-          <div style={{
+          <div key={item.id} style={{
             ...styles.itemStyle,
             background: isHighlighted ? colors.selection : 'white'
           }}>{item.label}</div>
         )}
+        renderMenu={function(items, value, style) {
+          // This uses `function` since it relies on
+          // `this` being set by Autocomplete call.
+          //
+          // Appends a "+ n more" message if the results are
+          // truncated (only common for districtwide users with
+          // short search texts like 1-2 characters).
+          return (
+            <div
+              style={{
+                ...this.menuStyle, // defaults
+                ...style, // positioning
+                ...styles.menu // our styling
+              }}
+              children={items.concat(maybeMoreItemsEl)}
+            />
+          );
+        }}
         {...(autocompleteProps || {})}
       />
     );
@@ -96,15 +171,15 @@ StudentSearchbar.propTypes = {
 
 const styles = {
   menu: {
-    borderRadius: '3px',
-    boxShadow: '0 2px 12px rgba(0, 0, 0, 0.1)',
-    background: 'rgba(255, 255, 255, 0.9)',
-    padding: '2px 0',
-    fontSize: '90%',
-    position: 'fixed',
-    overflow: 'auto',
-    maxHeight: '50%',
-    zIndex: 1 // this is the only change from the default
+    // borderRadius: 3,
+    // boxShadow: '0 2px 12px rgba(0, 0, 0, 0.1)',
+    // background: 'rgba(255, 255, 255, 0.9)',
+    // padding: '2px 0',
+    // fontSize: '90%',
+    position: 'absolute', // changed from the default
+    // overflow: 'auto',
+    // maxHeight: '50%',
+    zIndex: 1 // changed from the default
   },
   input: {
     fontSize: 14,
@@ -116,16 +191,30 @@ const styles = {
   itemStyle: {
     padding: 5,
     cursor: 'pointer'
+  },
+  more: {
+    color: '#999',
+    padding: 5,
+    paddingTop: 15,
+    paddingBottom: 10,
+    background: 'white'
   }
 };
 
-function matchStateToTerm(student, text) {
-  const tokens = text.split(' ').map(token => token.toLowerCase());
+function matchStateToTerm(student, tokens) {
   return _.every(tokens, token => student.label.toLowerCase().indexOf(token) !== -1);
 }
 
 export function clearStorage() {
   if (window.sessionStorage && window.sessionStorage.clear) {
     window.sessionStorage.clear();
+  }
+}
+
+function reportToRollbar(msg, err) {
+  if (window.Rollbar && window.Rollbar.error) {
+    window.Rollbar.error(msg, err);
+  } else {
+    console.error('Could not report to Rollbar', msg, err); // eslint-disable-line no-console
   }
 }
