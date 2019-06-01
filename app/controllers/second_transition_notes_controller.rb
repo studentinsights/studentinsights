dcbab23f527af1771d56d2b5b0b205d965ffd45d
kevinrobinson <kevin.robinson.0@gmail.com>
2019-06-01 12:13:39 -0400

    Update specs

--- app/controllers/second_transition_notes_controller.rb
+++ app/controllers/second_transition_notes_controller.rb
@@ -17,11 +17,7 @@ class SecondTransitionNotesController < ApplicationController
         :first_name,
         :last_name,
         :grade,
-        :house,
         :counselor,
-        :sped_liaison,
-        :program_assigned,
-        :sped_placement
       ],
       methods: [
         :has_photo
