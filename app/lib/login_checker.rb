e3a252b8982d71eb6d7ea53e9f8ac651c6c32c98
kevinrobinson <kevin.robinson.0@gmail.com>
2019-06-27 15:33:45 -0400

    Update specs to verify mailgun HTTP request

--- app/lib/login_checker.rb
+++ app/lib/login_checker.rb
@@ -5,9 +5,9 @@ class LoginChecker
     @time_now = options.fetch(:time_now, Time.now)
   end
 
-  def warn_if_suspicious
+  def alert_if_suspicious
     flags = infer_flags
-    warn_about(flags)
+    alert_about(flags)
     flags
   end
 
@@ -21,9 +21,68 @@ class LoginChecker
     flags.sort
   end
 
-  def warn_about(flags)
-    if flags.size > 0
-      Rollbar.warn('LoginChecker#warn_if_suspicious', flags: flags)
+  def alert_about(flags)
+    return if flags.size == 0
+    warning_id = SecureRandom.hex(64)
+
+    # alert developer
+    with_isolation do
+      Rollbar.error('LoginChecker#alert_if_suspicious', {
+        flags: flags,
+        warning_id: warning_id,
+        time_now: @time_now.to_i
+      })
     end
+
+    # notify educator via email
+    with_isolation do
+      send_email_to_user!(@educator.email, warning_id)
+    end
+  end
+
+  # Isolate alerting so if either fails the other still tries
+  def with_isolation(&block)
+    begin
+      block.call()
+    rescue => err
+      Rollbar.error('LoginChecker#with_isolation rescued', {
+        error_class: err.class.name,
+        error_message: err.message,
+        error_backtrace: err.backtrace
+      })
+    end
+    nil
+  end
+
+  def send_email_to_user!(educator_email, warning_id)
+    email_text = user_facing_email_text(warning_id)
+    mailgun_url = MailgunHelper.new.mailgun_url_from_env(ENV)
+    post_data = Net::HTTP.post_form(URI.parse(mailgun_url), {
+      :from => "Student Insights <security@studentinsights.org>",
+      :to => educator_email,
+      :bcc => 'security@studentinsights.org',
+      :subject => "Security alert for #{PerDistrict.new.canonical_domain}",
+      :html => "<html><body><pre style='font: monospace; font-size: 12px;'>#{email_text}</pre>"
+    })
+
+    # Alert if post to Mailgun failed
+    if post_data.code.to_i != 200
+      Rollbar.error("LoginChecker#send_email_to_user! failed with post_data.code: #{post_data.code}", {
+        warning_id: warning_id,
+        time_now: @time_now.to_i
+      })
+    end
+    nil
+  end
+
+  # Limit disclosing any other information here
+  def user_facing_email_text(warning_id)
+    [
+      "Your #{PerDistrict.new.canonical_domain} account was just signed into.  You're getting this email to make sure that it was you.",
+      "If this was you, there's nothing else you need to do.",
+      "If this wasn't you, your account may have been compromised.  Email security@studentinsights.org and the #{PerDistrict.new.district_name} IT administrator immediately.  They'll take it from there to investigate and protect the students and families in #{PerDistrict.new.district_name}.",
+      "",
+      "Security alert sent from #{PerDistrict.new.canonical_domain} at #{@time_now}."
+    ].join("\n\n")
   end
 end
--- spec/lib/login_checker_spec.rb
+++ spec/lib/login_checker_spec.rb
@@ -3,24 +3,76 @@ require 'spec_helper'
 RSpec.describe LoginChecker do
   let!(:pals) { TestPals.create! }
 
-  it '#warn_if_suspicious reports to Rollbar on :first_login_after_year' do
+  it '#alert_if_suspicious reports to Rollbar on :first_login_after_year' do
     LoginActivity.create!({
       user_id: pals.healey_vivian_teacher.id,
       created_at: pals.time_now - 2.years
     })
 
-    allow(Rollbar).to receive(:warn)
-    expect(Rollbar).to receive(:warn).once.with('LoginChecker#warn_if_suspicious', flags: [:first_login_after_year])
+    allow(Rollbar).to receive(:error)
+    expect(Rollbar).to receive(:error).once.with('LoginChecker#alert_if_suspicious', {
+      flags: [:first_login_after_year],
+      warning_id: anything(),
+      time_now: pals.time_now.to_i
+    })
     checker = LoginChecker.new(pals.healey_vivian_teacher, time_now: pals.time_now)
-    expect(checker.warn_if_suspicious).to eq [:first_login_after_year]
+    expect(checker.alert_if_suspicious).to eq [:first_login_after_year]
   end
 
-  it '#warn_if_suspicious reports to Rollbar on :first_login_month_after_creation' do
+  it '#alert_if_suspicious reports to Rollbar on :first_login_month_after_creation' do
     pals.healey_vivian_teacher.update!(created_at: pals.time_now - 32.days)
 
-    allow(Rollbar).to receive(:warn)
-    expect(Rollbar).to receive(:warn).once.with('LoginChecker#warn_if_suspicious', flags: [:first_login_month_after_creation])
+    allow(Rollbar).to receive(:error)
+    expect(Rollbar).to receive(:error).once.with('LoginChecker#alert_if_suspicious', {
+      flags: [:first_login_month_after_creation],
+      warning_id: anything(),
+      time_now: pals.time_now.to_i
+    })
     checker = LoginChecker.new(pals.healey_vivian_teacher, time_now: pals.time_now)
-    expect(checker.warn_if_suspicious).to eq [:first_login_month_after_creation]
+    expect(checker.alert_if_suspicious).to eq [:first_login_month_after_creation]
+  end
+
+  it '#with_isolation works' do
+    allow(Rollbar).to receive(:error)
+    expect(Rollbar).to receive(:error).once.with('LoginChecker#with_isolation rescued', {
+      error_class: 'RuntimeError',
+      error_message: 'something broke!',
+      error_backtrace: anything()
+    })
+    checker = LoginChecker.new(pals.healey_vivian_teacher, time_now: pals.time_now)
+    checker.alert_if_suspicious
+
+    expect { checker.send(:with_isolation) { raise 'something broke!' } }.not_to raise_error
+  end
+
+  it '#user_facing_email_text works' do
+    checker = LoginChecker.new(pals.healey_vivian_teacher, time_now: pals.time_now)
+    expect(checker.send(:user_facing_email_text, 'foo-warning-id')).to eq [
+      "Your localhost-studentinsights account was just signed into.  You're getting this email to make sure that it was you.",
+      "If this was you, there's nothing else you need to do.",
+      "If this wasn't you, your account may have been compromised.  Email security@studentinsights.org and the Localhost Public Schools IT administrator immediately.  They'll take it from there to investigate and protect the students and families in Localhost Public Schools.",
+      '',
+      "Security alert sent from localhost-studentinsights at 2018-03-13 11:03:00 UTC."
+    ].join("\n\n")
+  end
+
+  describe '#alert_if_suspicious' do
+    it 'calls send_email_to_user! and it makes an HTTP request to Mailgun' do
+      LoginActivity.create!({
+        user_id: pals.healey_vivian_teacher.id,
+        created_at: pals.time_now - 2.years
+      })
+
+      allow(Net::HTTP).to receive(:post_form)
+      expect(Net::HTTP).to receive(:post_form).once.with(URI.parse('https://api:fake-mailgun-api-key@api.mailgun.net/v3/fake-mailgun-domain/messages'), {
+        from: 'Student Insights <security@studentinsights.org>',
+        to: 'vivian@demo.studentinsights.org',
+        bcc: 'security@studentinsights.org',
+        subject: 'Security alert for localhost-studentinsights',
+        html: anything()
+      })
+      checker = LoginChecker.new(pals.healey_vivian_teacher, time_now: pals.time_now)
+      checker.alert_if_suspicious
+    end
   end
 end
