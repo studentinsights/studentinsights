ddb9ac37151a3d302ee56fc7d9786a0d2bf29442
kevinrobinson <kevin.robinson.0@gmail.com>
2019-09-03 14:31:15 -0400

    Update data flow specs

--- spec/importers/file_importers/file_importer_options_spec.rb
+++ spec/importers/file_importers/file_importer_options_spec.rb
@@ -20,7 +20,7 @@ RSpec.describe FileImporterOptions do
 
     it 'can describe data flows for all importer classes' do
       data_flows = FileImporterOptions.new.all_data_flows
-      expect(data_flows.size).to eq(13)
+      expect(data_flows.size).to eq(14)
       sorted_json = data_flows.as_json.sort_by {|j| j['importer'] }
       fixture_json = JSON.parse(IO.read("#{Rails.root}/spec/importers/helpers/data_flows_fixture.json"))
       expect(sorted_json).to eq(fixture_json)
@@ -45,7 +45,8 @@ RSpec.describe FileImporterOptions do
         'star_reading',
         'ed_plans',
         'ed_plan_accommodations',
-        '504'
+        '504',
+        'student_voice_surveys'
       ])
     end
   end
