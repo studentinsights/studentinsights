49d6001016fbec39ce345e90b471288c79f50579
kevinrobinson <kevin.robinson.0@gmail.com>
2019-09-04 18:27:23 -0400

    Homeroom paths: Match by id only, not slug

--- ui/App.js
+++ ui/App.js
@@ -110,7 +110,7 @@ export default class App extends React.Component {
         <Route exact path="/schools/:slug/reading/:grade/groups" render={this.renderReadingGroupingPage.bind(this)}/>
         <Route exact path="/reading/debug" render={this.renderReadingDebugPage.bind(this)}/>
         <Route exact path="/reading/debug_star" render={this.renderReadingDebugStarPage.bind(this)}/>
-        <Route exact path="/homerooms/:id_or_slug" render={this.renderHomeroomPage.bind(this)}/>
+        <Route exact path="/homerooms/:id" render={this.renderHomeroomPage.bind(this)}/>
         <Route exact path="/sections/:id" render={this.renderSectionPage.bind(this)}/>
         <Route exact path="/students/:id" render={this.renderStudentProfilePage.bind(this)}/>
         <Route exact path="/students/:id/v3" render={this.renderStudentProfilePage.bind(this)}/>
@@ -257,8 +257,8 @@ export default class App extends React.Component {
   }
 
   renderHomeroomPage(routeProps) {
-    const homeroomIdOrSlug = routeProps.match.params.id_or_slug;
-    return <HomeroomPage homeroomIdOrSlug={homeroomIdOrSlug} />;
+    const homeroomId = parseInt(routeProps.match.params.id, 10);
+    return <HomeroomPage homeroomId={homeroomId} />;
   }
 
   renderSectionPage(routeProps) {
