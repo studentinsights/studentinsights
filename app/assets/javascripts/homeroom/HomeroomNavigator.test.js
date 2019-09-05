bca4cadd3f9899fefed7ca1120ce095b3471f574
kevinrobinson <kevin.robinson.0@gmail.com>
2019-09-05 10:52:13 -0400

    Update JS test to verify select options, remove snapshot that didn't do that

--- app/assets/javascripts/homeroom/HomeroomNavigator.test.js
+++ app/assets/javascripts/homeroom/HomeroomNavigator.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import renderer from 'react-test-renderer';
+import {mount} from 'enzyme';
+import Select from 'react-select';
 import HomeroomNavigator from './HomeroomNavigator';
 import homeroomJson from './homeroomJson.fixture';
 
@@ -24,10 +25,15 @@ it('renders without crashing', () => {
   expect($(el).html()).toContain('button');
 });
 
-it('snapshots view', () => {
+it('passes options to Select, sorted and formatted as expected', () => {
   const props = testProps();
-  const tree = renderer
-    .create(<HomeroomNavigator {...props} />)
-    .toJSON();
-  expect(tree).toMatchSnapshot();
+  const wrapper = mount(<HomeroomNavigator {...props} />);
+  expect(wrapper.find(Select).props().options).toEqual([
+    { value: 101, label: 'HEA 003 (KF)' },
+    { value: 2, label: 'HEA 500 (5)' },
+    { value: 105, label: 'WSNS 501 (5)' },
+    { value: 102, label: 'SHS 917 (9)' },
+    { value: 103, label: 'SHS 942 (9)' },
+    { value: 104, label: 'SHS ALL (mixed)' }
+  ]);
 });
