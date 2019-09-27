46ded48e157c6b33da2daf56609f3b04196487b0
kevinrobinson <kevin.robinson.0@gmail.com>
2019-09-27 16:32:56 -0400

    Remove old MCAS method that doesn't include Next Gen MCAS

--- app/models/student_assessment.rb
+++ app/models/student_assessment.rb
@@ -40,25 +40,4 @@ class StudentAssessment < ApplicationRecord
   def self.find_by_student(student)
     where(student_id: student.id)
   end
-
-  # First step to implementing and enforcing proper validation in a new table.
-  def self.printout_present_mcas_fields
-    mcas_assessment_ids = Assessment.where(family: 'MCAS').pluck(:id)
-    records = StudentAssessment.where(assessment_id: mcas_assessment_ids)
-    total_records = records.count
-
-    scale_score_present = records.where.not(scale_score: nil)
-    growth_percentile_present = records.where.not(growth_percentile: nil)
-    performance_level_present = records.where.not(performance_level: nil)
-    percentile_rank_nil = records.where(percentile_rank: nil)
-
-    puts "MCAS:"
-    puts "Total records: #{total_records}"
-
-    puts "scale_score_present: #{scale_score_present.count}/#{total_records}"
-    puts "growth_percentile_present: #{growth_percentile_present.count}/#{total_records}"
-    puts "performance_level_present: #{performance_level_present.count}/#{total_records}"
-    puts "percentile_rank_nil: #{percentile_rank_nil.count}/#{total_records}"
-    puts
-  end
 end
