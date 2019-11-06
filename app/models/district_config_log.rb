3878776a5ef0d64ba86c7aa27cfc5c95e480a40a
kevinrobinson <kevin.robinson.0@gmail.com>
2019-11-06 11:27:40 -0500

    Fix bug in reading DistrictConfigLog, move to file

--- app/models/district_config_log.rb
+++ app/models/district_config_log.rb
@@ -8,7 +8,7 @@ class DistrictConfigLog < ApplicationRecord
   validates :key, inclusion: { in: [SFTP_FILENAMES] }
 
   def self.latest_json(key, fallback = nil)
-    where(key: key).order(created_at: :desc).limit(1).try(:json) || fallback
+    where(key: key).order(created_at: :desc).limit(1).try(:first).try(:json) || fallback
   end
 
   # These aren't entirely static, but this seeds with
@@ -18,30 +18,7 @@ class DistrictConfigLog < ApplicationRecord
 
     DistrictConfigLog.create!({
       key: DistrictConfigLog::SFTP_FILENAMES,
-      json: {
-        "FILENAME_FOR_EDUCATORS_IMPORT": "/data/insights-dev-or-test/educators_export.csv",
-        "FILENAME_FOR_STUDENTS_IMPORT": "/data/insights-dev-or-test/students_export.csv",
-        "FILENAME_FOR_BEHAVIOR_IMPORT": "/data/insights-dev-or-test/behavior_export.csv",
-        "FILENAME_FOR_ASSESSMENT_IMPORT": "/data/insights-dev-or-test/assessment_export.csv",
-        "FILENAME_FOR_ATTENDANCE_IMPORT": "/data/insights-dev-or-test/attendance_export.csv",
-        "FILENAME_FOR_STUDENT_AVERAGES_IMPORT": "/data/insights-dev-or-test/student_averages_export.csv",
-        "FILENAME_FOR_COURSE_SECTION_IMPORT": "/data/insights-dev-or-test/courses_sections_export.csv",
-        "FILENAME_FOR_STUDENTS_SECTION_ASSIGNMENT_IMPORT": "/data/insights-dev-or-test/student_section_assignment_export.csv",
-        "FILENAME_FOR_EDUCATOR_SECTION_ASSIGNMENT_IMPORT": "/data/insights-dev-or-test/educator_section_assignment_export.csv",
-        "FILENAME_FOR_ED_PLAN_IMPORT": "/data/insights-dev-or-test/student_ed_plan_export.csv",
-        "FILENAME_FOR_ED_PLAN_ACCOMMODATIONS_IMPORT": "/data/insights-dev-or-test/student_accommodations_export.csv",
-
-        "FILENAME_FOR_PHOTOS_ZIP": "/data/insights-dev-or-test/photos.zip",
-
-        "FILENAMES_FOR_IEP_PDF_ZIPS_ORDERED_OLDEST_TO_NEWEST": [
-          "/data/insights-dev-or-test/student-documents-6.zip",
-          "/data/insights-dev-or-test/student-documents-5.zip",
-          "/data/insights-dev-or-test/student-documents-4.zip",
-          "/data/insights-dev-or-test/student-documents-3.zip",
-          "/data/insights-dev-or-test/student-documents-2.zip",
-          "/data/insights-dev-or-test/student-documents-1.zip"
-        ]
-      }
+      json: JSON.parse(IO.read('config/sftp_filenames_development_fixture.json'))
     })
   end
 end
