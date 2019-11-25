d23cfcfb359bd3bd4c8b53a99e5c3dfa52526ca4
kevinrobinson <kevin.robinson.0@gmail.com>
2019-11-25 11:44:04 -0500

    Explicit eager load for LogSubscriber monkey patch that scrubs names from logs when downloading filenames

--- config/initializers/send_data_log_subscriber.rb
+++ config/initializers/send_data_log_subscriber.rb
@@ -1,3 +1,7 @@
+# With Zeitwerk in Rails 6, this is needed so we load the code before we monkeypatch it.
+# Otherwise, this defines some code and then Rails gets loaded and it gets overwritten.
+require 'action_controller/log_subscriber'
+
 # Monkey patching to override logging the filename; we want to give users
 # descriptive filenames, and that means they may have sensitive info that we
 # don't want logged.
@@ -5,6 +9,7 @@
 # see https://github.com/rails/rails/blob/master/actionpack/lib/action_controller/log_subscriber.rb#L53
 module ActionController
   class LogSubscriber < ActiveSupport::LogSubscriber
+    # override (monkey patch)
     def send_data(event)
       info { "Sent data [FILTERED] (#{event.duration.round(1)}ms)" }
     end
