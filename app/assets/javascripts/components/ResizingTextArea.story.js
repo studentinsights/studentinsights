1a73ea4416584824a326546c180cb3049e053470
kevinrobinson <kevin.robinson.0@gmail.com>
2019-12-03 11:57:46 -0500

    Done using span technique, with more test cases in story

--- app/assets/javascripts/components/ResizingTextArea.story.js
+++ app/assets/javascripts/components/ResizingTextArea.story.js
@@ -14,8 +14,12 @@ function TestCase({defaultText}) {
   const props = {
     value: text,
     onChange: e => setText(e.target.value),
+    containerStyle: {
+      fontSize: 14
+    },
     style: {
       outline: '1px solid black',
+      fontSize: 14,
       width: 300,
       border: 0,
       padding: 0,
@@ -28,22 +32,38 @@ TestCase.propTypes = {
   defaultText: PropTypes.string.isRequired
 };
 
-const longText = _.repeat('This is a really long sentence with many different clauses saying many, many important things.', 20);
+const longText = _.repeat('This is a really long sentence with many different clauses saying many, many important things.\n\n', 20);
 
 
 storiesOf('components/ResizingTextArea', module) // eslint-disable-line no-undef
   .add('all', () => {
     return (
-      <div style={{display: 'flex', flexDirection: 'row'}}>
-        <div style={{flex: 1}}>
-          <div style={{padding: 10}}><TestCase defaultText={`hello`} /></div>
-          <div style={{padding: 10}}><TestCase defaultText={`hello world`} /></div>
-          <div style={{padding: 10}}><TestCase defaultText={`hello\nworld`} /></div>
-          <div style={{padding: 10}}><TestCase defaultText={`hello\nworld\n`} /></div>
-          <div style={{padding: 10}}><TestCase defaultText={`hello\nworld\nwith\nresizing`} /></div>
-        </div>
-        <div style={{flex: 1}}>
-          <div style={{padding: 10}}><TestCase defaultText={longText} /></div>
+      <div>
+        <h2>test cases</h2>
+        <p>
+          This cannot be tested in jest because jsdom does not have a layout engine.  So this is to
+          help with manual testing across browsers.
+
+          Try:
+          <ol>
+            <li>passive: load, and watch it add text and expand to fit, remove text and contract, then add and expand again</li>
+            <li>passive: confirm above works with edge cases like trailing newline</li>
+            <li>interactive: load, scroll all the way down.  after first transition, screen should stay scrolled down until transition to short text.</li>
+            <li>interactive: after transitions, scroll down on long text.  when editing, scroll should not jump at all</li>
+            <li>interactive: after transitions, delete chunk of long text.  textarea ahouls contract, with no unexpected scroll or cursor jumps.</li>
+          </ol>
+        </p>
+        <div style={{display: 'flex', flexDirection: 'row'}}>
+          <div style={{flex: 1}}>
+            <div style={{padding: 10}}><TestCase defaultText={`hello1`} /></div>
+            <div style={{padding: 10}}><TestCase defaultText={`hello world`} /></div>
+            <div style={{padding: 10}}><TestCase defaultText={`hello\nworld`} /></div>
+            <div style={{padding: 10}}><TestCase defaultText={`hello\nworld\n`} /></div>
+            <div style={{padding: 10}}><TestCase defaultText={`hello\nworld\nwith\nresizing`} /></div>
+          </div>
+          <div style={{flex: 1}}>
+            <div style={{padding: 10}}><TestCase defaultText={longText} /></div>
+          </div>
         </div>
       </div>
     );
