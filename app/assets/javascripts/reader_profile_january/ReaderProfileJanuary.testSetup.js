78ae40a0bc9a7e540cbe2bdbbde01a3bd8be0973
kevinrobinson <kevin.robinson.0@gmail.com>
2020-01-22 16:40:24 -0500

    Fix to label tests, lint for JS

--- app/assets/javascripts/reader_profile_january/ReaderProfileJanuary.testSetup.js
+++ app/assets/javascripts/reader_profile_january/ReaderProfileJanuary.testSetup.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import {toMomentFromTimestamp} from '../helpers/toMoment';
 import {
   DIBELS_DORF_WPM,
--- spec/models/educator_spec.rb
+++ spec/models/educator_spec.rb
@@ -168,6 +168,7 @@ RSpec.describe Educator do
       expect(pals.uri.labels).to contain_exactly(*[
         'can_upload_student_voice_surveys',
         'enable_equity_experiments',
+        'enable_reader_profile_january',
         'enable_reading_benchmark_data_entry',
         'enable_reading_debug',
         'enable_reflection_on_notes_patterns',
