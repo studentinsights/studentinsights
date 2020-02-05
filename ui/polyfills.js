5e1127f763751916ab95f3dce32e1d3ff154a7a8
kevinrobinson <kevin.robinson.0@gmail.com>
2020-02-05 13:06:40 -0500

    Started with broken production build and polyfills, then core-js, then removing and babeling

--- ui/polyfills.js
+++ ui/polyfills.js
@@ -1,42 +1,37 @@
-// These are polyfills for ES5/ES6 features that aren't supported in
-// IE11.  If we want to use other things like fetch, Map/Set or Array methods,
-// add them in here.
-//
-// These have to be run before other code, so they're included first in the
-// webpack build config, rather than `imported` from `index.js`.
-//
+/*
+These are polyfills for ES5/ES6 features that aren't supported in
+IE11.  If we want to use other things like fetch, Map/Set or Array methods,
+add them in here.
+
+These have to be run before other code, so they're included first in the
+webpack build config, rather than `imported` from `index.js`.
+
+Over time, we should aim to align to create-react-app.
+See https://github.com/facebook/create-react-app/tree/master/packages/react-app-polyfill
+*/
+
+
 // Object.assign is particularly tricky since it doesn't come from our source directly,
 // but from our Babel transpilation process.
-import assign from 'object-assign';
-Object.assign = assign;
+import 'core-js/es/object';
 
-// Object.values doesn't exist in IE11
-import values from 'object.values';
-if (!Object.values) {
-  Object.values = values;
-}
+// react-beautiful-dnd uses Array#find specifically.
+import 'core-js/es/array';
 
-import Promise from 'promise-polyfill';
-if (!window.Promise) {
-  window.Promise = Promise;
-}
+// Promises
+import 'core-js/es/promise';
 
-import 'whatwg-fetch';
+// Polyfill NodeList#forEach for IE11, in order to
+// support `document.querySelectorAll(selector).forEach`.
+import 'core-js/web/dom-collections';
 
-import 'array.prototype.fill';
-
-// This is for react-beautiful-dnd in IE.
-import ArrayFindPolyfill from 'array.prototype.find';
-ArrayFindPolyfill.shim();
-
-// Polyfill NodeList#forEach for IE11, to
-// support document.querySelectorAll(selector).forEach.
-// see https://developer.mozilla.org/en-US/docs/Web/API/NodeList/forEach
-if (window.NodeList && !NodeList.prototype.forEach) {
-  NodeList.prototype.forEach = Array.prototype.forEach;
-}
 
 // For React 16 in IE
 // see https://reactjs.org/docs/javascript-environment-requirements.html
-import 'core-js/es6/map';
-import 'core-js/es6/set';
+import 'core-js/es/map';
+import 'core-js/es/set';
+
+
+// This isn't cross-platform, so not supported by core-js, which recommends
+// this polyfill instead.
+import 'whatwg-fetch';
