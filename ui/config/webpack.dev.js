84a3b1031439f09393fe7b1cddd05974387a9949
kevinrobinson <kevin.robinson.0@gmail.com>
2020-02-05 19:24:11 -0500

    Passing test-cli

--- ui/config/webpack.dev.js
+++ ui/config/webpack.dev.js
@@ -2,7 +2,7 @@
 const path = require('path');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'development',
@@ -16,7 +16,11 @@ module.exports = merge(common, {
   },
 
   plugins: [
-    new CleanWebpackPlugin(['../../public/dev'], { verbose: false, allowExternal: true })
+    new CleanWebpackPlugin({
+      dry: false, // required for `dangerouslyAllowCleanPatternsOutsideProject` to work
+      dangerouslyAllowCleanPatternsOutsideProject: true,
+      cleanOnceBeforeBuildPatterns: path.join(process.cwd(), '../../public/dev')
+    })
   ]
 });
 /* eslint-disable no-undef */
