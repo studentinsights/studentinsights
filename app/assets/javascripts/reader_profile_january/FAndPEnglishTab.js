125146ab7275ad18a0b44c84817aa627f53cf811
kevinrobinson <kevin.robinson.0@gmail.com>
2020-03-05 15:41:17 -0500

    Reading: Fix highlighting on F&P tab

--- app/assets/javascripts/reader_profile_january/FAndPEnglishTab.js
+++ app/assets/javascripts/reader_profile_january/FAndPEnglishTab.js
@@ -1,22 +1,16 @@
 import React from 'react';
 import tabProptypes from './tabPropTypes';
-import {mostRecentDataPoint} from './dibelsParsing';
 import {F_AND_P_ENGLISH} from '../reading/thresholds';
-import {Tab} from './Tabs';
+import GenericDibelsTab from './GenericDibelsTab';
 
 
 export default class FAndPEnglishTab extends React.Component {
   render() {
-    const {style, onClick, readerJson} = this.props;
-    
-    const dataPoint = mostRecentDataPoint(readerJson, F_AND_P_ENGLISH);
-    if (!dataPoint) return null;
     return (
-      <Tab
-        style={style}
-        text="F&P English"
-        orange={null} // TODO(kr)
-        onClick={onClick}
+      <GenericDibelsTab
+        {...this.props}
+        tabText="F&P English"
+        benchmarkAssessmentKey={F_AND_P_ENGLISH}
       />
     );
   }
