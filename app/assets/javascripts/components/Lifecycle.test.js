e28077ab847f604189abce780c5098c156a2633b
kevinrobinson <kevin.robinson.0@gmail.com>
2020-05-01 10:15:47 -0400

    Add another Lifecycle test case

--- app/assets/javascripts/components/Lifecycle.test.js
+++ app/assets/javascripts/components/Lifecycle.test.js
@@ -17,3 +17,14 @@ it('calls componentWillMount', () => {
   ReactDOM.render(<Lifecycle {...props}><div>hello</div></Lifecycle>, div);
   expect(props.componentWillMount).toHaveBeenCalled();
 });
+
+it('calls componentWillUnmount', () => {
+  const div = document.createElement('div');
+  const props = {
+    componentWillUnmount: jest.fn()
+  };
+  ReactDOM.render(<Lifecycle {...props}><div>hello</div></Lifecycle>, div);
+  expect(props.componentWillUnmount).not.toHaveBeenCalled();
+  ReactDOM.render(<b>goodbye</b>, div);
+  expect(props.componentWillUnmount).toHaveBeenCalled();
+});
