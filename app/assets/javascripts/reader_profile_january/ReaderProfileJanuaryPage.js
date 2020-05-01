e7602e9cdcd28e7ee74a49baf4d129c747153ded
kevinrobinson <kevin.robinson.0@gmail.com>
2020-05-01 13:33:09 -0400

    Reading: Soft-release reader profile, with framing copy in front

--- app/assets/javascripts/reader_profile_january/ReaderProfileJanuaryPage.js
+++ app/assets/javascripts/reader_profile_january/ReaderProfileJanuaryPage.js
@@ -12,19 +12,53 @@ import {readInstructionalStrategies} from './instructionalStrategies';
 // both to render their tabs in the overview, and also
 // to render the expanded view.
 export default class ReaderProfileJanuaryPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasClicked: false
+    };
+    this.onClick = this.onClick.bind(this);
+  }
+  
+  onClick(e) {
+    e.preventDefault();
+    this.setState({hasClicked: true});
+  }
+
   render() {
-    const {student} = this.props;
-    const url = `/api/students/${student.id}/reader_profile_json`;
+    const {hasClicked} = this.state;
 
     return (
       <RollbarErrorBoundary debugKey="ReaderProfileJanuaryPage">
         <div className="ReaderProfileJanuaryPage">
-          <SectionHeading>Reader Profile, v4 January</SectionHeading>
-          <GenericLoader
-            promiseFn={() => apiFetchJson(url)}
-            render={json => this.renderJson(json)} />
+          <SectionHeading>Reader Profile</SectionHeading>
+          {hasClicked ? this.renderSubstance() : this.renderMessage()}
         </div>
       </RollbarErrorBoundary>
+    );
+  }
+
+  renderMessage() {
+    const {student} = this.props;
+    return (
+      <div style={{padding: 0, margin: 10}}>
+        <div style={{display: 'flex', alignItems: 'flex-start', marginTop: 20, lineHeight: '1.5em'}}>
+          <div style={{flex: 1, marginRight: 20}}>Reader profiles aim to help facilitate problem-solving discussions around what actions or steps in the classroom or school can help students thrive in as readers.</div>
+          <div style={{flex: 1, marginRight: 20}}>Our profiles build from left to right, starting with how students see themselves as a reader, their oral language, and then moving to phonological awareness, phonics, and comprehension.</div>
+          <div style={{flex: 1, marginRight: 20}}>As we view the profile, we are also looking for what strengths the students have that we can leverage as we help the student to grow as a reader.</div>          
+        </div>
+        <button style={{marginTop: 20}} className="btn btn-primary" onClick={this.onClick}>{`${student.first_name}'s profile`}</button>
+      </div>
+    );
+  }
+
+  renderSubstance() {
+    const {student} = this.props;
+    const url = `/api/students/${student.id}/reader_profile_json`;
+    return (
+      <GenericLoader
+        promiseFn={() => apiFetchJson(url)}
+        render={json => this.renderJson(json)} />
     );
   }
 
@@ -44,6 +78,7 @@ export default class ReaderProfileJanuaryPage extends React.Component {
 ReaderProfileJanuaryPage.propTypes = {
   student: PropTypes.shape({
     id: PropTypes.number.isRequired,
+    first_name: PropTypes.string.isRequired,
     grade: PropTypes.any.isRequired
   }).isRequired
 };
